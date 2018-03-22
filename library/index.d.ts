7aebc1c9e571656dec191a9726017ba2b0b4dae7
Angular Builds <circleci@angular.io>
2018-03-22 16:21:57 +0000

    1a9ef3e fix(@angular-devkit/build-webpack): use basename for index path

--- library/index.d.ts
+++ library/index.d.ts
@@ -1,3 +1,3 @@
 import { Rule } from '@angular-devkit/schematics';
-import { Schema as GenerateLibraryOptions } from './schema';
-export default function (options: GenerateLibraryOptions): Rule;
+import { Schema as LibraryOptions } from './schema';
+export default function (options: LibraryOptions): Rule;
---
+++ utility/latest-versions.js
@@ -0,0 +1,16 @@
+"use strict";
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.latestVersions = {
+    Angular: '^5.2.9',
+    RxJs: '^5.5.7',
+    ZoneJs: '^0.8.20',
+    TypeScript: '~2.6.2',
+};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibGF0ZXN0LXZlcnNpb25zLmpzIiwic291cmNlUm9vdCI6Ii4vIiwic291cmNlcyI6WyJwYWNrYWdlcy9zY2hlbWF0aWNzL2FuZ3VsYXIvdXRpbGl0eS9sYXRlc3QtdmVyc2lvbnMudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBOzs7Ozs7R0FNRzs7QUFFVSxRQUFBLGNBQWMsR0FBRztJQUM1QixPQUFPLEVBQUUsUUFBUTtJQUNqQixJQUFJLEVBQUUsUUFBUTtJQUNkLE1BQU0sRUFBRSxTQUFTO0lBQ2pCLFVBQVUsRUFBRSxRQUFRO0NBQ3JCLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvKipcbiAqIEBsaWNlbnNlXG4gKiBDb3B5cmlnaHQgR29vZ2xlIEluYy4gQWxsIFJpZ2h0cyBSZXNlcnZlZC5cbiAqXG4gKiBVc2Ugb2YgdGhpcyBzb3VyY2UgY29kZSBpcyBnb3Zlcm5lZCBieSBhbiBNSVQtc3R5bGUgbGljZW5zZSB0aGF0IGNhbiBiZVxuICogZm91bmQgaW4gdGhlIExJQ0VOU0UgZmlsZSBhdCBodHRwczovL2FuZ3VsYXIuaW8vbGljZW5zZVxuICovXG5cbmV4cG9ydCBjb25zdCBsYXRlc3RWZXJzaW9ucyA9IHtcbiAgQW5ndWxhcjogJ141LjIuOScsXG4gIFJ4SnM6ICdeNS41LjcnLFxuICBab25lSnM6ICdeMC44LjIwJyxcbiAgVHlwZVNjcmlwdDogJ34yLjYuMicsXG59O1xuIl19
