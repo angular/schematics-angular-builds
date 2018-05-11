0c7897138320f229ada41788a511ccad9f88a3db
Angular Builds <circleci@angular.io>
2018-05-11 20:14:28 +0000

    b530ced ci: add no circular dependency rule to tslint

--- utility/ast-utils.d.ts
+++ utility/ast-utils.d.ts
@@ -8,6 +8,16 @@
 import * as ts from 'typescript';
 import { Change } from './change';
 /**
+ * Add Import `import { symbolName } from fileName` if the import doesn't exit
+ * already. Assumes fileToEdit can be resolved and accessed.
+ * @param fileToEdit (file we want to add import to)
+ * @param symbolName (item to import)
+ * @param fileName (path to the file)
+ * @param isDefault (if true, import follows style for importing default exports)
+ * @return Change
+ */
+export declare function insertImport(source: ts.SourceFile, fileToEdit: string, symbolName: string, fileName: string, isDefault?: boolean): Change;
+/**
  * Find all nodes from the AST in the subtree of node of SyntaxKind kind.
  * @param node
  * @param kind
