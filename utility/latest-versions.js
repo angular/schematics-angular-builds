74cd5887ac8e5b5d97b9749b59680444259628c4
Angular Builds <circleci@angular.io>
2019-04-23 21:43:35 +0000

    b369aed release: v8.0.0-beta.18

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -9,7 +9,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.latestVersions = {
     // These versions should be kept up to date with latest Angular peer dependencies.
-    Angular: '~8.0.0-beta.12',
+    Angular: '~8.0.0-beta.14',
     RxJs: '~6.4.0',
     ZoneJs: '~0.9.0',
     TypeScript: '~3.4.3',
