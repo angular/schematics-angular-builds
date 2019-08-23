752f3ac0325d88bf6c86faa2ef725488f086413c
Angular Builds <circleci@angular.io>
2019-08-23 17:44:16 +0000

    bef6e3687 build: update npm-package-arg to version 6.1.1 (#15406)

--- migrations/update-9/index.js
+++ migrations/update-9/index.js
@@ -8,13 +8,22 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 const schematics_1 = require("@angular-devkit/schematics");
+const tasks_1 = require("@angular-devkit/schematics/tasks");
 const ivy_libraries_1 = require("./ivy-libraries");
+const update_dependencies_1 = require("./update-dependencies");
 const update_workspace_config_1 = require("./update-workspace-config");
 function default_1() {
     return () => {
         return schematics_1.chain([
             update_workspace_config_1.UpdateWorkspaceConfig(),
             ivy_libraries_1.UpdateLibraries(),
+            update_dependencies_1.updateDependencies(),
+            (tree, context) => {
+                const packageChanges = tree.actions.some(a => a.path.endsWith('/package.json'));
+                if (packageChanges) {
+                    context.addTask(new tasks_1.NodePackageInstallTask());
+                }
+            },
         ]);
     };
 }
