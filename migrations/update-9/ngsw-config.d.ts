3a69cf61062f5fd5698b367c1e20deff758a635c
Angular Builds <circleci@angular.io>
2019-08-27 17:58:07 +0000

    26dd51221 style: collapse if statements (#15449)

--- migrations/update-9/ngsw-config.d.ts
+++
@@ -1,5 +0,0 @@
-import { Rule } from '@angular-devkit/schematics';
-/**
- * Update ngsw-config.json to fix issue https://github.com/angular/angular-cli/pull/15277
- */
-export declare function updateNGSWConfig(): Rule;
