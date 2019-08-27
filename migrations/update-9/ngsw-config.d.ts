b80b6f0fa93a5432de6c9f41a91715b003b3ef57
Angular Builds <circleci@angular.io>
2019-08-27 17:56:54 +0000

    4df025f5f fix(@angular-devkit/build-optimizer): don't mark tslib helpers which are suffixed with `$` and a number as pure (#15451)

---
+++ migrations/update-9/ngsw-config.d.ts
@@ -0,0 +1,5 @@
+import { Rule } from '@angular-devkit/schematics';
+/**
+ * Update ngsw-config.json to fix issue https://github.com/angular/angular-cli/pull/15277
+ */
+export declare function updateNGSWConfig(): Rule;
