427a975639fbc7c71c21206b49390711ca6cebe3
Angular Builds <circleci@angular.io>
2019-10-23 22:33:48 +0000

    e62069718 release: v8.3.14

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,10 +15,10 @@ exports.latestVersions = {
     TypeScript: '~3.5.3',
     TsLib: '^1.10.0',
     // The versions below must be manually updated when making a new devkit release.
-    DevkitBuildAngular: '~0.803.13',
-    DevkitBuildNgPackagr: '~0.803.13',
-    DevkitBuildWebpack: '~0.803.13',
-    AngularPWA: '~0.803.13',
+    DevkitBuildAngular: '~0.803.14',
+    DevkitBuildNgPackagr: '~0.803.14',
+    DevkitBuildWebpack: '~0.803.14',
+    AngularPWA: '~0.803.14',
     tsickle: '^0.37.0',
     ngPackagr: '^5.4.0',
 };
