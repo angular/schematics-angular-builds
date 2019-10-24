200aa57b3c5a3e0f8d6c9c0ff1d9f10b5f8ea727
Angular Builds <circleci@angular.io>
2019-10-24 19:37:56 +0000

    bcf887647 test: add debug helper package scripts

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,8 +15,8 @@ exports.latestVersions = {
     TypeScript: '~3.6.4',
     TsLib: '^1.10.0',
     // The versions below must be manually updated when making a new devkit release.
-    DevkitBuildAngular: '~0.900.0-next.14',
-    DevkitBuildNgPackagr: '~0.900.0-next.14',
-    DevkitBuildWebpack: '~0.900.0-next.14',
+    DevkitBuildAngular: '~0.900.0-next.15',
+    DevkitBuildNgPackagr: '~0.900.0-next.15',
+    DevkitBuildWebpack: '~0.900.0-next.15',
     ngPackagr: '^9.0.0-rc.0',
 };
