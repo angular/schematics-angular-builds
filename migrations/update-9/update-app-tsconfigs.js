da4ac129e68f9deb04ba8346aad34ea14944f14a
Angular Builds <circleci@angular.io>
2019-12-07 07:55:54 +0000

    90ae96abf build: update terser-webpack-plugin to version 2.2.2

--- migrations/update-9/update-app-tsconfigs.js
+++ migrations/update-9/update-app-tsconfigs.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = require("path");
 const json_utils_1 = require("../../utility/json-utils");
 const workspace_models_1 = require("../../utility/workspace-models");
 const utils_1 = require("./utils");
@@ -7,24 +8,28 @@ const utils_1 = require("./utils");
  * Update the tsconfig files for applications
  * - Removes enableIvy: true
  * - Sets stricter file inclusions
+ * - Sets module compiler option to esnext or commonjs
  */
 function updateApplicationTsConfigs() {
-    return (tree) => {
+    return (tree, context) => {
         const workspace = utils_1.getWorkspace(tree);
+        const logger = context.logger;
+        // Add `module` option in the workspace tsconfig
+        updateModuleCompilerOption(tree, '/tsconfig.json');
         for (const { target } of utils_1.getTargets(workspace, 'build', workspace_models_1.Builders.Browser)) {
-            updateTsConfig(tree, target, workspace_models_1.Builders.Browser);
+            updateTsConfig(tree, target, workspace_models_1.Builders.Browser, logger);
         }
         for (const { target } of utils_1.getTargets(workspace, 'server', workspace_models_1.Builders.Server)) {
-            updateTsConfig(tree, target, workspace_models_1.Builders.Server);
+            updateTsConfig(tree, target, workspace_models_1.Builders.Server, logger);
         }
         for (const { target } of utils_1.getTargets(workspace, 'test', workspace_models_1.Builders.Karma)) {
-            updateTsConfig(tree, target, workspace_models_1.Builders.Karma);
+            updateTsConfig(tree, target, workspace_models_1.Builders.Karma, logger);
         }
         return tree;
     };
 }
 exports.updateApplicationTsConfigs = updateApplicationTsConfigs;
-function updateTsConfig(tree, builderConfig, builderName) {
+function updateTsConfig(tree, builderConfig, builderName, logger) {
     const options = utils_1.getAllOptions(builderConfig);
     for (const option of options) {
         let recorder;
@@ -35,6 +40,7 @@ function updateTsConfig(tree, builderConfig, builderName) {
         const tsConfigPath = tsConfigOption.value;
         let tsConfigAst = utils_1.readJsonFileAsAstObject(tree, tsConfigPath);
         if (!tsConfigAst) {
+            logger.warn(`Cannot find file: ${tsConfigPath}`);
             continue;
         }
         // Remove 'enableIvy: true' since this is the default in version 9.
@@ -53,33 +59,81 @@ function updateTsConfig(tree, builderConfig, builderName) {
                 tree.commitUpdate(recorder);
             }
         }
+        // Update 'module' compilerOption
+        updateModuleCompilerOption(tree, tsConfigPath, builderName);
         // Add stricter file inclusions to avoid unused file warning during compilation
         if (builderName !== workspace_models_1.Builders.Karma) {
             // Note: we need to re-read the tsconfig after very commit because
             // otherwise the updates will be out of sync since we are ammending the same node.
+            // we are already checking that tsconfig exists above!
+            // tslint:disable-next-line: no-non-null-assertion
             tsConfigAst = utils_1.readJsonFileAsAstObject(tree, tsConfigPath);
-            const files = json_utils_1.findPropertyInAstObject(tsConfigAst, 'files');
             const include = json_utils_1.findPropertyInAstObject(tsConfigAst, 'include');
-            if (!files && !include) {
-                const rootInSrc = tsConfigPath.includes('src/');
-                const rootSrc = rootInSrc ? '' : 'src/';
-                const files = builderName === workspace_models_1.Builders.Server
-                    ? [`${rootSrc}main.server.ts`]
-                    : [`${rootSrc}main.ts`, `${rootSrc}polyfills.ts`];
-                recorder = tree.beginUpdate(tsConfigPath);
-                json_utils_1.insertPropertyInAstObjectInOrder(recorder, tsConfigAst, 'files', files, 2);
-                tree.commitUpdate(recorder);
-                if (builderName === workspace_models_1.Builders.Browser) {
-                    tsConfigAst = utils_1.readJsonFileAsAstObject(tree, tsConfigPath);
+            if (include && include.kind === 'array') {
+                const tsInclude = include.elements.find(({ value }) => typeof value === 'string' && value.endsWith('**/*.ts'));
+                if (tsInclude) {
+                    const { start, end } = tsInclude;
                     recorder = tree.beginUpdate(tsConfigPath);
-                    json_utils_1.insertPropertyInAstObjectInOrder(recorder, tsConfigAst, 'include', [`${rootSrc}**/*.d.ts`], 2);
+                    recorder.remove(start.offset, end.offset - start.offset);
+                    // Replace ts includes with d.ts
+                    recorder.insertLeft(start.offset, tsInclude.text.replace('.ts', '.d.ts'));
                     tree.commitUpdate(recorder);
                 }
-                tsConfigAst = utils_1.readJsonFileAsAstObject(tree, tsConfigPath);
+            }
+            const files = json_utils_1.findPropertyInAstObject(tsConfigAst, 'files');
+            if (!files) {
+                const newFiles = [];
+                const mainOption = json_utils_1.findPropertyInAstObject(option, 'main');
+                if (mainOption && mainOption.kind === 'string') {
+                    newFiles.push(path_1.posix.relative(path_1.posix.dirname(tsConfigPath), mainOption.value));
+                }
+                const polyfillsOption = json_utils_1.findPropertyInAstObject(option, 'polyfills');
+                if (polyfillsOption && polyfillsOption.kind === 'string') {
+                    newFiles.push(path_1.posix.relative(path_1.posix.dirname(tsConfigPath), polyfillsOption.value));
+                }
+                if (newFiles.length) {
+                    recorder = tree.beginUpdate(tsConfigPath);
+                    // tslint:disable-next-line: no-non-null-assertion
+                    tsConfigAst = utils_1.readJsonFileAsAstObject(tree, tsConfigPath);
+                    json_utils_1.insertPropertyInAstObjectInOrder(recorder, tsConfigAst, 'files', newFiles, 2);
+                    tree.commitUpdate(recorder);
+                }
                 recorder = tree.beginUpdate(tsConfigPath);
+                // tslint:disable-next-line: no-non-null-assertion
+                tsConfigAst = utils_1.readJsonFileAsAstObject(tree, tsConfigPath);
                 json_utils_1.removePropertyInAstObject(recorder, tsConfigAst, 'exclude');
                 tree.commitUpdate(recorder);
             }
         }
     }
+}
+function updateModuleCompilerOption(tree, tsConfigPath, builderName) {
+    const tsConfigAst = utils_1.readJsonFileAsAstObject(tree, tsConfigPath);
+    if (!tsConfigAst) {
+        return;
+    }
+    const compilerOptions = json_utils_1.findPropertyInAstObject(tsConfigAst, 'compilerOptions');
+    if (!compilerOptions || compilerOptions.kind !== 'object') {
+        return;
+    }
+    const configExtends = json_utils_1.findPropertyInAstObject(tsConfigAst, 'extends');
+    const isExtendedConfig = configExtends && configExtends.kind === 'string';
+    const recorder = tree.beginUpdate(tsConfigPath);
+    // Server tsconfig should have a module of commonjs
+    const moduleType = builderName === workspace_models_1.Builders.Server ? 'commonjs' : 'esnext';
+    if (isExtendedConfig && builderName !== workspace_models_1.Builders.Server) {
+        json_utils_1.removePropertyInAstObject(recorder, compilerOptions, 'module');
+    }
+    else {
+        const scriptModule = json_utils_1.findPropertyInAstObject(compilerOptions, 'module');
+        if (!scriptModule) {
+            json_utils_1.insertPropertyInAstObjectInOrder(recorder, compilerOptions, 'module', moduleType, 4);
+        }
+        else if (scriptModule.value !== moduleType) {
+            const { start, end } = scriptModule;
+            recorder.remove(start.offset, end.offset - start.offset);
+            recorder.insertLeft(start.offset, `"${moduleType}"`);
+        }
+    }
+    tree.commitUpdate(recorder);
 }
