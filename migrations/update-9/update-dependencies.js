79b3cdb602ecf5060e31cb0cf859153d43f2c554
Angular Builds <circleci@angular.io>
2020-01-02 21:21:48 +0000

    d259a3644 build: update rxjs to version 6.5.4

--- migrations/update-9/update-dependencies.js
+++ migrations/update-9/update-dependencies.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dependencies_1 = require("../../utility/dependencies");
 const latest_versions_1 = require("../../utility/latest-versions");
 function updateDependencies() {
-    return host => {
+    return (host, context) => {
         const dependenciesToUpdate = {
             '@angular-devkit/build-angular': latest_versions_1.latestVersions.DevkitBuildAngular,
             '@angular-devkit/build-ng-packagr': latest_versions_1.latestVersions.DevkitBuildNgPackagr,
@@ -28,6 +28,15 @@ function updateDependencies() {
         }
         // `@angular/pwa` package is only needed when running `ng-add`.
         dependencies_1.removePackageJsonDependency(host, '@angular/pwa');
+        // Check for @angular-devkit/schematics and @angular-devkit/core
+        for (const name of ['@angular-devkit/schematics', '@angular-devkit/core']) {
+            const current = dependencies_1.getPackageJsonDependency(host, name);
+            if (current) {
+                context.logger.info(`Package "${name}" found in the workspace package.json. ` +
+                    'This package typically does not need to be installed manually. ' +
+                    'If it is not being used by project code, it can be removed from the package.json.');
+            }
+        }
     };
 }
 exports.updateDependencies = updateDependencies;
