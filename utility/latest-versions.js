be45e0c437921f7f237bfdb4b5f157c58002f0ca
Angular Builds <circleci@angular.io>
2020-02-05 01:41:07 +0000

    778587a97 release: v9.0.0-rc.14

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,8 +15,8 @@ exports.latestVersions = {
     TypeScript: '~3.7.5',
     TsLib: '^1.10.0',
     // The versions below must be manually updated when making a new devkit release.
-    DevkitBuildAngular: '~0.900.0-rc.13',
-    DevkitBuildNgPackagr: '~0.900.0-rc.13',
-    DevkitBuildWebpack: '~0.900.0-rc.13',
+    DevkitBuildAngular: '~0.900.0-rc.14',
+    DevkitBuildNgPackagr: '~0.900.0-rc.14',
+    DevkitBuildWebpack: '~0.900.0-rc.14',
     ngPackagr: '^9.0.0-rc.8',
 };
