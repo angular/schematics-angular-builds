5b9fc536f7253f2c6c71e1407ad9c47a8f447fe0
Angular Builds <circleci@angular.io>
2020-02-07 01:31:00 +0000

    207b2ccc5 release: v9.0.1

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -9,14 +9,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.latestVersions = {
     // These versions should be kept up to date with latest Angular peer dependencies.
-    Angular: '~9.0.0-rc.14',
+    Angular: '~9.0.0',
     RxJs: '~6.5.4',
     ZoneJs: '~0.10.2',
     TypeScript: '~3.7.5',
     TsLib: '^1.10.0',
     // The versions below must be manually updated when making a new devkit release.
-    DevkitBuildAngular: '~0.900.0-rc.14',
-    DevkitBuildNgPackagr: '~0.900.0-rc.14',
-    DevkitBuildWebpack: '~0.900.0-rc.14',
-    ngPackagr: '^9.0.0-rc.8',
+    DevkitBuildAngular: '~0.900.1',
+    DevkitBuildNgPackagr: '~0.900.1',
+    DevkitBuildWebpack: '~0.900.1',
+    ngPackagr: '^9.0.0',
 };
