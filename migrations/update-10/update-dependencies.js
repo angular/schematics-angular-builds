90fdceb9fd52fe06c60fd8a4cb5fdc85bedaa4d9
Angular Builds <circleci@angular.io>
2020-04-13 19:19:39 +0000

    a78fe9668 test: update ng-packagr tests tsconfig as per CLI default

---
+++ migrations/update-10/update-dependencies.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const dependencies_1 = require("../../utility/dependencies");
+const latest_versions_1 = require("../../utility/latest-versions");
+function default_1() {
+    return (host, context) => {
+        const dependenciesToUpdate = {
+            'karma': '~5.0.0',
+            'ng-packagr': latest_versions_1.latestVersions.ngPackagr,
+        };
+        for (const [name, version] of Object.entries(dependenciesToUpdate)) {
+            const current = dependencies_1.getPackageJsonDependency(host, name);
+            if (!current || current.version === version) {
+                continue;
+            }
+            dependencies_1.addPackageJsonDependency(host, {
+                type: current.type,
+                name,
+                version,
+                overwrite: true,
+            });
+        }
+        // Check for @angular-devkit/schematics and @angular-devkit/core
+        for (const name of ['@angular-devkit/schematics', '@angular-devkit/core']) {
+            const current = dependencies_1.getPackageJsonDependency(host, name);
+            if (current) {
+                context.logger.info(`Package "${name}" found in the workspace package.json. ` +
+                    'This package typically does not need to be installed manually. ' +
+                    'If it is not being used by project code, it can be removed from the package.json.');
+            }
+        }
+    };
+}
+exports.default = default_1;
