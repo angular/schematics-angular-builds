d5fa2db9660e127727eadd0e51c171b51ad46ef5
Angular Builds <circleci@angular.io>
2020-04-22 18:12:40 +0000

    b1a5f4954 refactor(@angular-devkit/schematics): remove deprecated FileSystemHost

--- utility/workspace-models.d.ts
+++ utility/workspace-models.d.ts
@@ -55,7 +55,6 @@ export interface BrowserBuilderOptions extends BrowserBuilderBaseOptions {
         maximumWarning?: string;
         maximumError?: string;
     }[];
-    es5BrowserSupport?: boolean;
     webWorkerTsConfig?: string;
 }
 export interface ServeBuilderOptions {
