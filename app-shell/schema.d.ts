fa8ad0b44f3dae0abec060c0ce5dc1f5fad0bc63
Angular Builds <circleci@angular.io>
2020-05-06 19:15:12 +0000

    ac3aff20c refactor(@schematics/angular): remove deprecated options from app-shell and universal schematics

--- app-shell/schema.d.ts
+++ app-shell/schema.d.ts
@@ -15,29 +15,9 @@ export interface Schema {
      */
     clientProject: string;
     /**
-     * The name of the index file
-     * @deprecated This option has no effect.
-     */
-    index?: string;
-    /**
      * The name of the main entry-point file.
      */
     main?: string;
-    /**
-     * The HTML selector of the Universal app
-     * @deprecated This option has no effect.
-     */
-    name?: string;
-    /**
-     * The output directory for build results.
-     * @deprecated This option has no effect.
-     */
-    outDir?: string;
-    /**
-     * The root directory of the app.
-     * @deprecated This option has no effect.
-     */
-    root?: string;
     /**
      * The name of the root module class.
      */
@@ -51,27 +31,7 @@ export interface Schema {
      */
     route?: string;
     /**
-     * The path of the source directory.
-     * @deprecated This option has no effect.
-     */
-    sourceDir?: string;
-    /**
-     * The name of the test entry-point file.
-     * @deprecated This option has no effect.
-     */
-    test?: string;
-    /**
-     * The name of the TypeScript configuration file for tests.
-     * @deprecated This option has no effect.
-     */
-    testTsconfigFileName?: string;
-    /**
      * The name of the TypeScript configuration file.
      */
     tsconfigFileName?: string;
-    /**
-     * The name of related Universal app.
-     * @deprecated This option has no effect.
-     */
-    universalProject?: string;
 }
