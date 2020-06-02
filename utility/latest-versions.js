5b5f96d6dcbb2f945be35f6e17d00ea1f378eab4
Angular Builds <circleci@angular.io>
2020-06-02 17:33:23 +0000

    a1378a21f fix(@schematics/angular): update several testing dev dependencies

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -10,17 +10,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.latestVersions = void 0;
 exports.latestVersions = {
     // These versions should be kept up to date with latest Angular peer dependencies.
-    Angular: '~10.0.0-rc.0',
+    Angular: '~10.0.0-rc.2',
     RxJs: '~6.5.4',
     ZoneJs: '~0.10.2',
-    TypeScript: '~3.9.2',
+    TypeScript: '~3.9.3',
     TsLib: '^2.0.0',
     // The versions below must be manually updated when making a new devkit release.
     // For our e2e tests, these versions must match the latest tag present on the branch.
     // During RC periods they will not match the latest RC until there's a new git tag, and
     // should not be updated.
-    DevkitBuildAngular: '~0.1000.0-rc.0',
-    DevkitBuildNgPackagr: '~0.1000.0-rc.0',
-    DevkitBuildWebpack: '~0.1000.0-rc.0',
-    ngPackagr: '^10.0.0-next.2',
+    DevkitBuildAngular: '~0.1000.0-rc.2',
+    DevkitBuildNgPackagr: '~0.1000.0-rc.2',
+    DevkitBuildWebpack: '~0.1000.0-rc.2',
+    ngPackagr: '^10.0.0-rc.0',
 };
