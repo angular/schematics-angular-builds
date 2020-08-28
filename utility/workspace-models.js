0f3183c4968b939564d7f029bbe95dae5f60ed7a
Angular Builds <circleci@angular.io>
2020-08-28 08:28:05 +0000

    71360878d fix(@angular-devkit/build-angular): ensure ivy extraction file names match sourcemaps

--- utility/workspace-models.js
+++ utility/workspace-models.js
@@ -1,4 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Builders = exports.ProjectType = void 0;
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
@@ -6,8 +8,6 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Builders = exports.ProjectType = void 0;
 var ProjectType;
 (function (ProjectType) {
     ProjectType["Application"] = "application";
