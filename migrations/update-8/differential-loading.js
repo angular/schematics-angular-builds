8a54516bac0078322ccd9e8a4c991c2a60c26467
Angular Builds <circleci@angular.io>
2020-09-01 17:56:33 +0000

    ccfd58b0c fix(@schematics/angular): update karma for new workspaces to 5.2.0

--- migrations/update-8/differential-loading.js
+++ migrations/update-8/differential-loading.js
@@ -9,7 +9,9 @@ exports.updateES5Projects = void 0;
  * found in the LICENSE file at https://angular.io/license
  */
 const core_1 = require("@angular-devkit/core");
-const json_utils_1 = require("../../utility/json-utils");
+const json_file_1 = require("../../utility/json-file");
+const workspace_1 = require("../../utility/workspace");
+const workspace_models_1 = require("../../utility/workspace-models");
 const browserslistContent = `# This file is used by the build system to adjust CSS and JS output to support the specified browsers below.
 # For additional information regarding the format and rule options, please see:
 # https://github.com/browserslist/browserslist#queries
@@ -23,51 +25,44 @@ Firefox ESR
 not dead
 not IE 9-11 # For IE 9-11 support, remove 'not'.`;
 function updateES5Projects() {
-    return (tree) => {
+    return async (tree) => {
+        var _a, _b;
         // update workspace tsconfig
         updateTsConfig(tree, '/tsconfig.json');
-        const angularConfigContent = tree.read('angular.json') || tree.read('.angular.json');
-        if (!angularConfigContent) {
+        let workspace;
+        try {
+            workspace = await workspace_1.getWorkspace(tree);
+        }
+        catch (_c) {
             return;
         }
-        const angularJson = core_1.parseJson(angularConfigContent.toString(), core_1.JsonParseMode.Loose);
-        if (!core_1.isJsonObject(angularJson) || !core_1.isJsonObject(angularJson.projects)) {
-            // If that field isn't there, no use...
-            return;
-        }
-        // For all projects
-        for (const [name, project] of Object.entries(angularJson.projects)) {
-            if (!core_1.isJsonObject(project)) {
+        for (const [projectName, project] of workspace.projects) {
+            if (typeof project.root !== 'string') {
                 continue;
             }
-            if (typeof project.root != 'string' || project.projectType !== 'application') {
-                continue;
-            }
-            if (name.endsWith('-e2e')) {
+            if (projectName.endsWith('-e2e')) {
                 // Skip existing separate E2E projects
                 continue;
             }
-            // Older projects app and spec ts configs had script and module set in them.
-            const architect = project.architect;
-            if (!(core_1.isJsonObject(architect)
-                && core_1.isJsonObject(architect.build)
-                && architect.build.builder === '@angular-devkit/build-angular:browser')) {
-                // Skip projects who's build builder is not build-angular:browser
+            const buildTarget = project.targets.get('build');
+            if (!buildTarget || buildTarget.builder !== workspace_models_1.Builders.Browser) {
                 continue;
             }
-            const buildOptionsConfig = architect.build.options;
-            if (core_1.isJsonObject(buildOptionsConfig) && typeof buildOptionsConfig.tsConfig === 'string') {
-                updateTsConfig(tree, buildOptionsConfig.tsConfig);
+            const buildTsConfig = (_a = buildTarget === null || buildTarget === void 0 ? void 0 : buildTarget.options) === null || _a === void 0 ? void 0 : _a.tsConfig;
+            if (buildTsConfig && typeof buildTsConfig === 'string') {
+                updateTsConfig(tree, buildTsConfig);
             }
-            const testConfig = architect.test;
-            if (core_1.isJsonObject(testConfig)
-                && core_1.isJsonObject(testConfig.options)
-                && typeof testConfig.options.tsConfig === 'string') {
-                updateTsConfig(tree, testConfig.options.tsConfig);
+            const testTarget = project.targets.get('test');
+            if (!testTarget) {
+                continue;
+            }
+            const testTsConfig = (_b = testTarget === null || testTarget === void 0 ? void 0 : testTarget.options) === null || _b === void 0 ? void 0 : _b.tsConfig;
+            if (testTsConfig && typeof testTsConfig === 'string') {
+                updateTsConfig(tree, testTsConfig);
             }
             const browserslistPath = core_1.join(core_1.normalize(project.root), 'browserslist');
             // Move the CLI 7 style browserlist to root if it's there.
-            const sourceRoot = project.sourceRoot === 'string'
+            const sourceRoot = typeof project.sourceRoot === 'string'
                 ? project.sourceRoot
                 : core_1.join(core_1.normalize(project.root), 'src');
             const srcBrowsersList = core_1.join(core_1.normalize(sourceRoot), 'browserslist');
@@ -78,59 +73,31 @@ function updateES5Projects() {
                 tree.create(browserslistPath, browserslistContent);
             }
         }
-        return tree;
     };
 }
 exports.updateES5Projects = updateES5Projects;
 function updateTsConfig(tree, tsConfigPath) {
-    const buffer = tree.read(tsConfigPath);
-    if (!buffer) {
+    let tsConfigJson;
+    try {
+        tsConfigJson = new json_file_1.JSONFile(tree, tsConfigPath);
+    }
+    catch (_a) {
         return;
     }
-    const tsCfgAst = core_1.parseJsonAst(buffer.toString(), core_1.JsonParseMode.Loose);
-    if (tsCfgAst.kind !== 'object') {
+    const compilerOptions = tsConfigJson.get(['compilerOptions']);
+    if (!compilerOptions || typeof compilerOptions !== 'object') {
         return;
     }
-    const configExtends = json_utils_1.findPropertyInAstObject(tsCfgAst, 'extends');
-    const isExtendedConfig = configExtends && configExtends.kind === 'string';
-    const compilerOptions = json_utils_1.findPropertyInAstObject(tsCfgAst, 'compilerOptions');
-    if (!compilerOptions || compilerOptions.kind !== 'object') {
-        return;
-    }
-    const recorder = tree.beginUpdate(tsConfigPath);
-    if (isExtendedConfig) {
-        json_utils_1.removePropertyInAstObject(recorder, compilerOptions, 'target');
-        json_utils_1.removePropertyInAstObject(recorder, compilerOptions, 'module');
-        json_utils_1.removePropertyInAstObject(recorder, compilerOptions, 'downlevelIteration');
+    const configExtends = tsConfigJson.get(['extends']);
+    const isExtended = configExtends && typeof configExtends === 'string';
+    if (isExtended) {
+        tsConfigJson.remove(['compilerOptions', 'target']);
+        tsConfigJson.remove(['compilerOptions', 'module']);
+        tsConfigJson.remove(['compilerOptions', 'downlevelIteration']);
     }
     else {
-        const downlevelIteration = json_utils_1.findPropertyInAstObject(compilerOptions, 'downlevelIteration');
-        if (!downlevelIteration) {
-            json_utils_1.insertPropertyInAstObjectInOrder(recorder, compilerOptions, 'downlevelIteration', true, 4);
-        }
-        else if (!downlevelIteration.value) {
-            const { start, end } = downlevelIteration;
-            recorder.remove(start.offset, end.offset - start.offset);
-            recorder.insertLeft(start.offset, 'true');
-        }
-        const scriptTarget = json_utils_1.findPropertyInAstObject(compilerOptions, 'target');
-        if (!scriptTarget) {
-            json_utils_1.insertPropertyInAstObjectInOrder(recorder, compilerOptions, 'target', 'es2015', 4);
-        }
-        else if (scriptTarget.value !== 'es2015') {
-            const { start, end } = scriptTarget;
-            recorder.remove(start.offset, end.offset - start.offset);
-            recorder.insertLeft(start.offset, '"es2015"');
-        }
-        const scriptModule = json_utils_1.findPropertyInAstObject(compilerOptions, 'module');
-        if (!scriptModule) {
-            json_utils_1.insertPropertyInAstObjectInOrder(recorder, compilerOptions, 'module', 'esnext', 4);
-        }
-        else if (scriptModule.value !== 'esnext') {
-            const { start, end } = scriptModule;
-            recorder.remove(start.offset, end.offset - start.offset);
-            recorder.insertLeft(start.offset, '"esnext"');
-        }
+        tsConfigJson.modify(['compilerOptions', 'target'], 'es2015');
+        tsConfigJson.modify(['compilerOptions', 'module'], 'esnext');
+        tsConfigJson.modify(['compilerOptions', 'downlevelIteration'], true);
     }
-    tree.commitUpdate(recorder);
 }
