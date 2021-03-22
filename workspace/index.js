5f914fa0787c161bd96313def5c2e313d1b60555
Angular Builds <circleci@angular.io>
2021-03-22 13:56:30 +0000

    09daf7a7e fix(@schematics/angular): remove leftover workspace tslint config

--- workspace/index.js
+++ workspace/index.js
@@ -11,9 +11,8 @@ const core_1 = require("@angular-devkit/core");
 const schematics_1 = require("@angular-devkit/schematics");
 const latest_versions_1 = require("../utility/latest-versions");
 function default_1(options) {
-    const minimalFilesRegExp = /(.editorconfig|tslint.json)\.template$/;
     return schematics_1.mergeWith(schematics_1.apply(schematics_1.url('./files'), [
-        options.minimal ? schematics_1.filter(path => !minimalFilesRegExp.test(path)) : schematics_1.noop(),
+        options.minimal ? schematics_1.filter(path => !path.endsWith('editorconfig.template')) : schematics_1.noop(),
         schematics_1.applyTemplates({
             utils: core_1.strings,
             ...options,
