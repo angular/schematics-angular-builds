3e773ed381970f5ba6ad7578300114957bb82e36
Angular Builds <circleci@angular.io>
2021-04-19 15:21:47 +0000

    b8875397c docs(@angular-devkit/build-angular): add readme file

--- migrations/update-12/update-web-workers.d.ts
+++ migrations/update-12/update-web-workers.d.ts
@@ -6,4 +6,4 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import { Rule } from '@angular-devkit/schematics';
-export declare function updateLazyModulePaths(): Rule;
+export default function (): Rule;
