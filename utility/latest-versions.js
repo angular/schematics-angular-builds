08a903a915165e8130715fb33ed7828aecb037e6
Angular Builds <circleci@angular.io>
2021-04-27 07:15:16 +0000

    86333765d build: migrate all file header to use Google LLC rather than Google Inc

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -1,13 +1,13 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.latestVersions = void 0;
 /**
  * @license
- * Copyright Google Inc. All Rights Reserved.
+ * Copyright Google LLC All Rights Reserved.
  *
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.latestVersions = void 0;
 exports.latestVersions = {
     // These versions should be kept up to date with latest Angular peer dependencies.
     Angular: '~12.0.0-rc.0',
