12694644a963f9c405e31206e03288d5e24e1686
Angular Builds <circleci@angular.io>
2021-04-28 21:00:01 +0000

    df9975750 release: v12.0.0-rc.1

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -10,7 +10,7 @@ exports.latestVersions = void 0;
  */
 exports.latestVersions = {
     // These versions should be kept up to date with latest Angular peer dependencies.
-    Angular: '~12.0.0-rc.0',
+    Angular: '~12.0.0-rc.1',
     RxJs: '~6.6.0',
     ZoneJs: '~0.11.4',
     TypeScript: '~4.2.3',
