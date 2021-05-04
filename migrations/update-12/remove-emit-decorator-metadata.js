374dea7a328dfb7f6d111666e72fa3568fee5f44
Angular Builds <circleci@angular.io>
2021-05-04 15:14:09 +0000

    7521a8727 fix(@schematics/angular): only run `emitDecoratorMetadata` removal migration in safe workspaces

--- migrations/update-12/remove-emit-decorator-metadata.js
+++ migrations/update-12/remove-emit-decorator-metadata.js
@@ -9,6 +9,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@angular-devkit/core");
 const json_file_1 = require("../../utility/json-file");
+const workspace_1 = require("../../utility/workspace");
 function* visitJsonFiles(directory) {
     for (const path of directory.subfiles) {
         if (!path.endsWith('.json')) {
@@ -24,7 +25,18 @@ function* visitJsonFiles(directory) {
     }
 }
 function default_1() {
-    return (tree) => {
+    return async (tree, { logger }) => {
+        const workspace = await workspace_1.getWorkspace(tree);
+        const hasThirdPartyBuilders = [...workspace_1.allWorkspaceTargets(workspace)].some(([, target]) => {
+            const { builder } = target;
+            return !(builder.startsWith('@angular-devkit/build-angular') ||
+                builder.startsWith('@nguniversal/builders'));
+        });
+        if (hasThirdPartyBuilders) {
+            logger.warn('Skipping migration as the workspace uses third-party builders which may ' +
+                'require "emitDecoratorMetadata" TypeScript compiler option.');
+            return;
+        }
         for (const path of visitJsonFiles(tree.root)) {
             const content = tree.read(path);
             if (content === null || content === void 0 ? void 0 : content.toString().includes('"emitDecoratorMetadata"')) {
