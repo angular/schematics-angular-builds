3cfcd1e7ecbeae04de2b40a3ed8b9c03149779d8
Angular Builds <circleci@angular.io>
2021-06-02 19:33:32 +0000

    8690ed0a3 release: v12.1.0-next.4

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.latestVersions = void 0;
 exports.latestVersions = {
     // These versions should be kept up to date with latest Angular peer dependencies.
-    Angular: '~12.1.0-next.3',
+    Angular: '~12.1.0-next.4',
     RxJs: '~6.6.0',
     ZoneJs: '~0.11.4',
     TypeScript: '~4.2.3',
