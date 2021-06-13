a21348363a872e3a22f8263ee7284399335afebf
Angular Builds <circleci@angular.io>
2021-06-13 16:36:40 +0000

    b3d708014 build: enable `esModuleInterop` TypeScript option

--- migrations/update-10/remove-es5-browser-support.js
+++ migrations/update-10/remove-es5-browser-support.js
@@ -6,6 +6,25 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@angular-devkit/core");
 const workspace_1 = require("../../utility/workspace");
@@ -72,12 +91,12 @@ async function isES5SupportNeeded(projectRoot) {
     const criteria = ['y', 'a'];
     try {
         // eslint-disable-next-line import/no-extraneous-dependencies
-        const browserslist = await Promise.resolve().then(() => require('browserslist'));
+        const browserslist = (await Promise.resolve().then(() => __importStar(require('browserslist')))).default;
         const supportedBrowsers = browserslist(undefined, {
             path: core_1.getSystemPath(projectRoot),
         });
         // eslint-disable-next-line import/no-extraneous-dependencies
-        const { feature, features } = await Promise.resolve().then(() => require('caniuse-lite'));
+        const { feature, features } = await Promise.resolve().then(() => __importStar(require('caniuse-lite')));
         const data = feature(features['es6-module']);
         return supportedBrowsers.some((browser) => {
             const [agentId, version] = browser.split(' ');
