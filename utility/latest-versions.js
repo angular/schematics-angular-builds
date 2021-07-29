996c6e50eca5be39ca487ce08f1e31daa81f6aec
Angular Builds <circleci@angular.io>
2021-07-29 13:02:06 +0000

    bec21de06 refactor(@schematics/angular): hard code version of Angular in latest-verisons

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -8,21 +8,12 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.latestVersions = void 0;
-/** Retrieve the minor version for the provided version string. */
-function getAngularEarliestMinorVersion(version) {
-    const versionMatching = version.match(/^(\d+)\.(\d+)\.\d+(-\w+)?/);
-    if (versionMatching === null) {
-        throw Error('Unable to determine the minor version for the provided version');
-    }
-    const [_, major, minor, prerelease = ''] = versionMatching;
-    return `~${major}.${minor}.0${prerelease}`;
-}
 exports.latestVersions = {
     // We could have used TypeScripts' `resolveJsonModule` to make the `latestVersion` object typesafe,
     // but ts_library doesn't support JSON inputs.
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
-    Angular: getAngularEarliestMinorVersion(require('../package.json')['version']),
+    Angular: '~12.2.0-rc.0',
     // Since @angular-devkit/build-angular and @schematics/angular are always
     // published together from the same monorepo, and they are both
     // non-experimental, they will always have the same version.
