5a1f1ea213eb8df7a6daa426c6cbe0e8a3078f01
Angular Builds <circleci@angular.io>
2021-10-01 19:35:37 +0000

    000b0e51c feat(@angular-devkit/build-angular): remove deprecated dev-server options

--- migrations/update-13/update-angular-config.js
+++ migrations/update-13/update-angular-config.js
@@ -15,16 +15,26 @@ function default_1() {
                 // Delete removed tslint builder
                 if (target.builder === '@angular-devkit/build-angular:tslint') {
                     project.targets.delete(name);
-                    continue;
                 }
-                if (!target.builder.startsWith('@angular-devkit/build-angular')) {
-                    continue;
+                else if (target.builder === '@angular-devkit/build-angular:dev-server') {
+                    for (const [, options] of (0, workspace_1.allTargetOptions)(target)) {
+                        delete options.optimization;
+                        delete options.aot;
+                        delete options.progress;
+                        delete options.deployUrl;
+                        delete options.sourceMap;
+                        delete options.vendorChunk;
+                        delete options.commonChunk;
+                        delete options.baseHref;
+                        delete options.servePathDefaultWarning;
+                        delete options.hmrWarning;
+                    }
                 }
-                // Only interested in Angular Devkit builders
-                for (const [, options] of (0, workspace_1.allTargetOptions)(target)) {
-                    delete options.extractCss;
-                    delete options.servePathDefaultWarning;
-                    delete options.hmrWarning;
+                else if (target.builder.startsWith('@angular-devkit/build-angular')) {
+                    // Only interested in Angular Devkit builders
+                    for (const [, options] of (0, workspace_1.allTargetOptions)(target)) {
+                        delete options.extractCss;
+                    }
                 }
             }
         }
