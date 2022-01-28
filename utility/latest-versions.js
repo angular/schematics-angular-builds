ac0fdf8c4b54062daa3e9899f1f4940c46df3470
Angular Builds <circleci@angular.io>
2022-01-28 19:31:00 +0000

    b3082355c build: update `@angular/` packages to version 14 prerelease

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -13,7 +13,7 @@ exports.latestVersions = {
     // but ts_library doesn't support JSON inputs.
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
-    Angular: '^13.2.0-next.0',
+    Angular: '^14.0.0-next.0',
     // Since @angular-devkit/build-angular and @schematics/angular are always
     // published together from the same monorepo, and they are both
     // non-experimental, they will always have the same version.
