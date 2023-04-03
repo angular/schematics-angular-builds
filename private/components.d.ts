b9c77365c11abebbbc95324278f514845868e740
Angular Builds <circleci@angular.io>
2023-04-03 09:25:46 +0000

    f76466f4a build: update dependency minimatch to v8

--- private/components.d.ts
+++ private/components.d.ts
@@ -5,4 +5,4 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-export { addModuleImportToStandaloneBootstrap, addFunctionalProvidersToStandaloneBootstrap, callsProvidersFunction, findBootstrapApplicationCall, importsProvidersFrom, } from './standalone';
+export { addModuleImportToStandaloneBootstrap, findBootstrapApplicationCall, importsProvidersFrom, } from './standalone';
