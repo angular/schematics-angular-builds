4c988fd276f588bf8800fa8e7980631484a0178d
Angular Builds <circleci@angular.io>
2023-09-26 18:39:29 +0000

    a189962a5 feat(@schematics/angular): generate functional interceptors by default

--- ssr/index.js
+++ ssr/index.js
@@ -163,7 +163,7 @@ function updateWebpackBuilderServerTsConfigRule(options) {
 }
 function addDependencies() {
     return (0, schematics_1.chain)([
-        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.5+sha-81c7e82', {
+        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.5+sha-a189962', {
             type: utility_1.DependencyType.Default,
         }),
         (0, utility_1.addDependency)('express', latest_versions_1.latestVersions['express'], {
