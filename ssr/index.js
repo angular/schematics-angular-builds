331bac7ed47f3c8338470641b89db3a40e98e523
Angular Builds <circleci@angular.io>
2023-09-26 18:16:39 +0000

    81c7e8215 build: lock file maintenance

--- ssr/index.js
+++ ssr/index.js
@@ -163,7 +163,7 @@ function updateWebpackBuilderServerTsConfigRule(options) {
 }
 function addDependencies() {
     return (0, schematics_1.chain)([
-        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.5+sha-741cca7', {
+        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.5+sha-81c7e82', {
             type: utility_1.DependencyType.Default,
         }),
         (0, utility_1.addDependency)('express', latest_versions_1.latestVersions['express'], {
