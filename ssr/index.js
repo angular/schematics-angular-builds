ee032ffbfedf2f32b03341adf54f7e2cc69d848a
Angular Builds <circleci@angular.io>
2023-09-28 15:41:30 +0000

    5f42ee42c build: update github/codeql-action action to v2.21.9

--- ssr/index.js
+++ ssr/index.js
@@ -163,7 +163,7 @@ function updateWebpackBuilderServerTsConfigRule(options) {
 }
 function addDependencies() {
     return (0, schematics_1.chain)([
-        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.6+sha-e516a4b', {
+        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.6+sha-5f42ee4', {
             type: utility_1.DependencyType.Default,
         }),
         (0, utility_1.addDependency)('express', latest_versions_1.latestVersions['express'], {
