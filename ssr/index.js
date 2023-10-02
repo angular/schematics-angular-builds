6e8512c8909e14fd81ae9fa16b2074234c714c21
Angular Builds <circleci@angular.io>
2023-10-02 13:38:38 +0000

    dc6b6eaf6 feat(@schematics/angular): add migration to replace usages of `@nguniversal/builders`

--- ssr/index.js
+++ ssr/index.js
@@ -163,7 +163,7 @@ function updateWebpackBuilderServerTsConfigRule(options) {
 }
 function addDependencies() {
     return (0, schematics_1.chain)([
-        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.6+sha-2d2e799', {
+        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.6+sha-dc6b6ea', {
             type: utility_1.DependencyType.Default,
         }),
         (0, utility_1.addDependency)('express', latest_versions_1.latestVersions['express'], {
