723da9ef6af4b9a09626165edd5510e4c7711b1d
Angular Builds <circleci@angular.io>
2023-10-04 13:59:24 +0000

    0c20cc4dc fix(@angular-devkit/build-angular): re-add TestBed compileComponents in schematics to support defer block testing

--- ssr/index.js
+++ ssr/index.js
@@ -163,7 +163,7 @@ function updateWebpackBuilderServerTsConfigRule(options) {
 }
 function addDependencies() {
     return (0, schematics_1.chain)([
-        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.6+sha-03a1eaf', {
+        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.6+sha-0c20cc4', {
             type: utility_1.DependencyType.Default,
         }),
         (0, utility_1.addDependency)('express', latest_versions_1.latestVersions['express'], {
