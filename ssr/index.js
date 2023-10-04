1b26c5314821563aaa4351a4a56ece6b44d57aa3
Angular Builds <circleci@angular.io>
2023-10-04 13:58:26 +0000

    83020fc32 fix(@angular-devkit/build-angular): clear diagnostic cache when external templates change with esbuild builders

--- ssr/index.js
+++ ssr/index.js
@@ -163,7 +163,7 @@ function updateWebpackBuilderServerTsConfigRule(options) {
 }
 function addDependencies() {
     return (0, schematics_1.chain)([
-        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.6+sha-419b5c1', {
+        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.6+sha-83020fc', {
             type: utility_1.DependencyType.Default,
         }),
         (0, utility_1.addDependency)('express', latest_versions_1.latestVersions['express'], {
