ed023908884a32b8d4b481934c167a3eea20e8ed
Angular Builds <circleci@angular.io>
2023-10-05 17:40:21 +0000

    c98c049b6 test: update tests to account for changes in the build output structure

--- ssr/index.js
+++ ssr/index.js
@@ -163,7 +163,7 @@ function updateWebpackBuilderServerTsConfigRule(options) {
 }
 function addDependencies() {
     return (0, schematics_1.chain)([
-        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.7+sha-f29b744', {
+        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.7+sha-c98c049', {
             type: utility_1.DependencyType.Default,
         }),
         (0, utility_1.addDependency)('express', latest_versions_1.latestVersions['express'], {
