0083f4aba1f8435e389ce64614241d1ac82e212f
Angular Builds <circleci@angular.io>
2023-10-09 17:49:53 +0000

    3ad028bb4 fix(@angular-devkit/build-angular): ensure localize polyfill and locale specifier are injected when not inlining

--- ssr/index.js
+++ ssr/index.js
@@ -163,7 +163,7 @@ function updateWebpackBuilderServerTsConfigRule(options) {
 }
 function addDependencies() {
     return (0, schematics_1.chain)([
-        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.7+sha-2c33f09', {
+        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.7+sha-3ad028b', {
             type: utility_1.DependencyType.Default,
         }),
         (0, utility_1.addDependency)('express', latest_versions_1.latestVersions['express'], {
