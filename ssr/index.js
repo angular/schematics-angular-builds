2cf70f9edd49688dbbf4d12575e40a5be5e08da5
Angular Builds <circleci@angular.io>
2023-10-11 11:50:02 +0000

    c22fa3656 docs: release notes for the v16.2.6 release

--- ssr/index.js
+++ ssr/index.js
@@ -163,7 +163,7 @@ function updateWebpackBuilderServerTsConfigRule(options) {
 }
 function addDependencies() {
     return (0, schematics_1.chain)([
-        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.7+sha-e5849c8', {
+        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.7+sha-c22fa36', {
             type: utility_1.DependencyType.Default,
         }),
         (0, utility_1.addDependency)('express', latest_versions_1.latestVersions['express'], {
