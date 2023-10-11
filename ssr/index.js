ac99e30d3971928d598b06d7316ec5fb38efb921
Angular Builds <circleci@angular.io>
2023-10-11 14:07:10 +0000

    3938863b9 feat(@schematics/angular): add migration to migrate from `@nguniversal` to `@angular/ssr`

--- ssr/index.js
+++ ssr/index.js
@@ -163,7 +163,7 @@ function updateWebpackBuilderServerTsConfigRule(options) {
 }
 function addDependencies() {
     return (0, schematics_1.chain)([
-        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.8+sha-1635fdc', {
+        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.8+sha-3938863', {
             type: utility_1.DependencyType.Default,
         }),
         (0, utility_1.addDependency)('express', latest_versions_1.latestVersions['express'], {
