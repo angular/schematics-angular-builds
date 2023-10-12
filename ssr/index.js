9dc14b9704a562caf69121a151714a26c68b3f29
Angular Builds <circleci@angular.io>
2023-10-12 08:49:07 +0000

    55422d5fa release: bump the next branch to v17.1.0-next.0

--- ssr/index.js
+++ ssr/index.js
@@ -163,7 +163,7 @@ function updateWebpackBuilderServerTsConfigRule(options) {
 }
 function addDependencies() {
     return (0, schematics_1.chain)([
-        (0, utility_1.addDependency)('@angular/ssr', '^17.0.0-next.9+sha-ca3387c', {
+        (0, utility_1.addDependency)('@angular/ssr', '^17.1.0-next.0+sha-55422d5', {
             type: utility_1.DependencyType.Default,
         }),
         (0, utility_1.addDependency)('express', latest_versions_1.latestVersions['express'], {
