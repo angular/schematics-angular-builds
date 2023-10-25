0c2f398a26f66777e53eabdfedb2ae7aa8524f6e
Angular Builds <circleci@angular.io>
2023-10-25 11:02:45 +0000

    58c06d4d2 build: update ossf/scorecard-action action to v2.3.1

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0-next.0',
-    DevkitBuildAngular: '^17.1.0-next.0+sha-9768c18',
-    AngularSSR: '^17.1.0-next.0+sha-9768c18',
+    DevkitBuildAngular: '^17.1.0-next.0+sha-58c06d4',
+    AngularSSR: '^17.1.0-next.0+sha-58c06d4',
 };
