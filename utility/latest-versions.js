ee12327f97af963b5459f0ff41b9611db0bb9aa9
Angular Builds <circleci@angular.io>
2023-10-26 11:56:18 +0000

    07e2120da perf(@angular-devkit/build-angular): remove JavaScript transformer from server polyfills bundling

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0-next.0',
-    DevkitBuildAngular: '^17.0.0-rc.1+sha-b1aa416',
-    AngularSSR: '^17.0.0-rc.1+sha-b1aa416',
+    DevkitBuildAngular: '^17.0.0-rc.1+sha-07e2120',
+    AngularSSR: '^17.0.0-rc.1+sha-07e2120',
 };
