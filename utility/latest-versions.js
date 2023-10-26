d1e22a355b9f05dae4362d89019da9c5ce635512
Angular Builds <circleci@angular.io>
2023-10-26 14:21:26 +0000

    154833304 refactor: update `inquirer` to version `9.0.6`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0-next.0',
-    DevkitBuildAngular: '^17.0.0-rc.1+sha-e68a662',
-    AngularSSR: '^17.0.0-rc.1+sha-e68a662',
+    DevkitBuildAngular: '^17.0.0-rc.1+sha-1548333',
+    AngularSSR: '^17.0.0-rc.1+sha-1548333',
 };
