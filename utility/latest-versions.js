6eac9fd8415770deab421a2ea01b8335bf3490f3
Angular Builds <circleci@angular.io>
2023-10-27 16:36:45 +0000

    a64d8edfd fix(@angular-devkit/build-angular): add a maximum rendering timeout for SSR and SSG during development

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0-next.0',
-    DevkitBuildAngular: '^17.1.0-next.0+sha-aa9f052',
-    AngularSSR: '^17.1.0-next.0+sha-aa9f052',
+    DevkitBuildAngular: '^17.1.0-next.0+sha-a64d8ed',
+    AngularSSR: '^17.1.0-next.0+sha-a64d8ed',
 };
