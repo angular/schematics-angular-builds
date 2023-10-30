5aa36687a3595cdb99801c022df0c255b061add6
Angular Builds <circleci@angular.io>
2023-10-30 13:22:06 +0000

    b01ae6dc9 build: lock file maintenance

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0-next.0',
-    DevkitBuildAngular: '^17.0.0-rc.2+sha-1f7156b',
-    AngularSSR: '^17.0.0-rc.2+sha-1f7156b',
+    DevkitBuildAngular: '^17.0.0-rc.2+sha-b01ae6d',
+    AngularSSR: '^17.0.0-rc.2+sha-b01ae6d',
 };
