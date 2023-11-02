bd03870121d442178a8396aff809814764742212
Angular Builds <circleci@angular.io>
2023-11-02 13:08:34 +0000

    fd2c4c3 fix(@angular-devkit/build-angular): expose ssr-dev-server builder in the public api

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0-next.0',
-    DevkitBuildAngular: '^17.0.0-rc.3+sha-64bbaf1',
-    AngularSSR: '^17.0.0-rc.3+sha-64bbaf1',
+    DevkitBuildAngular: '^17.0.0-rc.3+sha-fd2c4c3',
+    AngularSSR: '^17.0.0-rc.3+sha-fd2c4c3',
 };
