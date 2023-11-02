55339c27f9846a2545f68a0c0126eebac52fb5c7
Angular Builds <circleci@angular.io>
2023-11-02 16:17:50 +0000

    98c46a0 test(@angular/cli): delay changes to reduce `tests/legacy-cli/e2e/tests/basic/rebuild.ts` flakiness

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0-next.0',
-    DevkitBuildAngular: '^17.0.0-rc.3+sha-f4b6380',
-    AngularSSR: '^17.0.0-rc.3+sha-f4b6380',
+    DevkitBuildAngular: '^17.0.0-rc.3+sha-98c46a0',
+    AngularSSR: '^17.0.0-rc.3+sha-98c46a0',
 };
