5d69fc103224f0dae36ffa2ea0d1b1356d2445b9
Angular Builds <circleci@angular.io>
2023-11-03 09:42:25 +0000

    6d8d948 refactor(@angular-devkit/build-angular): cache compiled load ESM file helper

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0-next.0',
-    DevkitBuildAngular: '^17.1.0-next.0+sha-31d17e0',
-    AngularSSR: '^17.1.0-next.0+sha-31d17e0',
+    DevkitBuildAngular: '^17.1.0-next.0+sha-6d8d948',
+    AngularSSR: '^17.1.0-next.0+sha-6d8d948',
 };
