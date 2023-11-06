c0e763b4c04eafab65127d37ef10a4f789ec6fba
Angular Builds <circleci@angular.io>
2023-11-06 15:42:28 +0000

    f87f22d fix(@angular-devkit/build-angular): keep dependencies pre-bundling validate between builds

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0-next.0',
-    DevkitBuildAngular: '^17.0.0-rc.3+sha-d46fb12',
-    AngularSSR: '^17.0.0-rc.3+sha-d46fb12',
+    DevkitBuildAngular: '^17.0.0-rc.3+sha-f87f22d',
+    AngularSSR: '^17.0.0-rc.3+sha-f87f22d',
 };
