e92949dfad7c701792116ee6bdcdafa7bc2bea35
Angular Builds <circleci@angular.io>
2023-11-06 14:24:59 +0000

    d46fb12 fix(@angular-devkit/build-angular): disable dependency optimization for SSR

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0-next.0',
-    DevkitBuildAngular: '^17.0.0-rc.3+sha-4733cd3',
-    AngularSSR: '^17.0.0-rc.3+sha-4733cd3',
+    DevkitBuildAngular: '^17.0.0-rc.3+sha-d46fb12',
+    AngularSSR: '^17.0.0-rc.3+sha-d46fb12',
 };
