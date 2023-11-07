23128459d18baec3a8642a69a5bdec0e58a4e03b
Angular Builds <circleci@angular.io>
2023-11-07 19:58:33 +0000

    8a9def5 test: add timeout to vite re-use cache

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0-next.0',
-    DevkitBuildAngular: '^17.0.0-rc.4+sha-9d4d11c',
-    AngularSSR: '^17.0.0-rc.4+sha-9d4d11c',
+    DevkitBuildAngular: '^17.0.0-rc.4+sha-8a9def5',
+    AngularSSR: '^17.0.0-rc.4+sha-8a9def5',
 };
