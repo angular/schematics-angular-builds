91788e6199a3c9411ec5ed618fc979e77d21587d
Angular Builds <circleci@angular.io>
2023-11-08 18:03:59 +0000

    8915ca9 test: remove old unneeded test

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0',
-    DevkitBuildAngular: '^17.1.0-next.0+sha-78c5a12',
-    AngularSSR: '^17.1.0-next.0+sha-78c5a12',
+    DevkitBuildAngular: '^17.1.0-next.0+sha-8915ca9',
+    AngularSSR: '^17.1.0-next.0+sha-8915ca9',
 };
