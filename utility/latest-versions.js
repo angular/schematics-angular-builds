30a6f6b8f67292e859007a47bb875066291a7cf9
Angular Builds <circleci@angular.io>
2023-11-15 21:53:54 +0000

    fa4d8ff fix(@angular-devkit/build-angular): add actionable error when file replacement is missing

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0',
-    DevkitBuildAngular: '^17.0.0+sha-8837719',
-    AngularSSR: '^17.0.0+sha-8837719',
+    DevkitBuildAngular: '^17.0.0+sha-fa4d8ff',
+    AngularSSR: '^17.0.0+sha-fa4d8ff',
 };
