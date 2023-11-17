14c7356640eeb32a85a3c2c685ebdf83b585290e
Angular Builds <circleci@angular.io>
2023-11-17 13:11:52 +0000

    3b99980 fix(@angular-devkit/build-angular): emit root files when `localize` is enabled when using the esbuild based builders

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0',
-    DevkitBuildAngular: '^17.0.1+sha-0027e4d',
-    AngularSSR: '^17.0.1+sha-0027e4d',
+    DevkitBuildAngular: '^17.0.1+sha-3b99980',
+    AngularSSR: '^17.0.1+sha-3b99980',
 };
