d3143518a9339e986308ae1a56ac76bb29bde3f9
Angular Builds <circleci@angular.io>
2023-11-20 14:33:34 +0000

    62d5138 fix(@angular-devkit/build-angular): only include vendor sourcemaps when using the dev-server when the option is enabled

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0',
-    DevkitBuildAngular: '^17.0.1+sha-5772abc',
-    AngularSSR: '^17.0.1+sha-5772abc',
+    DevkitBuildAngular: '^17.0.1+sha-62d5138',
+    AngularSSR: '^17.0.1+sha-62d5138',
 };
