730e6780942bbbc84f0e9e203cac9aa5e737e4a6
Angular Builds <circleci@angular.io>
2023-11-20 14:39:54 +0000

    0a9c2ab test: remove unneeded kill-all-processes calls in E2E tests

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.1.0-next.0+sha-e859784',
-    AngularSSR: '^17.1.0-next.0+sha-e859784',
+    DevkitBuildAngular: '^17.1.0-next.0+sha-0a9c2ab',
+    AngularSSR: '^17.1.0-next.0+sha-0a9c2ab',
 };
