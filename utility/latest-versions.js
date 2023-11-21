3d0791aa4374b26789e01268b241907b8ffae15c
Angular Builds <circleci@angular.io>
2023-11-21 16:59:51 +0000

    48b2732 perf(@angular-devkit/build-angular): only create one instance of postcss when needed

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.1.0-next.0+sha-07834da',
-    AngularSSR: '^17.1.0-next.0+sha-07834da',
+    DevkitBuildAngular: '^17.1.0-next.0+sha-48b2732',
+    AngularSSR: '^17.1.0-next.0+sha-48b2732',
 };
