63a0a2471f16ea6d8e152dae18ddc27fc01e5847
Angular Builds <circleci@angular.io>
2023-11-22 17:17:29 +0000

    657a07b fix(@angular-devkit/build-angular): treeshake unused class that use custom decorators

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0',
-    DevkitBuildAngular: '^17.0.3+sha-7e12fdf',
-    AngularSSR: '^17.0.3+sha-7e12fdf',
+    DevkitBuildAngular: '^17.0.3+sha-657a07b',
+    AngularSSR: '^17.0.3+sha-657a07b',
 };
