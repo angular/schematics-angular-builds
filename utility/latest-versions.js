58933041b2675135c599f7015620fe6fb661d3da
Angular Builds <circleci@angular.io>
2023-11-27 17:54:21 +0000

    876ca1e fix(@angular-devkit/build-angular): correct set locale when using esbuild based builders

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.1.0-next.0+sha-4da732d',
-    AngularSSR: '^17.1.0-next.0+sha-4da732d',
+    DevkitBuildAngular: '^17.1.0-next.0+sha-876ca1e',
+    AngularSSR: '^17.1.0-next.0+sha-876ca1e',
 };
