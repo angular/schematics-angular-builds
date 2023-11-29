48b5bf12e9f7c9026f165770a1049189f8bdd3f1
Angular Builds <circleci@angular.io>
2023-11-29 23:34:34 +0000

    ea5bbe7 Revert "fix(@angular-devkit/build-angular): improve file watching on Windows when using certain IDEs"

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0',
-    DevkitBuildAngular: '^17.0.3+sha-7455fdc',
-    AngularSSR: '^17.0.3+sha-7455fdc',
+    DevkitBuildAngular: '^17.0.4+sha-ea5bbe7',
+    AngularSSR: '^17.0.4+sha-ea5bbe7',
 };
