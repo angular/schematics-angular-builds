cf7f72ffdd28d0ca7243a0ac9159b51ad40caaaf
Angular Builds <circleci@angular.io>
2023-11-29 19:00:41 +0000

    ccba849 feat(@angular-devkit/build-angular): support keyboard command shortcuts in application dev server

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.1.0-next.0+sha-9b71f55',
-    AngularSSR: '^17.1.0-next.0+sha-9b71f55',
+    DevkitBuildAngular: '^17.1.0-next.0+sha-ccba849',
+    AngularSSR: '^17.1.0-next.0+sha-ccba849',
 };
