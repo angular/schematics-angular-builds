8209e9c5c18b19c307a7e1c70ebd447092d3ad40
Angular Builds <circleci@angular.io>
2023-12-01 07:54:58 +0000

    03985a4 refactor(@angular-devkit/build-angular): only use rename and change raw events

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.1.0-next.0+sha-f816162',
-    AngularSSR: '^17.1.0-next.0+sha-f816162',
+    DevkitBuildAngular: '^17.1.0-next.0+sha-03985a4',
+    AngularSSR: '^17.1.0-next.0+sha-03985a4',
 };
