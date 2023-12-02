3ce8a5c8b73888fb9251bc07def1af352f2fda8d
Angular Builds <circleci@angular.io>
2023-12-02 21:08:02 +0000

    e3820cb perf(@angular-devkit/build-angular): only enable advanced optimizations with script optimizations

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0',
-    DevkitBuildAngular: '^17.0.5+sha-da5d394',
-    AngularSSR: '^17.0.5+sha-da5d394',
+    DevkitBuildAngular: '^17.0.5+sha-e3820cb',
+    AngularSSR: '^17.0.5+sha-e3820cb',
 };
