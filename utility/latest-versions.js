899bd23c40647e70fd93949c1655f87a083b6ead
Angular Builds <circleci@angular.io>
2023-12-05 15:11:20 +0000

    f9e982c fix(@angular-devkit/build-angular): check namespaced Sass variables when rebasing URLs

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0',
-    DevkitBuildAngular: '^17.0.5+sha-7df4b06',
-    AngularSSR: '^17.0.5+sha-7df4b06',
+    DevkitBuildAngular: '^17.0.5+sha-f9e982c',
+    AngularSSR: '^17.0.5+sha-f9e982c',
 };
