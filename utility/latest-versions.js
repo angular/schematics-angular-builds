137461219895203691c3ea779430a188f6e9304d
Angular Builds <circleci@angular.io>
2023-12-05 15:29:57 +0000

    a8dbf1d fix(@angular-devkit/build-angular): watch symlink when using `preserveSymlinks` option

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0',
-    DevkitBuildAngular: '^17.0.5+sha-f9e982c',
-    AngularSSR: '^17.0.5+sha-f9e982c',
+    DevkitBuildAngular: '^17.0.5+sha-a8dbf1d',
+    AngularSSR: '^17.0.5+sha-a8dbf1d',
 };
