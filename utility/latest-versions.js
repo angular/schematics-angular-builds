45bf234f3b007b6bd67584db53a967b5d0075098
Angular Builds <circleci@angular.io>
2023-12-05 20:28:19 +0000

    c1ae7f3 refactor: update rxjs imports

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.1.0-next.0+sha-8edee8e',
-    AngularSSR: '^17.1.0-next.0+sha-8edee8e',
+    DevkitBuildAngular: '^17.1.0-next.0+sha-c1ae7f3',
+    AngularSSR: '^17.1.0-next.0+sha-c1ae7f3',
 };
