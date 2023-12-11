2fcb6427727dd89e5ec711e939ec89c35a56699e
Angular Builds <circleci@angular.io>
2023-12-11 09:35:09 +0000

    fe51926 refactor(@angular-devkit/build-angular): convert watching to use watchpack package

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.1.0-next.1+sha-1349363',
-    AngularSSR: '^17.1.0-next.1+sha-1349363',
+    DevkitBuildAngular: '^17.1.0-next.1+sha-fe51926',
+    AngularSSR: '^17.1.0-next.1+sha-fe51926',
 };
