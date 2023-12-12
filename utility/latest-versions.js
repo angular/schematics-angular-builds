5b54f412b3d39b9e9ce4ae2c1402c5ee91348359
Angular Builds <circleci@angular.io>
2023-12-12 16:48:40 +0000

    ef11781 fix(@angular-devkit/build-angular): allow vite to serve JavaScript and TypeScript assets

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0',
-    DevkitBuildAngular: '^17.0.6+sha-385eb77',
-    AngularSSR: '^17.0.6+sha-385eb77',
+    DevkitBuildAngular: '^17.0.6+sha-ef11781',
+    AngularSSR: '^17.0.6+sha-ef11781',
 };
