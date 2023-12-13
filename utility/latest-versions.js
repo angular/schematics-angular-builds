56760066b26fd35ef3e747603d291ad6ee29579d
Angular Builds <circleci@angular.io>
2023-12-13 17:21:11 +0000

    0fa1e34 fix(@angular-devkit/build-angular): ensure empty optimized Sass stylesheets stay empty

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.1.0-next.2+sha-e9a51a3',
-    AngularSSR: '^17.1.0-next.2+sha-e9a51a3',
+    DevkitBuildAngular: '^17.1.0-next.2+sha-0fa1e34',
+    AngularSSR: '^17.1.0-next.2+sha-0fa1e34',
 };
