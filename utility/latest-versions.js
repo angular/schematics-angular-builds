536925c4114000365b12a24af15475ddf072a7cd
Angular Builds <circleci@angular.io>
2023-12-13 12:29:12 +0000

    5370b03 test: delay `killAllProcesses` in serve test

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.1.0-next.1+sha-8df2043',
-    AngularSSR: '^17.1.0-next.1+sha-8df2043',
+    DevkitBuildAngular: '^17.1.0-next.1+sha-5370b03',
+    AngularSSR: '^17.1.0-next.1+sha-5370b03',
 };
