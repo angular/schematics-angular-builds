d7b06cf60543ab9352b3deda76d2b51f54757eab
Angular Builds <circleci@angular.io>
2023-12-14 11:07:06 +0000

    66edac4 refactor(@angular-devkit/build-angular): add index HTML transformer to application programmatic usage

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.1.0-next.2+sha-68dfe3b',
-    AngularSSR: '^17.1.0-next.2+sha-68dfe3b',
+    DevkitBuildAngular: '^17.1.0-next.2+sha-66edac4',
+    AngularSSR: '^17.1.0-next.2+sha-66edac4',
 };
