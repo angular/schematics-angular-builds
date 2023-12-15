180e75a5146ca008a4ec91add820cff422ee5144
Angular Builds <circleci@angular.io>
2023-12-15 15:39:04 +0000

    0b48acc fix(@angular/cli): re-add `-d` alias for `--dry-run`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0',
-    DevkitBuildAngular: '^17.0.7+sha-aa6c757',
-    AngularSSR: '^17.0.7+sha-aa6c757',
+    DevkitBuildAngular: '^17.0.7+sha-0b48acc',
+    AngularSSR: '^17.0.7+sha-0b48acc',
 };
