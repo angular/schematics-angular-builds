095e6f19ab37055461c480b70c4d28447d3e1341
Angular Builds <circleci@angular.io>
2024-01-03 08:51:39 +0000

    5cffd36 build: add function type annotation for bazel win32 build support

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.1.0-next.3+sha-24d1d53',
-    AngularSSR: '^17.1.0-next.3+sha-24d1d53',
+    DevkitBuildAngular: '^17.1.0-next.3+sha-5cffd36',
+    AngularSSR: '^17.1.0-next.3+sha-5cffd36',
 };
