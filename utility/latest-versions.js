8bfe2b4d162b8da5dd8ff565951c7da85a18a626
Angular Builds <circleci@angular.io>
2024-01-03 10:49:35 +0000

    88d6ca4 fix(@angular-devkit/schematics): replace template line endings with platform specific

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0',
-    DevkitBuildAngular: '^17.0.8+sha-eec44d0',
-    AngularSSR: '^17.0.8+sha-eec44d0',
+    DevkitBuildAngular: '^17.0.8+sha-88d6ca4',
+    AngularSSR: '^17.0.8+sha-88d6ca4',
 };
