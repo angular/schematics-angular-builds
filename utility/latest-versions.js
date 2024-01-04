7219801077a4a2bbd9a76d1eb17bfb65629208ad
Angular Builds <circleci@angular.io>
2024-01-04 19:12:29 +0000

    6ce7ae3 wip: debugging Windows CI for WTR

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.2.0-next.0+sha-1d874da',
-    AngularSSR: '^17.2.0-next.0+sha-1d874da',
+    DevkitBuildAngular: '^17.2.0-next.0+sha-6ce7ae3',
+    AngularSSR: '^17.2.0-next.0+sha-6ce7ae3',
 };
