163eaaa1c125664761655e34c81f3b839372c58b
Angular Builds <circleci@angular.io>
2024-01-04 20:50:21 +0000

    1d68685 ci: fix `sauce-connect-proxy` version

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.1.0-rc.0+sha-092da74',
-    AngularSSR: '^17.1.0-rc.0+sha-092da74',
+    DevkitBuildAngular: '^17.1.0-rc.0+sha-1d68685',
+    AngularSSR: '^17.1.0-rc.0+sha-1d68685',
 };
