48f1a7a87786aeba26692d22bac4ae0003198ccf
Angular Builds <circleci@angular.io>
2024-01-04 13:16:19 +0000

    4f53e0f refactor: remove `process.cwd` from Web Test Runner builder

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.2.0-next.0+sha-330495b',
-    AngularSSR: '^17.2.0-next.0+sha-330495b',
+    DevkitBuildAngular: '^17.2.0-next.0+sha-4f53e0f',
+    AngularSSR: '^17.2.0-next.0+sha-4f53e0f',
 };
