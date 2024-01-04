a000293fa110898969730946179a60504d9a5637
Angular Builds <circleci@angular.io>
2024-01-04 00:45:40 +0000

    701ceca ci: make Chrome available to Web Test Runner

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.2.0-next.0+sha-318ebc4',
-    AngularSSR: '^17.2.0-next.0+sha-318ebc4',
+    DevkitBuildAngular: '^17.2.0-next.0+sha-701ceca',
+    AngularSSR: '^17.2.0-next.0+sha-701ceca',
 };
