bd6a0ba0d0bcc35ae06d0a88ffac3baca51f7b15
Angular Builds <circleci@angular.io>
2024-01-05 18:42:02 +0000

    ecdad2a ci: re-enable WTR e2e test on Windows

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.1.0-rc.0+sha-8c81571',
-    AngularSSR: '^17.1.0-rc.0+sha-8c81571',
+    DevkitBuildAngular: '^17.1.0-rc.0+sha-ecdad2a',
+    AngularSSR: '^17.1.0-rc.0+sha-ecdad2a',
 };
