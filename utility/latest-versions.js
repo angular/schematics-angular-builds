9ce0f556ce4a56893e4e7f6f155922be84a502b1
Angular Builds <circleci@angular.io>
2024-01-05 09:52:35 +0000

    6d7fdb9 fix(@angular-devkit/build-angular): show diagnostic messages after build stats

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.1.0-rc.0+sha-626a5ad',
-    AngularSSR: '^17.1.0-rc.0+sha-626a5ad',
+    DevkitBuildAngular: '^17.1.0-rc.0+sha-6d7fdb9',
+    AngularSSR: '^17.1.0-rc.0+sha-6d7fdb9',
 };
