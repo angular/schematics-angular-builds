9a0544c8f0b386c76aee14635f535ccd42664d50
Angular Builds <circleci@angular.io>
2024-01-09 18:25:18 +0000

    c49f9ab Revert "build: update dependency webpack-dev-middleware to v7"

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.2.0-next.0+sha-7b8b65a',
-    AngularSSR: '^17.2.0-next.0+sha-7b8b65a',
+    DevkitBuildAngular: '^17.2.0-next.0+sha-c49f9ab',
+    AngularSSR: '^17.2.0-next.0+sha-c49f9ab',
 };
