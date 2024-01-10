50fc0284bd4aed6499380a9049d8253f3b67553d
Angular Builds <circleci@angular.io>
2024-01-10 19:13:58 +0000

    adc9dab release: cut the v17.0.10 release

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.0.0',
-    DevkitBuildAngular: '^17.0.9+sha-7913a8e',
-    AngularSSR: '^17.0.9+sha-7913a8e',
+    DevkitBuildAngular: '^17.0.10+sha-adc9dab',
+    AngularSSR: '^17.0.10+sha-adc9dab',
 };
