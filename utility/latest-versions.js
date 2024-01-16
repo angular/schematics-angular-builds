0f59edb9f3c44471c14fa504f60701584880a97f
Angular Builds <circleci@angular.io>
2024-01-16 08:15:27 +0000

    e76950e build: update dependency source-map-loader to v5

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.1.0-rc.1+sha-537e83e',
-    AngularSSR: '^17.1.0-rc.1+sha-537e83e',
+    DevkitBuildAngular: '^17.1.0-rc.1+sha-e76950e',
+    AngularSSR: '^17.1.0-rc.1+sha-e76950e',
 };
