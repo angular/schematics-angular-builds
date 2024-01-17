5b58bb68d68a5ce3aa43af20eb91647fb6144518
Angular Builds <circleci@angular.io>
2024-01-17 23:59:48 +0000

    753f726 build: update Angular version ranges for stable v17.1

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -13,7 +13,7 @@ exports.latestVersions = {
     // but ts_library doesn't support JSON inputs.
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
-    Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.1.0-rc.1+sha-a2ddb9f',
-    AngularSSR: '^17.1.0-rc.1+sha-a2ddb9f',
+    Angular: '^17.1.0',
+    DevkitBuildAngular: '^17.1.0-rc.1+sha-753f726',
+    AngularSSR: '^17.1.0-rc.1+sha-753f726',
 };
