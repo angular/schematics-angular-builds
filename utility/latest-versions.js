2a03ad87105b9f8a00c78bc50d7a5e481082210d
Angular Builds <circleci@angular.io>
2024-01-17 13:51:43 +0000

    9c15470 build: update dependency postcss-loader to v8

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.2.0-next.0+sha-c2789e1',
-    AngularSSR: '^17.2.0-next.0+sha-c2789e1',
+    DevkitBuildAngular: '^17.2.0-next.0+sha-9c15470',
+    AngularSSR: '^17.2.0-next.0+sha-9c15470',
 };
