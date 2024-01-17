0a12f53a80ead390c2aebf0295d3c7e58a1a0c35
Angular Builds <circleci@angular.io>
2024-01-17 11:07:28 +0000

    a2ddb9f build: update dependency ng-packagr to v17.1.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.1.0-rc.1+sha-329d800',
-    AngularSSR: '^17.1.0-rc.1+sha-329d800',
+    DevkitBuildAngular: '^17.1.0-rc.1+sha-a2ddb9f',
+    AngularSSR: '^17.1.0-rc.1+sha-a2ddb9f',
 };
