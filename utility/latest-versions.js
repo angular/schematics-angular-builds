f02b4b56bd7347d89d56fbc78a7065705d7d79a7
Angular Builds <circleci@angular.io>
2024-01-19 17:30:17 +0000

    383bcaa build: update actions/upload-artifact action to v4.2.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0-next.0',
-    DevkitBuildAngular: '^17.2.0-next.0+sha-a42660c',
-    AngularSSR: '^17.2.0-next.0+sha-a42660c',
+    DevkitBuildAngular: '^17.2.0-next.0+sha-383bcaa',
+    AngularSSR: '^17.2.0-next.0+sha-383bcaa',
 };
