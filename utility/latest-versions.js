82ef3afdc67a9e9ef3e05df49212d792bb31c3a5
Angular Builds <circleci@angular.io>
2024-01-22 15:01:47 +0000

    18879eb refactor(@angular-devkit/build-angular): use `for loop` instead of `filter` and `forEach`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0',
-    DevkitBuildAngular: '^17.1.0+sha-ed95a92',
-    AngularSSR: '^17.1.0+sha-ed95a92',
+    DevkitBuildAngular: '^17.1.0+sha-18879eb',
+    AngularSSR: '^17.1.0+sha-18879eb',
 };
