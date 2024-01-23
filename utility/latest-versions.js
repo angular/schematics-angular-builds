2138b161cfd8fbba641acf3e7b8e1d5058c64dfe
Angular Builds <circleci@angular.io>
2024-01-23 09:57:34 +0000

    8ebb754 fix(@angular/cli): update regex to validate the project-name

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0',
-    DevkitBuildAngular: '^17.1.0+sha-412fe6e',
-    AngularSSR: '^17.1.0+sha-412fe6e',
+    DevkitBuildAngular: '^17.1.0+sha-8ebb754',
+    AngularSSR: '^17.1.0+sha-8ebb754',
 };
