72016b1bbc6d6a309ad93dc38059422f3cb73f89
Angular Builds <circleci@angular.io>
2024-01-23 15:28:24 +0000

    45dea6f fix(@angular-devkit/build-angular): provide actionable error message when server bundle is missing default export

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0',
-    DevkitBuildAngular: '^17.1.0+sha-4e2b23f',
-    AngularSSR: '^17.1.0+sha-4e2b23f',
+    DevkitBuildAngular: '^17.1.0+sha-45dea6f',
+    AngularSSR: '^17.1.0+sha-45dea6f',
 };
