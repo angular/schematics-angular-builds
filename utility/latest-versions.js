fdaae31c3a92bffba6e3a3acb169bdef00994502
Angular Builds <circleci@angular.io>
2024-01-25 15:03:12 +0000

    33662ac refactor(@angular-devkit/build-angular): increase type safety in bundle-context

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0',
-    DevkitBuildAngular: '^17.1.1+sha-6815f13',
-    AngularSSR: '^17.1.1+sha-6815f13',
+    DevkitBuildAngular: '^17.1.1+sha-33662ac',
+    AngularSSR: '^17.1.1+sha-33662ac',
 };
