0bc1d5d4ab39e4e3d6a9e0498620a7220847c952
Angular Builds <circleci@angular.io>
2024-01-26 14:43:44 +0000

    37ffa5e fix(@angular-devkit/build-angular): resolve absolute `output-path` when using esbuild based builders

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.2.0-next.0',
-    DevkitBuildAngular: '^17.2.0-next.0+sha-910531a',
-    AngularSSR: '^17.2.0-next.0+sha-910531a',
+    DevkitBuildAngular: '^17.2.0-next.0+sha-37ffa5e',
+    AngularSSR: '^17.2.0-next.0+sha-37ffa5e',
 };
