6ecbb4ae36abc127cdb1489d9ecd9249095701e3
Angular Builds <circleci@angular.io>
2024-01-26 15:51:26 +0000

    235c840 fix(@angular-devkit/build-angular): handle regular expressions in proxy config when using Vite

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.1.0',
-    DevkitBuildAngular: '^17.1.1+sha-a0e3060',
-    AngularSSR: '^17.1.1+sha-a0e3060',
+    DevkitBuildAngular: '^17.1.1+sha-235c840',
+    AngularSSR: '^17.1.1+sha-235c840',
 };
