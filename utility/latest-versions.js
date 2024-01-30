4fa5b28b7f0f522874591735db32799ed6ed264d
Angular Builds <circleci@angular.io>
2024-01-30 20:10:45 +0000

    944cbcd fix(@angular-devkit/build-angular): limit the number of lazy chunks visible in the stats table

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.2.0-next.0',
-    DevkitBuildAngular: '^17.2.0-next.0+sha-45820d2',
-    AngularSSR: '^17.2.0-next.0+sha-45820d2',
+    DevkitBuildAngular: '^17.2.0-next.0+sha-944cbcd',
+    AngularSSR: '^17.2.0-next.0+sha-944cbcd',
 };
