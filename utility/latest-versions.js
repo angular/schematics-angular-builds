d686f8574b5e762848ef906db22409959f38e801
Angular Builds <circleci@angular.io>
2024-02-07 12:57:58 +0000

    8d5af1d fix(@angular-devkit/build-angular): ensure correct `.html` served with Vite dev-server

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.2.0-next.0',
-    DevkitBuildAngular: '^17.2.0-next.1+sha-b59f663',
-    AngularSSR: '^17.2.0-next.1+sha-b59f663',
+    DevkitBuildAngular: '^17.2.0-next.1+sha-8d5af1d',
+    AngularSSR: '^17.2.0-next.1+sha-8d5af1d',
 };
