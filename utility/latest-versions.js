ff7615efd5f65008bc1aba0265ea5e4f778a334e
Angular Builds <circleci@angular.io>
2024-02-08 17:35:19 +0000

    1aeeb7d fix(@angular-devkit/build-angular): ensure WebWorker main entry is used in output code

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.2.0-next.0',
-    DevkitBuildAngular: '^17.2.0-next.1+sha-a957ede',
-    AngularSSR: '^17.2.0-next.1+sha-a957ede',
+    DevkitBuildAngular: '^17.2.0-next.1+sha-1aeeb7d',
+    AngularSSR: '^17.2.0-next.1+sha-1aeeb7d',
 };
