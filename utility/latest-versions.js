15a8a197403d2706ef0a422ee421a273e6a7e0c5
Angular Builds <circleci@angular.io>
2024-02-09 14:46:47 +0000

    c9bc507 build: update to vite `5.1.0`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.2.0-next.0',
-    DevkitBuildAngular: '^17.3.0-next.0+sha-782c718',
-    AngularSSR: '^17.3.0-next.0+sha-782c718',
+    DevkitBuildAngular: '^17.3.0-next.0+sha-c9bc507',
+    AngularSSR: '^17.3.0-next.0+sha-c9bc507',
 };
