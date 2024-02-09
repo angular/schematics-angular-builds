d90c2dd3ddc43eb944b39cc10eeb3771292481ea
Angular Builds <circleci@angular.io>
2024-02-09 14:41:30 +0000

    b07246a fix(@angular/cli): prevent BOM errors in `package.json` during `ng update`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.2.0-next.0',
-    DevkitBuildAngular: '^17.2.0-rc.0+sha-da1c38c',
-    AngularSSR: '^17.2.0-rc.0+sha-da1c38c',
+    DevkitBuildAngular: '^17.2.0-rc.0+sha-b07246a',
+    AngularSSR: '^17.2.0-rc.0+sha-b07246a',
 };
