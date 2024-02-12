d940e5b3c49e08fdbac5827ed6f431a9b65b8d1e
Angular Builds <circleci@angular.io>
2024-02-12 07:44:06 +0000

    8598bb5 fix(@angular-devkit/build-angular): bypass Vite prebundling for absolute URL imports

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.2.0-next.0',
-    DevkitBuildAngular: '^17.2.0-rc.0+sha-b07246a',
-    AngularSSR: '^17.2.0-rc.0+sha-b07246a',
+    DevkitBuildAngular: '^17.2.0-rc.0+sha-8598bb5',
+    AngularSSR: '^17.2.0-rc.0+sha-8598bb5',
 };
