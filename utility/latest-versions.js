1ce7d8e5e41c6dbc64d4ec7149caf6b6a8444b9c
Angular Builds <circleci@angular.io>
2024-02-13 15:13:49 +0000

    d43efe7 build: update dependency @typescript-eslint/eslint-plugin to v7

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.2.0-next.0',
-    DevkitBuildAngular: '^17.3.0-next.0+sha-ae51ed6',
-    AngularSSR: '^17.3.0-next.0+sha-ae51ed6',
+    DevkitBuildAngular: '^17.3.0-next.0+sha-d43efe7',
+    AngularSSR: '^17.3.0-next.0+sha-d43efe7',
 };
