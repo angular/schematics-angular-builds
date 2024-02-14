d084c4f26137dcbaf1c7fcc0f1cbba3e225e50c2
Angular Builds <circleci@angular.io>
2024-02-14 19:44:16 +0000

    2f90b1d docs: release notes for the v17.1.4 release

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.2.0-next.0',
-    DevkitBuildAngular: '^17.3.0-next.0+sha-6e8d326',
-    AngularSSR: '^17.3.0-next.0+sha-6e8d326',
+    DevkitBuildAngular: '^17.3.0-next.0+sha-2f90b1d',
+    AngularSSR: '^17.3.0-next.0+sha-2f90b1d',
 };
