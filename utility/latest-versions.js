75f324ad03492109ca391b253c183ef55ddf23f4
Angular Builds <circleci@angular.io>
2024-02-22 18:10:15 +0000

    4fc5c6a Revert "refactor(@schematics/angular): remove private `/components` API"

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.2.0',
-    DevkitBuildAngular: '^17.2.0+sha-9e7c47b',
-    AngularSSR: '^17.2.0+sha-9e7c47b',
+    DevkitBuildAngular: '^17.2.0+sha-4fc5c6a',
+    AngularSSR: '^17.2.0+sha-4fc5c6a',
 };
