9eabe01f7da47fa6b00ce82d6ec9bbdcf8db8df4
Angular Builds <circleci@angular.io>
2024-02-23 12:15:43 +0000

    3394d3c fix(@angular-devkit/build-angular): ensure all related stylesheets are rebuilt when an import changes

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.2.0',
-    DevkitBuildAngular: '^17.2.1+sha-ca18ead',
-    AngularSSR: '^17.2.1+sha-ca18ead',
+    DevkitBuildAngular: '^17.2.1+sha-3394d3c',
+    AngularSSR: '^17.2.1+sha-3394d3c',
 };
