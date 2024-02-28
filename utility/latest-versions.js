2398f06d4f6f580fcc9f39805d6ceb7b458cd1e4
Angular Builds <circleci@angular.io>
2024-02-28 22:16:35 +0000

    ea843eb release: cut the v17.2.2 release

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.2.0',
-    DevkitBuildAngular: '^17.2.1+sha-ac779dd',
-    AngularSSR: '^17.2.1+sha-ac779dd',
+    DevkitBuildAngular: '^17.2.2+sha-ea843eb',
+    AngularSSR: '^17.2.2+sha-ea843eb',
 };
