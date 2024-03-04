1cf7346ecf7f394fab2ce1aec46b0a150b4b7c2e
Angular Builds <circleci@angular.io>
2024-03-04 08:41:24 +0000

    259ec72 fix(@angular-devkit/build-angular): avoid marking component styles as media with no output media directory

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.2.0',
-    DevkitBuildAngular: '^17.2.2+sha-ea843eb',
-    AngularSSR: '^17.2.2+sha-ea843eb',
+    DevkitBuildAngular: '^17.2.2+sha-259ec72',
+    AngularSSR: '^17.2.2+sha-259ec72',
 };
