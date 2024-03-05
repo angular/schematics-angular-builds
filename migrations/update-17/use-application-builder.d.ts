9fcbd7e769c36dd6eef94ccf6814153519fdab3c
Angular Builds <circleci@angular.io>
2024-03-05 15:06:46 +0000

    5ab71fc feat(@schematics/angular): update CSS/Sass import/use specifiers in application migration

--- migrations/update-17/use-application-builder.d.ts
+++ migrations/update-17/use-application-builder.d.ts
@@ -6,4 +6,7 @@
  * found in the LICENSE file at https://angular.io/license
  */
 import { Rule } from '@angular-devkit/schematics';
+/**
+ * Migration main entrypoint
+ */
 export default function (): Rule;
