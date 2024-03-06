7e0f6e3a28fc119502d7ec82fa6a73c4784ab52c
Angular Builds <circleci@angular.io>
2024-03-06 10:07:28 +0000

    164fcaf ci: disable auto updates for `critters`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.2.0-next.0',
-    DevkitBuildAngular: '^17.3.0-next.0+sha-0b7a101',
-    AngularSSR: '^17.3.0-next.0+sha-0b7a101',
+    DevkitBuildAngular: '^17.3.0-next.0+sha-164fcaf',
+    AngularSSR: '^17.3.0-next.0+sha-164fcaf',
 };
