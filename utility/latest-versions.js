61503662edab1cb08519d1975c1b2e850df050c7
Angular Builds <circleci@angular.io>
2024-03-11 21:51:35 +0000

    db4b5f3 build: update less-loader to v12.2.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.3.0-next.0',
-    DevkitBuildAngular: '^18.0.0-next.0+sha-e729f6e',
-    AngularSSR: '^18.0.0-next.0+sha-e729f6e',
+    DevkitBuildAngular: '^18.0.0-next.0+sha-db4b5f3',
+    AngularSSR: '^18.0.0-next.0+sha-db4b5f3',
 };
