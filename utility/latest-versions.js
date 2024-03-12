cfe910ab0835f0bff910097a54c46d83ec91f77e
Angular Builds <circleci@angular.io>
2024-03-12 14:33:11 +0000

    51debcd refactor(@angular-devkit/build-angular): properly display errors originating in ESM loader hooks

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.3.0-next.0',
-    DevkitBuildAngular: '^18.0.0-next.0+sha-01fa047',
-    AngularSSR: '^18.0.0-next.0+sha-01fa047',
+    DevkitBuildAngular: '^18.0.0-next.0+sha-51debcd',
+    AngularSSR: '^18.0.0-next.0+sha-51debcd',
 };
