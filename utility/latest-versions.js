40dec09098e5a8bd22a67898c3a3d34f9d54d9f7
Angular Builds <circleci@angular.io>
2024-03-13 23:49:40 +0000

    407c3a3 release: cut the v17.3.0 release

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.3.0',
-    DevkitBuildAngular: '^17.3.0-rc.0+sha-99fb28a',
-    AngularSSR: '^17.3.0-rc.0+sha-99fb28a',
+    DevkitBuildAngular: '^17.3.0+sha-407c3a3',
+    AngularSSR: '^17.3.0+sha-407c3a3',
 };
