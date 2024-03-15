4876771ac751ac8e97e06701bcb339d4951a5b5e
Angular Builds <circleci@angular.io>
2024-03-15 08:24:00 +0000

    99104cd perf(@angular-devkit/build-angular): avoid transforming empty component stylesheets

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.3.0-next.0',
-    DevkitBuildAngular: '^18.0.0-next.0+sha-af0d75c',
-    AngularSSR: '^18.0.0-next.0+sha-af0d75c',
+    DevkitBuildAngular: '^18.0.0-next.0+sha-99104cd',
+    AngularSSR: '^18.0.0-next.0+sha-99104cd',
 };
