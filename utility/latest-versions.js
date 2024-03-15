9dfa36adc4575efddc8007253899eddd89f2147c
Angular Builds <circleci@angular.io>
2024-03-15 17:26:27 +0000

    cf17331 build: update ng-dev config to work with Node.js 18.19

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.3.0',
-    DevkitBuildAngular: '^17.3.0+sha-79c44ad',
-    AngularSSR: '^17.3.0+sha-79c44ad',
+    DevkitBuildAngular: '^17.3.0+sha-cf17331',
+    AngularSSR: '^17.3.0+sha-cf17331',
 };
