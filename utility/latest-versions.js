2bf21f1b19ba5b44a2c7d51fe9d3dd91fecec5b2
Angular Builds <circleci@angular.io>
2024-03-19 14:15:25 +0000

    2809971 fix(@angular-devkit/build-angular): only generate `server` directory when SSR is  enabled

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.3.0',
-    DevkitBuildAngular: '^17.3.0+sha-cf17331',
-    AngularSSR: '^17.3.0+sha-cf17331',
+    DevkitBuildAngular: '^17.3.0+sha-2809971',
+    AngularSSR: '^17.3.0+sha-2809971',
 };
