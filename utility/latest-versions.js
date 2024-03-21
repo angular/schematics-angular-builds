66e20a8472f3c91c30693ad0879bc1eaaff865fb
Angular Builds <circleci@angular.io>
2024-03-21 14:35:35 +0000

    c9d4360 fix(@angular-devkit/build-angular): `Internal server error: Invalid URL` when using a non localhost IP

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^17.3.0',
-    DevkitBuildAngular: '^17.3.1+sha-1a3257a',
-    AngularSSR: '^17.3.1+sha-1a3257a',
+    DevkitBuildAngular: '^17.3.1+sha-c9d4360',
+    AngularSSR: '^17.3.1+sha-c9d4360',
 };
