610e2d49793f47c4b46e69e8f5e7bfd30af216e6
Angular Builds <circleci@angular.io>
2024-03-22 17:27:45 +0000

    950a445 fix(@schematics/angular): rename SSR port env variable

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,6 +14,6 @@ exports.latestVersions = {
     ...require('./latest-versions/package.json')['dependencies'],
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^18.0.0-next.0',
-    DevkitBuildAngular: '^18.0.0-next.0+sha-1ab035a',
-    AngularSSR: '^18.0.0-next.0+sha-1ab035a',
+    DevkitBuildAngular: '^18.0.0-next.0+sha-950a445',
+    AngularSSR: '^18.0.0-next.0+sha-950a445',
 };
