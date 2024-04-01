a7217bb90652cfc76919c25d9c88a7e8d845e8f9
Angular Builds <circleci@angular.io>
2024-04-01 16:21:40 +0000

    f97c0f3 build: update dependency ajv-formats to v3

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0-next.1+sha-1573293',
-    AngularSSR: '^18.0.0-next.1+sha-1573293',
+    DevkitBuildAngular: '^18.0.0-next.1+sha-f97c0f3',
+    AngularSSR: '^18.0.0-next.1+sha-f97c0f3',
 };
