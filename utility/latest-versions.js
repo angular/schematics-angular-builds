a3cd50f55d3d7ea9050fa0bc9ccce9d08b5fef07
Angular Builds <circleci@angular.io>
2024-04-02 16:40:46 +0000

    97da8ba build: add missing dep in test to fix CI

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^17.3.2+sha-9bb6ea0',
-    AngularSSR: '^17.3.2+sha-9bb6ea0',
+    DevkitBuildAngular: '^17.3.2+sha-97da8ba',
+    AngularSSR: '^17.3.2+sha-97da8ba',
 };
