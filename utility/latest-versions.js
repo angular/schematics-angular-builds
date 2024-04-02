5791d91ff8e0b600383a5eeae23d28ccc58f56a2
Angular Builds <circleci@angular.io>
2024-04-02 17:12:31 +0000

    16d8c55 Revert "build: update @bazel/buildifier to v7.1.0"

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0-next.1+sha-9f74174',
-    AngularSSR: '^18.0.0-next.1+sha-9f74174',
+    DevkitBuildAngular: '^18.0.0-next.1+sha-16d8c55',
+    AngularSSR: '^18.0.0-next.1+sha-16d8c55',
 };
