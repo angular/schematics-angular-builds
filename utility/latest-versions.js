86289f7964ecfa6840365e0d9fc9d73e3f674053
Angular Builds <circleci@angular.io>
2024-04-08 14:16:56 +0000

    6bd4f5f build: remove `eslintignore` file

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0-next.2+sha-afa76bb',
-    AngularSSR: '^18.0.0-next.2+sha-afa76bb',
+    DevkitBuildAngular: '^18.0.0-next.2+sha-6bd4f5f',
+    AngularSSR: '^18.0.0-next.2+sha-6bd4f5f',
 };
