807c1abe4a62c814c868a5ae32299470b0b29775
Angular Builds <circleci@angular.io>
2024-04-08 14:01:32 +0000

    1128bdd fix(@angular-devkit/build-angular): ensure esbuild-based builders exclusively produce ESM output

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^17.3.3+sha-1f47a10',
-    AngularSSR: '^17.3.3+sha-1f47a10',
+    DevkitBuildAngular: '^17.3.3+sha-1128bdd',
+    AngularSSR: '^17.3.3+sha-1128bdd',
 };
