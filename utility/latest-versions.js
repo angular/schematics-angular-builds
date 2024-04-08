d07d23b67b6325d686978e41b4e1653635cefcb5
Angular Builds <circleci@angular.io>
2024-04-08 15:06:19 +0000

    03eee05 refactor(@angular/cli): remove `ng doc` command

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0-next.2+sha-6bd4f5f',
-    AngularSSR: '^18.0.0-next.2+sha-6bd4f5f',
+    DevkitBuildAngular: '^18.0.0-next.2+sha-03eee05',
+    AngularSSR: '^18.0.0-next.2+sha-03eee05',
 };
