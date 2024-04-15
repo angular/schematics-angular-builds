a4aa0c845ca9bc2cfc09bf09d18897a781d59765
Angular Builds <circleci@angular.io>
2024-04-15 13:54:05 +0000

    4d27c38 refactor(@angular-devkit/build-angular): reduce Webpack specific overlap in bundle budget calculator

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0-next.2+sha-7549f1b',
-    AngularSSR: '^18.0.0-next.2+sha-7549f1b',
+    DevkitBuildAngular: '^18.0.0-next.2+sha-4d27c38',
+    AngularSSR: '^18.0.0-next.2+sha-4d27c38',
 };
