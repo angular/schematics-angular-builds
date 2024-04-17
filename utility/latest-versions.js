5c1ea7e83dddb6dc7f5e62144fea794429c4fce8
Angular Builds <circleci@angular.io>
2024-04-17 17:34:03 +0000

    d51cb59 feat(@angular-devkit/build-angular): inject event-dispatch in SSR HTML page

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0-next.2+sha-adf1197',
-    AngularSSR: '^18.0.0-next.2+sha-adf1197',
+    DevkitBuildAngular: '^18.0.0-next.2+sha-d51cb59',
+    AngularSSR: '^18.0.0-next.2+sha-d51cb59',
 };
