4b9be5000bc9310bddf6d603a37b5f30da7775c7
Angular Builds <circleci@angular.io>
2024-04-17 08:28:02 +0000

    6313036 test: move builder testing infrastructure to separate bazel target module

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0-next.2+sha-63dab46',
-    AngularSSR: '^18.0.0-next.2+sha-63dab46',
+    DevkitBuildAngular: '^18.0.0-next.2+sha-6313036',
+    AngularSSR: '^18.0.0-next.2+sha-6313036',
 };
