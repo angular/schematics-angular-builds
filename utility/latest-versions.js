52db102fe5f23f0b7ca896ec071857b6fe8b675b
Angular Builds <circleci@angular.io>
2024-04-22 13:11:50 +0000

    c2a8bf1 refactor(@angular-devkit/build-angular): move Vite dev-server @angular/build imports into separate file

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0-next.3+sha-eb8f1fa',
-    AngularSSR: '^18.0.0-next.3+sha-eb8f1fa',
+    DevkitBuildAngular: '^18.0.0-next.3+sha-c2a8bf1',
+    AngularSSR: '^18.0.0-next.3+sha-c2a8bf1',
 };
