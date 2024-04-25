428b67978da952ef6b042a7e8b264a774f2f7ef0
Angular Builds <circleci@angular.io>
2024-04-25 12:08:56 +0000

    4087728 feat(@angular/cli): support for Node.js v22

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0-next.3+sha-ffb5428',
-    AngularSSR: '^18.0.0-next.3+sha-ffb5428',
+    DevkitBuildAngular: '^18.0.0-next.3+sha-4087728',
+    AngularSSR: '^18.0.0-next.3+sha-4087728',
 };
