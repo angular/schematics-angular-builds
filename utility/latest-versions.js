ce10fe9c62248bebaa547e43f03a7ea0358567ac
Angular Builds <circleci@angular.io>
2024-04-25 17:08:36 +0000

    6f1906f docs: add explanation of how to set up a new NPM package in Wombat

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^17.3.5+sha-dcec597',
-    AngularSSR: '^17.3.5+sha-dcec597',
+    DevkitBuildAngular: '^17.3.5+sha-6f1906f',
+    AngularSSR: '^17.3.5+sha-6f1906f',
 };
