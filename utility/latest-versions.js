030e025acbbd0713099e9aa33c457b4cba88ae63
Angular Builds <circleci@angular.io>
2024-04-26 11:18:43 +0000

    921fa7c fix(@angular/build): add missing `ansi-colors` and `picomatch` dependencies

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0-next.4+sha-791cf75',
-    AngularSSR: '^18.0.0-next.4+sha-791cf75',
+    DevkitBuildAngular: '^18.0.0-next.4+sha-921fa7c',
+    AngularSSR: '^18.0.0-next.4+sha-921fa7c',
 };
