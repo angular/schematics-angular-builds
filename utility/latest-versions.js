5a9103255006842cd6cf060c816b50c7e6642aa0
Angular Builds <circleci@angular.io>
2024-04-26 17:54:24 +0000

    60752fd fix(@angular/build): disable Vite prebundling when script optimizations are enabled

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0-next.4+sha-580442f',
-    AngularSSR: '^18.0.0-next.4+sha-580442f',
+    DevkitBuildAngular: '^18.0.0-next.4+sha-60752fd',
+    AngularSSR: '^18.0.0-next.4+sha-60752fd',
 };
