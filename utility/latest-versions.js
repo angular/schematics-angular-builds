f18774e9146ed9646964f65f05d37da41fe45412
Angular Builds <circleci@angular.io>
2024-04-29 10:59:30 +0000

    b329103 build: update scorecard action dependencies

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0-next.5+sha-940e382',
-    AngularSSR: '^18.0.0-next.5+sha-940e382',
+    DevkitBuildAngular: '^18.0.0-next.5+sha-b329103',
+    AngularSSR: '^18.0.0-next.5+sha-b329103',
 };
