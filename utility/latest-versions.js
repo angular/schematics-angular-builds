996bb970b355aa8c4841ba1812c8ed67f35fc498
Angular Builds <circleci@angular.io>
2024-05-01 19:37:57 +0000

    c119996 build: replace `@angular/build` version with archive in local builds

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0-rc.0+sha-3d52648',
-    AngularSSR: '^18.0.0-rc.0+sha-3d52648',
+    DevkitBuildAngular: '^18.0.0-rc.0+sha-c119996',
+    AngularSSR: '^18.0.0-rc.0+sha-c119996',
 };
