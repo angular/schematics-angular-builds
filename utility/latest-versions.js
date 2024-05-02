694aa502be9324881a029e4d422106aaf96c224a
Angular Builds <circleci@angular.io>
2024-05-02 17:27:55 +0000

    b01325a release: cut the v18.0.0-rc.1 release

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0-rc.0+sha-2085365',
-    AngularSSR: '^18.0.0-rc.0+sha-2085365',
+    DevkitBuildAngular: '^18.0.0-rc.1+sha-b01325a',
+    AngularSSR: '^18.0.0-rc.1+sha-b01325a',
 };
