5917aeda9ede53458e528366f96afc7daaa81df8
Angular Builds <circleci@angular.io>
2024-05-03 13:25:44 +0000

    5f14787 fix(@angular/build): decode URL pathname decoding during SSG fetch

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.0-next.0+sha-a648a7c',
-    AngularSSR: '^18.1.0-next.0+sha-a648a7c',
+    DevkitBuildAngular: '^18.1.0-next.0+sha-5f14787',
+    AngularSSR: '^18.1.0-next.0+sha-5f14787',
 };
