cfae5152c3f3732c8fa1ac782a569454ddacbec4
Angular Builds <circleci@angular.io>
2024-05-06 08:34:42 +0000

    cd43c1f refactor(@schematics/angular): link for ignoring files

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^17.3.6+sha-998c720',
-    AngularSSR: '^17.3.6+sha-998c720',
+    DevkitBuildAngular: '^17.3.6+sha-cd43c1f',
+    AngularSSR: '^17.3.6+sha-cd43c1f',
 };
