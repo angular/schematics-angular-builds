0e49ae3221c466d73116e66e2781ce45a59b175c
Angular Builds <circleci@angular.io>
2024-05-08 17:19:48 +0000

    8cff5e7 fix(@angular/build): ensure recreated files are watched

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.0-next.0+sha-f724866',
-    AngularSSR: '^18.1.0-next.0+sha-f724866',
+    DevkitBuildAngular: '^18.1.0-next.0+sha-8cff5e7',
+    AngularSSR: '^18.1.0-next.0+sha-8cff5e7',
 };
