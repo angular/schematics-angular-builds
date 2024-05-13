76a0ea1a084e62c769d06a0b3116a8c7d9b4ff63
Angular Builds <circleci@angular.io>
2024-05-13 12:05:26 +0000

    0ead36d fix(@angular/cli): clarify optional migration instructions during ng update

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.0-next.0+sha-46e5993',
-    AngularSSR: '^18.1.0-next.0+sha-46e5993',
+    DevkitBuildAngular: '^18.1.0-next.0+sha-0ead36d',
+    AngularSSR: '^18.1.0-next.0+sha-0ead36d',
 };
