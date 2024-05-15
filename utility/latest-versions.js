34a05c464544ee0a86f0fcd6a6db11999d8a721c
Angular Builds <circleci@angular.io>
2024-05-15 17:46:45 +0000

    d087b5f refactor(@angular-devkit/schematics): reduce direct rxjs usage in filter host tree

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.0-next.0+sha-7b52b98',
-    AngularSSR: '^18.1.0-next.0+sha-7b52b98',
+    DevkitBuildAngular: '^18.1.0-next.0+sha-d087b5f',
+    AngularSSR: '^18.1.0-next.0+sha-d087b5f',
 };
