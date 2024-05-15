b634f608066a66418f1ac2bd8d448aa6f5d49270
Angular Builds <circleci@angular.io>
2024-05-15 03:01:45 +0000

    132f5e6 build: update angular

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.0-next.0+sha-d92b43d',
-    AngularSSR: '^18.1.0-next.0+sha-d92b43d',
+    DevkitBuildAngular: '^18.1.0-next.0+sha-132f5e6',
+    AngularSSR: '^18.1.0-next.0+sha-132f5e6',
 };
