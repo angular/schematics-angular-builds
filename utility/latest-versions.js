b1b47b2f355a033dc3820e33d034cce569f775f9
Angular Builds <circleci@angular.io>
2024-05-15 15:22:48 +0000

    7b52b98 fix(@angular-devkit/schematics): `SchematicTestRunner.runExternalSchematic` fails with "The encoded data was not valid for encoding utf-8"

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.0-next.0+sha-bc47fa1',
-    AngularSSR: '^18.1.0-next.0+sha-bc47fa1',
+    DevkitBuildAngular: '^18.1.0-next.0+sha-7b52b98',
+    AngularSSR: '^18.1.0-next.0+sha-7b52b98',
 };
