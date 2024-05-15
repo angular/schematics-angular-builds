97b5c01241e68239b5d64051ec275bc5ce21a7da
Angular Builds <circleci@angular.io>
2024-05-15 11:18:54 +0000

    a15eb7d perf(@angular/build): improve rebuild time for file loader usage with prebundling

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0-rc.2+sha-aba7c5c',
-    AngularSSR: '^18.0.0-rc.2+sha-aba7c5c',
+    DevkitBuildAngular: '^18.0.0-rc.2+sha-a15eb7d',
+    AngularSSR: '^18.0.0-rc.2+sha-a15eb7d',
 };
