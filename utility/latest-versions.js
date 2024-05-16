8147e4f0a0b967c5654f03c6d27ef3334d565016
Angular Builds <circleci@angular.io>
2024-05-16 23:25:25 +0000

    2265f63 refactor(@angular/build): cleanup persistent JS transformer cache on plugin dispose

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0-rc.2+sha-dc20948',
-    AngularSSR: '^18.0.0-rc.2+sha-dc20948',
+    DevkitBuildAngular: '^18.0.0-rc.2+sha-2265f63',
+    AngularSSR: '^18.0.0-rc.2+sha-2265f63',
 };
