1151555687e8d470d224f0b1c8d11e5b59dc6b70
Angular Builds <circleci@angular.io>
2024-05-17 15:14:14 +0000

    e7ef048 build: update actions/checkout action to v4.1.6

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0-rc.2+sha-aec632b',
-    AngularSSR: '^18.0.0-rc.2+sha-aec632b',
+    DevkitBuildAngular: '^18.0.0-rc.2+sha-e7ef048',
+    AngularSSR: '^18.0.0-rc.2+sha-e7ef048',
 };
