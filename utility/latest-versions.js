cbfd0b4163af58ae3f0e9b2421b801ca59b65ceb
Angular Builds <circleci@angular.io>
2024-05-22 21:58:52 +0000

    706423a fix(@angular/build): only import persistent cache store with active caching

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0+sha-2dbe21f',
-    AngularSSR: '^18.0.0+sha-2dbe21f',
+    DevkitBuildAngular: '^18.0.0+sha-706423a',
+    AngularSSR: '^18.0.0+sha-706423a',
 };
