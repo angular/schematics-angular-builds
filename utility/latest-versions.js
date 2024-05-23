dece340d5c1909bd0f8a85410263124bffcaa856
Angular Builds <circleci@angular.io>
2024-05-23 14:03:54 +0000

    6b6a76a fix(@angular/build): disable persistent disk caching inside webcontainers by default

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.0+sha-01842f5',
-    AngularSSR: '^18.0.0+sha-01842f5',
+    DevkitBuildAngular: '^18.0.0+sha-6b6a76a',
+    AngularSSR: '^18.0.0+sha-6b6a76a',
 };
