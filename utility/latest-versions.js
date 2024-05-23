1a4cfb68d96a50ccea2e859074dc115934cae7b1
Angular Builds <circleci@angular.io>
2024-05-23 11:51:58 +0000

    a82a741 fix(@angular/build): avoid rebasing URLs with function calls

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.0-next.0+sha-c7205ea',
-    AngularSSR: '^18.1.0-next.0+sha-c7205ea',
+    DevkitBuildAngular: '^18.1.0-next.0+sha-a82a741',
+    AngularSSR: '^18.1.0-next.0+sha-a82a741',
 };
