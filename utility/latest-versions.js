2f189c2a2cb338b60aa2d86232d0ad6e0443b293
Angular Builds <circleci@angular.io>
2024-05-28 09:09:48 +0000

    acbffd2 fix(@angular/pwa): set manifest `icons` location to match `assets` builder option

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.1+sha-de8d703',
-    AngularSSR: '^18.0.1+sha-de8d703',
+    DevkitBuildAngular: '^18.0.1+sha-acbffd2',
+    AngularSSR: '^18.0.1+sha-acbffd2',
 };
