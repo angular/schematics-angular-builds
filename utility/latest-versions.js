569e247a0784e1fe0584345e48f8784d49bd149b
Angular Builds <circleci@angular.io>
2024-05-28 11:07:51 +0000

    c4cf359 fix(@angular/build): print Sass `@warn` location

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.1+sha-acbffd2',
-    AngularSSR: '^18.0.1+sha-acbffd2',
+    DevkitBuildAngular: '^18.0.1+sha-c4cf359',
+    AngularSSR: '^18.0.1+sha-c4cf359',
 };
