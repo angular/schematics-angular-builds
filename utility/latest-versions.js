7db6573f05c0d3bd4f5f3c72009b0066558cfa84
Angular Builds <circleci@angular.io>
2024-05-30 11:15:49 +0000

    25624af test: remove E2E tests that are covered by integration tests

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.2+sha-43a2a7d',
-    AngularSSR: '^18.0.2+sha-43a2a7d',
+    DevkitBuildAngular: '^18.0.2+sha-25624af',
+    AngularSSR: '^18.0.2+sha-25624af',
 };
