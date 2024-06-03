01ceebce91170d5ed9b646efccac195f72fb824d
Angular Builds <circleci@angular.io>
2024-06-03 17:48:51 +0000

    6526a5f fix(@angular/build): watch all related files during a Sass error

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.2+sha-9acb5c7',
-    AngularSSR: '^18.0.2+sha-9acb5c7',
+    DevkitBuildAngular: '^18.0.2+sha-6526a5f',
+    AngularSSR: '^18.0.2+sha-6526a5f',
 };
