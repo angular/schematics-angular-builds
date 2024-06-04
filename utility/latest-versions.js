fdaf808097df041d158bc195590bdb4a3252b6bf
Angular Builds <circleci@angular.io>
2024-06-04 18:25:34 +0000

    b709d2a fix(@angular/cli): add `schema.json` options to parsed command, also when a version is passed to `ng add <package>@<version>`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.2+sha-6526a5f',
-    AngularSSR: '^18.0.2+sha-6526a5f',
+    DevkitBuildAngular: '^18.0.2+sha-b709d2a',
+    AngularSSR: '^18.0.2+sha-b709d2a',
 };
