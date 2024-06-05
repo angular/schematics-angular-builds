ff814fe710fdfa4759b212f5f953fd829d6aa5e1
Angular Builds <circleci@angular.io>
2024-06-05 14:10:49 +0000

    cd1c181 ci: update dependency-review-config - add `CC0-1.0` as valid licenses - excludes `thingies@1.21.0` which is `MIT and Unlicense`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.0-next.0+sha-c92bc77',
-    AngularSSR: '^18.1.0-next.0+sha-c92bc77',
+    DevkitBuildAngular: '^18.1.0-next.0+sha-cd1c181',
+    AngularSSR: '^18.1.0-next.0+sha-cd1c181',
 };
