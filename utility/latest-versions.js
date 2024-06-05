23317d0253e9f7a2ec8cbb330ff9b40dd1be3985
Angular Builds <circleci@angular.io>
2024-06-05 16:38:17 +0000

    97eff83 refactor(@schematics/angular): remove unnecessary whitespace in spec template

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.2+sha-b709d2a',
-    AngularSSR: '^18.0.2+sha-b709d2a',
+    DevkitBuildAngular: '^18.0.2+sha-97eff83',
+    AngularSSR: '^18.0.2+sha-97eff83',
 };
