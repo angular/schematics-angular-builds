e21c6d6c62fee9ad0dff190110e90d84eb10c88c
Angular Builds <circleci@angular.dev>
2024-06-07 13:03:49 +0000

    9315968 build: remove unused inquirer dependency

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.0-next.1+sha-ee9d404',
-    AngularSSR: '^18.1.0-next.1+sha-ee9d404',
+    DevkitBuildAngular: '^18.1.0-next.1+sha-9315968',
+    AngularSSR: '^18.1.0-next.1+sha-9315968',
 };
