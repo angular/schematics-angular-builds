7333500c21403b606d27be5a1031555bef33b0a7
Angular Builds <circleci@angular.dev>
2024-06-08 18:00:29 +0000

    20fc6ca fix(@angular/build): generate module preloads next to script elements in index HTML

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.3+sha-78c6117',
-    AngularSSR: '^18.0.3+sha-78c6117',
+    DevkitBuildAngular: '^18.0.3+sha-20fc6ca',
+    AngularSSR: '^18.0.3+sha-20fc6ca',
 };
