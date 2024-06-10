1a7d687a1feaba9b93151ce566deffa64e2772ce
Angular Builds <circleci@angular.dev>
2024-06-10 19:43:23 +0000

    49ee886 ci: only download saucelabs connect utility when running tests

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.0-next.1+sha-f102f81',
-    AngularSSR: '^18.1.0-next.1+sha-f102f81',
+    DevkitBuildAngular: '^18.1.0-next.1+sha-49ee886',
+    AngularSSR: '^18.1.0-next.1+sha-49ee886',
 };
