caafba5afe7c0fe9b9093d46ef189cf3801d7a84
Angular Builds <circleci@angular.dev>
2024-06-11 10:56:16 +0000

    4b95aec refactor: replace custom promise-based setTimeout with native Node.js setTimeout

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.0-next.1+sha-076afb7',
-    AngularSSR: '^18.1.0-next.1+sha-076afb7',
+    DevkitBuildAngular: '^18.1.0-next.1+sha-4b95aec',
+    AngularSSR: '^18.1.0-next.1+sha-4b95aec',
 };
