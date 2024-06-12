f157a63550089f6c36b4eb0d92cb072698e93100
Angular Builds <circleci@angular.dev>
2024-06-12 15:41:16 +0000

    8d256b0 build: update E2E setup to use NPM 10 to publish packages

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.0-next.1+sha-34f2a12',
-    AngularSSR: '^18.1.0-next.1+sha-34f2a12',
+    DevkitBuildAngular: '^18.1.0-next.1+sha-8d256b0',
+    AngularSSR: '^18.1.0-next.1+sha-8d256b0',
 };
