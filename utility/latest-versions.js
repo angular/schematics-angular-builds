508df1f64a439e9351c300a66de071711e267e35
Angular Builds <circleci@angular.dev>
2024-06-13 20:04:50 +0000

    405c148 fix(@angular/build): automatically resolve `.mjs` files when using Vite

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.4+sha-df783bb',
-    AngularSSR: '^18.0.4+sha-df783bb',
+    DevkitBuildAngular: '^18.0.4+sha-405c148',
+    AngularSSR: '^18.0.4+sha-405c148',
 };
