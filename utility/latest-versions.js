f8e10dbc7c3ad3a630f17fac5c3ebaf7c0779920
Angular Builds <circleci@angular.dev>
2024-06-13 12:23:10 +0000

    791ef80 fix(@angular/build): do not reference sourcemaps in web workers and global stylesheet bundles when hidden setting is enabled

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.3+sha-9320b65',
-    AngularSSR: '^18.0.3+sha-9320b65',
+    DevkitBuildAngular: '^18.0.3+sha-791ef80',
+    AngularSSR: '^18.0.3+sha-791ef80',
 };
