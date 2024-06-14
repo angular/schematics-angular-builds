4090fd97492cb3439e0f40bb2406da8c4c3b1dea
Angular Builds <circleci@angular.dev>
2024-06-14 17:57:24 +0000

    f989fa3 build: update github/codeql-action action to v3.25.10

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.0-next.2+sha-1039a64',
-    AngularSSR: '^18.1.0-next.2+sha-1039a64',
+    DevkitBuildAngular: '^18.1.0-next.2+sha-f989fa3',
+    AngularSSR: '^18.1.0-next.2+sha-f989fa3',
 };
