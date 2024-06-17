86f954b07d5b2ad1db935d899cf997c8c1296159
Angular Builds <circleci@angular.dev>
2024-06-17 10:44:09 +0000

    5c705e8 fix(@schematics/angular): update schematics to use RouterModule when --routing flag is present

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.4+sha-405c148',
-    AngularSSR: '^18.0.4+sha-405c148',
+    DevkitBuildAngular: '^18.0.4+sha-5c705e8',
+    AngularSSR: '^18.0.4+sha-5c705e8',
 };
