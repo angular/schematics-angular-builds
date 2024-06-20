43aa7c4b5e76c4de40f23e911eb5b7af21d503db
Angular Builds <circleci@angular.dev>
2024-06-20 12:53:45 +0000

    094efca docs: release notes for the v18.0.5 release

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.0-next.2+sha-9c647a7',
-    AngularSSR: '^18.1.0-next.2+sha-9c647a7',
+    DevkitBuildAngular: '^18.1.0-next.2+sha-094efca',
+    AngularSSR: '^18.1.0-next.2+sha-094efca',
 };
