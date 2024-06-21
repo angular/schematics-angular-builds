b66dd873a42cb73da0e0c0682b95862d9708b6e7
Angular Builds <circleci@angular.dev>
2024-06-21 15:49:42 +0000

    98a8a8a fix(@angular/build): show JavaScript cache store initialization warning

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.0.5+sha-00d4db8',
-    AngularSSR: '^18.0.5+sha-00d4db8',
+    DevkitBuildAngular: '^18.0.5+sha-98a8a8a',
+    AngularSSR: '^18.0.5+sha-98a8a8a',
 };
