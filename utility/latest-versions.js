76adcdbf40e2f75e084b8570dccf341a1396b6f9
Angular Builds <circleci@angular.dev>
2024-06-21 14:47:07 +0000

    1a481c5 refactor(@angular/build): support directly merging bundler results

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.0-next.3+sha-db1a6ae',
-    AngularSSR: '^18.1.0-next.3+sha-db1a6ae',
+    DevkitBuildAngular: '^18.1.0-next.3+sha-1a481c5',
+    AngularSSR: '^18.1.0-next.3+sha-1a481c5',
 };
