94e65d44249cba4d835af219c7f93e10bd200f65
Angular Builds <circleci@angular.dev>
2024-06-24 14:18:56 +0000

    3e359da fix(@angular/build): address rxjs undefined issues during SSR prebundling

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.0-next.3+sha-fd65792',
-    AngularSSR: '^18.1.0-next.3+sha-fd65792',
+    DevkitBuildAngular: '^18.1.0-next.3+sha-3e359da',
+    AngularSSR: '^18.1.0-next.3+sha-3e359da',
 };
