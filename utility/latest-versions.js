826cbc132f70eeefcc61c899d16ffe136c9d3f41
Angular Builds <circleci@angular.dev>
2024-06-28 13:42:49 +0000

    27bd670 fix(@angular/build): redirect to path with trailing slash for asset directories

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.0-rc.0+sha-8d314a1',
-    AngularSSR: '^18.1.0-rc.0+sha-8d314a1',
+    DevkitBuildAngular: '^18.1.0-rc.0+sha-27bd670',
+    AngularSSR: '^18.1.0-rc.0+sha-27bd670',
 };
