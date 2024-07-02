47601b4cb33565be042f4ede41c361ea5a6fe639
Angular Builds <circleci@angular.dev>
2024-07-02 14:17:48 +0000

    d449c9d build: update @discovery/json-ext to v0.6.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.0-next.0+sha-394e761',
-    AngularSSR: '^18.2.0-next.0+sha-394e761',
+    DevkitBuildAngular: '^18.2.0-next.0+sha-d449c9d',
+    AngularSSR: '^18.2.0-next.0+sha-d449c9d',
 };
