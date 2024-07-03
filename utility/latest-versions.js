de217816f11022304563c35034c202fbcbc013b9
Angular Builds <circleci@angular.dev>
2024-07-03 17:36:35 +0000

    aa88e68 fix(@angular/build): check inlineSourceMap option with isolated modules optimization

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.0-next.0+sha-3a7537b',
-    AngularSSR: '^18.2.0-next.0+sha-3a7537b',
+    DevkitBuildAngular: '^18.2.0-next.0+sha-aa88e68',
+    AngularSSR: '^18.2.0-next.0+sha-aa88e68',
 };
