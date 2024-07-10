4bd6050061fbd6abe7580abfaba8ea5c2e97a165
Angular Builds <circleci@angular.dev>
2024-07-10 07:12:53 +0000

    f80c3e5 build: update dependency @types/picomatch to v3

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.0-next.0+sha-10f882c',
-    AngularSSR: '^18.2.0-next.0+sha-10f882c',
+    DevkitBuildAngular: '^18.2.0-next.0+sha-f80c3e5',
+    AngularSSR: '^18.2.0-next.0+sha-f80c3e5',
 };
