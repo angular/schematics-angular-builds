68145cd90270ff5ae25bb691bdea9885365ca16f
Angular Builds <circleci@angular.dev>
2024-07-11 11:28:46 +0000

    ba13d03 build: update dependency terser to v5.31.2

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.0-next.0+sha-9b8baa6',
-    AngularSSR: '^18.2.0-next.0+sha-9b8baa6',
+    DevkitBuildAngular: '^18.2.0-next.0+sha-ba13d03',
+    AngularSSR: '^18.2.0-next.0+sha-ba13d03',
 };
