587eab2bb7d8e912c9a3cecb6e572852c05039a1
Angular Builds <circleci@angular.dev>
2024-07-16 13:51:26 +0000

    f5c250a refactor: rename internal vite plugin name.

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.0-next.0+sha-b168948',
-    AngularSSR: '^18.2.0-next.0+sha-b168948',
+    DevkitBuildAngular: '^18.2.0-next.0+sha-f5c250a',
+    AngularSSR: '^18.2.0-next.0+sha-f5c250a',
 };
