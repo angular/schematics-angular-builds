f251f8cea329ff0af636735bd1fe480a1547eeec
Angular Builds <circleci@angular.dev>
2024-07-18 13:56:05 +0000

    d868270 fix(@angular/build): prevent redirection loop

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.1.1+sha-2601941',
-    AngularSSR: '^18.1.1+sha-2601941',
+    DevkitBuildAngular: '^18.1.1+sha-d868270',
+    AngularSSR: '^18.1.1+sha-d868270',
 };
