c6c34af8b5ed55fe61361d9bc0e8a16042adb228
Angular Builds <circleci@angular.dev>
2024-07-19 16:57:29 +0000

    8ff687d fix(@angular/build): correctly detect comma in Sass URL lexer

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.0-next.1+sha-421b6e7',
-    AngularSSR: '^18.2.0-next.1+sha-421b6e7',
+    DevkitBuildAngular: '^18.2.0-next.1+sha-8ff687d',
+    AngularSSR: '^18.2.0-next.1+sha-8ff687d',
 };
