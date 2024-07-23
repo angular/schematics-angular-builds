8750cb368f1ef15f296f3c922388f024c5d18cf9
Angular Builds <circleci@angular.dev>
2024-07-23 17:22:34 +0000

    1266473 build: update dependency typescript to v5.5.4

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.0-next.1+sha-8470836',
-    AngularSSR: '^18.2.0-next.1+sha-8470836',
+    DevkitBuildAngular: '^18.2.0-next.1+sha-1266473',
+    AngularSSR: '^18.2.0-next.1+sha-1266473',
 };
