44e7e79589b4dddeaa2b7f35965144f8410965da
Angular Builds <circleci@angular.dev>
2024-07-24 16:11:01 +0000

    a131c9b build: update dependency sass-loader to v15

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.0-next.1+sha-048df34',
-    AngularSSR: '^18.2.0-next.1+sha-048df34',
+    DevkitBuildAngular: '^18.2.0-next.1+sha-a131c9b',
+    AngularSSR: '^18.2.0-next.1+sha-a131c9b',
 };
