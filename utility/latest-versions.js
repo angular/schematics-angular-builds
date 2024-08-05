d02081ab31d334c6e4b5c9363ec8ea374cccf45e
Angular Builds <circleci@angular.dev>
2024-08-05 17:51:13 +0000

    bb802d1 refactor(@angular-devkit/build-angular): only write test related files in Jest builder

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.0-next.3+sha-d257388',
-    AngularSSR: '^18.2.0-next.3+sha-d257388',
+    DevkitBuildAngular: '^18.2.0-next.3+sha-bb802d1',
+    AngularSSR: '^18.2.0-next.3+sha-bb802d1',
 };
