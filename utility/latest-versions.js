2696a60a29a166ccb93cfc1cfe8e18de2860802d
Angular Builds <circleci@angular.dev>
2024-08-05 19:24:51 +0000

    182ecbd fix(@angular/build): allow explicitly disabling TypeScript incremental mode

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.0-next.3+sha-da2a6bd',
-    AngularSSR: '^18.2.0-next.3+sha-da2a6bd',
+    DevkitBuildAngular: '^18.2.0-next.3+sha-182ecbd',
+    AngularSSR: '^18.2.0-next.3+sha-182ecbd',
 };
