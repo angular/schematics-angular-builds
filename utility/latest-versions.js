41bda3c41e954bdfcf95f4999c6432b8bae9e1a5
Angular Builds <circleci@angular.dev>
2024-08-09 12:21:04 +0000

    ce11a39 build: remove local patch for `@angular/build-tooling`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.3.0-next.0+sha-3c9697a',
-    AngularSSR: '^18.3.0-next.0+sha-3c9697a',
+    DevkitBuildAngular: '^18.3.0-next.0+sha-ce11a39',
+    AngularSSR: '^18.3.0-next.0+sha-ce11a39',
 };
