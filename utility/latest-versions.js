7151d7075414954cec029648d3d0e59d7e86829e
Angular Builds <circleci@angular.dev>
2024-08-13 11:58:37 +0000

    7af63b4 refactor(@angular-devkit/build-angular): reduce custom code in browser-esbuild implementation

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.3.0-next.0+sha-8d650d3',
-    AngularSSR: '^18.3.0-next.0+sha-8d650d3',
+    DevkitBuildAngular: '^18.3.0-next.0+sha-7af63b4',
+    AngularSSR: '^18.3.0-next.0+sha-7af63b4',
 };
