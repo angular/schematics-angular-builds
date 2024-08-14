5ddb918c0cf0820f87fd81d24ed20278d893b0a1
Angular Builds <circleci@angular.dev>
2024-08-14 20:13:11 +0000

    94e27c8 fix(@angular-devkit/schematics-cli): prevent bypassing select/checkbox prompts on validation failure

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.0+sha-ff5a0fe',
-    AngularSSR: '^18.2.0+sha-ff5a0fe',
+    DevkitBuildAngular: '^18.2.0+sha-94e27c8',
+    AngularSSR: '^18.2.0+sha-94e27c8',
 };
