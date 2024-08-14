5bd99201d1d1eb1086f7361504e00232a92e1695
Angular Builds <circleci@angular.dev>
2024-08-14 17:17:05 +0000

    474571f build: remove Bazel typings workaround

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.3.0-next.0+sha-91b561d',
-    AngularSSR: '^18.3.0-next.0+sha-91b561d',
+    DevkitBuildAngular: '^18.3.0-next.0+sha-474571f',
+    AngularSSR: '^18.3.0-next.0+sha-474571f',
 };
