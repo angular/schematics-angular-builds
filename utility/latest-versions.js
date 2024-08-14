fc53f761811ed59a27b3e6073a723c324f6394e8
Angular Builds <circleci@angular.dev>
2024-08-14 14:47:36 +0000

    4528be1 build: add missing `pkg_deps` for `@angular/create`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.0-rc.0+sha-d14c9ab',
-    AngularSSR: '^18.2.0-rc.0+sha-d14c9ab',
+    DevkitBuildAngular: '^18.2.0-rc.0+sha-4528be1',
+    AngularSSR: '^18.2.0-rc.0+sha-4528be1',
 };
