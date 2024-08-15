68b9f0fc7036e92305fe785d290cf7ac296f80e5
Angular Builds <circleci@angular.dev>
2024-08-15 13:57:14 +0000

    71c06c6 fix(@angular/build): improve error message when an unhandled exception occurs during prerendering

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.0+sha-e043c96',
-    AngularSSR: '^19.0.0-next.0+sha-e043c96',
+    DevkitBuildAngular: '^19.0.0-next.0+sha-71c06c6',
+    AngularSSR: '^19.0.0-next.0+sha-71c06c6',
 };
