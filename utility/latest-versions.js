e2f435c6e034163f0bd929762ab367ca3203ba50
Angular Builds <circleci@angular.dev>
2024-08-20 22:01:52 +0000

    7992218 fix(@schematics/angular): remove `declaration` and `sourceMap` from default tsconfig

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.0+sha-e72eb05',
-    AngularSSR: '^19.0.0-next.0+sha-e72eb05',
+    DevkitBuildAngular: '^19.0.0-next.0+sha-7992218',
+    AngularSSR: '^19.0.0-next.0+sha-7992218',
 };
