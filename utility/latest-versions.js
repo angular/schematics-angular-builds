766cf576566ec6c2b664bdb2963e51a32da1b891
Angular Builds <circleci@angular.dev>
2024-08-20 12:15:48 +0000

    a655b92 build: downgrade sass version

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.0+sha-83b2699',
-    AngularSSR: '^18.2.0+sha-83b2699',
+    DevkitBuildAngular: '^18.2.0+sha-a655b92',
+    AngularSSR: '^18.2.0+sha-a655b92',
 };
