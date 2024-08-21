2b2fd2a75f364f21975980ef60719e252054c37f
Angular Builds <circleci@angular.dev>
2024-08-21 12:30:29 +0000

    0d8a100 refactor(@angular-devkit/core): remove deprecated `fileBuffer` function in favor of `stringToFileBuffer`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.0+sha-90b94e0',
-    AngularSSR: '^19.0.0-next.0+sha-90b94e0',
+    DevkitBuildAngular: '^19.0.0-next.0+sha-0d8a100',
+    AngularSSR: '^19.0.0-next.0+sha-0d8a100',
 };
