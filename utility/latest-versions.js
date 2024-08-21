e863af19f4b0bece5f228ff984e799162c286b94
Angular Builds <circleci@angular.dev>
2024-08-21 13:46:35 +0000

    b2d1477 build: mark server tests as flaky

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.1+sha-15f3d5a',
-    AngularSSR: '^18.2.1+sha-15f3d5a',
+    DevkitBuildAngular: '^18.2.1+sha-b2d1477',
+    AngularSSR: '^18.2.1+sha-b2d1477',
 };
