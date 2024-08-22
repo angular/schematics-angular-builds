98e3b4b1b36fab98c7860f60fedfdb1a11eaaabd
Angular Builds <circleci@angular.dev>
2024-08-22 18:49:44 +0000

    4aa95bd build: clean up redundant `exclude` field from `tsconfig-build.json`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.1+sha-7ea85e9',
-    AngularSSR: '^19.0.0-next.1+sha-7ea85e9',
+    DevkitBuildAngular: '^19.0.0-next.1+sha-4aa95bd',
+    AngularSSR: '^19.0.0-next.1+sha-4aa95bd',
 };
