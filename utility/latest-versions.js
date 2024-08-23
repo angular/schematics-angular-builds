c66b96599fe7704c1a82c6fc02ce7ad9478e616f
Angular Builds <circleci@angular.dev>
2024-08-23 19:29:04 +0000

    e5b4c5c ci: run modules and packages tests using a single bazel invocation

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.1+sha-3ee53c9',
-    AngularSSR: '^18.2.1+sha-3ee53c9',
+    DevkitBuildAngular: '^18.2.1+sha-e5b4c5c',
+    AngularSSR: '^18.2.1+sha-e5b4c5c',
 };
