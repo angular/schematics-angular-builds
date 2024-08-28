2aae11020922d95976a67ba80e1e516283edd0a9
Angular Builds <circleci@angular.dev>
2024-08-28 21:38:34 +0000

    6df7578 release: cut the v19.0.0-next.2 release

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.1+sha-afe2891',
-    AngularSSR: '^19.0.0-next.1+sha-afe2891',
+    DevkitBuildAngular: '^19.0.0-next.2+sha-6df7578',
+    AngularSSR: '^19.0.0-next.2+sha-6df7578',
 };
