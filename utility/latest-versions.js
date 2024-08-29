1b0d4f0e5389963268cbc610d9b039a338d42f8e
Angular Builds <circleci@angular.dev>
2024-08-29 19:04:47 +0000

    3ee2163 fix(@angular-devkit/build-angular): clear context in Karma by default for single run executions

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.2+sha-7f570c5',
-    AngularSSR: '^19.0.0-next.2+sha-7f570c5',
+    DevkitBuildAngular: '^19.0.0-next.2+sha-3ee2163',
+    AngularSSR: '^19.0.0-next.2+sha-3ee2163',
 };
