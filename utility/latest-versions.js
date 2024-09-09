c66512b993a9081d6491454c9b7572c1b2f4ad5a
Angular Builds <circleci@angular.dev>
2024-09-09 17:40:44 +0000

    e12a66a docs(@angular/cli): add playwright to e2e targets

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.3+sha-328ed65',
-    AngularSSR: '^18.2.3+sha-328ed65',
+    DevkitBuildAngular: '^18.2.3+sha-e12a66a',
+    AngularSSR: '^18.2.3+sha-e12a66a',
 };
