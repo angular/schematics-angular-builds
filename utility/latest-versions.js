3fe85e62ee350db61e693aa36e92607eef894e93
Angular Builds <circleci@angular.dev>
2024-09-09 14:48:46 +0000

    eb97c43 build: update schematics dependencies to ~5.3.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.3+sha-d87822c',
-    AngularSSR: '^19.0.0-next.3+sha-d87822c',
+    DevkitBuildAngular: '^19.0.0-next.3+sha-eb97c43',
+    AngularSSR: '^19.0.0-next.3+sha-eb97c43',
 };
