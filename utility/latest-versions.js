2c298afc458b48ca48a5863839099ac04658ffc9
Angular Builds <circleci@angular.dev>
2024-09-11 13:33:54 +0000

    765309a fix(@angular/build): prevent transformation of Node.js internal dependencies by Vite

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.3+sha-e12a66a',
-    AngularSSR: '^18.2.3+sha-e12a66a',
+    DevkitBuildAngular: '^18.2.3+sha-765309a',
+    AngularSSR: '^18.2.3+sha-765309a',
 };
