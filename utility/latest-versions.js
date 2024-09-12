7e030defe10ef397de30e6e1559a3efece26a934
Angular Builds <circleci@angular.dev>
2024-09-12 07:54:38 +0000

    78f7648 feat(@angular-devkit/architect): merge object options from CLI

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.4+sha-af6e200',
-    AngularSSR: '^19.0.0-next.4+sha-af6e200',
+    DevkitBuildAngular: '^19.0.0-next.4+sha-78f7648',
+    AngularSSR: '^19.0.0-next.4+sha-78f7648',
 };
