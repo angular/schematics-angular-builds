1f848aec96802f00729d4abe18f76a8d47d3715c
Angular Builds <circleci@angular.dev>
2024-09-18 13:36:02 +0000

    7074316 fix(@angular/build): support HTTP HEAD requests for virtual output files

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.4+sha-1032b3d',
-    AngularSSR: '^18.2.4+sha-1032b3d',
+    DevkitBuildAngular: '^18.2.4+sha-7074316',
+    AngularSSR: '^18.2.4+sha-7074316',
 };
