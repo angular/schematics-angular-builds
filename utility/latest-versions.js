eb26fc693c1d2986019518366a79c969e2085486
Angular Builds <circleci@angular.dev>
2024-09-18 10:50:55 +0000

    1032b3d fix(@angular/build): update vite to `5.4.6`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.4+sha-f29db1d',
-    AngularSSR: '^18.2.4+sha-f29db1d',
+    DevkitBuildAngular: '^18.2.4+sha-1032b3d',
+    AngularSSR: '^18.2.4+sha-1032b3d',
 };
