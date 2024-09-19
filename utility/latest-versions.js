846644904227ecd94c4d8550e2b16cbee4bf187b
Angular Builds <circleci@angular.dev>
2024-09-19 19:11:06 +0000

    8723e0c build: update yarn to v4.5.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.7+sha-fedeeb0',
-    AngularSSR: '^19.0.0-next.7+sha-fedeeb0',
+    DevkitBuildAngular: '^19.0.0-next.7+sha-8723e0c',
+    AngularSSR: '^19.0.0-next.7+sha-8723e0c',
 };
