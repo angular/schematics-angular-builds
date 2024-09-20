5971a78d89aec95020b496790ee886b4a4b6dcba
Angular Builds <circleci@angular.io>
2024-09-20 17:59:39 +0000

    30489d8 fix(@angular-devkit/build-angular): update vite to 4.1.8

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^17.3.9+sha-e56adb0',
-    AngularSSR: '^17.3.9+sha-e56adb0',
+    DevkitBuildAngular: '^17.3.9+sha-30489d8',
+    AngularSSR: '^17.3.9+sha-30489d8',
 };
