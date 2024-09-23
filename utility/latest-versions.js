3e76b5fd1a78b9d66a7c05c59da03efb845bd0ad
Angular Builds <circleci@angular.dev>
2024-09-23 18:01:32 +0000

    f05b053 test: unlink symlink in ng add base E2E test

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.7+sha-d486c19',
-    AngularSSR: '^19.0.0-next.7+sha-d486c19',
+    DevkitBuildAngular: '^19.0.0-next.7+sha-f05b053',
+    AngularSSR: '^19.0.0-next.7+sha-f05b053',
 };
