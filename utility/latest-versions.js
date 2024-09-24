139757b300d38615c64703317fe8b37bcfd8614e
Angular Builds <circleci@angular.dev>
2024-09-24 19:24:49 +0000

    c710539 test: update test logic to fix windows CI

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.5+sha-6a52dcc',
-    AngularSSR: '^18.2.5+sha-6a52dcc',
+    DevkitBuildAngular: '^18.2.5+sha-c710539',
+    AngularSSR: '^18.2.5+sha-c710539',
 };
