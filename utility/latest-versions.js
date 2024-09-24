9996aff24de0cdd06482fa4b1935c59ac4b5d4aa
Angular Builds <circleci@angular.dev>
2024-09-24 21:02:17 +0000

    5382ed4 build: update dependency @rollup/plugin-commonjs to v28

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.7+sha-1548827',
-    AngularSSR: '^19.0.0-next.7+sha-1548827',
+    DevkitBuildAngular: '^19.0.0-next.7+sha-5382ed4',
+    AngularSSR: '^19.0.0-next.7+sha-5382ed4',
 };
