92b1facb0a7c5be7e5ff347ddf30f66c9c1a4e58
Angular Builds <circleci@angular.dev>
2024-09-25 16:21:08 +0000

    ca28ec7 test(@angular-devkit/build-angular): add application/browser test runs

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.6+sha-9758a48',
-    AngularSSR: '^18.2.6+sha-9758a48',
+    DevkitBuildAngular: '^18.2.6+sha-ca28ec7',
+    AngularSSR: '^18.2.6+sha-ca28ec7',
 };
