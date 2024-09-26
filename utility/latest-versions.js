5fab51024af301c4acfeeddc5d391fa8a65e8a84
Angular Builds <circleci@angular.dev>
2024-09-26 20:55:15 +0000

    64c5252 fix(@angular/ssr): show error when multiple routes are set with `RenderMode.AppShell`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.8+sha-8f038de',
-    AngularSSR: '^19.0.0-next.8+sha-8f038de',
+    DevkitBuildAngular: '^19.0.0-next.8+sha-64c5252',
+    AngularSSR: '^19.0.0-next.8+sha-64c5252',
 };
