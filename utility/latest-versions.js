c4653f4cf525d04216ca01432c45a8c179e9c203
Angular Builds <circleci@angular.dev>
2024-09-26 16:29:02 +0000

    4179bf2 fix(@schematics/angular): support single quote setting in JetBrains IDEs

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.7+sha-e52ae7f',
-    AngularSSR: '^19.0.0-next.7+sha-e52ae7f',
+    DevkitBuildAngular: '^19.0.0-next.7+sha-4179bf2',
+    AngularSSR: '^19.0.0-next.7+sha-4179bf2',
 };
