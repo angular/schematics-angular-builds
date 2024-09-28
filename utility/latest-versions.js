f1b1b5e9709e34fc4a14c5f050ae605883ac50cd
Angular Builds <circleci@angular.dev>
2024-09-28 19:28:33 +0000

    be3f3ff build: update dependency pacote to v19

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.8+sha-9d7613d',
-    AngularSSR: '^19.0.0-next.8+sha-9d7613d',
+    DevkitBuildAngular: '^19.0.0-next.8+sha-be3f3ff',
+    AngularSSR: '^19.0.0-next.8+sha-be3f3ff',
 };
