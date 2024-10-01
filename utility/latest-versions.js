3cd64488e1935cedcc970d2dd8273d669afa43d0
Angular Builds <circleci@angular.dev>
2024-10-01 14:37:07 +0000

    475340a refactor(@angular/build): incomplete string escaping or encoding

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.8+sha-ef1907f',
-    AngularSSR: '^19.0.0-next.8+sha-ef1907f',
+    DevkitBuildAngular: '^19.0.0-next.8+sha-475340a',
+    AngularSSR: '^19.0.0-next.8+sha-475340a',
 };
