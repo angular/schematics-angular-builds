bcddddad0f25223a43b479072243c3e3a24aab3f
Angular Builds <circleci@angular.dev>
2024-10-01 16:23:25 +0000

    c0b76e3 fix(@angular/build): separate Vite cache by project

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.8+sha-475340a',
-    AngularSSR: '^19.0.0-next.8+sha-475340a',
+    DevkitBuildAngular: '^19.0.0-next.8+sha-c0b76e3',
+    AngularSSR: '^19.0.0-next.8+sha-c0b76e3',
 };
