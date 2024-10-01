e842ccea738413a7287024d66f1823bee9b43c6a
Angular Builds <circleci@angular.dev>
2024-10-01 09:37:09 +0000

    e9b29be refactor(@angular/build): remove deprecated Vite node server hot field usage

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.8+sha-f630726',
-    AngularSSR: '^19.0.0-next.8+sha-f630726',
+    DevkitBuildAngular: '^19.0.0-next.8+sha-e9b29be',
+    AngularSSR: '^19.0.0-next.8+sha-e9b29be',
 };
