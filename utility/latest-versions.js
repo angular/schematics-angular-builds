93c4103a8a7a0b5972a890e0a2721f9b1cdf7164
Angular Builds <circleci@angular.dev>
2024-10-02 13:13:42 +0000

    80686be refactor(@angular/build): avoid hydration warnings when `RenderMode.Client` is set

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.8+sha-41934e7',
-    AngularSSR: '^19.0.0-next.8+sha-41934e7',
+    DevkitBuildAngular: '^19.0.0-next.8+sha-80686be',
+    AngularSSR: '^19.0.0-next.8+sha-80686be',
 };
