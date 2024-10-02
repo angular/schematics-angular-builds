b2b8f7acc0ddcffc44789ceb6b1e55ca37958850
Angular Builds <circleci@angular.dev>
2024-10-02 11:13:34 +0000

    41934e7 build: update the `@angular/bazel` packager and set up the license attribute

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.8+sha-b305b76',
-    AngularSSR: '^19.0.0-next.8+sha-b305b76',
+    DevkitBuildAngular: '^19.0.0-next.8+sha-41934e7',
+    AngularSSR: '^19.0.0-next.8+sha-41934e7',
 };
