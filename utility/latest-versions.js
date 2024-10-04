b1b1b3899fba45b0812c0767a970e3115308307a
Angular Builds <circleci@angular.dev>
2024-10-04 11:20:46 +0000

    4941725 refactor: remove unused methods and locals

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.9+sha-d3709e8',
-    AngularSSR: '^19.0.0-next.9+sha-d3709e8',
+    DevkitBuildAngular: '^19.0.0-next.9+sha-4941725',
+    AngularSSR: '^19.0.0-next.9+sha-4941725',
 };
