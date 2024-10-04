51c91b6dd4c76ea219fc73824b11692ec2fd1c51
Angular Builds <circleci@angular.dev>
2024-10-04 13:30:38 +0000

    54cb005 test: fix typo in directory name

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.9+sha-4941725',
-    AngularSSR: '^19.0.0-next.9+sha-4941725',
+    DevkitBuildAngular: '^19.0.0-next.9+sha-54cb005',
+    AngularSSR: '^19.0.0-next.9+sha-54cb005',
 };
