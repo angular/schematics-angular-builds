2d9a54821a1d7fe8d2c085df272b6c2bc048a6ff
Angular Builds <circleci@angular.dev>
2024-10-04 08:02:44 +0000

    3264c17 test(@angular/build): add test harness rebuild case helper function

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.9+sha-6c618d4',
-    AngularSSR: '^19.0.0-next.9+sha-6c618d4',
+    DevkitBuildAngular: '^19.0.0-next.9+sha-3264c17',
+    AngularSSR: '^19.0.0-next.9+sha-3264c17',
 };
