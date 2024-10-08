18a929f5438a32268f3f876b8dbf38d5334aa5d0
Angular Builds <circleci@angular.dev>
2024-10-08 11:54:12 +0000

    834925d refactor(@angular/build): only ignore watching node modules when watching root

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.9+sha-6ce5c69',
-    AngularSSR: '^19.0.0-next.9+sha-6ce5c69',
+    DevkitBuildAngular: '^19.0.0-next.9+sha-834925d',
+    AngularSSR: '^19.0.0-next.9+sha-834925d',
 };
