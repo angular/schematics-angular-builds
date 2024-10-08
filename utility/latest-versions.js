c4851e492f1d4612b5fc1eb4023f0361e225d075
Angular Builds <circleci@angular.dev>
2024-10-08 15:33:30 +0000

    9233e5e fix(@angular/build): show error message when error stack is undefined

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.9+sha-834925d',
-    AngularSSR: '^19.0.0-next.9+sha-834925d',
+    DevkitBuildAngular: '^19.0.0-next.9+sha-9233e5e',
+    AngularSSR: '^19.0.0-next.9+sha-9233e5e',
 };
