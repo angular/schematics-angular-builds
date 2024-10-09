acf13d82e5ee2895aac1bb437aeba57608515196
Angular Builds <circleci@angular.dev>
2024-10-09 18:36:04 +0000

    7d883a1 feat(@angular/build): introduce `ssr.experimentalPlatform` option

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.10+sha-accaa57',
-    AngularSSR: '^19.0.0-next.10+sha-accaa57',
+    DevkitBuildAngular: '^19.0.0-next.10+sha-7d883a1',
+    AngularSSR: '^19.0.0-next.10+sha-7d883a1',
 };
