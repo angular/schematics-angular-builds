5bbfdbf28746303bfaf325f8efb4d0fc6a89936f
Angular Builds <circleci@angular.dev>
2024-10-14 09:25:29 +0000

    cb943ec build: update dependency jasmine-core to ~5.4.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.10+sha-44f8081',
-    AngularSSR: '^19.0.0-next.10+sha-44f8081',
+    DevkitBuildAngular: '^19.0.0-next.10+sha-cb943ec',
+    AngularSSR: '^19.0.0-next.10+sha-cb943ec',
 };
