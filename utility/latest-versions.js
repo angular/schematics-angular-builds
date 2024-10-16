b887bcfe11c621a8df996e6a398493647ec9b225
Angular Builds <circleci@angular.dev>
2024-10-16 14:04:19 +0000

    13b65df fix(@angular/build): allow direct bundling of TSX files with application builder

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.10+sha-b6951f4',
-    AngularSSR: '^19.0.0-next.10+sha-b6951f4',
+    DevkitBuildAngular: '^19.0.0-next.10+sha-13b65df',
+    AngularSSR: '^19.0.0-next.10+sha-13b65df',
 };
