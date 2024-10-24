2ac923f998056bdce7459ad2cfa226bbba86311a
Angular Builds <circleci@angular.dev>
2024-10-24 14:20:50 +0000

    31f9037 fix(@angular/build): avoid hashing development external component stylesheets

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-0a1f171',
-    AngularSSR: '^19.1.0-next.0+sha-0a1f171',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-31f9037',
+    AngularSSR: '^19.1.0-next.0+sha-31f9037',
 };
