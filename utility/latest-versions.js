de4abae3989de7b54fdddcde4406da5a0b4a8414
Angular Builds <circleci@angular.dev>
2024-10-28 15:14:19 +0000

    306273a refactor(@ngtools/webpack): remove unused `augmentHostWithVersioning` method

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.13+sha-4e2a5fe',
-    AngularSSR: '^19.0.0-next.13+sha-4e2a5fe',
+    DevkitBuildAngular: '^19.0.0-next.13+sha-306273a',
+    AngularSSR: '^19.0.0-next.13+sha-306273a',
 };
