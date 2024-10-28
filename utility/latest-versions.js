95071923b453dee4335b98ad78ded6422406367f
Angular Builds <circleci@angular.dev>
2024-10-28 18:21:18 +0000

    4df44dc refactor(@angular/build): use ETags for dev-server resource requests

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-b3fe50f',
-    AngularSSR: '^19.1.0-next.0+sha-b3fe50f',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-4df44dc',
+    AngularSSR: '^19.1.0-next.0+sha-4df44dc',
 };
