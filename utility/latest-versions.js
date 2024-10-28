d400c39719d619624c1a60c091fd6d3208e83a34
Angular Builds <circleci@angular.dev>
2024-10-28 16:33:51 +0000

    b73b98d refactor(@angular/build): use ETag headers for development external component styles

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-63722c3',
-    AngularSSR: '^19.1.0-next.0+sha-63722c3',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-b73b98d',
+    AngularSSR: '^19.1.0-next.0+sha-b73b98d',
 };
