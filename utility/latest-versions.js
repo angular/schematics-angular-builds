5e8f45b68f09613cd16ccb6f91a6c07e8160729a
Angular Builds <circleci@angular.dev>
2024-10-28 18:02:18 +0000

    dfa3d4c refactor(@angular/ssr): switch from esbuild to rollup for bundling critters/beasties

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-next.13+sha-e42d841',
-    AngularSSR: '^19.0.0-next.13+sha-e42d841',
+    DevkitBuildAngular: '^19.0.0-next.13+sha-dfa3d4c',
+    AngularSSR: '^19.0.0-next.13+sha-dfa3d4c',
 };
