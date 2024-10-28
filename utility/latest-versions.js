3c220d177edfd1836918bee5da86bb806cab8a6e
Angular Builds <circleci@angular.dev>
2024-10-28 19:56:42 +0000

    a723cee build: patch `rollup-license-plugin` to fix on windows

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-afec929',
-    AngularSSR: '^19.1.0-next.0+sha-afec929',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-a723cee',
+    AngularSSR: '^19.1.0-next.0+sha-a723cee',
 };
