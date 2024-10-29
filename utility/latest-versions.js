e16f383084d1fabe6948e6ea6080a4fe1d2af7db
Angular Builds <circleci@angular.dev>
2024-10-29 06:49:54 +0000

    8e2829c refactor(@angular/build): add handling for `ngServerMode`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-6a558f6',
-    AngularSSR: '^19.1.0-next.0+sha-6a558f6',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-8e2829c',
+    AngularSSR: '^19.1.0-next.0+sha-8e2829c',
 };
