0e5502d7c929143081db12381520b241391400a7
Angular Builds <circleci@angular.dev>
2024-10-30 16:32:37 +0000

    170bb19 build: update all non-major dependencies

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-ed8f96a',
-    AngularSSR: '^19.1.0-next.0+sha-ed8f96a',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-170bb19',
+    AngularSSR: '^19.1.0-next.0+sha-170bb19',
 };
