783e8eb14e3e0756c126e4dfbec42d565910a687
Angular Builds <circleci@angular.dev>
2024-10-31 16:00:22 +0000

    d6dfce1 refactor(@angular/ssr): add route configs to public API

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-3371f5b',
-    AngularSSR: '^19.1.0-next.0+sha-3371f5b',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-d6dfce1',
+    AngularSSR: '^19.1.0-next.0+sha-d6dfce1',
 };
