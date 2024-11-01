6427b83f6879cdedd6801e115f1ec26b8ca81d17
Angular Builds <circleci@angular.dev>
2024-11-01 22:46:47 +0000

    46e868c refactor(@angular/build): avoid need to pre-import angular compiler package in dev server

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-rc.0+sha-f460b91',
-    AngularSSR: '^19.0.0-rc.0+sha-f460b91',
+    DevkitBuildAngular: '^19.0.0-rc.0+sha-46e868c',
+    AngularSSR: '^19.0.0-rc.0+sha-46e868c',
 };
