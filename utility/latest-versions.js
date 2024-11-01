404b5935d49fd468c163871e24949fe9ddef6353
Angular Builds <circleci@angular.dev>
2024-11-01 14:18:58 +0000

    3e7f6f5 refactor(@angular/build): allow caching of TypeScript build contexts for rebuilds

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-rc.0+sha-3aca1e6',
-    AngularSSR: '^19.0.0-rc.0+sha-3aca1e6',
+    DevkitBuildAngular: '^19.0.0-rc.0+sha-3e7f6f5',
+    AngularSSR: '^19.0.0-rc.0+sha-3e7f6f5',
 };
