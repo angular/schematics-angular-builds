4ec5120600356519621b9c4e0397c0ab43790239
Angular Builds <circleci@angular.dev>
2024-11-01 18:56:47 +0000

    505521e perf(@angular/ssr): integrate ETags for prerendered pages

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-7c6cb95',
-    AngularSSR: '^19.1.0-next.0+sha-7c6cb95',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-505521e',
+    AngularSSR: '^19.1.0-next.0+sha-505521e',
 };
