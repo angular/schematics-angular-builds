774350172aa582c07c73e479675f1c599687936a
Angular Builds <circleci@angular.dev>
2024-11-02 12:32:04 +0000

    aaaa4bc refactor(@angular/build): allow in-memory caching of server main code bundling during rebuilds

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-7eabe7d',
-    AngularSSR: '^19.1.0-next.0+sha-7eabe7d',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-aaaa4bc',
+    AngularSSR: '^19.1.0-next.0+sha-aaaa4bc',
 };
