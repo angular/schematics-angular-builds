ac687d4dc570a7c22849054149cf0e8b02409c0d
Angular Builds <circleci@angular.dev>
2024-11-02 20:03:05 +0000

    71534aa fix(@angular/build): check referenced files against native file paths

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-rc.0+sha-49fe6df',
-    AngularSSR: '^19.0.0-rc.0+sha-49fe6df',
+    DevkitBuildAngular: '^19.0.0-rc.0+sha-71534aa',
+    AngularSSR: '^19.0.0-rc.0+sha-71534aa',
 };
