7be06121f7643bf544bf65c916f18c9be19aeaee
Angular Builds <circleci@angular.dev>
2024-11-05 16:33:11 +0000

    e6ff801 fix(@angular/build): workaround Vite CSS ShadowDOM hot replacement

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-a1fa483',
-    AngularSSR: '^19.1.0-next.0+sha-a1fa483',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-e6ff801',
+    AngularSSR: '^19.1.0-next.0+sha-e6ff801',
 };
