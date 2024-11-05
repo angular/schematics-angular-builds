edae80d019d664d9addf7ddaecf66dc7ab3fefa3
Angular Builds <circleci@angular.dev>
2024-11-05 16:25:08 +0000

    a1fa483 fix(@angular/build): handle `APP_BASE_HREF` correctly in prerendered routes

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-daea0ab',
-    AngularSSR: '^19.1.0-next.0+sha-daea0ab',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-a1fa483',
+    AngularSSR: '^19.1.0-next.0+sha-a1fa483',
 };
