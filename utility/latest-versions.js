3cb9baf0d5ecb8463102fc9c49a7b2311e375851
Angular Builds <circleci@angular.dev>
2024-11-05 10:04:44 +0000

    4633562 fix(@angular/build): disable dev-server websocket when live reload is disabled

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-ae9dfdd',
-    AngularSSR: '^19.1.0-next.0+sha-ae9dfdd',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-4633562',
+    AngularSSR: '^19.1.0-next.0+sha-4633562',
 };
