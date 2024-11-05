67b2aa036a21b6ad51dbcfed6620ae5c6b58da39
Angular Builds <circleci@angular.dev>
2024-11-05 07:29:57 +0000

    27847cd ci: migrate renovate config (#28783)

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-71b3de8',
-    AngularSSR: '^19.1.0-next.0+sha-71b3de8',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-27847cd',
+    AngularSSR: '^19.1.0-next.0+sha-27847cd',
 };
