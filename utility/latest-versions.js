16e462de42c5173b5fd33b3ca90204e1539e9fe6
Angular Builds <circleci@angular.dev>
2024-11-05 14:05:33 +0000

    daea0ab docs: replace `@note` with `@remarks`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-4633562',
-    AngularSSR: '^19.1.0-next.0+sha-4633562',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-daea0ab',
+    AngularSSR: '^19.1.0-next.0+sha-daea0ab',
 };
