f09fc8fd42950e9563c4d17917d61d7caea23b37
Angular Builds <circleci@angular.dev>
2024-11-05 09:36:26 +0000

    ae9dfdd refactor(@angular/build): integrate template update function generation with builder results

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-bcb24b9',
-    AngularSSR: '^19.1.0-next.0+sha-bcb24b9',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-ae9dfdd',
+    AngularSSR: '^19.1.0-next.0+sha-ae9dfdd',
 };
