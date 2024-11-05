d37bcb1468b14a54bc58e426a6ed064d9b70357e
Angular Builds <circleci@angular.dev>
2024-11-05 22:41:02 +0000

    faabbbf fix(@angular-devkit/build-angular): remove double-watch in karma

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-18b6aea',
-    AngularSSR: '^19.1.0-next.0+sha-18b6aea',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-faabbbf',
+    AngularSSR: '^19.1.0-next.0+sha-faabbbf',
 };
