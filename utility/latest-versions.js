1775460b403c372ab7c3fe1e6ed49f378f461692
Angular Builds <circleci@angular.dev>
2024-11-06 00:04:07 +0000

    1278112 fix(@angular-devkit/build-angular): serve assets

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-faabbbf',
-    AngularSSR: '^19.1.0-next.0+sha-faabbbf',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-1278112',
+    AngularSSR: '^19.1.0-next.0+sha-1278112',
 };
