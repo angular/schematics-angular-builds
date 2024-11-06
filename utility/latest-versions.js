69192d451f81782abeb2ef91b3ed1c511262901e
Angular Builds <circleci@angular.dev>
2024-11-06 09:18:59 +0000

    e16cbb9 refactor(@angular/ssr): remove duplicate code and streamline functionality

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-1278112',
-    AngularSSR: '^19.1.0-next.0+sha-1278112',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-e16cbb9',
+    AngularSSR: '^19.1.0-next.0+sha-e16cbb9',
 };
