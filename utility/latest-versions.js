cc2ab9a0b6e7db35cd7075733d7945224468a696
Angular Builds <circleci@angular.dev>
2024-11-06 12:44:23 +0000

    cf0228b fix(@angular/build): skip wildcard routes from being listed as prerendered routes

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-rc.0+sha-408ca40',
-    AngularSSR: '^19.0.0-rc.0+sha-408ca40',
+    DevkitBuildAngular: '^19.0.0-rc.0+sha-cf0228b',
+    AngularSSR: '^19.0.0-rc.0+sha-cf0228b',
 };
