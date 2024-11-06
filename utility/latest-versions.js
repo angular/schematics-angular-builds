38ccf2fe1aa427f32bbb24f4835d7ff4047b2392
Angular Builds <circleci@angular.dev>
2024-11-06 15:25:11 +0000

    2ec877d fix(@angular-devkit/build-angular): handle basename collisions

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-rc.0+sha-cf0228b',
-    AngularSSR: '^19.0.0-rc.0+sha-cf0228b',
+    DevkitBuildAngular: '^19.0.0-rc.0+sha-2ec877d',
+    AngularSSR: '^19.0.0-rc.0+sha-2ec877d',
 };
