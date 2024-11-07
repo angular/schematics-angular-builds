55271f9f6d939eed43bb78a7af6f457d9bd20acc
Angular Builds <circleci@angular.dev>
2024-11-07 22:38:33 +0000

    ce68b32 build: update `rollup-license-plugin` to `3.0.1`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-rc.1+sha-280ebbd',
-    AngularSSR: '^19.0.0-rc.1+sha-280ebbd',
+    DevkitBuildAngular: '^19.0.0-rc.1+sha-ce68b32',
+    AngularSSR: '^19.0.0-rc.1+sha-ce68b32',
 };
