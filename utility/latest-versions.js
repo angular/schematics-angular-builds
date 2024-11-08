7e02f90c879f6cb761e341af95bc741e0caddfd9
Angular Builds <circleci@angular.dev>
2024-11-08 21:38:01 +0000

    073e81e refactor(@angular/ssr): remove redundant `new URL`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-a325c9a',
-    AngularSSR: '^19.1.0-next.0+sha-a325c9a',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-073e81e',
+    AngularSSR: '^19.1.0-next.0+sha-073e81e',
 };
