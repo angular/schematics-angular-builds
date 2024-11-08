7310e85de2c29d0ef3fbe868787164b742fc4e66
Angular Builds <circleci@angular.dev>
2024-11-08 18:57:17 +0000

    a39825e refactor(@angular/ssr): remove `RenderMode.AppShell` in favor of new configuration option

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-rc.1+sha-5a7a292',
-    AngularSSR: '^19.0.0-rc.1+sha-5a7a292',
+    DevkitBuildAngular: '^19.0.0-rc.1+sha-a39825e',
+    AngularSSR: '^19.0.0-rc.1+sha-a39825e',
 };
