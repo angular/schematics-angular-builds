6294bcc41ffb286d6e9f70d7b05667a8f7ffa4b4
Angular Builds <circleci@angular.dev>
2024-11-08 19:13:17 +0000

    53b6cd3 fix(@angular/build): allow .js file replacements in all configuration cases

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-rc.1+sha-a39825e',
-    AngularSSR: '^19.0.0-rc.1+sha-a39825e',
+    DevkitBuildAngular: '^19.0.0-rc.1+sha-53b6cd3',
+    AngularSSR: '^19.0.0-rc.1+sha-53b6cd3',
 };
