a49ac649c17b371a24f1b2cda2f31efeb468646b
Angular Builds <circleci@angular.dev>
2024-11-08 18:27:59 +0000

    5a7a292 fix(@angular/build): add missing redirect in SSR manifest

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-rc.1+sha-3602bbb',
-    AngularSSR: '^19.0.0-rc.1+sha-3602bbb',
+    DevkitBuildAngular: '^19.0.0-rc.1+sha-5a7a292',
+    AngularSSR: '^19.0.0-rc.1+sha-5a7a292',
 };
