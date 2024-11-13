06f284fc64873579d52e714b2f03e724e7804a92
Angular Builds <dev-infra@angular.dev>
2024-11-13 17:14:17 +0000

    52dcd55 fix(@angular/cli): support default options for multiselect list x-prompt

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-rc.1+sha-1fc5d70',
-    AngularSSR: '^19.0.0-rc.1+sha-1fc5d70',
+    DevkitBuildAngular: '^19.0.0-rc.1+sha-52dcd55',
+    AngularSSR: '^19.0.0-rc.1+sha-52dcd55',
 };
