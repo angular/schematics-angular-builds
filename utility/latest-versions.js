7effce0d61dba18225adb50c301bef697befcc61
Angular Builds <dev-infra@angular.dev>
2024-11-18 18:43:42 +0000

    fb05e7f fix(@angular/ssr): use wildcard server route configuration on the '/' route when the app router is empty

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-rc.2+sha-2551df5',
-    AngularSSR: '^19.0.0-rc.2+sha-2551df5',
+    DevkitBuildAngular: '^19.0.0-rc.2+sha-fb05e7f',
+    AngularSSR: '^19.0.0-rc.2+sha-fb05e7f',
 };
