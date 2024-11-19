25e7103352072a762c2e1eab4dde03ede840c41a
Angular Builds <dev-infra@angular.dev>
2024-11-19 15:06:21 +0000

    86acfe0 build: update Angular packages to v19 stable

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0-rc.3+sha-805379e',
-    AngularSSR: '^19.0.0-rc.3+sha-805379e',
+    DevkitBuildAngular: '^19.0.0-rc.3+sha-86acfe0',
+    AngularSSR: '^19.0.0-rc.3+sha-86acfe0',
 };
