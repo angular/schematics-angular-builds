24857bc05856793db5030161c2312996ddd100c1
Angular Builds <dev-infra@angular.dev>
2024-11-19 16:24:13 +0000

    f0a5af0 docs: release notes for the v19.0.0 release

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-23dac18',
-    AngularSSR: '^19.1.0-next.0+sha-23dac18',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-f0a5af0',
+    AngularSSR: '^19.1.0-next.0+sha-f0a5af0',
 };
