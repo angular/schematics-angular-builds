8761eb712373f72578ed977505952c7a16a29fda
Angular Builds <dev-infra@angular.dev>
2024-11-20 12:20:40 +0000

    74461da fix(@angular/build): ensure accurate content length for server assets

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0+sha-d622e59',
-    AngularSSR: '^19.0.0+sha-d622e59',
+    DevkitBuildAngular: '^19.0.0+sha-74461da',
+    AngularSSR: '^19.0.0+sha-74461da',
 };
