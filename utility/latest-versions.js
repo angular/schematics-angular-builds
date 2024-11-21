9fb21d392a0e5b09cf938c8943ad350fdf1d8046
Angular Builds <dev-infra@angular.dev>
2024-11-21 17:53:49 +0000

    1b4dced fix(@angular/build): use `sha256` instead of `sha-256` as hash algorithm name

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.0+sha-b63123f',
-    AngularSSR: '^19.0.0+sha-b63123f',
+    DevkitBuildAngular: '^19.0.0+sha-1b4dced',
+    AngularSSR: '^19.0.0+sha-1b4dced',
 };
