f7c39f9418232190894278592ae58e0510a0ad1f
Angular Builds <dev-infra@angular.dev>
2024-11-21 16:34:50 +0000

    83b32a6 test: add end-to-end tests for SSR setup

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-a6eaf2f',
-    AngularSSR: '^19.1.0-next.0+sha-a6eaf2f',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-83b32a6',
+    AngularSSR: '^19.1.0-next.0+sha-83b32a6',
 };
