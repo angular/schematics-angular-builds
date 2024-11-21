cda857bae96f9ce88ea7ee6447f39e7070a54676
Angular Builds <dev-infra@angular.dev>
2024-11-21 07:25:35 +0000

    8c534da fix(@angular/ssr): handle baseHref that start with `./`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-1bc5d65',
-    AngularSSR: '^19.1.0-next.0+sha-1bc5d65',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-8c534da',
+    AngularSSR: '^19.1.0-next.0+sha-8c534da',
 };
