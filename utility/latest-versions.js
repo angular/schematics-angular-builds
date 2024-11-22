845dde608bfe7796adcdc9e57eecd6cd01ee31b1
Angular Builds <dev-infra@angular.dev>
2024-11-22 10:15:49 +0000

    c8cd90e fix(@angular/ssr): handle nested redirects not explicitly defined in router config

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.1+sha-8cd6aa9',
-    AngularSSR: '^19.0.1+sha-8cd6aa9',
+    DevkitBuildAngular: '^19.0.1+sha-c8cd90e',
+    AngularSSR: '^19.0.1+sha-c8cd90e',
 };
