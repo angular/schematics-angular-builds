ec32372b04c4f22c35aeafb774eb04db405ff213
Angular Builds <dev-infra@angular.dev>
2024-11-22 18:45:25 +0000

    20411f6 refactor(@angular/ssr): replace `:params` with `*`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-e7d3b72',
-    AngularSSR: '^19.1.0-next.0+sha-e7d3b72',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-20411f6',
+    AngularSSR: '^19.1.0-next.0+sha-20411f6',
 };
