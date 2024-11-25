0b30b30ad7f12b1620ca4d1be98b01cafb6ed359
Angular Builds <dev-infra@angular.dev>
2024-11-25 19:39:00 +0000

    7b0aa3d ci: add @angular/build integration test to workflows

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-399bbb7',
-    AngularSSR: '^19.1.0-next.0+sha-399bbb7',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-7b0aa3d',
+    AngularSSR: '^19.1.0-next.0+sha-7b0aa3d',
 };
