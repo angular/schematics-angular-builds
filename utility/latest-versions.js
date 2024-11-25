961402bfd81beb068bcb7f614c5ab389f46dffb3
Angular Builds <dev-infra@angular.dev>
2024-11-25 20:09:54 +0000

    d8cbf3b refactor(@angular/build): maintain previous compiler plugin factory signature

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.1+sha-5ac03f4',
-    AngularSSR: '^19.0.1+sha-5ac03f4',
+    DevkitBuildAngular: '^19.0.1+sha-d8cbf3b',
+    AngularSSR: '^19.0.1+sha-d8cbf3b',
 };
