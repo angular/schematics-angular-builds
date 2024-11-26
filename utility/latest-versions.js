f63c999709636f9f3d1b4e3580720cb1f6e711db
Angular Builds <dev-infra@angular.dev>
2024-11-26 14:05:17 +0000

    ab4e77c fix(@angular/build): allow .json file replacements with application builds

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.2+sha-d261246',
-    AngularSSR: '^19.0.2+sha-d261246',
+    DevkitBuildAngular: '^19.0.2+sha-ab4e77c',
+    AngularSSR: '^19.0.2+sha-ab4e77c',
 };
