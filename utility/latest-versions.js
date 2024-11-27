94d842c964410c57b4cf27828ebb6e5ecc63b6a1
Angular Builds <dev-infra@angular.dev>
2024-11-27 14:05:57 +0000

    4ef45ec fix(@angular/cli): correctly select package versions in descending order during `ng add`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-3a22d44',
-    AngularSSR: '^19.1.0-next.0+sha-3a22d44',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-4ef45ec',
+    AngularSSR: '^19.1.0-next.0+sha-4ef45ec',
 };
