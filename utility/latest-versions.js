00c525a8207c88eb1d742a459ab47f47385d435c
Angular Builds <dev-infra@angular.dev>
2024-12-02 14:45:38 +0000

    e4448bb fix(@angular/ssr): correctly handle serving of prerendered i18n pages

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-ea2075a',
-    AngularSSR: '^19.1.0-next.0+sha-ea2075a',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-e4448bb',
+    AngularSSR: '^19.1.0-next.0+sha-e4448bb',
 };
