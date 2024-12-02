4bcb16b699ae668c8820408d566755bb07d4143d
Angular Builds <dev-infra@angular.dev>
2024-12-02 13:20:30 +0000

    4d466bf build: update to TypeScript 5.7

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-3d1c52b',
-    AngularSSR: '^19.1.0-next.0+sha-3d1c52b',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-4d466bf',
+    AngularSSR: '^19.1.0-next.0+sha-4d466bf',
 };
