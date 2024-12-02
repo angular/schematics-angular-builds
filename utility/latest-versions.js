302a8785493c0a223276903e8e79affbc1456a6f
Angular Builds <dev-infra@angular.dev>
2024-12-02 09:24:13 +0000

    28a51cc fix(@schematics/angular): add required type to `CanDeactivate` guard (#29004)

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.2+sha-3d5f41f',
-    AngularSSR: '^19.0.2+sha-3d5f41f',
+    DevkitBuildAngular: '^19.0.2+sha-28a51cc',
+    AngularSSR: '^19.0.2+sha-28a51cc',
 };
