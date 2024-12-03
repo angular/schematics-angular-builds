b94db924c8d4480afc291909c0665de1317a9aa4
Angular Builds <dev-infra@angular.dev>
2024-12-03 14:15:48 +0000

    cd5e119 ci: add addtional perf workflows for tracking

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-686c51a',
-    AngularSSR: '^19.1.0-next.0+sha-686c51a',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-cd5e119',
+    AngularSSR: '^19.1.0-next.0+sha-cd5e119',
 };
