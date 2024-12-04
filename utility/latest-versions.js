d09783379894c0fdd6c003d6f1647fbc047a75ff
Angular Builds <dev-infra@angular.dev>
2024-12-04 09:18:48 +0000

    75b4e92 refactor(@angular/ssr): replace `Map` with `Record` in SSR manifest

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.2+sha-775e6f7',
-    AngularSSR: '^19.0.2+sha-775e6f7',
+    DevkitBuildAngular: '^19.0.2+sha-75b4e92',
+    AngularSSR: '^19.0.2+sha-75b4e92',
 };
