5f896e0060885a6e6869ff69fed316c76cd9e46c
Angular Builds <dev-infra@angular.dev>
2024-12-05 19:23:39 +0000

    f46d0b1 refactor(@angular/build): move `getDepOptimizationConfig` into utils file

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.4+sha-6cfbb88',
-    AngularSSR: '^19.0.4+sha-6cfbb88',
+    DevkitBuildAngular: '^19.0.4+sha-f46d0b1',
+    AngularSSR: '^19.0.4+sha-f46d0b1',
 };
