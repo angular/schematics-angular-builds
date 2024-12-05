3f1e7523f8a4c3933869f856f551376810050d28
Angular Builds <dev-infra@angular.dev>
2024-12-05 17:35:36 +0000

    fc41f50 fix(@angular/build): show error when Node.js built-ins are used during `ng serve`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.3+sha-14451e2',
-    AngularSSR: '^19.0.3+sha-14451e2',
+    DevkitBuildAngular: '^19.0.3+sha-fc41f50',
+    AngularSSR: '^19.0.3+sha-fc41f50',
 };
