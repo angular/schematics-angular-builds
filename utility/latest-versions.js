90c965142baa5a780592adcfec4d28f55da7c246
Angular Builds <dev-infra@angular.dev>
2024-12-05 15:18:10 +0000

    eb0f4f2 ci: update to latest version of dev-infra actions

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-75998eb',
-    AngularSSR: '^19.1.0-next.0+sha-75998eb',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-eb0f4f2',
+    AngularSSR: '^19.1.0-next.0+sha-eb0f4f2',
 };
