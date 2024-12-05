3c488981836159d89afa17646e9322b8499d84b3
Angular Builds <dev-infra@angular.dev>
2024-12-05 15:27:22 +0000

    1ca260e docs(@angular/ssr): remove duplicate `@return` comment

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-eb0f4f2',
-    AngularSSR: '^19.1.0-next.0+sha-eb0f4f2',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-1ca260e',
+    AngularSSR: '^19.1.0-next.0+sha-1ca260e',
 };
