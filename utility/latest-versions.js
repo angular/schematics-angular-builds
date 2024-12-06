06d944f1c0789c8cd23096bc5840f23e46a662a8
Angular Builds <dev-infra@angular.dev>
2024-12-06 07:29:11 +0000

    6647247 test(@angular/ssr): refine spec setup to resolve component ID collision warnings

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-fb41d18',
-    AngularSSR: '^19.1.0-next.0+sha-fb41d18',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-6647247',
+    AngularSSR: '^19.1.0-next.0+sha-6647247',
 };
