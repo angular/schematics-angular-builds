5afe82c7514655e1114fb48bd955904046b6a990
Angular Builds <dev-infra@angular.dev>
2024-12-10 12:15:55 +0000

    41ece63 feat(@angular/ssr): redirect to preferred locale when accessing root route without a specified locale

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-8d7a51d',
-    AngularSSR: '^19.1.0-next.0+sha-8d7a51d',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-41ece63',
+    AngularSSR: '^19.1.0-next.0+sha-41ece63',
 };
