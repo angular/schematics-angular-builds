d85c7c653ad0e42af9e47ce857d2989c5985f8eb
Angular Builds <dev-infra@angular.dev>
2024-12-10 15:26:12 +0000

    43127dd refactor(@angular/build): add `globalThis['ngServerMode']` only when `externalDependencies` are present

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-41ece63',
-    AngularSSR: '^19.1.0-next.0+sha-41ece63',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-43127dd',
+    AngularSSR: '^19.1.0-next.0+sha-43127dd',
 };
