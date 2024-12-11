f82dae42c2af1493263c79387173427f7c96957c
Angular Builds <dev-infra@angular.dev>
2024-12-11 17:21:59 +0000

    4203efb fix(@angular/ssr): disable component bootstrapping during route extraction

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.4+sha-5ea9ce3',
-    AngularSSR: '^19.0.4+sha-5ea9ce3',
+    DevkitBuildAngular: '^19.0.4+sha-4203efb',
+    AngularSSR: '^19.0.4+sha-4203efb',
 };
