fdb9df7760467da568d19345105c70b3766353b8
Angular Builds <dev-infra@angular.dev>
2024-12-11 20:09:27 +0000

    64f32c7 fix(@angular/build): provide component HMR update modules to dev-server SSR

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.0+sha-10a5b8b',
-    AngularSSR: '^19.1.0-next.0+sha-10a5b8b',
+    DevkitBuildAngular: '^19.1.0-next.0+sha-64f32c7',
+    AngularSSR: '^19.1.0-next.0+sha-64f32c7',
 };
