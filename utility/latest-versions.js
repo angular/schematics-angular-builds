f08c15af7c4475aab18ca3be0bb185b3c78ac0bc
Angular Builds <dev-infra@angular.dev>
2024-12-12 21:49:02 +0000

    f6b8c5e test: e2e tests for autoCsp

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.1+sha-c66782b',
-    AngularSSR: '^19.1.0-next.1+sha-c66782b',
+    DevkitBuildAngular: '^19.1.0-next.1+sha-f6b8c5e',
+    AngularSSR: '^19.1.0-next.1+sha-f6b8c5e',
 };
