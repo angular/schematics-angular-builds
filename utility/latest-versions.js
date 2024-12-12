df7de332a76c82d2a7c68d50b59989e322c9eb78
Angular Builds <dev-infra@angular.dev>
2024-12-12 18:20:40 +0000

    aa627f7 refactor(@angular/build): only load chunk optimizer if enabled

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.5+sha-6537ccd',
-    AngularSSR: '^19.0.5+sha-6537ccd',
+    DevkitBuildAngular: '^19.0.5+sha-aa627f7',
+    AngularSSR: '^19.0.5+sha-aa627f7',
 };
