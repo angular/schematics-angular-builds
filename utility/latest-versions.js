97863e9256fa477279dfe878a313a467a6da7083
Angular Builds <dev-infra@angular.dev>
2024-12-13 20:17:08 +0000

    a9a3470 fix(@angular-devkit/build-angular): jasmine.clock with app builder

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.1+sha-8d35b9b',
-    AngularSSR: '^19.1.0-next.1+sha-8d35b9b',
+    DevkitBuildAngular: '^19.1.0-next.1+sha-a9a3470',
+    AngularSSR: '^19.1.0-next.1+sha-a9a3470',
 };
