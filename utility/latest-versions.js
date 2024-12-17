b65270ada27ed7f1b410408a107ff4acbd157ada
Angular Builds <dev-infra@angular.dev>
2024-12-17 20:38:50 +0000

    ad1d7d7 fix(@angular/ssr): ensure correct `Location` header for redirects behind a proxy

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.1+sha-f7c0a83',
-    AngularSSR: '^19.1.0-next.1+sha-f7c0a83',
+    DevkitBuildAngular: '^19.1.0-next.1+sha-ad1d7d7',
+    AngularSSR: '^19.1.0-next.1+sha-ad1d7d7',
 };
