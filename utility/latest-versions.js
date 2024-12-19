a43854683428ad75144ecefc86e6c7ace4859092
Angular Builds <dev-infra@angular.dev>
2024-12-19 16:44:09 +0000

    3feecdd fix(@angular/ssr): disable component boostrapping when running route extraction

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.2+sha-9479484',
-    AngularSSR: '^19.1.0-next.2+sha-9479484',
+    DevkitBuildAngular: '^19.1.0-next.2+sha-3feecdd',
+    AngularSSR: '^19.1.0-next.2+sha-3feecdd',
 };
