3ddc0fe8b2620eb4fe0e1b2825679db53bbee62f
Angular Builds <dev-infra@angular.dev>
2024-12-19 07:20:27 +0000

    307eda1 test: disable WTR e2e test

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.2+sha-07a8bce',
-    AngularSSR: '^19.1.0-next.2+sha-07a8bce',
+    DevkitBuildAngular: '^19.1.0-next.2+sha-307eda1',
+    AngularSSR: '^19.1.0-next.2+sha-307eda1',
 };
