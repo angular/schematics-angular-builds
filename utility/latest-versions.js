1f6cafb20622b6eae4929a92781f3b4d63e73e63
Angular Builds <dev-infra@angular.dev>
2024-12-20 17:24:40 +0000

    673388a ci: ensure passing build before rest of CI jobs

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.2+sha-13e233a',
-    AngularSSR: '^19.1.0-next.2+sha-13e233a',
+    DevkitBuildAngular: '^19.1.0-next.2+sha-673388a',
+    AngularSSR: '^19.1.0-next.2+sha-673388a',
 };
