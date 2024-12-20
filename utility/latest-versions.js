33e5e1e4add5189a7d52c0cc41d0e7240ccf1a5a
Angular Builds <dev-infra@angular.dev>
2024-12-20 17:02:42 +0000

    6ff3144 build: set a mnemonic for template expand and file write

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.6+sha-b43c648',
-    AngularSSR: '^19.0.6+sha-b43c648',
+    DevkitBuildAngular: '^19.0.6+sha-6ff3144',
+    AngularSSR: '^19.0.6+sha-6ff3144',
 };
