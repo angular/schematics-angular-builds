cc0e7e6bdfa5b382e7287e8d10cf2804e270c1b5
Angular Builds <dev-infra@angular.dev>
2024-12-21 13:48:40 +0000

    5a93866 build: migrate `@ngtools/webpack` to `ts_project`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.6+sha-073b55c',
-    AngularSSR: '^19.0.6+sha-073b55c',
+    DevkitBuildAngular: '^19.0.6+sha-5a93866',
+    AngularSSR: '^19.0.6+sha-5a93866',
 };
