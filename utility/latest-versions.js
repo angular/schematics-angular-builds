e4a3d562ba0cca353ccebae08dddda78bd84653e
Angular Builds <dev-infra@angular.dev>
2024-12-27 14:15:48 +0000

    3c12d72 build: migrate `@angular/build` to `ts_project`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.6+sha-afd1e72',
-    AngularSSR: '^19.0.6+sha-afd1e72',
+    DevkitBuildAngular: '^19.0.6+sha-3c12d72',
+    AngularSSR: '^19.0.6+sha-3c12d72',
 };
