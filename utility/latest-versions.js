7147dec79b9469e14aa50dcdf8db117a79a72349
Angular Builds <dev-infra@angular.dev>
2025-01-06 13:22:41 +0000

    ba16ad6 fix(@angular/build): support incremental build file results in watch mode

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.2+sha-9b57ff0',
-    AngularSSR: '^19.1.0-next.2+sha-9b57ff0',
+    DevkitBuildAngular: '^19.1.0-next.2+sha-ba16ad6',
+    AngularSSR: '^19.1.0-next.2+sha-ba16ad6',
 };
