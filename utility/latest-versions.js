7b6c6603eb429656969b3d822cccd42e05e37b60
Angular Builds <dev-infra@angular.dev>
2025-01-06 23:57:53 +0000

    739fd8d ci: run less jobs per executor for RBE

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.6+sha-8a78b27',
-    AngularSSR: '^19.0.6+sha-8a78b27',
+    DevkitBuildAngular: '^19.0.6+sha-739fd8d',
+    AngularSSR: '^19.0.6+sha-739fd8d',
 };
