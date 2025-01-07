d593c5eff34392fabe985e669de7b9f531ea9114
Angular Builds <dev-infra@angular.dev>
2025-01-07 19:29:28 +0000

    7d16638 ci: add flag for preventing NpmPackageExtract from executing on RBE

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.6+sha-f752234',
-    AngularSSR: '^19.0.6+sha-f752234',
+    DevkitBuildAngular: '^19.0.6+sha-7d16638',
+    AngularSSR: '^19.0.6+sha-7d16638',
 };
