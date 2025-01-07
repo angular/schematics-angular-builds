aa0997c1dc11dfa4a6702aef68b5f315a77ac6d8
Angular Builds <dev-infra@angular.dev>
2025-01-07 13:00:04 +0000

    b78ccb0 test: remove browser builder AOT tests

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.2+sha-3b5afbb',
-    AngularSSR: '^19.1.0-next.2+sha-3b5afbb',
+    DevkitBuildAngular: '^19.1.0-next.2+sha-b78ccb0',
+    AngularSSR: '^19.1.0-next.2+sha-b78ccb0',
 };
