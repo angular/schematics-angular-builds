662f3d7bfca7aafa08bcb82b46cf4aaed5af6470
Angular Builds <dev-infra@angular.dev>
2025-01-07 12:26:36 +0000

    3b5afbb test(@angular/build): separate application and dev-server integration test targets

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.2+sha-1c011a2',
-    AngularSSR: '^19.1.0-next.2+sha-1c011a2',
+    DevkitBuildAngular: '^19.1.0-next.2+sha-3b5afbb',
+    AngularSSR: '^19.1.0-next.2+sha-3b5afbb',
 };
