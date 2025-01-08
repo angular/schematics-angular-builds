3a22a2a6854387f2246338ff9a24781a56212bca
Angular Builds <dev-infra@angular.dev>
2025-01-08 13:47:13 +0000

    48988ea build: update `@angular/ng-dev` to support `rulesJsInteropMode`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.0.6+sha-0b7962d',
-    AngularSSR: '^19.0.6+sha-0b7962d',
+    DevkitBuildAngular: '^19.0.6+sha-48988ea',
+    AngularSSR: '^19.0.6+sha-48988ea',
 };
