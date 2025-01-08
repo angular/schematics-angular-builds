2be0c6446ac9ea449d07f9103adc44e8d2804920
Angular Builds <dev-infra@angular.dev>
2025-01-08 16:36:11 +0000

    bfe9ee3 fix(@angular/build): warn when `@angular/localize/init` is imported directly

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.2+sha-22bd88c',
-    AngularSSR: '^19.1.0-next.2+sha-22bd88c',
+    DevkitBuildAngular: '^19.1.0-next.2+sha-bfe9ee3',
+    AngularSSR: '^19.1.0-next.2+sha-bfe9ee3',
 };
