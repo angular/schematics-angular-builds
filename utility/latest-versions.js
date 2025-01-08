4650a5e35e7fa014d2c62a1c44c8548e55344d88
Angular Builds <dev-infra@angular.dev>
2025-01-08 12:56:22 +0000

    e66b24b build: resolve issue with uncompiled `@angular/build/private` being included

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-next.2+sha-7519593',
-    AngularSSR: '^19.1.0-next.2+sha-7519593',
+    DevkitBuildAngular: '^19.1.0-next.2+sha-e66b24b',
+    AngularSSR: '^19.1.0-next.2+sha-e66b24b',
 };
