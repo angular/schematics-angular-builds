e011e48919445b1a077c3872b86c1bda34c16533
Angular Builds <dev-infra@angular.dev>
2025-01-10 19:58:59 +0000

    81a83df fix(@angular/build): correctly validate locales `subPath`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,7 +15,7 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.2.0-next.0+sha-1c83656',
-    AngularBuild: '^19.2.0-next.0+sha-1c83656',
-    AngularSSR: '^19.2.0-next.0+sha-1c83656',
+    DevkitBuildAngular: '^19.2.0-next.0+sha-81a83df',
+    AngularBuild: '^19.2.0-next.0+sha-81a83df',
+    AngularSSR: '^19.2.0-next.0+sha-81a83df',
 };
