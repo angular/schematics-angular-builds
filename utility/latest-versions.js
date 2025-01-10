b57177dc0758122d3e91e10b165e4391041aa4c3
Angular Builds <dev-infra@angular.dev>
2025-01-10 19:18:41 +0000

    955acef fix(@angular/build): trigger browser reload on asset changes with Vite dev server

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,7 +15,7 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-rc.0+sha-3b9bd77',
-    AngularBuild: '^19.1.0-rc.0+sha-3b9bd77',
-    AngularSSR: '^19.1.0-rc.0+sha-3b9bd77',
+    DevkitBuildAngular: '^19.1.0-rc.0+sha-955acef',
+    AngularBuild: '^19.1.0-rc.0+sha-955acef',
+    AngularSSR: '^19.1.0-rc.0+sha-955acef',
 };
