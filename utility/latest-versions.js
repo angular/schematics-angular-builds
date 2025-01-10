e9d74129e561a5339caa8aee6282c331c1ac2de0
Angular Builds <dev-infra@angular.dev>
2025-01-10 16:42:23 +0000

    ecadcae build: update bazel

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,7 +15,7 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.2.0-next.0+sha-e4ee60d',
-    AngularBuild: '^19.2.0-next.0+sha-e4ee60d',
-    AngularSSR: '^19.2.0-next.0+sha-e4ee60d',
+    DevkitBuildAngular: '^19.2.0-next.0+sha-ecadcae',
+    AngularBuild: '^19.2.0-next.0+sha-ecadcae',
+    AngularSSR: '^19.2.0-next.0+sha-ecadcae',
 };
