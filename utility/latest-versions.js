30da340b2445bc01b6bcb3653345a63bc8c37ed1
Angular Builds <dev-infra@angular.dev>
2025-01-10 14:13:23 +0000

    e4ee60d ci: disable fixup commit messages

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,7 +15,7 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.2.0-next.0+sha-b55bd31',
-    AngularBuild: '^19.2.0-next.0+sha-b55bd31',
-    AngularSSR: '^19.2.0-next.0+sha-b55bd31',
+    DevkitBuildAngular: '^19.2.0-next.0+sha-e4ee60d',
+    AngularBuild: '^19.2.0-next.0+sha-e4ee60d',
+    AngularSSR: '^19.2.0-next.0+sha-e4ee60d',
 };
