40f4d85c9d9aaa8af98063d12d02f019697ea88b
Angular Builds <dev-infra@angular.dev>
2025-01-14 08:52:17 +0000

    2f55209 fix(@angular-devkit/schematics): update `Rule` type to support returning a `Promise` of `Tree`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,7 +15,7 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-rc.0+sha-8e110bb',
-    AngularBuild: '^19.1.0-rc.0+sha-8e110bb',
-    AngularSSR: '^19.1.0-rc.0+sha-8e110bb',
+    DevkitBuildAngular: '^19.1.0-rc.0+sha-2f55209',
+    AngularBuild: '^19.1.0-rc.0+sha-2f55209',
+    AngularSSR: '^19.1.0-rc.0+sha-2f55209',
 };
