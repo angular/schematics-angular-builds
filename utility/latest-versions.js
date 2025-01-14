e2175fa1e71b83f4525fe8acdd7287dc0781ea2c
Angular Builds <dev-infra@angular.dev>
2025-01-14 15:14:11 +0000

    d52f853 build: migrate `@angular-devkit/schematics` to `npm_package`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,7 +15,7 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-rc.0+sha-2f55209',
-    AngularBuild: '^19.1.0-rc.0+sha-2f55209',
-    AngularSSR: '^19.1.0-rc.0+sha-2f55209',
+    DevkitBuildAngular: '^19.1.0-rc.0+sha-d52f853',
+    AngularBuild: '^19.1.0-rc.0+sha-d52f853',
+    AngularSSR: '^19.1.0-rc.0+sha-d52f853',
 };
