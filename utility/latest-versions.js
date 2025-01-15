4c7b72767ba004529e15cc98d5aebe51c6d53954
Angular Builds <dev-infra@angular.dev>
2025-01-15 19:06:15 +0000

    a092dfd build: migrate `@angular-devkit/architect` jasmine test to native `rules_js`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,7 +15,7 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.0-rc.0+sha-84bbfa5',
-    AngularBuild: '^19.1.0-rc.0+sha-84bbfa5',
-    AngularSSR: '^19.1.0-rc.0+sha-84bbfa5',
+    DevkitBuildAngular: '^19.1.0-rc.0+sha-a092dfd',
+    AngularBuild: '^19.1.0-rc.0+sha-a092dfd',
+    AngularSSR: '^19.1.0-rc.0+sha-a092dfd',
 };
