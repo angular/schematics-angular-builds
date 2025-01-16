66aa39d806da25e38a09568d18a87fb348186c28
Angular Builds <dev-infra@angular.dev>
2025-01-16 17:16:48 +0000

    939d161 fix(@angular/build): perform incremental background file updates with component updates

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,7 +15,7 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.1+sha-069818b',
-    AngularBuild: '^19.1.1+sha-069818b',
-    AngularSSR: '^19.1.1+sha-069818b',
+    DevkitBuildAngular: '^19.1.1+sha-939d161',
+    AngularBuild: '^19.1.1+sha-939d161',
+    AngularSSR: '^19.1.1+sha-939d161',
 };
