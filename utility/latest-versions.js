3488bff3ec7c02826dd502ec3abc833d11525c75
Angular Builds <dev-infra@angular.dev>
2025-01-17 18:28:39 +0000

    148acbd fix(@angular/build): reset component updates on dev-server index request

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,7 +15,7 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.1+sha-3040272',
-    AngularBuild: '^19.1.1+sha-3040272',
-    AngularSSR: '^19.1.1+sha-3040272',
+    DevkitBuildAngular: '^19.1.1+sha-148acbd',
+    AngularBuild: '^19.1.1+sha-148acbd',
+    AngularSSR: '^19.1.1+sha-148acbd',
 };
