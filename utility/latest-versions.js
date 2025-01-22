2b982821db977f52fa94bc078275a2c0225b8515
Angular Builds <dev-infra@angular.dev>
2025-01-22 10:03:12 +0000

    c5d9038 refactor(@angular/ssr): update `getPathSegments` to use `stripTrailingSlash` instead of `filter(Boolean)`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,7 +15,7 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.3+sha-9bacf39',
-    AngularBuild: '^19.1.3+sha-9bacf39',
-    AngularSSR: '^19.1.3+sha-9bacf39',
+    DevkitBuildAngular: '^19.1.3+sha-c5d9038',
+    AngularBuild: '^19.1.3+sha-c5d9038',
+    AngularSSR: '^19.1.3+sha-c5d9038',
 };
