d0adf7c8bc8c1e4c02480b73c0b58591c664648c
Angular Builds <dev-infra@angular.dev>
2025-01-24 12:29:27 +0000

    bd9d379 fix(@angular/build): disable TypeScript `removeComments` option

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,7 +15,7 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.4+sha-94643d5',
-    AngularBuild: '^19.1.4+sha-94643d5',
-    AngularSSR: '^19.1.4+sha-94643d5',
+    DevkitBuildAngular: '^19.1.4+sha-bd9d379',
+    AngularBuild: '^19.1.4+sha-bd9d379',
+    AngularSSR: '^19.1.4+sha-bd9d379',
 };
