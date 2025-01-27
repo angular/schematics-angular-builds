a28def0e3ba8c4a0cb1c5af69d3fb5433c5040aa
Angular Builds <dev-infra@angular.dev>
2025-01-27 18:35:19 +0000

    d53d25f fix(@angular/build): allow tailwindcss 4.x as a peer dependency

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,7 +15,7 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.4+sha-2011d34',
-    AngularBuild: '^19.1.4+sha-2011d34',
-    AngularSSR: '^19.1.4+sha-2011d34',
+    DevkitBuildAngular: '^19.1.4+sha-d53d25f',
+    AngularBuild: '^19.1.4+sha-d53d25f',
+    AngularSSR: '^19.1.4+sha-d53d25f',
 };
