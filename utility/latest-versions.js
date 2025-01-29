b421d9b5950f695155131961280ec9c82eaf3a67
Angular Builds <dev-infra@angular.dev>
2025-01-29 07:25:39 +0000

    bb41345 fix(@angular/build): keep background referenced HMR update chunks

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,7 +15,7 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.4+sha-3cc197c',
-    AngularBuild: '^19.1.4+sha-3cc197c',
-    AngularSSR: '^19.1.4+sha-3cc197c',
+    DevkitBuildAngular: '^19.1.4+sha-bb41345',
+    AngularBuild: '^19.1.4+sha-bb41345',
+    AngularSSR: '^19.1.4+sha-bb41345',
 };
