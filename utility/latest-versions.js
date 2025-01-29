c0c95dcff5ff6455e434dbe5c603aba942ac4095
Angular Builds <dev-infra@angular.dev>
2025-01-29 08:11:19 +0000

    832bfff build: migrate `@angular-devkit/build-angular` tests to `rules_js`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^19.2.0-next.0+sha-0fe4a54',
-    AngularBuild: '^19.2.0-next.0+sha-0fe4a54',
-    AngularSSR: '^19.2.0-next.0+sha-0fe4a54',
+    DevkitBuildAngular: '^19.2.0-next.0+sha-832bfff',
+    AngularBuild: '^19.2.0-next.0+sha-832bfff',
+    AngularSSR: '^19.2.0-next.0+sha-832bfff',
 };
