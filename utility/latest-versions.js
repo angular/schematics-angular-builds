b25ea0b207877728d6b754cc18e19039c5b9999c
Angular Builds <dev-infra@angular.dev>
2025-01-29 15:58:23 +0000

    5f3b622 build: update framework and ng-packagr peer-deps

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^19.2.0-next.0+sha-95189d5',
-    AngularBuild: '^19.2.0-next.0+sha-95189d5',
-    AngularSSR: '^19.2.0-next.0+sha-95189d5',
+    DevkitBuildAngular: '^19.2.0-next.0+sha-5f3b622',
+    AngularBuild: '^19.2.0-next.0+sha-5f3b622',
+    AngularSSR: '^19.2.0-next.0+sha-5f3b622',
 };
