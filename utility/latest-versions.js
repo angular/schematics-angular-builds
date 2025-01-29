99d1d5ac30c9a9f0e5d97d350ad2388c5f086d1e
Angular Builds <dev-infra@angular.dev>
2025-01-29 18:03:36 +0000

    319fbb6 refactor(@angular-devkit/core): add missing function return types

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^19.2.0-next.0+sha-5f3b622',
-    AngularBuild: '^19.2.0-next.0+sha-5f3b622',
-    AngularSSR: '^19.2.0-next.0+sha-5f3b622',
+    DevkitBuildAngular: '^19.2.0-next.1+sha-319fbb6',
+    AngularBuild: '^19.2.0-next.1+sha-319fbb6',
+    AngularSSR: '^19.2.0-next.1+sha-319fbb6',
 };
