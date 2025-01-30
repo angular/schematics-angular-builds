167b5ebe1b8ddfb76631b63b73be2bbd284b51ea
Angular Builds <dev-infra@angular.dev>
2025-01-30 10:25:20 +0000

    c0d20e0 build: switch beasties bundling to `rules_js`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^19.2.0-next.1+sha-448e920',
-    AngularBuild: '^19.2.0-next.1+sha-448e920',
-    AngularSSR: '^19.2.0-next.1+sha-448e920',
+    DevkitBuildAngular: '^19.2.0-next.1+sha-c0d20e0',
+    AngularBuild: '^19.2.0-next.1+sha-c0d20e0',
+    AngularSSR: '^19.2.0-next.1+sha-c0d20e0',
 };
