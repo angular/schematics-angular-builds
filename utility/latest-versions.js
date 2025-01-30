66626ab23f3ef9035f352488b41aeb12a76013a8
Angular Builds <dev-infra@angular.dev>
2025-01-30 17:12:27 +0000

    8efd4ca build: remove now unused bazel dependencies

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^19.2.0-next.1+sha-2552093',
-    AngularBuild: '^19.2.0-next.1+sha-2552093',
-    AngularSSR: '^19.2.0-next.1+sha-2552093',
+    DevkitBuildAngular: '^19.2.0-next.1+sha-8efd4ca',
+    AngularBuild: '^19.2.0-next.1+sha-8efd4ca',
+    AngularSSR: '^19.2.0-next.1+sha-8efd4ca',
 };
