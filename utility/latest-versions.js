95b8748b3d91f42f3f31813f8484a88c94ccdd9c
Angular Builds <dev-infra@angular.dev>
2025-02-03 22:43:44 +0000

    4b4eeca build: Preserve newlines when gathering stdout

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,7 +15,7 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.5+sha-3f70426',
-    AngularBuild: '^19.1.5+sha-3f70426',
-    AngularSSR: '^19.1.5+sha-3f70426',
+    DevkitBuildAngular: '^19.1.5+sha-4b4eeca',
+    AngularBuild: '^19.1.5+sha-4b4eeca',
+    AngularSSR: '^19.1.5+sha-4b4eeca',
 };
