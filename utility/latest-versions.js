3eaaa9f71b7535d214760c7e4b1d87225de87b94
Angular Builds <dev-infra@angular.dev>
2025-02-06 20:01:08 +0000

    f5d9745 fix(@angular/ssr): accurately calculate content length for static pages with `\r\n`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,7 +15,7 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.6+sha-f4de3d2',
-    AngularBuild: '^19.1.6+sha-f4de3d2',
-    AngularSSR: '^19.1.6+sha-f4de3d2',
+    DevkitBuildAngular: '^19.1.6+sha-f5d9745',
+    AngularBuild: '^19.1.6+sha-f5d9745',
+    AngularSSR: '^19.1.6+sha-f5d9745',
 };
