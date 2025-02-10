e9258d78ead05ea2a4724098e66150175f1b4ecf
Angular Builds <dev-infra@angular.dev>
2025-02-10 16:41:46 +0000

    b553069 fix(@angular/build): handle unlocalizable files correctly in localized prerender

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^19.2.0-next.1+sha-880a50c',
-    AngularBuild: '^19.2.0-next.1+sha-880a50c',
-    AngularSSR: '^19.2.0-next.1+sha-880a50c',
+    DevkitBuildAngular: '^19.2.0-next.1+sha-b553069',
+    AngularBuild: '^19.2.0-next.1+sha-b553069',
+    AngularSSR: '^19.2.0-next.1+sha-b553069',
 };
