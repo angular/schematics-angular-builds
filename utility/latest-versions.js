b57a8873f9ed1507e144b51fb0f22b4f90696a70
Angular Builds <dev-infra@angular.dev>
2025-02-10 23:45:17 +0000

    833dc98 fix(@angular/ssr): properly handle baseHref with protocol

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^19.2.0-next.1+sha-b553069',
-    AngularBuild: '^19.2.0-next.1+sha-b553069',
-    AngularSSR: '^19.2.0-next.1+sha-b553069',
+    DevkitBuildAngular: '^19.2.0-next.1+sha-833dc98',
+    AngularBuild: '^19.2.0-next.1+sha-833dc98',
+    AngularSSR: '^19.2.0-next.1+sha-833dc98',
 };
