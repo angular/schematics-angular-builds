a1d8853cb5296c40de2677cfb1d3bc9663f866e2
Angular Builds <dev-infra@angular.dev>
2025-02-11 16:34:51 +0000

    36afdf5 refactor(@angular/build): remove outdated `allowedHosts` warning

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,7 +15,7 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^19.1.6+sha-c26ea16',
-    AngularBuild: '^19.1.6+sha-c26ea16',
-    AngularSSR: '^19.1.6+sha-c26ea16',
+    DevkitBuildAngular: '^19.1.6+sha-36afdf5',
+    AngularBuild: '^19.1.6+sha-36afdf5',
+    AngularSSR: '^19.1.6+sha-36afdf5',
 };
