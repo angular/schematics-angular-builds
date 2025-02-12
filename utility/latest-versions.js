7990e3ab86c1057b4d6a735940e61f9cdaf4e432
Angular Builds <dev-infra@angular.dev>
2025-02-12 09:53:24 +0000

    be15b88 fix(@angular/build): configure Vite CORS option

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^19.2.0-next.1+sha-b50b6ee',
-    AngularBuild: '^19.2.0-next.1+sha-b50b6ee',
-    AngularSSR: '^19.2.0-next.1+sha-b50b6ee',
+    DevkitBuildAngular: '^19.2.0-next.1+sha-be15b88',
+    AngularBuild: '^19.2.0-next.1+sha-be15b88',
+    AngularSSR: '^19.2.0-next.1+sha-be15b88',
 };
