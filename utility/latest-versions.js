b1f4b05cc78f032c98ebb5cc3cc91cd9b16f93bb
Angular Builds <dev-infra@angular.dev>
2025-02-12 13:23:00 +0000

    25007e1 ci: temporary disable updating esbuild

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^19.2.0-next.1+sha-be15b88',
-    AngularBuild: '^19.2.0-next.1+sha-be15b88',
-    AngularSSR: '^19.2.0-next.1+sha-be15b88',
+    DevkitBuildAngular: '^19.2.0-next.1+sha-25007e1',
+    AngularBuild: '^19.2.0-next.1+sha-25007e1',
+    AngularSSR: '^19.2.0-next.1+sha-25007e1',
 };
