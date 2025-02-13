c9e1c0878d3bef58d651ff095960ceb996330b78
Angular Builds <dev-infra@angular.dev>
2025-02-13 15:18:56 +0000

    f5689e9 refactor(@angular/cli): handle undefined `ng add` collection name

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^19.2.0-next.2+sha-751a598',
-    AngularBuild: '^19.2.0-next.2+sha-751a598',
-    AngularSSR: '^19.2.0-next.2+sha-751a598',
+    DevkitBuildAngular: '^19.2.0-next.2+sha-f5689e9',
+    AngularBuild: '^19.2.0-next.2+sha-f5689e9',
+    AngularSSR: '^19.2.0-next.2+sha-f5689e9',
 };
