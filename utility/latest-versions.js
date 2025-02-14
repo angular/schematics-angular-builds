fd6f9d6db94bc2a727a5ec06ab3e0a8590886bed
Angular Builds <dev-infra@angular.dev>
2025-02-14 22:05:50 +0000

    e6deb82 fix(@angular/build): update critical CSS inlining to support `autoCsp`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^19.2.0-next.2+sha-33ed6e8',
-    AngularBuild: '^19.2.0-next.2+sha-33ed6e8',
-    AngularSSR: '^19.2.0-next.2+sha-33ed6e8',
+    DevkitBuildAngular: '^19.2.0-next.2+sha-e6deb82',
+    AngularBuild: '^19.2.0-next.2+sha-e6deb82',
+    AngularSSR: '^19.2.0-next.2+sha-e6deb82',
 };
