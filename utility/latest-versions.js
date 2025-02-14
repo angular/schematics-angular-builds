2ff940156e38064dd7bc1f9ac3a5803bf2179585
Angular Builds <dev-infra@angular.dev>
2025-02-14 12:30:59 +0000

    87b310a test: reduce flakiness of `reuse-dep-optimization-cache` e2e test

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^19.2.0-next.2+sha-04f331a',
-    AngularBuild: '^19.2.0-next.2+sha-04f331a',
-    AngularSSR: '^19.2.0-next.2+sha-04f331a',
+    DevkitBuildAngular: '^19.2.0-next.2+sha-87b310a',
+    AngularBuild: '^19.2.0-next.2+sha-87b310a',
+    AngularSSR: '^19.2.0-next.2+sha-87b310a',
 };
