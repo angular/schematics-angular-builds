68e9669ab50461679b06c38c1cb97d05db71ebcf
Angular Builds <dev-infra@angular.dev>
2025-02-18 11:40:36 +0000

    1574183 build: lock file maintenance

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^19.2.0-next.2+sha-e6deb82',
-    AngularBuild: '^19.2.0-next.2+sha-e6deb82',
-    AngularSSR: '^19.2.0-next.2+sha-e6deb82',
+    DevkitBuildAngular: '^19.2.0-next.2+sha-1574183',
+    AngularBuild: '^19.2.0-next.2+sha-1574183',
+    AngularSSR: '^19.2.0-next.2+sha-1574183',
 };
