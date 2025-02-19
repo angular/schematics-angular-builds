d068d96fda9bbfc99fb92edb518b95da51fa2406
Angular Builds <dev-infra@angular.dev>
2025-02-19 18:02:52 +0000

    c9273f8 build: add @types/node to assorted locations where node types are being relied on and currently obtained transitively.

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^19.2.0-next.2+sha-a5fcf80',
-    AngularBuild: '^19.2.0-next.2+sha-a5fcf80',
-    AngularSSR: '^19.2.0-next.2+sha-a5fcf80',
+    DevkitBuildAngular: '^19.2.0-next.2+sha-c9273f8',
+    AngularBuild: '^19.2.0-next.2+sha-c9273f8',
+    AngularSSR: '^19.2.0-next.2+sha-c9273f8',
 };
