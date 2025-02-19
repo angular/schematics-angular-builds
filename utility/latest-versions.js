f8707d5e877a1ca55bef291c5894d051fdb6a8ca
Angular Builds <dev-infra@angular.dev>
2025-02-19 22:44:47 +0000

    4255779 release: switch the next branch to v20.0.0-next.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^19.2.0-next.2+sha-8f73dad',
-    AngularBuild: '^19.2.0-next.2+sha-8f73dad',
-    AngularSSR: '^19.2.0-next.2+sha-8f73dad',
+    DevkitBuildAngular: '^20.0.0-next.0+sha-4255779',
+    AngularBuild: '^20.0.0-next.0+sha-4255779',
+    AngularSSR: '^20.0.0-next.0+sha-4255779',
 };
