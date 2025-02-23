5f0395e632aaa3a3c5cb2f1d394f97e4d7d7bb0c
Angular Builds <dev-infra@angular.dev>
2025-02-23 14:49:39 +0000

    4b4b25b build: update dependency rxjs to v7.8.2

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.0+sha-b77a1b3',
-    AngularBuild: '^20.0.0-next.0+sha-b77a1b3',
-    AngularSSR: '^20.0.0-next.0+sha-b77a1b3',
+    DevkitBuildAngular: '^20.0.0-next.0+sha-4b4b25b',
+    AngularBuild: '^20.0.0-next.0+sha-4b4b25b',
+    AngularSSR: '^20.0.0-next.0+sha-4b4b25b',
 };
