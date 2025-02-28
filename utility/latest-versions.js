22ec5ed3d2dffe588863f89aa6a4b39d816846c7
Angular Builds <dev-infra@angular.dev>
2025-02-28 16:38:50 +0000

    9ceca0c build: update dependency babel-loader to v10

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0 || ^20.0.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.0+sha-25ea098',
-    AngularBuild: '^20.0.0-next.0+sha-25ea098',
-    AngularSSR: '^20.0.0-next.0+sha-25ea098',
+    DevkitBuildAngular: '^20.0.0-next.0+sha-9ceca0c',
+    AngularBuild: '^20.0.0-next.0+sha-9ceca0c',
+    AngularSSR: '^20.0.0-next.0+sha-9ceca0c',
 };
