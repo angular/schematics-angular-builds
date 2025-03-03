a2e47dc37602428b1383c8b5f4586e74d67c6db8
Angular Builds <dev-infra@angular.dev>
2025-03-03 13:38:20 +0000

    9bce4c4 build: update to TypeScript 5.8 final

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0 || ^20.0.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.0+sha-38ef5d7',
-    AngularBuild: '^20.0.0-next.0+sha-38ef5d7',
-    AngularSSR: '^20.0.0-next.0+sha-38ef5d7',
+    DevkitBuildAngular: '^20.0.0-next.0+sha-9bce4c4',
+    AngularBuild: '^20.0.0-next.0+sha-9bce4c4',
+    AngularSSR: '^20.0.0-next.0+sha-9bce4c4',
 };
