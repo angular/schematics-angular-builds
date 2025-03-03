2c0bca57f9ba4180ca0d9289761587d0a194ed94
Angular Builds <dev-infra@angular.dev>
2025-03-03 16:06:59 +0000

    c39c344 refactor(@angular/build): replace `fast-glob` usage with `tinyglobby`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0-next.0 || ^20.0.0-next.0',
     NgPackagr: '^19.2.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.0+sha-d63e31c',
-    AngularBuild: '^20.0.0-next.0+sha-d63e31c',
-    AngularSSR: '^20.0.0-next.0+sha-d63e31c',
+    DevkitBuildAngular: '^20.0.0-next.0+sha-c39c344',
+    AngularBuild: '^20.0.0-next.0+sha-c39c344',
+    AngularSSR: '^20.0.0-next.0+sha-c39c344',
 };
