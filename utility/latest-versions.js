11f5d2915d28f0d32a23473645420b6d33f50163
Angular Builds <dev-infra@angular.dev>
2025-03-04 09:03:51 +0000

    c073309 fix(@angular/build): allow component HMR with a service worker

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0',
     NgPackagr: '^19.2.0',
-    DevkitBuildAngular: '^19.2.0+sha-4e4fb8a',
-    AngularBuild: '^19.2.0+sha-4e4fb8a',
-    AngularSSR: '^19.2.0+sha-4e4fb8a',
+    DevkitBuildAngular: '^19.2.0+sha-c073309',
+    AngularBuild: '^19.2.0+sha-c073309',
+    AngularSSR: '^19.2.0+sha-c073309',
 };
