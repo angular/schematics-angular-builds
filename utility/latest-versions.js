3920d2be221e97ab70d7309b2a11b5486db68418
Angular Builds <dev-infra@angular.dev>
2025-03-05 16:24:43 +0000

    3c91721 feat(@angular/build): integrate Chrome automatic  workspace folders

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.0+sha-896d98a',
-    AngularBuild: '^20.0.0-next.0+sha-896d98a',
-    AngularSSR: '^20.0.0-next.0+sha-896d98a',
+    DevkitBuildAngular: '^20.0.0-next.0+sha-3c91721',
+    AngularBuild: '^20.0.0-next.0+sha-3c91721',
+    AngularSSR: '^20.0.0-next.0+sha-3c91721',
 };
