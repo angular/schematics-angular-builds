b069172ebdb0af1b95762445a98955acbd6d57a3
Angular Builds <dev-infra@angular.dev>
2025-03-07 14:42:38 +0000

    beab546 fix(@angular/build): remove duplicate prebundling warning

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.0+sha-b5a8670',
-    AngularBuild: '^20.0.0-next.0+sha-b5a8670',
-    AngularSSR: '^20.0.0-next.0+sha-b5a8670',
+    DevkitBuildAngular: '^20.0.0-next.0+sha-beab546',
+    AngularBuild: '^20.0.0-next.0+sha-beab546',
+    AngularSSR: '^20.0.0-next.0+sha-beab546',
 };
