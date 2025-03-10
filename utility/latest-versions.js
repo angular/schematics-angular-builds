c676e696524c4f5e23132dafb0ea38aa0030b63e
Angular Builds <dev-infra@angular.dev>
2025-03-10 13:51:35 +0000

    3e35167 fix(@angular/build): handle postcss compilation errors gracefully

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.0+sha-596b9ae',
-    AngularBuild: '^20.0.0-next.0+sha-596b9ae',
-    AngularSSR: '^20.0.0-next.0+sha-596b9ae',
+    DevkitBuildAngular: '^20.0.0-next.0+sha-3e35167',
+    AngularBuild: '^20.0.0-next.0+sha-3e35167',
+    AngularSSR: '^20.0.0-next.0+sha-3e35167',
 };
