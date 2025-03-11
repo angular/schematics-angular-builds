ec6449bf89ce9f2a41c0a956d13c5d6cc0a9fd66
Angular Builds <dev-infra@angular.dev>
2025-03-11 14:33:28 +0000

    ee8466d fix(@angular/ssr): prevent stream draining if `write` does not return a boolean

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.0+sha-5ff4c28',
-    AngularBuild: '^20.0.0-next.0+sha-5ff4c28',
-    AngularSSR: '^20.0.0-next.0+sha-5ff4c28',
+    DevkitBuildAngular: '^20.0.0-next.0+sha-ee8466d',
+    AngularBuild: '^20.0.0-next.0+sha-ee8466d',
+    AngularSSR: '^20.0.0-next.0+sha-ee8466d',
 };
