b5e8dcec397ac83c04cf6eedc93900916eb75fb9
Angular Builds <dev-infra@angular.dev>
2025-03-11 09:13:39 +0000

    9dd3f03 Further clean-up `rules_nodejs` `npm` workspace and remove `yarn.lock`  (#29779)

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.0+sha-a05d93a',
-    AngularBuild: '^20.0.0-next.0+sha-a05d93a',
-    AngularSSR: '^20.0.0-next.0+sha-a05d93a',
+    DevkitBuildAngular: '^20.0.0-next.0+sha-9dd3f03',
+    AngularBuild: '^20.0.0-next.0+sha-9dd3f03',
+    AngularSSR: '^20.0.0-next.0+sha-9dd3f03',
 };
