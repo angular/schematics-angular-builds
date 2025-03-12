4634f82ee4c3f186354428d7b1b80df2ecc9c4ed
Angular Builds <dev-infra@angular.dev>
2025-03-12 16:45:09 +0000

    6136c6a build: update `ng-dev` to include latest pnpm release tool fixes

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.0+sha-23829b0',
-    AngularBuild: '^20.0.0-next.0+sha-23829b0',
-    AngularSSR: '^20.0.0-next.0+sha-23829b0',
+    DevkitBuildAngular: '^20.0.0-next.0+sha-6136c6a',
+    AngularBuild: '^20.0.0-next.0+sha-6136c6a',
+    AngularSSR: '^20.0.0-next.0+sha-6136c6a',
 };
