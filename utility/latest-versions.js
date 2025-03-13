ba5478ddf568b4f797f12417401dc056dba3c924
Angular Builds <dev-infra@angular.dev>
2025-03-13 13:58:49 +0000

    b9591eb docs(@angular-devkit/build-angular): update `forceEsbuild` option description

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.1+sha-09d3ced',
-    AngularBuild: '^20.0.0-next.1+sha-09d3ced',
-    AngularSSR: '^20.0.0-next.1+sha-09d3ced',
+    DevkitBuildAngular: '^20.0.0-next.1+sha-b9591eb',
+    AngularBuild: '^20.0.0-next.1+sha-b9591eb',
+    AngularSSR: '^20.0.0-next.1+sha-b9591eb',
 };
