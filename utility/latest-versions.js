559545bb2dea581df51e1092f924287f841ba212
Angular Builds <dev-infra@angular.dev>
2025-03-13 18:00:32 +0000

    f780e8b fix(@angular/build): ensure relative karma stack traces for test failures

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.1+sha-18e13e2',
-    AngularBuild: '^20.0.0-next.1+sha-18e13e2',
-    AngularSSR: '^20.0.0-next.1+sha-18e13e2',
+    DevkitBuildAngular: '^20.0.0-next.1+sha-f780e8b',
+    AngularBuild: '^20.0.0-next.1+sha-f780e8b',
+    AngularSSR: '^20.0.0-next.1+sha-f780e8b',
 };
