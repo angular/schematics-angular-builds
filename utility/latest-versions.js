651f4826d50f663542de25d647e5368b8ed45ce9
Angular Builds <dev-infra@angular.dev>
2025-03-14 14:48:49 +0000

    43706b3 docs: correct bazel e2e commands

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0',
     NgPackagr: '^19.2.0',
-    DevkitBuildAngular: '^19.2.3+sha-7b637fc',
-    AngularBuild: '^19.2.3+sha-7b637fc',
-    AngularSSR: '^19.2.3+sha-7b637fc',
+    DevkitBuildAngular: '^19.2.3+sha-43706b3',
+    AngularBuild: '^19.2.3+sha-43706b3',
+    AngularSSR: '^19.2.3+sha-43706b3',
 };
