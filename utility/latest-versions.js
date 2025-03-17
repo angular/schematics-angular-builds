e23b622fdd56e29bf255889b17faba4e1e4a4e85
Angular Builds <dev-infra@angular.dev>
2025-03-17 21:25:26 +0000

    e07491e build: move devkit core specific dependencies out of root

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.1+sha-e774476',
-    AngularBuild: '^20.0.0-next.1+sha-e774476',
-    AngularSSR: '^20.0.0-next.1+sha-e774476',
+    DevkitBuildAngular: '^20.0.0-next.1+sha-e07491e',
+    AngularBuild: '^20.0.0-next.1+sha-e07491e',
+    AngularSSR: '^20.0.0-next.1+sha-e07491e',
 };
