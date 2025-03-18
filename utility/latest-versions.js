fe6035687ba05a6a6529d816a2fe15084dd0a312
Angular Builds <dev-infra@angular.dev>
2025-03-18 14:00:58 +0000

    74d54a9 build: improve debugging mode of e2e test runner

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0',
     NgPackagr: '^19.2.0',
-    DevkitBuildAngular: '^19.2.3+sha-43706b3',
-    AngularBuild: '^19.2.3+sha-43706b3',
-    AngularSSR: '^19.2.3+sha-43706b3',
+    DevkitBuildAngular: '^19.2.3+sha-74d54a9',
+    AngularBuild: '^19.2.3+sha-74d54a9',
+    AngularSSR: '^19.2.3+sha-74d54a9',
 };
