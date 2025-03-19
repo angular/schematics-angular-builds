0ee29e111f8b9625790ddb921e7154ec3ff15015
Angular Builds <dev-infra@angular.dev>
2025-03-19 18:03:02 +0000

    d512635 test: correct test description typos

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.1+sha-5c0d76e',
-    AngularBuild: '^20.0.0-next.1+sha-5c0d76e',
-    AngularSSR: '^20.0.0-next.1+sha-5c0d76e',
+    DevkitBuildAngular: '^20.0.0-next.1+sha-d512635',
+    AngularBuild: '^20.0.0-next.1+sha-d512635',
+    AngularSSR: '^20.0.0-next.1+sha-d512635',
 };
