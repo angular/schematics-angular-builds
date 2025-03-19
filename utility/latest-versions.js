5ce37235cf24445dea8cdfc967d26750bfd2ba7d
Angular Builds <dev-infra@angular.dev>
2025-03-19 14:30:47 +0000

    689ab8a build: update development Node.js version to 22.14.0 and auto-update

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.1+sha-6473d10',
-    AngularBuild: '^20.0.0-next.1+sha-6473d10',
-    AngularSSR: '^20.0.0-next.1+sha-6473d10',
+    DevkitBuildAngular: '^20.0.0-next.1+sha-689ab8a',
+    AngularBuild: '^20.0.0-next.1+sha-689ab8a',
+    AngularSSR: '^20.0.0-next.1+sha-689ab8a',
 };
