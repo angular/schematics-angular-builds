3bb96d582f5c74ff10e7d34fb5f72c1f330fc704
Angular Builds <dev-infra@angular.dev>
2025-03-19 16:38:23 +0000

    b0b643e fix(@angular/build): ensure errors for missing component resources

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0',
     NgPackagr: '^19.2.0',
-    DevkitBuildAngular: '^19.2.3+sha-0a4e96b',
-    AngularBuild: '^19.2.3+sha-0a4e96b',
-    AngularSSR: '^19.2.3+sha-0a4e96b',
+    DevkitBuildAngular: '^19.2.3+sha-b0b643e',
+    AngularBuild: '^19.2.3+sha-b0b643e',
+    AngularSSR: '^19.2.3+sha-b0b643e',
 };
