ce816162ecb60e9f57f2a180e05f1de402571ff0
Angular Builds <dev-infra@angular.dev>
2025-03-19 14:58:54 +0000

    da6ef62 fix(@schematics/angular): ensure app-shell schematic consistently uses `withAppShell`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.1+sha-689ab8a',
-    AngularBuild: '^20.0.0-next.1+sha-689ab8a',
-    AngularSSR: '^20.0.0-next.1+sha-689ab8a',
+    DevkitBuildAngular: '^20.0.0-next.1+sha-da6ef62',
+    AngularBuild: '^20.0.0-next.1+sha-da6ef62',
+    AngularSSR: '^20.0.0-next.1+sha-da6ef62',
 };
