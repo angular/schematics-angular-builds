2625756bc641e6aae84a5a7f00f95a309fc3c0d6
Angular Builds <dev-infra@angular.dev>
2025-03-20 13:43:17 +0000

    9b682e6 feat(@angular/build): support a default outputPath option for applications

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.2+sha-156a14e',
-    AngularBuild: '^20.0.0-next.2+sha-156a14e',
-    AngularSSR: '^20.0.0-next.2+sha-156a14e',
+    DevkitBuildAngular: '^20.0.0-next.2+sha-9b682e6',
+    AngularBuild: '^20.0.0-next.2+sha-9b682e6',
+    AngularSSR: '^20.0.0-next.2+sha-9b682e6',
 };
