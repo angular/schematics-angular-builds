9635dda475c94f02aa6edd7fa99122632c6658d8
Angular Builds <dev-infra@angular.dev>
2025-03-20 13:19:01 +0000

    d89c2cb refactor: use `platformBrowser` instead of `platformBrowserTesting`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.2+sha-5169f21',
-    AngularBuild: '^20.0.0-next.2+sha-5169f21',
-    AngularSSR: '^20.0.0-next.2+sha-5169f21',
+    DevkitBuildAngular: '^20.0.0-next.2+sha-d89c2cb',
+    AngularBuild: '^20.0.0-next.2+sha-d89c2cb',
+    AngularSSR: '^20.0.0-next.2+sha-d89c2cb',
 };
