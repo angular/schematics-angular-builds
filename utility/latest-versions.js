2ec7d7f2eb37e0326b1bdff4f05ecd7ee4e6c098
Angular Builds <dev-infra@angular.dev>
2025-03-21 13:54:51 +0000

    9e6b9b5 fix(@schematics/angular): add type checking of host bindings to strict config

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.2+sha-4153bd8',
-    AngularBuild: '^20.0.0-next.2+sha-4153bd8',
-    AngularSSR: '^20.0.0-next.2+sha-4153bd8',
+    DevkitBuildAngular: '^20.0.0-next.2+sha-9e6b9b5',
+    AngularBuild: '^20.0.0-next.2+sha-9e6b9b5',
+    AngularSSR: '^20.0.0-next.2+sha-9e6b9b5',
 };
