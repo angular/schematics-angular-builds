645dfe150171621752a6ec96d428f0445c3c2369
Angular Builds <dev-infra@angular.dev>
2025-03-21 14:35:03 +0000

    6bd7b9b perf(@angular/ssr): optimized request handling performance

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.2+sha-a8817a3',
-    AngularBuild: '^20.0.0-next.2+sha-a8817a3',
-    AngularSSR: '^20.0.0-next.2+sha-a8817a3',
+    DevkitBuildAngular: '^20.0.0-next.2+sha-6bd7b9b',
+    AngularBuild: '^20.0.0-next.2+sha-6bd7b9b',
+    AngularSSR: '^20.0.0-next.2+sha-6bd7b9b',
 };
