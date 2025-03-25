761aeb882bdfda39fed611be5dad0bb84affc6c3
Angular Builds <dev-infra@angular.dev>
2025-03-25 08:10:13 +0000

    80a778a build: update devinfra digest to be69a70

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.2+sha-3f506f5',
-    AngularBuild: '^20.0.0-next.2+sha-3f506f5',
-    AngularSSR: '^20.0.0-next.2+sha-3f506f5',
+    DevkitBuildAngular: '^20.0.0-next.2+sha-80a778a',
+    AngularBuild: '^20.0.0-next.2+sha-80a778a',
+    AngularSSR: '^20.0.0-next.2+sha-80a778a',
 };
