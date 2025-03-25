1b617e2c6e2f946eee422f0822f6582ad74edd91
Angular Builds <dev-infra@angular.dev>
2025-03-25 16:42:04 +0000

    c1de633 feat(@angular/build): allow control of source map sources content for application builds

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.2+sha-80a778a',
-    AngularBuild: '^20.0.0-next.2+sha-80a778a',
-    AngularSSR: '^20.0.0-next.2+sha-80a778a',
+    DevkitBuildAngular: '^20.0.0-next.2+sha-c1de633',
+    AngularBuild: '^20.0.0-next.2+sha-c1de633',
+    AngularSSR: '^20.0.0-next.2+sha-c1de633',
 };
