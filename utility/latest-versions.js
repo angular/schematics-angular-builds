e1604ced58544da0e46befc9ec933cae1dfaa085
Angular Builds <dev-infra@angular.dev>
2025-03-26 08:37:30 +0000

    2b73b72 test: ensure atime and mtime are rounded consistently in tests

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.2+sha-1f62b28',
-    AngularBuild: '^20.0.0-next.2+sha-1f62b28',
-    AngularSSR: '^20.0.0-next.2+sha-1f62b28',
+    DevkitBuildAngular: '^20.0.0-next.2+sha-2b73b72',
+    AngularBuild: '^20.0.0-next.2+sha-2b73b72',
+    AngularSSR: '^20.0.0-next.2+sha-2b73b72',
 };
