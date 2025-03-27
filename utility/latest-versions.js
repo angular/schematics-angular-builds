62a69eae7331745ddc5ee316f31a205b533bae1b
Angular Builds <dev-infra@angular.dev>
2025-03-27 09:19:07 +0000

    52fbffc fix(@angular/build): warn and remove jsdom launcher when used with karma

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.3+sha-25514b8',
-    AngularBuild: '^20.0.0-next.3+sha-25514b8',
-    AngularSSR: '^20.0.0-next.3+sha-25514b8',
+    DevkitBuildAngular: '^20.0.0-next.3+sha-52fbffc',
+    AngularBuild: '^20.0.0-next.3+sha-52fbffc',
+    AngularSSR: '^20.0.0-next.3+sha-52fbffc',
 };
