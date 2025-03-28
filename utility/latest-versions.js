9972a50443ef67ee24c39820fb13ec88f780c936
Angular Builds <dev-infra@angular.dev>
2025-03-28 13:08:39 +0000

    a9aa317 refactor(@angular/cli): avoid implicit undefined array defaults for `@angular/build` builders

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.3+sha-2703f0a',
-    AngularBuild: '^20.0.0-next.3+sha-2703f0a',
-    AngularSSR: '^20.0.0-next.3+sha-2703f0a',
+    DevkitBuildAngular: '^20.0.0-next.3+sha-a9aa317',
+    AngularBuild: '^20.0.0-next.3+sha-a9aa317',
+    AngularSSR: '^20.0.0-next.3+sha-a9aa317',
 };
