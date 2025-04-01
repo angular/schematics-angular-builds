e2e8eca9317170a0d28d2298cf48fcbebf3abc28
Angular Builds <dev-infra@angular.dev>
2025-04-01 14:13:06 +0000

    da7063f refactor(@angular/build): provide a pre-link resolution condition for APF

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.3+sha-4955ee0',
-    AngularBuild: '^20.0.0-next.3+sha-4955ee0',
-    AngularSSR: '^20.0.0-next.3+sha-4955ee0',
+    DevkitBuildAngular: '^20.0.0-next.3+sha-da7063f',
+    AngularBuild: '^20.0.0-next.3+sha-da7063f',
+    AngularSSR: '^20.0.0-next.3+sha-da7063f',
 };
