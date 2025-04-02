58db8420642f371d5809fb7dafcc9c2f40cf847d
Angular Builds <circleci@angular.io>
2025-04-02 13:37:38 +0000

    0525fec fix(@angular-devkit/build-angular): update vite to 5.4.16 due to a security issues

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^17.3.14+sha-c52d751',
-    AngularSSR: '^17.3.14+sha-c52d751',
+    DevkitBuildAngular: '^17.3.14+sha-0525fec',
+    AngularSSR: '^17.3.14+sha-0525fec',
 };
