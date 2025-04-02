64574d16a761542768574a16f8185773162b2d31
Angular Builds <dev-infra@angular.dev>
2025-04-02 14:27:24 +0000

    1e137ca feat(@schematics/angular): add migration to update `moduleResolution` to `bundler`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.3+sha-296873c',
-    AngularBuild: '^20.0.0-next.3+sha-296873c',
-    AngularSSR: '^20.0.0-next.3+sha-296873c',
+    DevkitBuildAngular: '^20.0.0-next.3+sha-1e137ca',
+    AngularBuild: '^20.0.0-next.3+sha-1e137ca',
+    AngularSSR: '^20.0.0-next.3+sha-1e137ca',
 };
