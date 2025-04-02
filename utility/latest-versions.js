ab887d6aaae88514c920e894c14c67b8c1d16507
Angular Builds <dev-infra@angular.dev>
2025-04-02 14:52:53 +0000

    34307e9 build: update dependency @types/picomatch to v4

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.3+sha-1e137ca',
-    AngularBuild: '^20.0.0-next.3+sha-1e137ca',
-    AngularSSR: '^20.0.0-next.3+sha-1e137ca',
+    DevkitBuildAngular: '^20.0.0-next.3+sha-34307e9',
+    AngularBuild: '^20.0.0-next.3+sha-34307e9',
+    AngularSSR: '^20.0.0-next.3+sha-34307e9',
 };
