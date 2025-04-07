e6db75a726cc49a64f41fc93cdf94bbccac5fa01
Angular Builds <dev-infra@angular.dev>
2025-04-07 11:26:40 +0000

    df85980 refactor(@schematics/angular): use `generateFromFiles` helper in even more cases

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.4+sha-c8c46ab',
-    AngularBuild: '^20.0.0-next.4+sha-c8c46ab',
-    AngularSSR: '^20.0.0-next.4+sha-c8c46ab',
+    DevkitBuildAngular: '^20.0.0-next.4+sha-df85980',
+    AngularBuild: '^20.0.0-next.4+sha-df85980',
+    AngularSSR: '^20.0.0-next.4+sha-df85980',
 };
