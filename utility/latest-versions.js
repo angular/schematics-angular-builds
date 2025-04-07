e825c0185a9dff80b31d1f610f56eeba0f35067c
Angular Builds <dev-infra@angular.dev>
2025-04-07 10:02:00 +0000

    c8c46ab build: update dependency typescript to v5.8.3

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.4+sha-319b8e0',
-    AngularBuild: '^20.0.0-next.4+sha-319b8e0',
-    AngularSSR: '^20.0.0-next.4+sha-319b8e0',
+    DevkitBuildAngular: '^20.0.0-next.4+sha-c8c46ab',
+    AngularBuild: '^20.0.0-next.4+sha-c8c46ab',
+    AngularSSR: '^20.0.0-next.4+sha-c8c46ab',
 };
