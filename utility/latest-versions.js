7439ce24a6060488cfc2dbca2dcaa89b12e6b29e
Angular Builds <dev-infra@angular.dev>
2025-04-08 00:15:46 +0000

    415165e build: update webpack to v5.99.1

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.4+sha-fc0e05f',
-    AngularBuild: '^20.0.0-next.4+sha-fc0e05f',
-    AngularSSR: '^20.0.0-next.4+sha-fc0e05f',
+    DevkitBuildAngular: '^20.0.0-next.4+sha-415165e',
+    AngularBuild: '^20.0.0-next.4+sha-415165e',
+    AngularSSR: '^20.0.0-next.4+sha-415165e',
 };
