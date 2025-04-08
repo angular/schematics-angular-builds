e1a1dfeabd8759f1087b5f74a25d4dfc9c3d2cdc
Angular Builds <dev-infra@angular.dev>
2025-04-08 15:16:06 +0000

    74f70af build: remove unneeded `rollup-plugin-sourcemaps` dependency from root

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.4+sha-fdc6291',
-    AngularBuild: '^20.0.0-next.4+sha-fdc6291',
-    AngularSSR: '^20.0.0-next.4+sha-fdc6291',
+    DevkitBuildAngular: '^20.0.0-next.4+sha-74f70af',
+    AngularBuild: '^20.0.0-next.4+sha-74f70af',
+    AngularSSR: '^20.0.0-next.4+sha-74f70af',
 };
