73488ebab6460a9a8066d92ce1c9e7208fef35b0
Angular Builds <dev-infra@angular.dev>
2025-04-09 08:18:35 +0000

    eeba3a8 refactor: add missing peer depedency on `@angular/core` and `tslib`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.4+sha-ee2b4cb',
-    AngularBuild: '^20.0.0-next.4+sha-ee2b4cb',
-    AngularSSR: '^20.0.0-next.4+sha-ee2b4cb',
+    DevkitBuildAngular: '^20.0.0-next.4+sha-eeba3a8',
+    AngularBuild: '^20.0.0-next.4+sha-eeba3a8',
+    AngularSSR: '^20.0.0-next.4+sha-eeba3a8',
 };
