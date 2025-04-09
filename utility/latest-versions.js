9b41d9e02db855706e7d5825665d014dc5dc80b8
Angular Builds <dev-infra@angular.dev>
2025-04-09 00:07:05 +0000

    0a84d6c build: update github/codeql-action action to v3.28.15

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.4+sha-a175d28',
-    AngularBuild: '^20.0.0-next.4+sha-a175d28',
-    AngularSSR: '^20.0.0-next.4+sha-a175d28',
+    DevkitBuildAngular: '^20.0.0-next.4+sha-0a84d6c',
+    AngularBuild: '^20.0.0-next.4+sha-0a84d6c',
+    AngularSSR: '^20.0.0-next.4+sha-0a84d6c',
 };
