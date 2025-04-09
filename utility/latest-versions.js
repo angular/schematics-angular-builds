eb0e3734be3d65a11ad087b314d9bace7f0ba4f9
Angular Builds <dev-infra@angular.dev>
2025-04-09 18:47:54 +0000

    60cf465 docs: release notes for the v19.2.7 release

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.4+sha-427bd84',
-    AngularBuild: '^20.0.0-next.4+sha-427bd84',
-    AngularSSR: '^20.0.0-next.4+sha-427bd84',
+    DevkitBuildAngular: '^20.0.0-next.4+sha-60cf465',
+    AngularBuild: '^20.0.0-next.4+sha-60cf465',
+    AngularSSR: '^20.0.0-next.4+sha-60cf465',
 };
