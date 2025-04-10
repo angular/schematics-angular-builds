6bf8d3b2219e85750de19d9654c740e988b0e5af
Angular Builds <dev-infra@angular.dev>
2025-04-10 16:45:02 +0000

    6e6315d fix(@angular/build): properly resolve transitive external dependencies in vite-dev-server

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.5+sha-f669657',
-    AngularBuild: '^20.0.0-next.5+sha-f669657',
-    AngularSSR: '^20.0.0-next.5+sha-f669657',
+    DevkitBuildAngular: '^20.0.0-next.5+sha-6e6315d',
+    AngularBuild: '^20.0.0-next.5+sha-6e6315d',
+    AngularSSR: '^20.0.0-next.5+sha-6e6315d',
 };
