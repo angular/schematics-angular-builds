3c4d1495863c784d8bc33e44096a9a304da79116
Angular Builds <dev-infra@angular.dev>
2025-04-16 15:25:05 +0000

    95d16dc fix(@angular/build): pass `preserveSymlinks` option to Karma esbuild builder

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.5+sha-be6f13e',
-    AngularBuild: '^20.0.0-next.5+sha-be6f13e',
-    AngularSSR: '^20.0.0-next.5+sha-be6f13e',
+    DevkitBuildAngular: '^20.0.0-next.5+sha-95d16dc',
+    AngularBuild: '^20.0.0-next.5+sha-95d16dc',
+    AngularSSR: '^20.0.0-next.5+sha-95d16dc',
 };
