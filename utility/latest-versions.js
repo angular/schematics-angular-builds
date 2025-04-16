5a53d1260997cf05421d3500ddb0563ab90c1efe
Angular Builds <dev-infra@angular.dev>
2025-04-16 07:14:25 +0000

    11d1f2e ci: re-enable updates for `@types/express`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.5+sha-5078c8b',
-    AngularBuild: '^20.0.0-next.5+sha-5078c8b',
-    AngularSSR: '^20.0.0-next.5+sha-5078c8b',
+    DevkitBuildAngular: '^20.0.0-next.5+sha-11d1f2e',
+    AngularBuild: '^20.0.0-next.5+sha-11d1f2e',
+    AngularSSR: '^20.0.0-next.5+sha-11d1f2e',
 };
