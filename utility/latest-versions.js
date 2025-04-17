7e4adca5c79e7a7dbde39a46395b805f6776bc1d
Angular Builds <dev-infra@angular.dev>
2025-04-17 16:10:38 +0000

    36d2644 refactor: replace usage of ng_package with new ng_package from rules_angular

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0',
     NgPackagr: '^19.2.0',
-    DevkitBuildAngular: '^19.2.8+sha-cc5229a',
-    AngularBuild: '^19.2.8+sha-cc5229a',
-    AngularSSR: '^19.2.8+sha-cc5229a',
+    DevkitBuildAngular: '^19.2.8+sha-36d2644',
+    AngularBuild: '^19.2.8+sha-36d2644',
+    AngularSSR: '^19.2.8+sha-36d2644',
 };
