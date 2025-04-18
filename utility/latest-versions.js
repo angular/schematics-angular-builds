5868f08a0b597c856dc8acbb5efa06d156a8e31b
Angular Builds <circleci@angular.dev>
2025-04-18 10:57:49 +0000

    01cc617 fix(@angular-devkit/build-angular): update http-proxy-middleware to v3.0.5

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.18+sha-2b1dc21',
-    AngularSSR: '^18.2.18+sha-2b1dc21',
+    DevkitBuildAngular: '^18.2.18+sha-01cc617',
+    AngularSSR: '^18.2.18+sha-01cc617',
 };
