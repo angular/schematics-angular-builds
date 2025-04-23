3277802ea0413959c15138da72926b1ea08677f3
Angular Builds <dev-infra@angular.dev>
2025-04-23 06:31:00 +0000

    e03f2b8 feat(@schematics/angular): Add global error listeners to new app generation

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.6+sha-566de64',
-    AngularBuild: '^20.0.0-next.6+sha-566de64',
-    AngularSSR: '^20.0.0-next.6+sha-566de64',
+    DevkitBuildAngular: '^20.0.0-next.6+sha-e03f2b8',
+    AngularBuild: '^20.0.0-next.6+sha-e03f2b8',
+    AngularSSR: '^20.0.0-next.6+sha-e03f2b8',
 };
