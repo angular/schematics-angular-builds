81894fe96eebd984934b7ab2fbbfb3b515d66000
Angular Builds <dev-infra@angular.dev>
2025-04-23 13:37:11 +0000

    12def3a feat(@angular/build): add experimental vitest unit-testing support

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.7+sha-0ab1ddf',
-    AngularBuild: '^20.0.0-next.7+sha-0ab1ddf',
-    AngularSSR: '^20.0.0-next.7+sha-0ab1ddf',
+    DevkitBuildAngular: '^20.0.0-next.7+sha-12def3a',
+    AngularBuild: '^20.0.0-next.7+sha-12def3a',
+    AngularSSR: '^20.0.0-next.7+sha-12def3a',
 };
