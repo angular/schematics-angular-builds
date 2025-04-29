3672ebe4741257c9aa94668920adf66904737eb7
Angular Builds <dev-infra@angular.dev>
2025-04-29 16:51:03 +0000

    95554fd refactor(@angular/build): use utility helper to get project root paths

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.8+sha-6b46e5b',
-    AngularBuild: '^20.0.0-next.8+sha-6b46e5b',
-    AngularSSR: '^20.0.0-next.8+sha-6b46e5b',
+    DevkitBuildAngular: '^20.0.0-next.8+sha-95554fd',
+    AngularBuild: '^20.0.0-next.8+sha-95554fd',
+    AngularSSR: '^20.0.0-next.8+sha-95554fd',
 };
