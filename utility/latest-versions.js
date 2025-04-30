62dc766154e5948ffaaabf040a087c3882aaf501
Angular Builds <dev-infra@angular.dev>
2025-04-30 09:51:21 +0000

    1a7d52c ci: setup bazel repository caching for windows jobs

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.8+sha-52839c5',
-    AngularBuild: '^20.0.0-next.8+sha-52839c5',
-    AngularSSR: '^20.0.0-next.8+sha-52839c5',
+    DevkitBuildAngular: '^20.0.0-next.8+sha-1a7d52c',
+    AngularBuild: '^20.0.0-next.8+sha-1a7d52c',
+    AngularSSR: '^20.0.0-next.8+sha-1a7d52c',
 };
