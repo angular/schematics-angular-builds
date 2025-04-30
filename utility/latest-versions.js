b7246ac806e46f3abb438123b5dc566e0729d240
Angular Builds <dev-infra@angular.dev>
2025-04-30 07:23:56 +0000

    b5b6737 build: fix `bazel run` invocations of `js_binary/js_test`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.8+sha-5ae23f6',
-    AngularBuild: '^20.0.0-next.8+sha-5ae23f6',
-    AngularSSR: '^20.0.0-next.8+sha-5ae23f6',
+    DevkitBuildAngular: '^20.0.0-next.8+sha-b5b6737',
+    AngularBuild: '^20.0.0-next.8+sha-b5b6737',
+    AngularSSR: '^20.0.0-next.8+sha-b5b6737',
 };
