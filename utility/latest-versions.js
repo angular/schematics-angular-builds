bd256eaa7e78aac8c0f51013923246cee051ed5b
Angular Builds <dev-infra@angular.dev>
2025-04-30 15:31:34 +0000

    c557a19 fix(@schematics/angular): avoid empty polyfill option for new zoneless application

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.8+sha-100571f',
-    AngularBuild: '^20.0.0-next.8+sha-100571f',
-    AngularSSR: '^20.0.0-next.8+sha-100571f',
+    DevkitBuildAngular: '^20.0.0-next.8+sha-c557a19',
+    AngularBuild: '^20.0.0-next.8+sha-c557a19',
+    AngularSSR: '^20.0.0-next.8+sha-c557a19',
 };
