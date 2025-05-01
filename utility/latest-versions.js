8c4282384d75d9be5a1941c2fecd6fd25caf6da4
Angular Builds <dev-infra@angular.dev>
2025-05-01 17:08:13 +0000

    7bb1f87 fix(@angular/build): enable unit-test reporters option

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.9+sha-2b28ecd',
-    AngularBuild: '^20.0.0-next.9+sha-2b28ecd',
-    AngularSSR: '^20.0.0-next.9+sha-2b28ecd',
+    DevkitBuildAngular: '^20.0.0-next.9+sha-7bb1f87',
+    AngularBuild: '^20.0.0-next.9+sha-7bb1f87',
+    AngularSSR: '^20.0.0-next.9+sha-7bb1f87',
 };
