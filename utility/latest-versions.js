414e02a36ff0af94ee27bbf5dec7d08762ec5efc
Angular Builds <dev-infra@angular.dev>
2025-05-02 20:21:22 +0000

    0695531 ci: try simplify windows test preparation with bazel 7

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0',
     NgPackagr: '^19.2.0',
-    DevkitBuildAngular: '^19.2.10+sha-36e911b',
-    AngularBuild: '^19.2.10+sha-36e911b',
-    AngularSSR: '^19.2.10+sha-36e911b',
+    DevkitBuildAngular: '^19.2.10+sha-0695531',
+    AngularBuild: '^19.2.10+sha-0695531',
+    AngularSSR: '^19.2.10+sha-0695531',
 };
