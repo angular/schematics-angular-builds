5bc3f47b04971ca87d0a70fd9162de359cf43af9
Angular Builds <dev-infra@angular.dev>
2025-05-02 20:31:07 +0000

    ba844ae fix(@angular/build): avoid internal karma request cache for assets

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.9+sha-c8ceeca',
-    AngularBuild: '^20.0.0-next.9+sha-c8ceeca',
-    AngularSSR: '^20.0.0-next.9+sha-c8ceeca',
+    DevkitBuildAngular: '^20.0.0-next.9+sha-ba844ae',
+    AngularBuild: '^20.0.0-next.9+sha-ba844ae',
+    AngularSSR: '^20.0.0-next.9+sha-ba844ae',
 };
