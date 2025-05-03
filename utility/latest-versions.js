04333bbc1d75da5f79386d25632ce2dfdd835741
Angular Builds <dev-infra@angular.dev>
2025-05-03 16:11:12 +0000

    f73e3a3 perf(@angular/build): fix unnecessary esbuild rebuilds

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-90d1db3',
-    AngularBuild: '^20.1.0-next.0+sha-90d1db3',
-    AngularSSR: '^20.1.0-next.0+sha-90d1db3',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-f73e3a3',
+    AngularBuild: '^20.1.0-next.0+sha-f73e3a3',
+    AngularSSR: '^20.1.0-next.0+sha-f73e3a3',
 };
