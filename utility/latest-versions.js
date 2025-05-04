23f4e3082d5986a1f1e400d361d8ef7e811f350f
Angular Builds <dev-infra@angular.dev>
2025-05-04 10:16:00 +0000

    7170f40 build: improve stability of windows jobs further

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-f73e3a3',
-    AngularBuild: '^20.1.0-next.0+sha-f73e3a3',
-    AngularSSR: '^20.1.0-next.0+sha-f73e3a3',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-7170f40',
+    AngularBuild: '^20.1.0-next.0+sha-7170f40',
+    AngularSSR: '^20.1.0-next.0+sha-7170f40',
 };
