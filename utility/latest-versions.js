9a9372e94ae28a147661eb5d8c7640a9251bcb57
Angular Builds <dev-infra@angular.dev>
2025-05-05 13:12:15 +0000

    3fb0e3e refactor: update `Piscina` to version 5

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.9+sha-ce03490',
-    AngularBuild: '^20.0.0-next.9+sha-ce03490',
-    AngularSSR: '^20.0.0-next.9+sha-ce03490',
+    DevkitBuildAngular: '^20.0.0-next.9+sha-3fb0e3e',
+    AngularBuild: '^20.0.0-next.9+sha-3fb0e3e',
+    AngularSSR: '^20.0.0-next.9+sha-3fb0e3e',
 };
