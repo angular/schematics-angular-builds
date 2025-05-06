8dc741830004b9f998413d6302c1b1fa220b26a4
Angular Builds <dev-infra@angular.dev>
2025-05-06 11:19:53 +0000

    635999d fix(@angular-devkit/build-angular): correctly set i18n subPath in webpack browser builder

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.9+sha-888d0b2',
-    AngularBuild: '^20.0.0-next.9+sha-888d0b2',
-    AngularSSR: '^20.0.0-next.9+sha-888d0b2',
+    DevkitBuildAngular: '^20.0.0-next.9+sha-635999d',
+    AngularBuild: '^20.0.0-next.9+sha-635999d',
+    AngularSSR: '^20.0.0-next.9+sha-635999d',
 };
