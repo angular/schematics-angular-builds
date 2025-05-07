d9cf946f20d6d2bded9d7c7633879b0fdb4847e2
Angular Builds <dev-infra@angular.dev>
2025-05-07 17:15:29 +0000

    f1213e3 ci: enable e2e test runs on Node.js 24

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-next.9+sha-a3e35ee',
-    AngularBuild: '^20.0.0-next.9+sha-a3e35ee',
-    AngularSSR: '^20.0.0-next.9+sha-a3e35ee',
+    DevkitBuildAngular: '^20.0.0-next.9+sha-f1213e3',
+    AngularBuild: '^20.0.0-next.9+sha-f1213e3',
+    AngularSSR: '^20.0.0-next.9+sha-f1213e3',
 };
