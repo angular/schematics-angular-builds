70bc59463535c890533a7e3987758fee9f6a77e8
Angular Builds <dev-infra@angular.dev>
2025-05-07 16:24:56 +0000

    16ae68a refactor(@angular-devkit/core): use more precise iterator typings

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-24453e7',
-    AngularBuild: '^20.1.0-next.0+sha-24453e7',
-    AngularSSR: '^20.1.0-next.0+sha-24453e7',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-16ae68a',
+    AngularBuild: '^20.1.0-next.0+sha-16ae68a',
+    AngularSSR: '^20.1.0-next.0+sha-16ae68a',
 };
