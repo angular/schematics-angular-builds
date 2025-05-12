ab79527fe52a01aa9e027baebd0567cf4504c62c
Angular Builds <dev-infra@angular.dev>
2025-05-12 08:11:59 +0000

    259f8c5 ci: refactor renovate config to use dev-infra preset

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-505c836',
-    AngularBuild: '^20.1.0-next.0+sha-505c836',
-    AngularSSR: '^20.1.0-next.0+sha-505c836',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-259f8c5',
+    AngularBuild: '^20.1.0-next.0+sha-259f8c5',
+    AngularSSR: '^20.1.0-next.0+sha-259f8c5',
 };
