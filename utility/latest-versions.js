1bcd5bdbb7e9d88059e9420cc511c927037817a5
Angular Builds <dev-infra@angular.dev>
2025-05-12 13:39:54 +0000

    e36bf96 fix(@angular/build): allow TestBed provider configuration with vitest unit-testing

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-rc.0+sha-0af4e10',
-    AngularBuild: '^20.0.0-rc.0+sha-0af4e10',
-    AngularSSR: '^20.0.0-rc.0+sha-0af4e10',
+    DevkitBuildAngular: '^20.0.0-rc.0+sha-e36bf96',
+    AngularBuild: '^20.0.0-rc.0+sha-e36bf96',
+    AngularSSR: '^20.0.0-rc.0+sha-e36bf96',
 };
