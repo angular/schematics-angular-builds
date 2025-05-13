dc69ccb80c6c88b782e26cf4a9998aff249e304f
Angular Builds <dev-infra@angular.dev>
2025-05-13 17:20:30 +0000

    7b8c9af fix(@angular/build): provide vitest globals in unit-test builder

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-9d3071d',
-    AngularBuild: '^20.1.0-next.0+sha-9d3071d',
-    AngularSSR: '^20.1.0-next.0+sha-9d3071d',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-7b8c9af',
+    AngularBuild: '^20.1.0-next.0+sha-7b8c9af',
+    AngularSSR: '^20.1.0-next.0+sha-7b8c9af',
 };
