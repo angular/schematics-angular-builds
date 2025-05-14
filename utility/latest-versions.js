a8a6ebc0e1b4845c6013a4e48c0293a9a958793d
Angular Builds <dev-infra@angular.dev>
2025-05-14 16:38:32 +0000

    07ac174 fix(@schematics/angular): support using default browser option when not present

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-9d07249',
-    AngularBuild: '^20.1.0-next.0+sha-9d07249',
-    AngularSSR: '^20.1.0-next.0+sha-9d07249',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-07ac174',
+    AngularBuild: '^20.1.0-next.0+sha-07ac174',
+    AngularSSR: '^20.1.0-next.0+sha-07ac174',
 };
