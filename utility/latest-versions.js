7c3b2b5d2e5a8ff45aa39b2794c9a08716546a35
Angular Builds <dev-infra@angular.dev>
2025-05-14 19:48:49 +0000

    d2bfc6b fix(@angular/build): revert setup unit-test polyfills before TestBed init

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-rc.1+sha-c2030ed',
-    AngularBuild: '^20.0.0-rc.1+sha-c2030ed',
-    AngularSSR: '^20.0.0-rc.1+sha-c2030ed',
+    DevkitBuildAngular: '^20.0.0-rc.1+sha-d2bfc6b',
+    AngularBuild: '^20.0.0-rc.1+sha-d2bfc6b',
+    AngularSSR: '^20.0.0-rc.1+sha-d2bfc6b',
 };
