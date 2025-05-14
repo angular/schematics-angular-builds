a27679f96cb19b3d874d35f43c797ce14ebae39c
Angular Builds <dev-infra@angular.dev>
2025-05-14 13:37:40 +0000

    9d07249 build: update dependency terser to v5.39.1

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-8b2ca7a',
-    AngularBuild: '^20.1.0-next.0+sha-8b2ca7a',
-    AngularSSR: '^20.1.0-next.0+sha-8b2ca7a',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-9d07249',
+    AngularBuild: '^20.1.0-next.0+sha-9d07249',
+    AngularSSR: '^20.1.0-next.0+sha-9d07249',
 };
