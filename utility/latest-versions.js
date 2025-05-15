7afa88ff216917af4e51a5356f9935785fbb9642
Angular Builds <dev-infra@angular.dev>
2025-05-15 13:28:23 +0000

    9c29cf7 refactor: remove unused function

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-rc.2+sha-a5caafd',
-    AngularBuild: '^20.0.0-rc.2+sha-a5caafd',
-    AngularSSR: '^20.0.0-rc.2+sha-a5caafd',
+    DevkitBuildAngular: '^20.0.0-rc.2+sha-9c29cf7',
+    AngularBuild: '^20.0.0-rc.2+sha-9c29cf7',
+    AngularSSR: '^20.0.0-rc.2+sha-9c29cf7',
 };
