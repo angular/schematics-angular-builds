30e89447d79563b66fe26df11c7fe5a75a27de58
Angular Builds <dev-infra@angular.dev>
2025-05-16 02:13:47 +0000

    b155ba1 fix(@angular/build): enable unit-test builder watch outside CI

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-rc.2+sha-059c10e',
-    AngularBuild: '^20.0.0-rc.2+sha-059c10e',
-    AngularSSR: '^20.0.0-rc.2+sha-059c10e',
+    DevkitBuildAngular: '^20.0.0-rc.2+sha-b155ba1',
+    AngularBuild: '^20.0.0-rc.2+sha-b155ba1',
+    AngularSSR: '^20.0.0-rc.2+sha-b155ba1',
 };
