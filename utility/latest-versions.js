0ae6bfa97770d7202337c39cda54ecc7319961d0
Angular Builds <dev-infra@angular.dev>
2025-05-16 13:20:08 +0000

    0b4e3f3 refactor: disable chrome sandbox as it no longer works in CI

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-85e6a86',
-    AngularBuild: '^20.1.0-next.0+sha-85e6a86',
-    AngularSSR: '^20.1.0-next.0+sha-85e6a86',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-0b4e3f3',
+    AngularBuild: '^20.1.0-next.0+sha-0b4e3f3',
+    AngularSSR: '^20.1.0-next.0+sha-0b4e3f3',
 };
