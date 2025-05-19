22f9c623eb647debd58f740426d7a4987094af93
Angular Builds <dev-infra@angular.dev>
2025-05-19 12:44:03 +0000

    5b43174 fix(@angular/pwa): remove `background_color` and `theme_color` from manifest

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-rc.2+sha-b264517',
-    AngularBuild: '^20.0.0-rc.2+sha-b264517',
-    AngularSSR: '^20.0.0-rc.2+sha-b264517',
+    DevkitBuildAngular: '^20.0.0-rc.2+sha-5b43174',
+    AngularBuild: '^20.0.0-rc.2+sha-5b43174',
+    AngularSSR: '^20.0.0-rc.2+sha-5b43174',
 };
