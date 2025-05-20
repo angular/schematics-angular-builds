9a354d62dfa65966926ae3f39da2d6faa0b5e90d
Angular Builds <dev-infra@angular.dev>
2025-05-20 15:51:49 +0000

    a42e045 fix(@angular-devkit/build-angular): respect i18nDuplicateTranslation option when duplicates exist

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-rc.2+sha-36c2a82',
-    AngularBuild: '^20.0.0-rc.2+sha-36c2a82',
-    AngularSSR: '^20.0.0-rc.2+sha-36c2a82',
+    DevkitBuildAngular: '^20.0.0-rc.2+sha-a42e045',
+    AngularBuild: '^20.0.0-rc.2+sha-a42e045',
+    AngularSSR: '^20.0.0-rc.2+sha-a42e045',
 };
