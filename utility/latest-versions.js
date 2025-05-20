33327ec250ab66d59726af479b1df88f0f0e57fa
Angular Builds <dev-infra@angular.dev>
2025-05-20 12:39:19 +0000

    787e510 fix(@angular/cli): update min Node.js support to 20.19, 22.12, and 24.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-rc.2+sha-aec9504',
-    AngularBuild: '^20.0.0-rc.2+sha-aec9504',
-    AngularSSR: '^20.0.0-rc.2+sha-aec9504',
+    DevkitBuildAngular: '^20.0.0-rc.2+sha-787e510',
+    AngularBuild: '^20.0.0-rc.2+sha-787e510',
+    AngularSSR: '^20.0.0-rc.2+sha-787e510',
 };
