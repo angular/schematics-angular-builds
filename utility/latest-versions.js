21af09748a7a320b9a03d5070bee8c46a22f0acf
Angular Builds <dev-infra@angular.dev>
2025-05-21 12:29:29 +0000

    e50d03d build: update dependency watchpack to v2.4.4

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-a9021ae',
-    AngularBuild: '^20.1.0-next.0+sha-a9021ae',
-    AngularSSR: '^20.1.0-next.0+sha-a9021ae',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-e50d03d',
+    AngularBuild: '^20.1.0-next.0+sha-e50d03d',
+    AngularSSR: '^20.1.0-next.0+sha-e50d03d',
 };
