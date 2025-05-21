c0de1fe18118ee8ff5adf2923cc77f38bb756cd5
Angular Builds <dev-infra@angular.dev>
2025-05-21 14:07:32 +0000

    b776d32 refactor(@angular/build): check that AOT is enabled in HMR logic

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-rc.3+sha-be8cfba',
-    AngularBuild: '^20.0.0-rc.3+sha-be8cfba',
-    AngularSSR: '^20.0.0-rc.3+sha-be8cfba',
+    DevkitBuildAngular: '^20.0.0-rc.3+sha-b776d32',
+    AngularBuild: '^20.0.0-rc.3+sha-b776d32',
+    AngularSSR: '^20.0.0-rc.3+sha-b776d32',
 };
