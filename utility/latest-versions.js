81d11b1f23467a1a932ad3e7eda7ff38c2d050a8
Angular Builds <dev-infra@angular.dev>
2025-05-21 14:49:49 +0000

    a3504fd fix(@angular/build): HMR requires AOT do not show HMR enabled when using JIT

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0',
     NgPackagr: '^19.2.0',
-    DevkitBuildAngular: '^19.2.13+sha-6336f9c',
-    AngularBuild: '^19.2.13+sha-6336f9c',
-    AngularSSR: '^19.2.13+sha-6336f9c',
+    DevkitBuildAngular: '^19.2.13+sha-a3504fd',
+    AngularBuild: '^19.2.13+sha-a3504fd',
+    AngularSSR: '^19.2.13+sha-a3504fd',
 };
