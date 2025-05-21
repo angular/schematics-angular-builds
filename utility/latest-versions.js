67ed9458039eb52b621b4e9469694c42afb4b0c7
Angular Builds <dev-infra@angular.dev>
2025-05-21 20:45:58 +0000

    5ce9f96 fix(@angular/build): include full metadata for AOT unit-testing

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0',
     NgPackagr: '^19.2.0',
-    DevkitBuildAngular: '^19.2.13+sha-a3504fd',
-    AngularBuild: '^19.2.13+sha-a3504fd',
-    AngularSSR: '^19.2.13+sha-a3504fd',
+    DevkitBuildAngular: '^19.2.13+sha-5ce9f96',
+    AngularBuild: '^19.2.13+sha-5ce9f96',
+    AngularSSR: '^19.2.13+sha-5ce9f96',
 };
