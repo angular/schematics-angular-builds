569faf583a43fef8edaf2c0a64f3a2e9bc4a7150
Angular Builds <dev-infra@angular.dev>
2025-05-22 18:05:00 +0000

    dfeb37f perf(@angular/build): directly check code for Angular partial linking

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-233495f',
-    AngularBuild: '^20.1.0-next.0+sha-233495f',
-    AngularSSR: '^20.1.0-next.0+sha-233495f',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-dfeb37f',
+    AngularBuild: '^20.1.0-next.0+sha-dfeb37f',
+    AngularSSR: '^20.1.0-next.0+sha-dfeb37f',
 };
