ea47ad9f1169bd91e2a869a7f7e9c9f13852a5c5
Angular Builds <dev-infra@angular.dev>
2025-05-22 20:14:15 +0000

    eed60d1 refactor(@angular/build): update `ensureWorkerPool` to do an early exit

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-dfeb37f',
-    AngularBuild: '^20.1.0-next.0+sha-dfeb37f',
-    AngularSSR: '^20.1.0-next.0+sha-dfeb37f',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-eed60d1',
+    AngularBuild: '^20.1.0-next.0+sha-eed60d1',
+    AngularSSR: '^20.1.0-next.0+sha-eed60d1',
 };
