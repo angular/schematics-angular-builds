7687c7c29375fc25193dcfbc0077723136777f42
Angular Builds <dev-infra@angular.dev>
2025-05-22 11:31:57 +0000

    233495f refactor(@angular/cli): adjust type usage for eventual ESM building

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-bd9e4a4',
-    AngularBuild: '^20.1.0-next.0+sha-bd9e4a4',
-    AngularSSR: '^20.1.0-next.0+sha-bd9e4a4',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-233495f',
+    AngularBuild: '^20.1.0-next.0+sha-233495f',
+    AngularSSR: '^20.1.0-next.0+sha-233495f',
 };
