f6aefa3b915ebdbbfe55fe35f45894b395d1ca46
Angular Builds <dev-infra@angular.dev>
2025-05-23 06:55:58 +0000

    5f7ac55 build: update rules_angular digest to 652e5b0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-eed60d1',
-    AngularBuild: '^20.1.0-next.0+sha-eed60d1',
-    AngularSSR: '^20.1.0-next.0+sha-eed60d1',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-5f7ac55',
+    AngularBuild: '^20.1.0-next.0+sha-5f7ac55',
+    AngularSSR: '^20.1.0-next.0+sha-5f7ac55',
 };
