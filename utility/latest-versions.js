174c6c3bde2aea11fbc8b265ad34af81852f5fcd
Angular Builds <dev-infra@angular.dev>
2025-05-23 11:19:13 +0000

    a2d77d3 refactor(@angular-devkit/architect): remove redundant internal job schema validation

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-rc.3+sha-ec94eb9',
-    AngularBuild: '^20.0.0-rc.3+sha-ec94eb9',
-    AngularSSR: '^20.0.0-rc.3+sha-ec94eb9',
+    DevkitBuildAngular: '^20.0.0-rc.3+sha-a2d77d3',
+    AngularBuild: '^20.0.0-rc.3+sha-a2d77d3',
+    AngularSSR: '^20.0.0-rc.3+sha-a2d77d3',
 };
