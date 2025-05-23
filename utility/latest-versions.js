ba28e19eda6df78db8ba3e81a562e13a4981098a
Angular Builds <dev-infra@angular.dev>
2025-05-23 07:33:15 +0000

    ec94eb9 build: correctly replace `BUILD_SCM_HASH-PLACEHOLDER` with `BUILD_SCM_ABBREV_HASH`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.0.0-rc.3+sha-25c26ff',
-    AngularBuild: '^20.0.0-rc.3+sha-25c26ff',
-    AngularSSR: '^20.0.0-rc.3+sha-25c26ff',
+    DevkitBuildAngular: '^20.0.0-rc.3+sha-ec94eb9',
+    AngularBuild: '^20.0.0-rc.3+sha-ec94eb9',
+    AngularSSR: '^20.0.0-rc.3+sha-ec94eb9',
 };
