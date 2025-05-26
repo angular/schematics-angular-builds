bdde277b89c24f24751d1d69d6e0f8dad7390af4
Angular Builds <dev-infra@angular.dev>
2025-05-26 09:56:31 +0000

    3ffd43a build: preserve `peerDependenciesMeta` in snapshots

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0',
     NgPackagr: '^19.2.0',
-    DevkitBuildAngular: '^19.2.13+sha-cf4e676',
-    AngularBuild: '^19.2.13+sha-cf4e676',
-    AngularSSR: '^19.2.13+sha-cf4e676',
+    DevkitBuildAngular: '^19.2.13+sha-3ffd43a',
+    AngularBuild: '^19.2.13+sha-3ffd43a',
+    AngularSSR: '^19.2.13+sha-3ffd43a',
 };
