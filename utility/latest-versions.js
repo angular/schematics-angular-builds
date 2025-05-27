61d315993b4fd6c7710aec423d2a800612581530
Angular Builds <dev-infra@angular.dev>
2025-05-27 05:39:15 +0000

    f7bfb59 build: update dependency yargs-parser to v22

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-84d7b58',
-    AngularBuild: '^20.1.0-next.0+sha-84d7b58',
-    AngularSSR: '^20.1.0-next.0+sha-84d7b58',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-f7bfb59',
+    AngularBuild: '^20.1.0-next.0+sha-f7bfb59',
+    AngularSSR: '^20.1.0-next.0+sha-f7bfb59',
 };
