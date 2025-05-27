4f272598c98a3f0890056d1754fc4c213fbd2245
Angular Builds <dev-infra@angular.dev>
2025-05-27 16:56:44 +0000

    dd9153a build: update dependency ng-packagr to v20.0.0-rc.2

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-425d6bd',
-    AngularBuild: '^20.1.0-next.0+sha-425d6bd',
-    AngularSSR: '^20.1.0-next.0+sha-425d6bd',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-dd9153a',
+    AngularBuild: '^20.1.0-next.0+sha-dd9153a',
+    AngularSSR: '^20.1.0-next.0+sha-dd9153a',
 };
