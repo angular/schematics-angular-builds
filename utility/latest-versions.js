15989faed2c4efed4ccbc7a38561006f213ce7bc
Angular Builds <dev-infra@angular.dev>
2025-05-28 20:50:25 +0000

    9bb7447 build: update dependency yargs to v18

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-7d5f392',
-    AngularBuild: '^20.1.0-next.0+sha-7d5f392',
-    AngularSSR: '^20.1.0-next.0+sha-7d5f392',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-9bb7447',
+    AngularBuild: '^20.1.0-next.0+sha-9bb7447',
+    AngularSSR: '^20.1.0-next.0+sha-9bb7447',
 };
