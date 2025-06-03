cf92033aa6ef8239178582499591e5b5810d6644
Angular Builds <dev-infra@angular.dev>
2025-06-03 10:47:31 +0000

    bec42f7 fix(@angular/build): resolve junit karma reporter output to workspace root

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-1b39126',
-    AngularBuild: '^20.1.0-next.0+sha-1b39126',
-    AngularSSR: '^20.1.0-next.0+sha-1b39126',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-bec42f7',
+    AngularBuild: '^20.1.0-next.0+sha-bec42f7',
+    AngularSSR: '^20.1.0-next.0+sha-bec42f7',
 };
