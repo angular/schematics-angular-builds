6090826498c9338c825e2027fc9c154452d49970
Angular Builds <dev-infra@angular.dev>
2025-06-04 11:49:27 +0000

    abe69dd fix(@angular/cli): improve Node.js version check and error messages

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0-next.0',
     NgPackagr: '^20.0.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-63d542c',
-    AngularBuild: '^20.1.0-next.0+sha-63d542c',
-    AngularSSR: '^20.1.0-next.0+sha-63d542c',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-abe69dd',
+    AngularBuild: '^20.1.0-next.0+sha-abe69dd',
+    AngularSSR: '^20.1.0-next.0+sha-abe69dd',
 };
