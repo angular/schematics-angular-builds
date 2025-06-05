10bde855736e4e166c2be0826bbe59a25c8cddfe
Angular Builds <dev-infra@angular.dev>
2025-06-05 17:31:25 +0000

    3d65518 build: fix perf workflows

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0-next.0',
     NgPackagr: '^20.1.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-1ad7261',
-    AngularBuild: '^20.1.0-next.0+sha-1ad7261',
-    AngularSSR: '^20.1.0-next.0+sha-1ad7261',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-3d65518',
+    AngularBuild: '^20.1.0-next.0+sha-3d65518',
+    AngularSSR: '^20.1.0-next.0+sha-3d65518',
 };
