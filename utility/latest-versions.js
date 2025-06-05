a8631514881c4d55200bca8e1277b6a08da5ca28
Angular Builds <dev-infra@angular.dev>
2025-06-05 16:26:11 +0000

    1db861c build: improve globbing of jasmine test files

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0',
     NgPackagr: '^20.0.0',
-    DevkitBuildAngular: '^20.0.1+sha-cb258a3',
-    AngularBuild: '^20.0.1+sha-cb258a3',
-    AngularSSR: '^20.0.1+sha-cb258a3',
+    DevkitBuildAngular: '^20.0.1+sha-1db861c',
+    AngularBuild: '^20.0.1+sha-1db861c',
+    AngularSSR: '^20.0.1+sha-1db861c',
 };
