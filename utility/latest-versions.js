35b750d0a8c45ee56dd0bb6c62f372aa0554fac6
Angular Builds <circleci@angular.dev>
2025-06-10 22:43:10 +0000

    f048078 build: update dependency webpack-dev-server to v5.2.2

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -15,6 +15,6 @@ exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: dependencies['@angular/core'],
-    DevkitBuildAngular: '^18.2.19+sha-cfb0a4f',
-    AngularSSR: '^18.2.19+sha-cfb0a4f',
+    DevkitBuildAngular: '^18.2.19+sha-f048078',
+    AngularSSR: '^18.2.19+sha-f048078',
 };
