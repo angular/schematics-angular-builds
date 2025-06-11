5a466df1fdcb445f7a8bafce7c28832314758619
Angular Builds <dev-infra@angular.dev>
2025-06-11 17:05:38 +0000

    a415a49 fix(@angular/build): improve default coverage reporter handling for vitest

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0-next.0',
     NgPackagr: '^20.1.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.0+sha-cbc18b5',
-    AngularBuild: '^20.1.0-next.0+sha-cbc18b5',
-    AngularSSR: '^20.1.0-next.0+sha-cbc18b5',
+    DevkitBuildAngular: '^20.1.0-next.0+sha-a415a49',
+    AngularBuild: '^20.1.0-next.0+sha-a415a49',
+    AngularSSR: '^20.1.0-next.0+sha-a415a49',
 };
