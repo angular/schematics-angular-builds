03cc8d7235331a5bb67ece32f3bf4bb57d4a9a54
Angular Builds <dev-infra@angular.dev>
2025-06-13 21:09:07 +0000

    049e688 fix(@angular/build): include custom bundle name scripts with karma

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0-next.0',
     NgPackagr: '^20.1.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.1+sha-60a16a8',
-    AngularBuild: '^20.1.0-next.1+sha-60a16a8',
-    AngularSSR: '^20.1.0-next.1+sha-60a16a8',
+    DevkitBuildAngular: '^20.1.0-next.1+sha-049e688',
+    AngularBuild: '^20.1.0-next.1+sha-049e688',
+    AngularSSR: '^20.1.0-next.1+sha-049e688',
 };
