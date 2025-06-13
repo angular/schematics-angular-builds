02f105469e820c85069bcd55a86d0e0cd5f24d25
Angular Builds <dev-infra@angular.dev>
2025-06-13 15:57:32 +0000

    60a16a8 fix(@angular/build): set scripts option output as classic script for karma

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0-next.0',
     NgPackagr: '^20.1.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.1+sha-ef742db',
-    AngularBuild: '^20.1.0-next.1+sha-ef742db',
-    AngularSSR: '^20.1.0-next.1+sha-ef742db',
+    DevkitBuildAngular: '^20.1.0-next.1+sha-60a16a8',
+    AngularBuild: '^20.1.0-next.1+sha-60a16a8',
+    AngularSSR: '^20.1.0-next.1+sha-60a16a8',
 };
