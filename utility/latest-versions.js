03b18bd2af6d8fe5b6d67ec34b5b2964361ddd54
Angular Builds <dev-infra@angular.dev>
2025-06-18 08:12:09 +0000

    1d76d0e fix(@angular/build): increase worker idle timeout

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0-next.0',
     NgPackagr: '^20.1.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.1+sha-2672f6e',
-    AngularBuild: '^20.1.0-next.1+sha-2672f6e',
-    AngularSSR: '^20.1.0-next.1+sha-2672f6e',
+    DevkitBuildAngular: '^20.1.0-next.1+sha-1d76d0e',
+    AngularBuild: '^20.1.0-next.1+sha-1d76d0e',
+    AngularSSR: '^20.1.0-next.1+sha-1d76d0e',
 };
