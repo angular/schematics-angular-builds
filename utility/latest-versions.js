006f8fed343356d9a5b947b5dd020ad4a2c72c25
Angular Builds <dev-infra@angular.dev>
2025-06-24 22:31:18 +0000

    2316fe2 fix(@schematics/angular): add missing prettier config

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0',
     NgPackagr: '^20.0.0',
-    DevkitBuildAngular: '^20.0.3+sha-82691b9',
-    AngularBuild: '^20.0.3+sha-82691b9',
-    AngularSSR: '^20.0.3+sha-82691b9',
+    DevkitBuildAngular: '^20.0.3+sha-2316fe2',
+    AngularBuild: '^20.0.3+sha-2316fe2',
+    AngularSSR: '^20.0.3+sha-2316fe2',
 };
