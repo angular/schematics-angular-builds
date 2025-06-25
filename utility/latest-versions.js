d51744342967a73401396ed704c9b932f298dcba
Angular Builds <dev-infra@angular.dev>
2025-06-25 09:07:04 +0000

    0df9939 ci: disable auto updates for `io_bazel_rules_webtesting`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0-next.0',
     NgPackagr: '^20.1.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.2+sha-d4bfadc',
-    AngularBuild: '^20.1.0-next.2+sha-d4bfadc',
-    AngularSSR: '^20.1.0-next.2+sha-d4bfadc',
+    DevkitBuildAngular: '^20.1.0-next.2+sha-0df9939',
+    AngularBuild: '^20.1.0-next.2+sha-0df9939',
+    AngularSSR: '^20.1.0-next.2+sha-0df9939',
 };
