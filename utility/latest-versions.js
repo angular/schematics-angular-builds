a47a4acc188242775906629be7759ee92d845329
Angular Builds <dev-infra@angular.dev>
2025-06-27 11:36:38 +0000

    f1d41b0 fix(@angular-devkit/build-angular): remove unused `@vitejs/plugin-basic-ssl` dependency

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0-next.0',
     NgPackagr: '^20.1.0-next.0',
-    DevkitBuildAngular: '^20.1.0-next.3+sha-ad93a7e',
-    AngularBuild: '^20.1.0-next.3+sha-ad93a7e',
-    AngularSSR: '^20.1.0-next.3+sha-ad93a7e',
+    DevkitBuildAngular: '^20.1.0-next.3+sha-f1d41b0',
+    AngularBuild: '^20.1.0-next.3+sha-f1d41b0',
+    AngularSSR: '^20.1.0-next.3+sha-f1d41b0',
 };
