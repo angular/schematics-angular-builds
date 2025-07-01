0ef80d99c5cae5b5a44b8cf2a5882eedc287ae2f
Angular Builds <dev-infra@angular.dev>
2025-07-01 13:10:46 +0000

    05cebdb fix(@angular/build): proxy karma request from `/` to `/base`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0',
     NgPackagr: '^20.0.0',
-    DevkitBuildAngular: '^20.0.4+sha-434ebb1',
-    AngularBuild: '^20.0.4+sha-434ebb1',
-    AngularSSR: '^20.0.4+sha-434ebb1',
+    DevkitBuildAngular: '^20.0.4+sha-05cebdb',
+    AngularBuild: '^20.0.4+sha-05cebdb',
+    AngularSSR: '^20.0.4+sha-05cebdb',
 };
