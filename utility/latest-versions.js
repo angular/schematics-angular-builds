08e405b886af063fabe12042764579ddb67b4b30
Angular Builds <dev-infra@angular.dev>
2025-07-02 21:38:40 +0000

    41e28ff test: raise timeout of `//packages/ngtools/webpack:test`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.0.0',
     NgPackagr: '^20.0.0',
-    DevkitBuildAngular: '^20.0.5+sha-f5c2d67',
-    AngularBuild: '^20.0.5+sha-f5c2d67',
-    AngularSSR: '^20.0.5+sha-f5c2d67',
+    DevkitBuildAngular: '^20.0.5+sha-41e28ff',
+    AngularBuild: '^20.0.5+sha-41e28ff',
+    AngularSSR: '^20.0.5+sha-41e28ff',
 };
