0065868bc545371e2160df2b4cd7d47115291816
Angular Builds <dev-infra@angular.dev>
2025-07-03 12:40:56 +0000

    1aeefa7 fix(@angular/build): failed to proxy error for assets

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0-next.0',
     NgPackagr: '^20.1.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.0+sha-1cde40e',
-    AngularBuild: '^20.2.0-next.0+sha-1cde40e',
-    AngularSSR: '^20.2.0-next.0+sha-1cde40e',
+    DevkitBuildAngular: '^20.2.0-next.0+sha-1aeefa7',
+    AngularBuild: '^20.2.0-next.0+sha-1aeefa7',
+    AngularSSR: '^20.2.0-next.0+sha-1aeefa7',
 };
