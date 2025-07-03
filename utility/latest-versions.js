4e56dd7ec5f94c64395200b91d1c812ea1a2ee72
Angular Builds <dev-infra@angular.dev>
2025-07-03 09:10:24 +0000

    343ae2d fix(@angular/build): correctly remap Angular diagnostics

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0-next.0',
     NgPackagr: '^20.1.0-next.0',
-    DevkitBuildAngular: '^20.1.0-rc.0+sha-71a7ecd',
-    AngularBuild: '^20.1.0-rc.0+sha-71a7ecd',
-    AngularSSR: '^20.1.0-rc.0+sha-71a7ecd',
+    DevkitBuildAngular: '^20.1.0-rc.0+sha-343ae2d',
+    AngularBuild: '^20.1.0-rc.0+sha-343ae2d',
+    AngularSSR: '^20.1.0-rc.0+sha-343ae2d',
 };
