679e3fbc26778bde0cd203250a99b3b11929d16a
Angular Builds <dev-infra@angular.dev>
2025-07-03 13:23:46 +0000

    cefea43 fix(@schematics/angular): remove constructor from service template

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0-next.0',
     NgPackagr: '^20.1.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.0+sha-1aeefa7',
-    AngularBuild: '^20.2.0-next.0+sha-1aeefa7',
-    AngularSSR: '^20.2.0-next.0+sha-1aeefa7',
+    DevkitBuildAngular: '^20.2.0-next.0+sha-cefea43',
+    AngularBuild: '^20.2.0-next.0+sha-cefea43',
+    AngularSSR: '^20.2.0-next.0+sha-cefea43',
 };
