fedaf02cfb853e681c0bfff7e0937e5d6ac0ea0d
Angular Builds <dev-infra@angular.dev>
2025-07-08 14:36:17 +0000

    0e63440 test: increase timeout for ssr dev server tests

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0-next.0',
     NgPackagr: '^20.1.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.0+sha-f5c953d',
-    AngularBuild: '^20.2.0-next.0+sha-f5c953d',
-    AngularSSR: '^20.2.0-next.0+sha-f5c953d',
+    DevkitBuildAngular: '^20.2.0-next.0+sha-0e63440',
+    AngularBuild: '^20.2.0-next.0+sha-0e63440',
+    AngularSSR: '^20.2.0-next.0+sha-0e63440',
 };
