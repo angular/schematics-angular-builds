c8c4e47b1693e9cc87d53eb6facdedaad276a6ad
Angular Builds <dev-infra@angular.dev>
2025-07-08 06:50:26 +0000

    b160819 refactor(@angular/build): clean up duplicate of code in the karma builders

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0-next.0',
     NgPackagr: '^20.1.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.0+sha-8b613c8',
-    AngularBuild: '^20.2.0-next.0+sha-8b613c8',
-    AngularSSR: '^20.2.0-next.0+sha-8b613c8',
+    DevkitBuildAngular: '^20.2.0-next.0+sha-b160819',
+    AngularBuild: '^20.2.0-next.0+sha-b160819',
+    AngularSSR: '^20.2.0-next.0+sha-b160819',
 };
