3362d06f7faa394af7774824b281087b8725948c
Angular Builds <dev-infra@angular.dev>
2025-07-10 09:04:05 +0000

    5bfd3db build: update dependency parse5-html-rewriting-stream to v8

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0-next.0',
     NgPackagr: '^20.1.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.0+sha-2c3a46b',
-    AngularBuild: '^20.2.0-next.0+sha-2c3a46b',
-    AngularSSR: '^20.2.0-next.0+sha-2c3a46b',
+    DevkitBuildAngular: '^20.2.0-next.0+sha-5bfd3db',
+    AngularBuild: '^20.2.0-next.0+sha-5bfd3db',
+    AngularSSR: '^20.2.0-next.0+sha-5bfd3db',
 };
