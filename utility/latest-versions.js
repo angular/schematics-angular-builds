166a1748d3bc81161e9302a8533eef70023609f9
Angular Builds <dev-infra@angular.dev>
2025-07-10 14:05:09 +0000

    3c38877 refactor: use `executeWithCases` instead of RXJS boillerplate

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0-next.0',
     NgPackagr: '^20.1.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.0+sha-c182286',
-    AngularBuild: '^20.2.0-next.0+sha-c182286',
-    AngularSSR: '^20.2.0-next.0+sha-c182286',
+    DevkitBuildAngular: '^20.2.0-next.0+sha-3c38877',
+    AngularBuild: '^20.2.0-next.0+sha-3c38877',
+    AngularSSR: '^20.2.0-next.0+sha-3c38877',
 };
