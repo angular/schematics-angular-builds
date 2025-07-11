6fa7d636efcb2e773f69d15b3f2440df8f2007c2
Angular Builds <dev-infra@angular.dev>
2025-07-11 20:43:32 +0000

    51b54c5 build: update `@types/node` to `20.19.7`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0-next.0',
     NgPackagr: '^20.1.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.0+sha-5af81b7',
-    AngularBuild: '^20.2.0-next.0+sha-5af81b7',
-    AngularSSR: '^20.2.0-next.0+sha-5af81b7',
+    DevkitBuildAngular: '^20.2.0-next.0+sha-51b54c5',
+    AngularBuild: '^20.2.0-next.0+sha-51b54c5',
+    AngularSSR: '^20.2.0-next.0+sha-51b54c5',
 };
