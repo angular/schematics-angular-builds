5638c039de8d5c9c2a9a6fdfb159f33b027254e2
Angular Builds <dev-infra@angular.dev>
2025-07-11 12:24:18 +0000

    1602181 refactor(@angular/build): use the `toPosixPath` util to convert windows file

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0',
     NgPackagr: '^20.1.0',
-    DevkitBuildAngular: '^20.1.0+sha-e46cedb',
-    AngularBuild: '^20.1.0+sha-e46cedb',
-    AngularSSR: '^20.1.0+sha-e46cedb',
+    DevkitBuildAngular: '^20.1.0+sha-1602181',
+    AngularBuild: '^20.1.0+sha-1602181',
+    AngularSSR: '^20.1.0+sha-1602181',
 };
