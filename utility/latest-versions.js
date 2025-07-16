97998965557a625cb628734d20fd85cfa4d5e1bb
Angular Builds <dev-infra@angular.dev>
2025-07-16 18:57:21 +0000

    0925ebd refactor(@schematics/angular): fix layout of pill button

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0',
     NgPackagr: '^20.1.0',
-    DevkitBuildAngular: '^20.1.0+sha-3ccd1e8',
-    AngularBuild: '^20.1.0+sha-3ccd1e8',
-    AngularSSR: '^20.1.0+sha-3ccd1e8',
+    DevkitBuildAngular: '^20.1.0+sha-0925ebd',
+    AngularBuild: '^20.1.0+sha-0925ebd',
+    AngularSSR: '^20.1.0+sha-0925ebd',
 };
