823a73d431440930a5fa616544861b9a2198c639
Angular Builds <dev-infra@angular.dev>
2025-07-16 15:05:14 +0000

    3faa3b4 refactor(@angular/build): provide private factory API for internal compilation infrastructure

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0-next.0',
     NgPackagr: '^20.1.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.0+sha-0ee847c',
-    AngularBuild: '^20.2.0-next.0+sha-0ee847c',
-    AngularSSR: '^20.2.0-next.0+sha-0ee847c',
+    DevkitBuildAngular: '^20.2.0-next.0+sha-3faa3b4',
+    AngularBuild: '^20.2.0-next.0+sha-3faa3b4',
+    AngularSSR: '^20.2.0-next.0+sha-3faa3b4',
 };
