cb7287655d2f2834cb2636969564823234c40304
Angular Builds <dev-infra@angular.dev>
2025-07-18 19:01:09 +0000

    0d0040b fix(@angular-devkit/core): use crypto.randomUUID instead of Date.now for unique string in tmp file names

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0',
     NgPackagr: '^20.1.0',
-    DevkitBuildAngular: '^20.1.1+sha-0a2340b',
-    AngularBuild: '^20.1.1+sha-0a2340b',
-    AngularSSR: '^20.1.1+sha-0a2340b',
+    DevkitBuildAngular: '^20.1.1+sha-0d0040b',
+    AngularBuild: '^20.1.1+sha-0d0040b',
+    AngularSSR: '^20.1.1+sha-0d0040b',
 };
