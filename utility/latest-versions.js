6a75dbf6a791e0d38b95a9a95894226d2942bf82
Angular Builds <dev-infra@angular.dev>
2025-07-21 09:08:36 +0000

    a41a176 build: update schematics dependencies to ~5.9.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.0+sha-7595e1f',
-    AngularBuild: '^20.2.0-next.0+sha-7595e1f',
-    AngularSSR: '^20.2.0-next.0+sha-7595e1f',
+    DevkitBuildAngular: '^20.2.0-next.0+sha-a41a176',
+    AngularBuild: '^20.2.0-next.0+sha-a41a176',
+    AngularSSR: '^20.2.0-next.0+sha-a41a176',
 };
