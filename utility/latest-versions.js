cdad29f33f0b0d3cf77c63699ed60aae2d99841b
Angular Builds <dev-infra@angular.dev>
2025-07-21 20:26:31 +0000

    58065c8 refactor(@angular/cli): improve description of MCP documentation search tool

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.0+sha-fefa7a4',
-    AngularBuild: '^20.2.0-next.0+sha-fefa7a4',
-    AngularSSR: '^20.2.0-next.0+sha-fefa7a4',
+    DevkitBuildAngular: '^20.2.0-next.0+sha-58065c8',
+    AngularBuild: '^20.2.0-next.0+sha-58065c8',
+    AngularSSR: '^20.2.0-next.0+sha-58065c8',
 };
