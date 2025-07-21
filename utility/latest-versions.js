cf22064485001298e386469df4a3c59dbbe41a51
Angular Builds <dev-infra@angular.dev>
2025-07-21 15:03:01 +0000

    fefa7a4 fix(@angular/cli): `define` option is being included multiple times in the JSON help

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.0+sha-e9ac671',
-    AngularBuild: '^20.2.0-next.0+sha-e9ac671',
-    AngularSSR: '^20.2.0-next.0+sha-e9ac671',
+    DevkitBuildAngular: '^20.2.0-next.0+sha-fefa7a4',
+    AngularBuild: '^20.2.0-next.0+sha-fefa7a4',
+    AngularSSR: '^20.2.0-next.0+sha-fefa7a4',
 };
