b0e8509321407a9c89713bfc0272fb4e003cb74c
Angular Builds <dev-infra@angular.dev>
2025-07-22 12:02:36 +0000

    14da042 refactor(@angular/cli): move MCP list projects tool to separate file

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0',
     NgPackagr: '^20.1.0',
-    DevkitBuildAngular: '^20.1.1+sha-ada3e20',
-    AngularBuild: '^20.1.1+sha-ada3e20',
-    AngularSSR: '^20.1.1+sha-ada3e20',
+    DevkitBuildAngular: '^20.1.1+sha-14da042',
+    AngularBuild: '^20.1.1+sha-14da042',
+    AngularSSR: '^20.1.1+sha-14da042',
 };
