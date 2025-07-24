70e6cf3abf7c8c01f090076824d61ab8d004f4b8
Angular Builds <dev-infra@angular.dev>
2025-07-24 12:49:48 +0000

    c70dae4 test(@angular/build): add unit tests for unit-test builder options

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.1+sha-f68bdb2',
-    AngularBuild: '^20.2.0-next.1+sha-f68bdb2',
-    AngularSSR: '^20.2.0-next.1+sha-f68bdb2',
+    DevkitBuildAngular: '^20.2.0-next.1+sha-c70dae4',
+    AngularBuild: '^20.2.0-next.1+sha-c70dae4',
+    AngularSSR: '^20.2.0-next.1+sha-c70dae4',
 };
