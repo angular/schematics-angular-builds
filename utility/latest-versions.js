20cd76ccb5c8c43b70c1c85e5cfe19faa6d46c45
Angular Builds <dev-infra@angular.dev>
2025-07-24 13:12:49 +0000

    ec0dfc9 refactor(@angular/build): use rolldown for experimental chunk optimization

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.1+sha-c70dae4',
-    AngularBuild: '^20.2.0-next.1+sha-c70dae4',
-    AngularSSR: '^20.2.0-next.1+sha-c70dae4',
+    DevkitBuildAngular: '^20.2.0-next.1+sha-ec0dfc9',
+    AngularBuild: '^20.2.0-next.1+sha-ec0dfc9',
+    AngularSSR: '^20.2.0-next.1+sha-ec0dfc9',
 };
