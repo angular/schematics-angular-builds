9b1450cf51dd15a8583040d0aa00ba13bd73bbf6
Angular Builds <dev-infra@angular.dev>
2025-07-25 23:58:11 +0000

    fc012b7 refactor(@angular/cli): provide a find examples MCP server tool

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.1+sha-ace41c6',
-    AngularBuild: '^20.2.0-next.1+sha-ace41c6',
-    AngularSSR: '^20.2.0-next.1+sha-ace41c6',
+    DevkitBuildAngular: '^20.2.0-next.1+sha-fc012b7',
+    AngularBuild: '^20.2.0-next.1+sha-fc012b7',
+    AngularSSR: '^20.2.0-next.1+sha-fc012b7',
 };
