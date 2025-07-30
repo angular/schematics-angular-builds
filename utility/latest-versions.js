eed50a142658a80bb32444a5cd53990ec5cbb922
Angular Builds <dev-infra@angular.dev>
2025-07-30 17:13:19 +0000

    9c12bfd ci: temporarily disable Windows CI for PRs

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.1+sha-193b394',
-    AngularBuild: '^20.2.0-next.1+sha-193b394',
-    AngularSSR: '^20.2.0-next.1+sha-193b394',
+    DevkitBuildAngular: '^20.2.0-next.1+sha-9c12bfd',
+    AngularBuild: '^20.2.0-next.1+sha-9c12bfd',
+    AngularSSR: '^20.2.0-next.1+sha-9c12bfd',
 };
