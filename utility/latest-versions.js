7ac20fa291cae37964847ae3faeb60a9d8e40c88
Angular Builds <dev-infra@angular.dev>
2025-07-31 14:00:11 +0000

    ecea1e0 refactor(@angular/cli): add instructional text to MCP server

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.2+sha-be488e8',
-    AngularBuild: '^20.2.0-next.2+sha-be488e8',
-    AngularSSR: '^20.2.0-next.2+sha-be488e8',
+    DevkitBuildAngular: '^20.2.0-next.2+sha-ecea1e0',
+    AngularBuild: '^20.2.0-next.2+sha-ecea1e0',
+    AngularSSR: '^20.2.0-next.2+sha-ecea1e0',
 };
