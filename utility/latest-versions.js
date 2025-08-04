76d1b7b3fdb1565e43bc2cb87c8a91d5c9e146eb
Angular Builds <dev-infra@angular.dev>
2025-08-04 14:14:32 +0000

    b6b2578 refactor(@angular/cli): centralize environment variable parsing

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.2+sha-6f936fb',
-    AngularBuild: '^20.2.0-next.2+sha-6f936fb',
-    AngularSSR: '^20.2.0-next.2+sha-6f936fb',
+    DevkitBuildAngular: '^20.2.0-next.2+sha-b6b2578',
+    AngularBuild: '^20.2.0-next.2+sha-b6b2578',
+    AngularSSR: '^20.2.0-next.2+sha-b6b2578',
 };
