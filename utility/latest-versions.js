e3316b6d52f528ae435807662e010e0a5e917283
Angular Builds <dev-infra@angular.dev>
2025-08-06 15:42:27 +0000

    761bc78 build: update to latest dev-infra in the workspace

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0',
     NgPackagr: '^20.1.0',
-    DevkitBuildAngular: '^20.1.4+sha-48ca044',
-    AngularBuild: '^20.1.4+sha-48ca044',
-    AngularSSR: '^20.1.4+sha-48ca044',
+    DevkitBuildAngular: '^20.1.4+sha-761bc78',
+    AngularBuild: '^20.1.4+sha-761bc78',
+    AngularSSR: '^20.1.4+sha-761bc78',
 };
