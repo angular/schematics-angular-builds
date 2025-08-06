ebd3e870f31e655ffcaea5bc8a8c72930832b02f
Angular Builds <dev-infra@angular.dev>
2025-08-06 15:17:34 +0000

    48ca044 fix(@angular/cli): cache MCP best practices content and add tool annotations

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0',
     NgPackagr: '^20.1.0',
-    DevkitBuildAngular: '^20.1.4+sha-8601f06',
-    AngularBuild: '^20.1.4+sha-8601f06',
-    AngularSSR: '^20.1.4+sha-8601f06',
+    DevkitBuildAngular: '^20.1.4+sha-48ca044',
+    AngularBuild: '^20.1.4+sha-48ca044',
+    AngularSSR: '^20.1.4+sha-48ca044',
 };
