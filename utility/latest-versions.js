45a603b77b368e3be963d43a235217a20187f677
Angular Builds <dev-infra@angular.dev>
2025-08-07 00:26:51 +0000

    5c6cacb refactor(@angular/cli): separate the version command's analysis logic from logging

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.2+sha-31f2384',
-    AngularBuild: '^20.2.0-next.2+sha-31f2384',
-    AngularSSR: '^20.2.0-next.2+sha-31f2384',
+    DevkitBuildAngular: '^20.2.0-next.2+sha-5c6cacb',
+    AngularBuild: '^20.2.0-next.2+sha-5c6cacb',
+    AngularSSR: '^20.2.0-next.2+sha-5c6cacb',
 };
