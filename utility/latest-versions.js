af3259cbd7699040e1cddfaf0c057f05e8e1a482
Angular Builds <dev-infra@angular.dev>
2025-08-07 15:26:15 +0000

    5eeebf9 refactor(@angular/build): provide private component stylesheet bundling API for internal compilation infrastructure

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.2+sha-82fda14',
-    AngularBuild: '^20.2.0-next.2+sha-82fda14',
-    AngularSSR: '^20.2.0-next.2+sha-82fda14',
+    DevkitBuildAngular: '^20.2.0-next.2+sha-5eeebf9',
+    AngularBuild: '^20.2.0-next.2+sha-5eeebf9',
+    AngularSSR: '^20.2.0-next.2+sha-5eeebf9',
 };
