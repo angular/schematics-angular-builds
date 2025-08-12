4440d6288503395e035319b48e19703cb6d0e3ef
Angular Builds <dev-infra@angular.dev>
2025-08-12 07:28:03 +0000

    8d758ba build: update actions/checkout action to v5

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^20.2.0-next.3+sha-b366548',
-    AngularBuild: '^20.2.0-next.3+sha-b366548',
-    AngularSSR: '^20.2.0-next.3+sha-b366548',
+    DevkitBuildAngular: '^20.2.0-next.3+sha-8d758ba',
+    AngularBuild: '^20.2.0-next.3+sha-8d758ba',
+    AngularSSR: '^20.2.0-next.3+sha-8d758ba',
 };
