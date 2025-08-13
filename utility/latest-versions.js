9b6eb1f9f7eb3f7cec7d2a358061e0d89d599044
Angular Builds <dev-infra@angular.dev>
2025-08-13 14:58:25 +0000

    8ba6b0b fix(@angular/cli): use correct path for MCP get_best_practices tool

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^20.2.0-rc.0+sha-406cfa6',
-    AngularBuild: '^20.2.0-rc.0+sha-406cfa6',
-    AngularSSR: '^20.2.0-rc.0+sha-406cfa6',
+    DevkitBuildAngular: '^20.2.0-rc.0+sha-8ba6b0b',
+    AngularBuild: '^20.2.0-rc.0+sha-8ba6b0b',
+    AngularSSR: '^20.2.0-rc.0+sha-8ba6b0b',
 };
