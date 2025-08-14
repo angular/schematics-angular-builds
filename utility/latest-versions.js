0227e9f6268a9842f5c3f3b33ee83b73c977fdf0
Angular Builds <dev-infra@angular.dev>
2025-08-14 12:23:50 +0000

    3482633 build: update `dev-infra` action SHA

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^20.2.0-rc.0+sha-6fe1125',
-    AngularBuild: '^20.2.0-rc.0+sha-6fe1125',
-    AngularSSR: '^20.2.0-rc.0+sha-6fe1125',
+    DevkitBuildAngular: '^20.2.0-rc.0+sha-3482633',
+    AngularBuild: '^20.2.0-rc.0+sha-3482633',
+    AngularSSR: '^20.2.0-rc.0+sha-3482633',
 };
