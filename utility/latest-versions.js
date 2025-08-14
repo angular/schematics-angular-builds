171eb14f894902f6238a324b2dcc348646530d89
Angular Builds <dev-infra@angular.dev>
2025-08-14 13:56:36 +0000

    9d7fb47 build: add retry-delay for `accept4 failed 110`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^20.2.0-rc.0+sha-3482633',
-    AngularBuild: '^20.2.0-rc.0+sha-3482633',
-    AngularSSR: '^20.2.0-rc.0+sha-3482633',
+    DevkitBuildAngular: '^20.2.0-rc.0+sha-9d7fb47',
+    AngularBuild: '^20.2.0-rc.0+sha-9d7fb47',
+    AngularSSR: '^20.2.0-rc.0+sha-9d7fb47',
 };
