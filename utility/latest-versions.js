beab784bf93f664aa82e270e1248fd9c711b08c0
Angular Builds <dev-infra@angular.dev>
2025-08-14 12:09:45 +0000

    6fe1125 refactor: await `silentNpm`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^20.2.0-rc.0+sha-201d433',
-    AngularBuild: '^20.2.0-rc.0+sha-201d433',
-    AngularSSR: '^20.2.0-rc.0+sha-201d433',
+    DevkitBuildAngular: '^20.2.0-rc.0+sha-6fe1125',
+    AngularBuild: '^20.2.0-rc.0+sha-6fe1125',
+    AngularSSR: '^20.2.0-rc.0+sha-6fe1125',
 };
