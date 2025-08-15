94c96ff86d89af45cb29da86806130a9a79a7924
Angular Builds <dev-infra@angular.dev>
2025-08-15 16:12:29 +0000

    dd90a81 fix(@angular-devkit/schematics): address Node.js deprecation DEP0190

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.0+sha-b8eada5',
-    AngularBuild: '^21.0.0-next.0+sha-b8eada5',
-    AngularSSR: '^21.0.0-next.0+sha-b8eada5',
+    DevkitBuildAngular: '^21.0.0-next.0+sha-dd90a81',
+    AngularBuild: '^21.0.0-next.0+sha-dd90a81',
+    AngularSSR: '^21.0.0-next.0+sha-dd90a81',
 };
