c6d007d3a440a3bf5a79a091f74cd8917fb59c0d
Angular Builds <dev-infra@angular.dev>
2025-08-15 17:03:52 +0000

    7d67a14 fix(@angular/cli): add choices to command line parser when type is array and has an enum

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0',
     NgPackagr: '^20.1.0',
-    DevkitBuildAngular: '^20.1.6+sha-0ecf2dc',
-    AngularBuild: '^20.1.6+sha-0ecf2dc',
-    AngularSSR: '^20.1.6+sha-0ecf2dc',
+    DevkitBuildAngular: '^20.1.6+sha-7d67a14',
+    AngularBuild: '^20.1.6+sha-7d67a14',
+    AngularSSR: '^20.1.6+sha-7d67a14',
 };
