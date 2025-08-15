6006870b5e5aed4571d8e7336e8b59dc2a0106b9
Angular Builds <dev-infra@angular.dev>
2025-08-15 18:17:14 +0000

    8abe5cd refactor(@angular/cli): improve analytics handling

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^20.2.0-rc.1+sha-a3b25f6',
-    AngularBuild: '^20.2.0-rc.1+sha-a3b25f6',
-    AngularSSR: '^20.2.0-rc.1+sha-a3b25f6',
+    DevkitBuildAngular: '^20.2.0-rc.1+sha-8abe5cd',
+    AngularBuild: '^20.2.0-rc.1+sha-8abe5cd',
+    AngularSSR: '^20.2.0-rc.1+sha-8abe5cd',
 };
