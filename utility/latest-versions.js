4b2320bd6488e490b2adca35742049858b3dc9fc
Angular Builds <dev-infra@angular.dev>
2025-08-18 16:40:32 +0000

    6023da5 fix(@angular/cli): apply default to array types

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.1.0',
     NgPackagr: '^20.1.0',
-    DevkitBuildAngular: '^20.1.6+sha-c0e7b6e',
-    AngularBuild: '^20.1.6+sha-c0e7b6e',
-    AngularSSR: '^20.1.6+sha-c0e7b6e',
+    DevkitBuildAngular: '^20.1.6+sha-6023da5',
+    AngularBuild: '^20.1.6+sha-6023da5',
+    AngularSSR: '^20.1.6+sha-6023da5',
 };
