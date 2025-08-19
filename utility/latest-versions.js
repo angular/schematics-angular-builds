212137ace8ed718ed1491a129d70c9128baa6566
Angular Builds <dev-infra@angular.dev>
2025-08-19 15:12:19 +0000

    f54cdff build: add missing `@angular/create` package.json to `npm_translate_lock` data

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.0+sha-0505f95',
-    AngularBuild: '^21.0.0-next.0+sha-0505f95',
-    AngularSSR: '^21.0.0-next.0+sha-0505f95',
+    DevkitBuildAngular: '^21.0.0-next.0+sha-f54cdff-with-local-changes',
+    AngularBuild: '^21.0.0-next.0+sha-f54cdff-with-local-changes',
+    AngularSSR: '^21.0.0-next.0+sha-f54cdff-with-local-changes',
 };
