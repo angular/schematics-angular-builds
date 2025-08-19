3a4940c0fa7068822ee0a095546266d69d3709fd
Angular Builds <dev-infra@angular.dev>
2025-08-19 15:57:26 +0000

    754c7b4 build: update bazel module lock file

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.0+sha-f54cdff-with-local-changes',
-    AngularBuild: '^21.0.0-next.0+sha-f54cdff-with-local-changes',
-    AngularSSR: '^21.0.0-next.0+sha-f54cdff-with-local-changes',
+    DevkitBuildAngular: '^21.0.0-next.0+sha-754c7b4',
+    AngularBuild: '^21.0.0-next.0+sha-754c7b4',
+    AngularSSR: '^21.0.0-next.0+sha-754c7b4',
 };
