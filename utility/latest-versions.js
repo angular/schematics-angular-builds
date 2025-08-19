263c5ae1ba689615d90d5c8a6060a4ddc3ee28fd
Angular Builds <dev-infra@angular.dev>
2025-08-19 20:53:24 +0000

    2a94e21 refactor(@angular/cli): improve discoverability of mcp server options

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0-next.0',
     NgPackagr: '^20.2.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.0+sha-b3dfc91',
-    AngularBuild: '^21.0.0-next.0+sha-b3dfc91',
-    AngularSSR: '^21.0.0-next.0+sha-b3dfc91',
+    DevkitBuildAngular: '^21.0.0-next.0+sha-2a94e21',
+    AngularBuild: '^21.0.0-next.0+sha-2a94e21',
+    AngularSSR: '^21.0.0-next.0+sha-2a94e21',
 };
