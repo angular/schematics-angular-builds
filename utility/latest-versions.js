bae3bf16bb29e3227eeb823fc870b93cfefc8edb
Angular Builds <dev-infra@angular.dev>
2025-08-20 14:31:23 +0000

    309dde6 build: update dependency rollup to v4.46.4

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.0+sha-2c99210',
-    AngularBuild: '^21.0.0-next.0+sha-2c99210',
-    AngularSSR: '^21.0.0-next.0+sha-2c99210',
+    DevkitBuildAngular: '^21.0.0-next.0+sha-309dde6',
+    AngularBuild: '^21.0.0-next.0+sha-309dde6',
+    AngularSSR: '^21.0.0-next.0+sha-309dde6',
 };
