3436f4a44efcc2d17eba930d9fb4eaee31390488
Angular Builds <dev-infra@angular.dev>
2025-08-21 18:49:49 +0000

    f942424 ci: set `E2E_SHARD_TOTAL` as env variable

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0',
     NgPackagr: '^20.2.0',
-    DevkitBuildAngular: '^20.2.0+sha-ffff9ea',
-    AngularBuild: '^20.2.0+sha-ffff9ea',
-    AngularSSR: '^20.2.0+sha-ffff9ea',
+    DevkitBuildAngular: '^20.2.0+sha-f942424',
+    AngularBuild: '^20.2.0+sha-f942424',
+    AngularSSR: '^20.2.0+sha-f942424',
 };
