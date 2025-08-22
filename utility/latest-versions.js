a52d14c81f0691db9de0c81b284f4031c1734118
Angular Builds <dev-infra@angular.dev>
2025-08-22 16:16:39 +0000

    f89aa0a build(@angular-devkit/schematics-cli): move TypeScript to development dependencies

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0',
     NgPackagr: '^20.2.0',
-    DevkitBuildAngular: '^20.2.0+sha-f942424',
-    AngularBuild: '^20.2.0+sha-f942424',
-    AngularSSR: '^20.2.0+sha-f942424',
+    DevkitBuildAngular: '^20.2.0+sha-f89aa0a',
+    AngularBuild: '^20.2.0+sha-f89aa0a',
+    AngularSSR: '^20.2.0+sha-f89aa0a',
 };
