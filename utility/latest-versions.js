6e8d2dc2ed875d2e5290e1cb141128249d794a2d
Angular Builds <dev-infra@angular.dev>
2025-08-25 16:23:45 +0000

    5179d80 ci: Disable PNPM cache on Windows

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0',
     NgPackagr: '^20.2.0',
-    DevkitBuildAngular: '^20.2.0+sha-3071317',
-    AngularBuild: '^20.2.0+sha-3071317',
-    AngularSSR: '^20.2.0+sha-3071317',
+    DevkitBuildAngular: '^20.2.0+sha-5179d80',
+    AngularBuild: '^20.2.0+sha-5179d80',
+    AngularSSR: '^20.2.0+sha-5179d80',
 };
