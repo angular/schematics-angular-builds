1f805dfabc1b712e881c173b0c5508aac5be1320
Angular Builds <dev-infra@angular.dev>
2025-08-26 13:53:21 +0000

    ecc0198 ci: removing WSL for a simpler, more performant approach

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0',
     NgPackagr: '^20.2.0',
-    DevkitBuildAngular: '^20.2.0+sha-5c76d4c',
-    AngularBuild: '^20.2.0+sha-5c76d4c',
-    AngularSSR: '^20.2.0+sha-5c76d4c',
+    DevkitBuildAngular: '^20.2.0+sha-ecc0198',
+    AngularBuild: '^20.2.0+sha-ecc0198',
+    AngularSSR: '^20.2.0+sha-ecc0198',
 };
