54cdfa2c09577bd172fcfa77e99d2f8b286088b4
Angular Builds <dev-infra@angular.dev>
2025-08-26 14:34:18 +0000

    aed26c3 fix(@angular-devkit/schematics-cli): correctly set default array values

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.0+sha-8a512cc',
-    AngularBuild: '^21.0.0-next.0+sha-8a512cc',
-    AngularSSR: '^21.0.0-next.0+sha-8a512cc',
+    DevkitBuildAngular: '^21.0.0-next.0+sha-aed26c3',
+    AngularBuild: '^21.0.0-next.0+sha-aed26c3',
+    AngularSSR: '^21.0.0-next.0+sha-aed26c3',
 };
