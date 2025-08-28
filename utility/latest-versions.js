1a4b76f27f501d5f9516a2cca449f64317b4ea88
Angular Builds <dev-infra@angular.dev>
2025-08-28 09:14:06 +0000

    f5e6894 test(@schematics/angular): refactor tailwind schematic tests for clarity

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.1+sha-280f257',
-    AngularBuild: '^21.0.0-next.1+sha-280f257',
-    AngularSSR: '^21.0.0-next.1+sha-280f257',
+    DevkitBuildAngular: '^21.0.0-next.1+sha-f5e6894',
+    AngularBuild: '^21.0.0-next.1+sha-f5e6894',
+    AngularSSR: '^21.0.0-next.1+sha-f5e6894',
 };
