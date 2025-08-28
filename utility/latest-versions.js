e8cc0c6ffa52ca00804797338bae2ecfee69758e
Angular Builds <dev-infra@angular.dev>
2025-08-28 09:31:50 +0000

    0a450ca build: define typescript version via string in module.bazel file

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.1+sha-f5e6894',
-    AngularBuild: '^21.0.0-next.1+sha-f5e6894',
-    AngularSSR: '^21.0.0-next.1+sha-f5e6894',
+    DevkitBuildAngular: '^21.0.0-next.1+sha-0a450ca',
+    AngularBuild: '^21.0.0-next.1+sha-0a450ca',
+    AngularSSR: '^21.0.0-next.1+sha-0a450ca',
 };
