a92cef45e0653d94df4d99e2246578199c19bf07
Angular Builds <dev-infra@angular.dev>
2025-08-28 13:51:56 +0000

    7e8ae01 refactor(@angular/cli): improve logging with `ng add` for packages without schematics

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.1+sha-14c0a9b',
-    AngularBuild: '^21.0.0-next.1+sha-14c0a9b',
-    AngularSSR: '^21.0.0-next.1+sha-14c0a9b',
+    DevkitBuildAngular: '^21.0.0-next.1+sha-7e8ae01',
+    AngularBuild: '^21.0.0-next.1+sha-7e8ae01',
+    AngularSSR: '^21.0.0-next.1+sha-7e8ae01',
 };
