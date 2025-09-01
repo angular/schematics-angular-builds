ad8bec1c910e52c2b0ffff61f18219b7e5255eb0
Angular Builds <dev-infra@angular.dev>
2025-09-01 23:04:40 +0000

    33835d9 refactor(@angular/cli): rewrite best-practices MCP tool description for clarity

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.1+sha-fd89fe4',
-    AngularBuild: '^21.0.0-next.1+sha-fd89fe4',
-    AngularSSR: '^21.0.0-next.1+sha-fd89fe4',
+    DevkitBuildAngular: '^21.0.0-next.1+sha-33835d9',
+    AngularBuild: '^21.0.0-next.1+sha-33835d9',
+    AngularSSR: '^21.0.0-next.1+sha-33835d9',
 };
