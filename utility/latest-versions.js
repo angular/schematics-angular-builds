f0d9a458dbf5c1914fbf426b4210390caf523313
Angular Builds <dev-infra@angular.dev>
2025-09-02 17:54:32 +0000

    5c2abff fix(@angular/build): avoid extra tick in SSR dev-server builds

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0',
     NgPackagr: '^20.2.0',
-    DevkitBuildAngular: '^20.2.1+sha-0eaec23',
-    AngularBuild: '^20.2.1+sha-0eaec23',
-    AngularSSR: '^20.2.1+sha-0eaec23',
+    DevkitBuildAngular: '^20.2.1+sha-5c2abff',
+    AngularBuild: '^20.2.1+sha-5c2abff',
+    AngularSSR: '^20.2.1+sha-5c2abff',
 };
