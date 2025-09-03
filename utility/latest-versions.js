9f2b3ba13ed27397a1f719c4971ccb77f26c2dba
Angular Builds <dev-infra@angular.dev>
2025-09-03 13:45:56 +0000

    425bfe4 refactor(@angular/cli): update MCP example tool format for if example

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0',
     NgPackagr: '^20.2.0',
-    DevkitBuildAngular: '^20.2.1+sha-2736599',
-    AngularBuild: '^20.2.1+sha-2736599',
-    AngularSSR: '^20.2.1+sha-2736599',
+    DevkitBuildAngular: '^20.2.1+sha-425bfe4',
+    AngularBuild: '^20.2.1+sha-425bfe4',
+    AngularSSR: '^20.2.1+sha-425bfe4',
 };
