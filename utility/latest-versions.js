1c9c77c08c9a756d4820ef4c37b32259086d3e03
Angular Builds <dev-infra@angular.dev>
2025-09-03 13:18:55 +0000

    e192e8c fix(@angular/cli): set process title when running architect commands

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.1+sha-9b7ab11',
-    AngularBuild: '^21.0.0-next.1+sha-9b7ab11',
-    AngularSSR: '^21.0.0-next.1+sha-9b7ab11',
+    DevkitBuildAngular: '^21.0.0-next.1+sha-e192e8c',
+    AngularBuild: '^21.0.0-next.1+sha-e192e8c',
+    AngularSSR: '^21.0.0-next.1+sha-e192e8c',
 };
