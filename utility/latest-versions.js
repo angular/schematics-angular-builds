c41832f160f756289bfdd8d5a649645cefd09be7
Angular Builds <dev-infra@angular.dev>
2025-09-03 12:54:43 +0000

    f3c8268 fix(@angular/build): maintain media output hashing with vitest unit-testing

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0',
     NgPackagr: '^20.2.0',
-    DevkitBuildAngular: '^20.2.1+sha-99526bd',
-    AngularBuild: '^20.2.1+sha-99526bd',
-    AngularSSR: '^20.2.1+sha-99526bd',
+    DevkitBuildAngular: '^20.2.1+sha-f3c8268',
+    AngularBuild: '^20.2.1+sha-f3c8268',
+    AngularSSR: '^20.2.1+sha-f3c8268',
 };
