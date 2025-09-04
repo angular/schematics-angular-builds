c050027c4754d457331cdb1ff52e44d5788ceb44
Angular Builds <dev-infra@angular.dev>
2025-09-04 14:50:26 +0000

    982d6c5 refactor(@angular/build): improve readability of Vitest executor

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.2+sha-bf4f042',
-    AngularBuild: '^21.0.0-next.2+sha-bf4f042',
-    AngularSSR: '^21.0.0-next.2+sha-bf4f042',
+    DevkitBuildAngular: '^21.0.0-next.2+sha-982d6c5',
+    AngularBuild: '^21.0.0-next.2+sha-982d6c5',
+    AngularSSR: '^21.0.0-next.2+sha-982d6c5',
 };
