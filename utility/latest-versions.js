be41501380d0fc8b062015a0406e7ba3c8e7c665
Angular Builds <dev-infra@angular.dev>
2025-09-05 15:48:12 +0000

    58da860 fix(@angular/build): preserve names in esbuild for improved debugging in dev mode

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.2+sha-2981fca',
-    AngularBuild: '^21.0.0-next.2+sha-2981fca',
-    AngularSSR: '^21.0.0-next.2+sha-2981fca',
+    DevkitBuildAngular: '^21.0.0-next.2+sha-58da860',
+    AngularBuild: '^21.0.0-next.2+sha-58da860',
+    AngularSSR: '^21.0.0-next.2+sha-58da860',
 };
