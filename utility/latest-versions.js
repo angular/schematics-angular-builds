73c647dbfcb34ec92fcdf1402c4106b0e2b12997
Angular Builds <dev-infra@angular.dev>
2025-09-08 15:18:43 +0000

    2037b91 fix(@angular/cli): improve bun lockfile detection and optimize lockfile checks

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.2+sha-1529595',
-    AngularBuild: '^21.0.0-next.2+sha-1529595',
-    AngularSSR: '^21.0.0-next.2+sha-1529595',
+    DevkitBuildAngular: '^21.0.0-next.2+sha-2037b91',
+    AngularBuild: '^21.0.0-next.2+sha-2037b91',
+    AngularSSR: '^21.0.0-next.2+sha-2037b91',
 };
