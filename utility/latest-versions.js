5443d72659ac84809f148ab6215e4121b9e5b74f
Angular Builds <dev-infra@angular.dev>
2025-09-08 10:48:45 +0000

    1529595 fix(@angular/build): drop support for TypeScript 5.8

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.2+sha-722c44f',
-    AngularBuild: '^21.0.0-next.2+sha-722c44f',
-    AngularSSR: '^21.0.0-next.2+sha-722c44f',
+    DevkitBuildAngular: '^21.0.0-next.2+sha-1529595',
+    AngularBuild: '^21.0.0-next.2+sha-1529595',
+    AngularSSR: '^21.0.0-next.2+sha-1529595',
 };
