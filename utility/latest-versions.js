cd98181d8024ac630d963d4c0e1f75d841064f6d
Angular Builds <dev-infra@angular.dev>
2025-09-09 18:09:06 +0000

    a60de9b ci: update renovate base branch to 20.3.x

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.2+sha-9749ec6',
-    AngularBuild: '^21.0.0-next.2+sha-9749ec6',
-    AngularSSR: '^21.0.0-next.2+sha-9749ec6',
+    DevkitBuildAngular: '^21.0.0-next.2+sha-a60de9b',
+    AngularBuild: '^21.0.0-next.2+sha-a60de9b',
+    AngularSSR: '^21.0.0-next.2+sha-a60de9b',
 };
