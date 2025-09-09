c16595bf4692d33dda8845b289af52e4bda84744
Angular Builds <dev-infra@angular.dev>
2025-09-09 13:06:53 +0000

    dbf1aaf fix(@angular/cli): add snippet support to example search MCP tool

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.2+sha-af65d3a',
-    AngularBuild: '^21.0.0-next.2+sha-af65d3a',
-    AngularSSR: '^21.0.0-next.2+sha-af65d3a',
+    DevkitBuildAngular: '^21.0.0-next.2+sha-dbf1aaf',
+    AngularBuild: '^21.0.0-next.2+sha-dbf1aaf',
+    AngularSSR: '^21.0.0-next.2+sha-dbf1aaf',
 };
