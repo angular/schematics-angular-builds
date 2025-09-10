fd11cf7529347e8a96641e0dc231d8a6d87ebc3a
Angular Builds <dev-infra@angular.dev>
2025-09-10 12:31:30 +0000

    8e7e555 fix(@schematics/angular): align labels in ai-config schema

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.2.0',
     NgPackagr: '^20.2.0',
-    DevkitBuildAngular: '^20.2.2+sha-caa6e72',
-    AngularBuild: '^20.2.2+sha-caa6e72',
-    AngularSSR: '^20.2.2+sha-caa6e72',
+    DevkitBuildAngular: '^20.2.2+sha-8e7e555',
+    AngularBuild: '^20.2.2+sha-8e7e555',
+    AngularSSR: '^20.2.2+sha-8e7e555',
 };
