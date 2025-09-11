c219d13fabbd51c920aa8f1e7414565bd46f154e
Angular Builds <dev-infra@angular.dev>
2025-09-11 13:07:53 +0000

    f5701cb refactor(@angular/cli): remove unused local variable

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.3+sha-af1a2a5',
-    AngularBuild: '^21.0.0-next.3+sha-af1a2a5',
-    AngularSSR: '^21.0.0-next.3+sha-af1a2a5',
+    DevkitBuildAngular: '^21.0.0-next.3+sha-f5701cb',
+    AngularBuild: '^21.0.0-next.3+sha-f5701cb',
+    AngularSSR: '^21.0.0-next.3+sha-f5701cb',
 };
