67bdd458f03d07644c34649f6797330140264981
Angular Builds <dev-infra@angular.dev>
2025-09-12 18:10:34 +0000

    cfac923 build: update rules_angular digest to 4010ef9

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.3+sha-6686139',
-    AngularBuild: '^21.0.0-next.3+sha-6686139',
-    AngularSSR: '^21.0.0-next.3+sha-6686139',
+    DevkitBuildAngular: '^21.0.0-next.3+sha-cfac923',
+    AngularBuild: '^21.0.0-next.3+sha-cfac923',
+    AngularSSR: '^21.0.0-next.3+sha-cfac923',
 };
