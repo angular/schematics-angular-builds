89abf3d4772b1986d4c10b0a2e507a6b7c97f42b
Angular Builds <dev-infra@angular.dev>
2025-09-17 12:20:00 +0000

    d2ca92a test(@angular/build): use expectLog and expectNoLog helpers in tests

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.3+sha-dae7320',
-    AngularBuild: '^21.0.0-next.3+sha-dae7320',
-    AngularSSR: '^21.0.0-next.3+sha-dae7320',
+    DevkitBuildAngular: '^21.0.0-next.3+sha-d2ca92a',
+    AngularBuild: '^21.0.0-next.3+sha-d2ca92a',
+    AngularSSR: '^21.0.0-next.3+sha-d2ca92a',
 };
