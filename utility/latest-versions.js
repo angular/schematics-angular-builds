92c4efb6c5852d83cb9d5d5c2b3984b050ae6901
Angular Builds <dev-infra@angular.dev>
2025-09-17 13:38:01 +0000

    365d525 fix(@angular/build): update `vite` to `6.3.6`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0',
     NgPackagr: '^19.2.0',
-    DevkitBuildAngular: '^19.2.16+sha-3ea403a',
-    AngularBuild: '^19.2.16+sha-3ea403a',
-    AngularSSR: '^19.2.16+sha-3ea403a',
+    DevkitBuildAngular: '^19.2.16+sha-365d525',
+    AngularBuild: '^19.2.16+sha-365d525',
+    AngularSSR: '^19.2.16+sha-365d525',
 };
