f30a19d22fae4515508ad7a2ef7e81e96a662e45
Angular Builds <dev-infra@angular.dev>
2025-09-17 14:56:37 +0000

    5050a15 build: update dependency ora to v9

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.3+sha-d7998f2',
-    AngularBuild: '^21.0.0-next.3+sha-d7998f2',
-    AngularSSR: '^21.0.0-next.3+sha-d7998f2',
+    DevkitBuildAngular: '^21.0.0-next.3+sha-5050a15',
+    AngularBuild: '^21.0.0-next.3+sha-5050a15',
+    AngularSSR: '^21.0.0-next.3+sha-5050a15',
 };
