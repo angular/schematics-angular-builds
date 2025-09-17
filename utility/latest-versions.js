5a2e6fcd74f9df872475b83d80244da6bbf0e43b
Angular Builds <dev-infra@angular.dev>
2025-09-17 13:17:44 +0000

    d7998f2 build: enable minimumReleaseAge to mitigate dependency chain attacks

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.3+sha-d2ca92a',
-    AngularBuild: '^21.0.0-next.3+sha-d2ca92a',
-    AngularSSR: '^21.0.0-next.3+sha-d2ca92a',
+    DevkitBuildAngular: '^21.0.0-next.3+sha-d7998f2',
+    AngularBuild: '^21.0.0-next.3+sha-d7998f2',
+    AngularSSR: '^21.0.0-next.3+sha-d7998f2',
 };
