c4dbd118bdd3707888b234c92087c6561276b1f0
Angular Builds <dev-infra@angular.dev>
2025-09-19 14:44:49 +0000

    907eabd fix(@angular/build): support ESM PostCSS plugins

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.4+sha-a0f45f9',
-    AngularBuild: '^21.0.0-next.4+sha-a0f45f9',
-    AngularSSR: '^21.0.0-next.4+sha-a0f45f9',
+    DevkitBuildAngular: '^21.0.0-next.4+sha-907eabd',
+    AngularBuild: '^21.0.0-next.4+sha-907eabd',
+    AngularSSR: '^21.0.0-next.4+sha-907eabd',
 };
