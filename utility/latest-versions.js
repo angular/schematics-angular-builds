b27e2d80830d11529ceaf95ca00716484d118434
Angular Builds <dev-infra@angular.dev>
2025-09-19 10:51:47 +0000

    a0f45f9 refactor(@angular/build): optimize test entrypoint name generation

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.4+sha-1ad7816',
-    AngularBuild: '^21.0.0-next.4+sha-1ad7816',
-    AngularSSR: '^21.0.0-next.4+sha-1ad7816',
+    DevkitBuildAngular: '^21.0.0-next.4+sha-a0f45f9',
+    AngularBuild: '^21.0.0-next.4+sha-a0f45f9',
+    AngularSSR: '^21.0.0-next.4+sha-a0f45f9',
 };
