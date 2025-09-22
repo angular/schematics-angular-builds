47bf3ee1ff0598f4e28f9ecb2f1ca7ebe0a4805c
Angular Builds <dev-infra@angular.dev>
2025-09-22 14:19:24 +0000

    d7f392b build: lock file maintenance

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.2+sha-24e1638',
-    AngularBuild: '^20.3.2+sha-24e1638',
-    AngularSSR: '^20.3.2+sha-24e1638',
+    DevkitBuildAngular: '^20.3.2+sha-d7f392b',
+    AngularBuild: '^20.3.2+sha-d7f392b',
+    AngularSSR: '^20.3.2+sha-d7f392b',
 };
