f8bec480d2f7287c26d65dc2ea59b5ab4a0f5e3a
Angular Builds <dev-infra@angular.dev>
2025-09-25 12:35:12 +0000

    bc6b631 fix(@angular-devkit/build-angular): mark `InjectionToken` as pure for improved tree-shaking

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.3+sha-2b45442',
-    AngularBuild: '^20.3.3+sha-2b45442',
-    AngularSSR: '^20.3.3+sha-2b45442',
+    DevkitBuildAngular: '^20.3.3+sha-bc6b631',
+    AngularBuild: '^20.3.3+sha-bc6b631',
+    AngularSSR: '^20.3.3+sha-bc6b631',
 };
