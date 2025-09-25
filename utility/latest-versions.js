0a0a108a02fdbb9b2a2bfb30f07e745099d48ceb
Angular Builds <dev-infra@angular.dev>
2025-09-25 21:43:52 +0000

    05ba42e build: update cross-repo angular dependencies

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.5+sha-c053ee7',
-    AngularBuild: '^21.0.0-next.5+sha-c053ee7',
-    AngularSSR: '^21.0.0-next.5+sha-c053ee7',
+    DevkitBuildAngular: '^21.0.0-next.5+sha-05ba42e',
+    AngularBuild: '^21.0.0-next.5+sha-05ba42e',
+    AngularSSR: '^21.0.0-next.5+sha-05ba42e',
 };
