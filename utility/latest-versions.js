70e0520fb1f0ab1e72a2f3f5366adbebc012fd52
Angular Builds <dev-infra@angular.dev>
2025-09-25 21:54:28 +0000

    b69d85b refactor(@angular/cli): introduce new package manager abstraction

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.5+sha-05ba42e',
-    AngularBuild: '^21.0.0-next.5+sha-05ba42e',
-    AngularSSR: '^21.0.0-next.5+sha-05ba42e',
+    DevkitBuildAngular: '^21.0.0-next.5+sha-b69d85b',
+    AngularBuild: '^21.0.0-next.5+sha-b69d85b',
+    AngularSSR: '^21.0.0-next.5+sha-b69d85b',
 };
