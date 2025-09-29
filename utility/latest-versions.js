a72eeeedc2f89eef4075ee8012ae7f84a5f6fcc9
Angular Builds <dev-infra@angular.dev>
2025-09-29 16:23:52 +0000

    57075a3 fix(@schematics/angular): use bracket notation for `process.env['pm_id']`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.5+sha-bfd0d9e',
-    AngularBuild: '^21.0.0-next.5+sha-bfd0d9e',
-    AngularSSR: '^21.0.0-next.5+sha-bfd0d9e',
+    DevkitBuildAngular: '^21.0.0-next.5+sha-57075a3',
+    AngularBuild: '^21.0.0-next.5+sha-57075a3',
+    AngularSSR: '^21.0.0-next.5+sha-57075a3',
 };
