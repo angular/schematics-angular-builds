bdce58d685d293bc6dc3006faba4d101a66e27c3
Angular Builds <dev-infra@angular.dev>
2025-09-29 15:35:54 +0000

    c94bf7f fix(@schematics/angular): Out of the box support for PM2

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.3+sha-0b5481e',
-    AngularBuild: '^20.3.3+sha-0b5481e',
-    AngularSSR: '^20.3.3+sha-0b5481e',
+    DevkitBuildAngular: '^20.3.3+sha-c94bf7f',
+    AngularBuild: '^20.3.3+sha-c94bf7f',
+    AngularSSR: '^20.3.3+sha-c94bf7f',
 };
