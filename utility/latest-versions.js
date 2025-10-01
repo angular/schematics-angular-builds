f65c6480708335cdb92a0da5f7cd7c5944844e8e
Angular Builds <dev-infra@angular.dev>
2025-10-01 13:08:46 +0000

    c14ebe7 build: update ossf/scorecard-action action to v2.4.3

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.3+sha-ee2e4ee',
-    AngularBuild: '^20.3.3+sha-ee2e4ee',
-    AngularSSR: '^20.3.3+sha-ee2e4ee',
+    DevkitBuildAngular: '^20.3.3+sha-c14ebe7',
+    AngularBuild: '^20.3.3+sha-c14ebe7',
+    AngularSSR: '^20.3.3+sha-c14ebe7',
 };
