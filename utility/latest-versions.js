efb3d9513a5d01770a544385479da53e5da19618
Angular Builds <dev-infra@angular.dev>
2025-10-01 15:36:42 +0000

    4779fe3 refactor(@angular/build): switch back to rollup for chunk optimizer

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.3+sha-c14ebe7',
-    AngularBuild: '^20.3.3+sha-c14ebe7',
-    AngularSSR: '^20.3.3+sha-c14ebe7',
+    DevkitBuildAngular: '^20.3.3+sha-4779fe3',
+    AngularBuild: '^20.3.3+sha-4779fe3',
+    AngularSSR: '^20.3.3+sha-4779fe3',
 };
