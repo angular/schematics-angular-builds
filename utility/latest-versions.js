1060f0053240b89647f13c8fb289e12ed86997e5
Angular Builds <dev-infra@angular.dev>
2025-10-03 12:49:45 +0000

    e469d52 release: cut the v21.0.0-next.6 release

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.5+sha-3e0209d',
-    AngularBuild: '^21.0.0-next.5+sha-3e0209d',
-    AngularSSR: '^21.0.0-next.5+sha-3e0209d',
+    DevkitBuildAngular: '^21.0.0-next.6+sha-e469d52',
+    AngularBuild: '^21.0.0-next.6+sha-e469d52',
+    AngularSSR: '^21.0.0-next.6+sha-e469d52',
 };
