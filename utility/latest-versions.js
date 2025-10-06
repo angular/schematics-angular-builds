83fa4b2e8ad4e228e1070ee23bb7c4483c53b4e8
Angular Builds <dev-infra@angular.dev>
2025-10-06 14:57:08 +0000

    7362199 fix(@angular/build): normalize paths for Vitest runner output files

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.6+sha-e4fa5f3',
-    AngularBuild: '^21.0.0-next.6+sha-e4fa5f3',
-    AngularSSR: '^21.0.0-next.6+sha-e4fa5f3',
+    DevkitBuildAngular: '^21.0.0-next.6+sha-7362199',
+    AngularBuild: '^21.0.0-next.6+sha-7362199',
+    AngularSSR: '^21.0.0-next.6+sha-7362199',
 };
