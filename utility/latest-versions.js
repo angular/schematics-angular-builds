65ef74a5c0d4654481a1346405a2fafff349160a
Angular Builds <dev-infra@angular.dev>
2025-10-06 11:43:46 +0000

    104c907 feat(@angular/cli): enhance `ng version` output with more details

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.6+sha-07bb777',
-    AngularBuild: '^21.0.0-next.6+sha-07bb777',
-    AngularSSR: '^21.0.0-next.6+sha-07bb777',
+    DevkitBuildAngular: '^21.0.0-next.6+sha-104c907',
+    AngularBuild: '^21.0.0-next.6+sha-104c907',
+    AngularSSR: '^21.0.0-next.6+sha-104c907',
 };
