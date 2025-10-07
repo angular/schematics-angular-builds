cb8b9185702fa5ea139ab8a55ef40a924feee83b
Angular Builds <dev-infra@angular.dev>
2025-10-07 09:44:31 +0000

    3bfa1e9 build: clean up BUILD files and update dependencies

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.6+sha-1f5d80b',
-    AngularBuild: '^21.0.0-next.6+sha-1f5d80b',
-    AngularSSR: '^21.0.0-next.6+sha-1f5d80b',
+    DevkitBuildAngular: '^21.0.0-next.6+sha-3bfa1e9',
+    AngularBuild: '^21.0.0-next.6+sha-3bfa1e9',
+    AngularSSR: '^21.0.0-next.6+sha-3bfa1e9',
 };
