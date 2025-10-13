b10de58c3c85d6f5330506548847816ca259bdd3
Angular Builds <dev-infra@angular.dev>
2025-10-13 10:50:25 +0000

    a90bea5 feat(@angular/build): support `.test.ts` files by default in unit test builder

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.7+sha-8f0f6a5',
-    AngularBuild: '^21.0.0-next.7+sha-8f0f6a5',
-    AngularSSR: '^21.0.0-next.7+sha-8f0f6a5',
+    DevkitBuildAngular: '^21.0.0-next.7+sha-a90bea5',
+    AngularBuild: '^21.0.0-next.7+sha-a90bea5',
+    AngularSSR: '^21.0.0-next.7+sha-a90bea5',
 };
 //# sourceMappingURL=latest-versions.js.map
