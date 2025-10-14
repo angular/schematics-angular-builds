9aa7077fe8ac95ae48a1d64fdff09c2b9564a423
Angular Builds <dev-infra@angular.dev>
2025-10-14 17:34:58 +0000

    92ddc42 test(@schematics/angular): add additional test coverage for jasmine-to-vitest schematic

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.7+sha-629f5cb',
-    AngularBuild: '^21.0.0-next.7+sha-629f5cb',
-    AngularSSR: '^21.0.0-next.7+sha-629f5cb',
+    DevkitBuildAngular: '^21.0.0-next.7+sha-92ddc42',
+    AngularBuild: '^21.0.0-next.7+sha-92ddc42',
+    AngularSSR: '^21.0.0-next.7+sha-92ddc42',
 };
 //# sourceMappingURL=latest-versions.js.map
