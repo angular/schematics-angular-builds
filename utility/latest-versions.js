f2c5bc87f4d742c8669078d5cefb9c3195c43ef3
Angular Builds <dev-infra@angular.dev>
2025-10-15 17:28:50 +0000

    5271547 fix(@angular/ssr): prevent malicious URL from overriding host

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.5+sha-16656a1',
-    AngularBuild: '^20.3.5+sha-16656a1',
-    AngularSSR: '^20.3.5+sha-16656a1',
+    DevkitBuildAngular: '^20.3.5+sha-5271547',
+    AngularBuild: '^20.3.5+sha-5271547',
+    AngularSSR: '^20.3.5+sha-5271547',
 };
