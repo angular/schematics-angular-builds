18f1a5820e568f607e07bf22d65f68e841c229ff
Angular Builds <dev-infra@angular.dev>
2025-10-15 11:35:55 +0000

    03e3bfc docs(@angular-devkit/build-angular): add descriptions for browser builder options

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.5+sha-82f8900',
-    AngularBuild: '^20.3.5+sha-82f8900',
-    AngularSSR: '^20.3.5+sha-82f8900',
+    DevkitBuildAngular: '^20.3.5+sha-03e3bfc',
+    AngularBuild: '^20.3.5+sha-03e3bfc',
+    AngularSSR: '^20.3.5+sha-03e3bfc',
 };
