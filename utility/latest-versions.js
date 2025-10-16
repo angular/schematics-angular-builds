ba1cfcea2f40ad86de9275f8e110340da785889b
Angular Builds <dev-infra@angular.dev>
2025-10-16 01:57:43 +0000

    53180a8 refactor(@angular/cli): make list_projects tool resilient to symlink loops

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.8+sha-45024e8',
-    AngularBuild: '^21.0.0-next.8+sha-45024e8',
-    AngularSSR: '^21.0.0-next.8+sha-45024e8',
+    DevkitBuildAngular: '^21.0.0-next.8+sha-53180a8',
+    AngularBuild: '^21.0.0-next.8+sha-53180a8',
+    AngularSSR: '^21.0.0-next.8+sha-53180a8',
 };
 //# sourceMappingURL=latest-versions.js.map
