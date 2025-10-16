5cdff4413369542ed002b7eb44286cd3f8e55cb7
Angular Builds <dev-infra@angular.dev>
2025-10-16 13:32:27 +0000

    85c18b4 fix(@angular/ssr): correctly handle routes with matrix parameters

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.8+sha-ad80848',
-    AngularBuild: '^21.0.0-next.8+sha-ad80848',
-    AngularSSR: '^21.0.0-next.8+sha-ad80848',
+    DevkitBuildAngular: '^21.0.0-next.8+sha-85c18b4',
+    AngularBuild: '^21.0.0-next.8+sha-85c18b4',
+    AngularSSR: '^21.0.0-next.8+sha-85c18b4',
 };
 //# sourceMappingURL=latest-versions.js.map
