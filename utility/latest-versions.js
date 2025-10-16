b7cf90efc25732888f5faef00dccb85f44ebc843
Angular Builds <dev-infra@angular.dev>
2025-10-16 16:58:34 +0000

    58dcfd1 fix(@angular/ssr): ensure server-side navigation triggers a redirect

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.8+sha-85c18b4',
-    AngularBuild: '^21.0.0-next.8+sha-85c18b4',
-    AngularSSR: '^21.0.0-next.8+sha-85c18b4',
+    DevkitBuildAngular: '^21.0.0-next.8+sha-58dcfd1',
+    AngularBuild: '^21.0.0-next.8+sha-58dcfd1',
+    AngularSSR: '^21.0.0-next.8+sha-58dcfd1',
 };
 //# sourceMappingURL=latest-versions.js.map
