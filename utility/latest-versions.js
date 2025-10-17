ff794b4cada01c1279ed5aec59c8d5414d6deb00
Angular Builds <dev-infra@angular.dev>
2025-10-17 15:17:28 +0000

    b1d6d2f fix(@angular/build): resolve Angular locale data namespace in esbuild

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.8+sha-44d9539',
-    AngularBuild: '^21.0.0-next.8+sha-44d9539',
-    AngularSSR: '^21.0.0-next.8+sha-44d9539',
+    DevkitBuildAngular: '^21.0.0-next.8+sha-b1d6d2f',
+    AngularBuild: '^21.0.0-next.8+sha-b1d6d2f',
+    AngularSSR: '^21.0.0-next.8+sha-b1d6d2f',
 };
 //# sourceMappingURL=latest-versions.js.map
