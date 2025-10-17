ceb40b74ee255e50f0b6851d934b23f98f50289b
Angular Builds <dev-infra@angular.dev>
2025-10-17 20:47:53 +0000

    ec739d7 refactor(@angular/build): allow Bazel to inject a custom esbuild plugin

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.8+sha-6639085',
-    AngularBuild: '^21.0.0-next.8+sha-6639085',
-    AngularSSR: '^21.0.0-next.8+sha-6639085',
+    DevkitBuildAngular: '^21.0.0-next.8+sha-ec739d7',
+    AngularBuild: '^21.0.0-next.8+sha-ec739d7',
+    AngularSSR: '^21.0.0-next.8+sha-ec739d7',
 };
 //# sourceMappingURL=latest-versions.js.map
