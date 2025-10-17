52a8902b64db914f11b0acc1b045de33b8cdefaf
Angular Builds <dev-infra@angular.dev>
2025-10-17 02:42:53 +0000

    3040b77 feat(@angular/cli): add style language detection to list_projects tool

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.8+sha-de17f18',
-    AngularBuild: '^21.0.0-next.8+sha-de17f18',
-    AngularSSR: '^21.0.0-next.8+sha-de17f18',
+    DevkitBuildAngular: '^21.0.0-next.8+sha-3040b77',
+    AngularBuild: '^21.0.0-next.8+sha-3040b77',
+    AngularSSR: '^21.0.0-next.8+sha-3040b77',
 };
 //# sourceMappingURL=latest-versions.js.map
