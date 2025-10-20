8dc24921cea914903fcb57abb9581e539f089a41
Angular Builds <dev-infra@angular.dev>
2025-10-20 15:59:41 +0000

    aec58b8 build: set `include_npm` to `true`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.8+sha-848b4c2',
-    AngularBuild: '^21.0.0-next.8+sha-848b4c2',
-    AngularSSR: '^21.0.0-next.8+sha-848b4c2',
+    DevkitBuildAngular: '^21.0.0-next.8+sha-aec58b8',
+    AngularBuild: '^21.0.0-next.8+sha-aec58b8',
+    AngularSSR: '^21.0.0-next.8+sha-aec58b8',
 };
 //# sourceMappingURL=latest-versions.js.map
