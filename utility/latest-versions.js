2f6f9731a5c1a8b26810b0b0daee22cf3940443f
Angular Builds <dev-infra@angular.dev>
2025-10-20 11:35:33 +0000

    341057f build: switch to @aspect_rules_jasmine for jasmine_test

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.8+sha-ab3249e',
-    AngularBuild: '^21.0.0-next.8+sha-ab3249e',
-    AngularSSR: '^21.0.0-next.8+sha-ab3249e',
+    DevkitBuildAngular: '^21.0.0-next.8+sha-341057f',
+    AngularBuild: '^21.0.0-next.8+sha-341057f',
+    AngularSSR: '^21.0.0-next.8+sha-341057f',
 };
 //# sourceMappingURL=latest-versions.js.map
