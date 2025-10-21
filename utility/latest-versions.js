bb29cbf551f589af2cd6d1dde00a69818096db91
Angular Builds <dev-infra@angular.dev>
2025-10-21 16:03:35 +0000

    a31533c fix(@angular-devkit/schematics): respect `--force` option when schematic contains `host.create`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.6+sha-72bc4da',
-    AngularBuild: '^20.3.6+sha-72bc4da',
-    AngularSSR: '^20.3.6+sha-72bc4da',
+    DevkitBuildAngular: '^20.3.6+sha-a31533c',
+    AngularBuild: '^20.3.6+sha-a31533c',
+    AngularSSR: '^20.3.6+sha-a31533c',
 };
