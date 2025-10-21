87865e0ed9f75548ce6823755520671930fec546
Angular Builds <dev-infra@angular.dev>
2025-10-21 09:39:46 +0000

    d6a986f build: update rules_browsers digest to 6a699bf

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.8+sha-aec58b8',
-    AngularBuild: '^21.0.0-next.8+sha-aec58b8',
-    AngularSSR: '^21.0.0-next.8+sha-aec58b8',
+    DevkitBuildAngular: '^21.0.0-next.8+sha-d6a986f',
+    AngularBuild: '^21.0.0-next.8+sha-d6a986f',
+    AngularSSR: '^21.0.0-next.8+sha-d6a986f',
 };
 //# sourceMappingURL=latest-versions.js.map
