749c5a9133946d860c0a4614310cfd606f7338d7
Angular Builds <dev-infra@angular.dev>
2025-10-22 20:35:47 +0000

    a660d69 refactor(@schematics/angular): add trailing commas and remove leading commas in component template

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.8+sha-b524ba4',
-    AngularBuild: '^21.0.0-next.8+sha-b524ba4',
-    AngularSSR: '^21.0.0-next.8+sha-b524ba4',
+    DevkitBuildAngular: '^21.0.0-next.8+sha-a660d69',
+    AngularBuild: '^21.0.0-next.8+sha-a660d69',
+    AngularSSR: '^21.0.0-next.8+sha-a660d69',
 };
 //# sourceMappingURL=latest-versions.js.map
