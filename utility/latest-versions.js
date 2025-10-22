1ec08cc818e11309fda700a14e8e5e8f167f9d87
Angular Builds <dev-infra@angular.dev>
2025-10-22 01:20:04 +0000

    9f59b77 refactor(@angular/build): make `runner` optional in unit-test builder The `runner` option in the `unit-test` builder is now optional to streamline the user configuration.

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.8+sha-bf468e1',
-    AngularBuild: '^21.0.0-next.8+sha-bf468e1',
-    AngularSSR: '^21.0.0-next.8+sha-bf468e1',
+    DevkitBuildAngular: '^21.0.0-next.8+sha-9f59b77',
+    AngularBuild: '^21.0.0-next.8+sha-9f59b77',
+    AngularSSR: '^21.0.0-next.8+sha-9f59b77',
 };
 //# sourceMappingURL=latest-versions.js.map
