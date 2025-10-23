2cda7e7d9bec46d57d1ba6425a1e04975955175e
Angular Builds <dev-infra@angular.dev>
2025-10-23 14:38:35 +0000

    b79f8fb build: remove `minimumReleaseAgeExclude` for `vitest`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.9+sha-107dcc8',
-    AngularBuild: '^21.0.0-next.9+sha-107dcc8',
-    AngularSSR: '^21.0.0-next.9+sha-107dcc8',
+    DevkitBuildAngular: '^21.0.0-next.9+sha-b79f8fb',
+    AngularBuild: '^21.0.0-next.9+sha-b79f8fb',
+    AngularSSR: '^21.0.0-next.9+sha-b79f8fb',
 };
 //# sourceMappingURL=latest-versions.js.map
