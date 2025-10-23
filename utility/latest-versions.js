043441903ba87136c1330f1763b175a8e1dc388e
Angular Builds <dev-infra@angular.dev>
2025-10-23 20:44:44 +0000

    49f12fd fix(@schematics/angular): correct style guide paths for standalone components

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.0+sha-ffa7266',
-    AngularBuild: '^21.1.0-next.0+sha-ffa7266',
-    AngularSSR: '^21.1.0-next.0+sha-ffa7266',
+    DevkitBuildAngular: '^21.1.0-next.0+sha-49f12fd',
+    AngularBuild: '^21.1.0-next.0+sha-49f12fd',
+    AngularSSR: '^21.1.0-next.0+sha-49f12fd',
 };
 //# sourceMappingURL=latest-versions.js.map
