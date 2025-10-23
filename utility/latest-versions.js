f26a8f98cc8a20d7bdc4d0fbb39dfa784117b732
Angular Builds <dev-infra@angular.dev>
2025-10-23 16:23:26 +0000

    9b93e26 build: enable auto merge strategy for pull requests

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.9+sha-9d7cf4f',
-    AngularBuild: '^21.0.0-next.9+sha-9d7cf4f',
-    AngularSSR: '^21.0.0-next.9+sha-9d7cf4f',
+    DevkitBuildAngular: '^21.0.0-next.9+sha-9b93e26',
+    AngularBuild: '^21.0.0-next.9+sha-9b93e26',
+    AngularSSR: '^21.0.0-next.9+sha-9b93e26',
 };
 //# sourceMappingURL=latest-versions.js.map
