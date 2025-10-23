f56605ba44944937ae2a88dd3db480501ac261da
Angular Builds <dev-infra@angular.dev>
2025-10-23 05:53:53 +0000

    bdc584b build: update dependency aspect_rules_js to v2.7.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.7+sha-3cea706',
-    AngularBuild: '^20.3.7+sha-3cea706',
-    AngularSSR: '^20.3.7+sha-3cea706',
+    DevkitBuildAngular: '^20.3.7+sha-bdc584b',
+    AngularBuild: '^20.3.7+sha-bdc584b',
+    AngularSSR: '^20.3.7+sha-bdc584b',
 };
