4901a1ee39d3e03d365e61353a155e9efa14a24d
Angular Builds <dev-infra@angular.dev>
2025-10-23 12:28:38 +0000

    813cba9 fix(@angular-devkit/build-angular): expand jest and jest-environment-jsdom to allow version 30

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.7+sha-7b521dd',
-    AngularBuild: '^20.3.7+sha-7b521dd',
-    AngularSSR: '^20.3.7+sha-7b521dd',
+    DevkitBuildAngular: '^20.3.7+sha-813cba9',
+    AngularBuild: '^20.3.7+sha-813cba9',
+    AngularSSR: '^20.3.7+sha-813cba9',
 };
