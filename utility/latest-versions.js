3cf0b8d974558b093a209a18acc7b2cc09f5d4da
Angular Builds <dev-infra@angular.dev>
2025-10-24 11:59:27 +0000

    f2248ba fix(@angular/build): add --ui option for Vitest runner

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.0+sha-1dfb8c2',
-    AngularBuild: '^21.1.0-next.0+sha-1dfb8c2',
-    AngularSSR: '^21.1.0-next.0+sha-1dfb8c2',
+    DevkitBuildAngular: '^21.1.0-next.0+sha-f2248ba',
+    AngularBuild: '^21.1.0-next.0+sha-f2248ba',
+    AngularSSR: '^21.1.0-next.0+sha-f2248ba',
 };
 //# sourceMappingURL=latest-versions.js.map
