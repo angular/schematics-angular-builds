a0c24964c015e8f7516b9266239baa12d60524ce
Angular Builds <dev-infra@angular.dev>
2025-10-27 13:28:12 +0000

    14f056e build: update rules_angular digest to 1f1aa3e

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.0+sha-30bedd2',
-    AngularBuild: '^21.1.0-next.0+sha-30bedd2',
-    AngularSSR: '^21.1.0-next.0+sha-30bedd2',
+    DevkitBuildAngular: '^21.1.0-next.0+sha-14f056e',
+    AngularBuild: '^21.1.0-next.0+sha-14f056e',
+    AngularSSR: '^21.1.0-next.0+sha-14f056e',
 };
 //# sourceMappingURL=latest-versions.js.map
