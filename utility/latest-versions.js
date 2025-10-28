9a05b413414b9d8d3b7e60f782d54c398f00b7f2
Angular Builds <dev-infra@angular.dev>
2025-10-28 11:56:11 +0000

    e6ccc74 refactor(@schematics/angular): add trailing comma to generated services

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.7+sha-2c7581c',
-    AngularBuild: '^20.3.7+sha-2c7581c',
-    AngularSSR: '^20.3.7+sha-2c7581c',
+    DevkitBuildAngular: '^20.3.7+sha-e6ccc74',
+    AngularBuild: '^20.3.7+sha-e6ccc74',
+    AngularSSR: '^20.3.7+sha-e6ccc74',
 };
