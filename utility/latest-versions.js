8cc53e70496ca6f9c8d07b3e98f8526b1d7e2f29
Angular Builds <dev-infra@angular.dev>
2025-10-28 19:55:01 +0000

    cc16b10 fix(@angular/build): handle redirects from guards during prerendering

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.0+sha-50de34f',
-    AngularBuild: '^21.1.0-next.0+sha-50de34f',
-    AngularSSR: '^21.1.0-next.0+sha-50de34f',
+    DevkitBuildAngular: '^21.1.0-next.0+sha-cc16b10',
+    AngularBuild: '^21.1.0-next.0+sha-cc16b10',
+    AngularSSR: '^21.1.0-next.0+sha-cc16b10',
 };
 //# sourceMappingURL=latest-versions.js.map
