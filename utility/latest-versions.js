eeee3a8fcb372d37dba36fb87df9ef1cabeb22a9
Angular Builds <dev-infra@angular.dev>
2025-10-29 17:38:37 +0000

    0fe6db9 fix(@angular/build): introduce vitest-base.config for test configuration

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.0+sha-4395c5b',
-    AngularBuild: '^21.1.0-next.0+sha-4395c5b',
-    AngularSSR: '^21.1.0-next.0+sha-4395c5b',
+    DevkitBuildAngular: '^21.1.0-next.0+sha-0fe6db9',
+    AngularBuild: '^21.1.0-next.0+sha-0fe6db9',
+    AngularSSR: '^21.1.0-next.0+sha-0fe6db9',
 };
 //# sourceMappingURL=latest-versions.js.map
