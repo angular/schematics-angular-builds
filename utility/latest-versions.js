f136ecf17c89af9910adb5418c4e5c3ab350143f
Angular Builds <dev-infra@angular.dev>
2025-10-29 16:48:05 +0000

    4211534 fix(@angular/build): add `define` option to dev-server

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.0+sha-f8983bd',
-    AngularBuild: '^21.1.0-next.0+sha-f8983bd',
-    AngularSSR: '^21.1.0-next.0+sha-f8983bd',
+    DevkitBuildAngular: '^21.1.0-next.0+sha-4211534',
+    AngularBuild: '^21.1.0-next.0+sha-4211534',
+    AngularSSR: '^21.1.0-next.0+sha-4211534',
 };
 //# sourceMappingURL=latest-versions.js.map
