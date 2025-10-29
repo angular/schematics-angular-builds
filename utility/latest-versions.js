0076b542aa8bd8f948fb217957133abec893bbe1
Angular Builds <dev-infra@angular.dev>
2025-10-29 18:20:06 +0000

    026a9c1 refactor(@schematics/angular): add trailing commas to generated directive, pipe, and guard files

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-next.9+sha-bab5806',
-    AngularBuild: '^21.0.0-next.9+sha-bab5806',
-    AngularSSR: '^21.0.0-next.9+sha-bab5806',
+    DevkitBuildAngular: '^21.0.0-next.9+sha-026a9c1',
+    AngularBuild: '^21.0.0-next.9+sha-026a9c1',
+    AngularSSR: '^21.0.0-next.9+sha-026a9c1',
 };
 //# sourceMappingURL=latest-versions.js.map
