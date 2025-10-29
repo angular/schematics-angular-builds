c7022ffa4c4e0fb61d9d8ae739d4ebffa0a489b8
Angular Builds <dev-infra@angular.dev>
2025-10-29 17:15:34 +0000

    4395c5b refactor(@angular/build): remove Angular packages from externals for browser tests

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.0+sha-4211534',
-    AngularBuild: '^21.1.0-next.0+sha-4211534',
-    AngularSSR: '^21.1.0-next.0+sha-4211534',
+    DevkitBuildAngular: '^21.1.0-next.0+sha-4395c5b',
+    AngularBuild: '^21.1.0-next.0+sha-4395c5b',
+    AngularSSR: '^21.1.0-next.0+sha-4395c5b',
 };
 //# sourceMappingURL=latest-versions.js.map
