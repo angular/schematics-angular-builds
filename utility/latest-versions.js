8a01fa28ca885a9f04d21fe881dcb8963f678e81
Angular Builds <dev-infra@angular.dev>
2025-10-29 13:16:45 +0000

    542973a fix(@angular/build): add adapters to new reporter

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.7+sha-45e498f',
-    AngularBuild: '^20.3.7+sha-45e498f',
-    AngularSSR: '^20.3.7+sha-45e498f',
+    DevkitBuildAngular: '^20.3.7+sha-542973a',
+    AngularBuild: '^20.3.7+sha-542973a',
+    AngularSSR: '^20.3.7+sha-542973a',
 };
