fd8b47f03b9d86374f37a602e187033188989147
Angular Builds <dev-infra@angular.dev>
2025-11-03 21:59:37 +0000

    dfb4242 fix(@angular/cli): add vitest to version command output

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-rc.0+sha-8694369',
-    AngularBuild: '^21.0.0-rc.0+sha-8694369',
-    AngularSSR: '^21.0.0-rc.0+sha-8694369',
+    DevkitBuildAngular: '^21.0.0-rc.0+sha-dfb4242',
+    AngularBuild: '^21.0.0-rc.0+sha-dfb4242',
+    AngularSSR: '^21.0.0-rc.0+sha-dfb4242',
 };
 //# sourceMappingURL=latest-versions.js.map
