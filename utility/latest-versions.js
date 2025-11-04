07bcd5cb9788d559ce8cef3418349afaf37bfe4d
Angular Builds <dev-infra@angular.dev>
2025-11-04 14:53:43 +0000

    683697e fix(@angular/ssr): improve route matching for wildcard routes

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.8+sha-4ac89c5',
-    AngularBuild: '^20.3.8+sha-4ac89c5',
-    AngularSSR: '^20.3.8+sha-4ac89c5',
+    DevkitBuildAngular: '^20.3.8+sha-683697e',
+    AngularBuild: '^20.3.8+sha-683697e',
+    AngularSSR: '^20.3.8+sha-683697e',
 };
