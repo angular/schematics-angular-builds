b3ade5e2479ce7799c6813fa463d8bc59f008b87
Angular Builds <dev-infra@angular.dev>
2025-11-05 10:39:27 +0000

    5dd4f8f refactor(@angular/build): improve vitest `runnerConfig` support via plugin

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-rc.0+sha-5f4cbe8',
-    AngularBuild: '^21.0.0-rc.0+sha-5f4cbe8',
-    AngularSSR: '^21.0.0-rc.0+sha-5f4cbe8',
+    DevkitBuildAngular: '^21.0.0-rc.0+sha-5dd4f8f',
+    AngularBuild: '^21.0.0-rc.0+sha-5dd4f8f',
+    AngularSSR: '^21.0.0-rc.0+sha-5dd4f8f',
 };
 //# sourceMappingURL=latest-versions.js.map
