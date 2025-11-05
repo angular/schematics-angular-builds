81c8a9729e2cb2ffa6fe9ac9e1cec9fc0decbb91
Angular Builds <dev-infra@angular.dev>
2025-11-05 22:36:30 +0000

    50072cd fix(@angular/build): add webcontainer support for Vitest browser provider

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.0+sha-ea4fb9d',
-    AngularBuild: '^21.1.0-next.0+sha-ea4fb9d',
-    AngularSSR: '^21.1.0-next.0+sha-ea4fb9d',
+    DevkitBuildAngular: '^21.1.0-next.0+sha-50072cd',
+    AngularBuild: '^21.1.0-next.0+sha-50072cd',
+    AngularSSR: '^21.1.0-next.0+sha-50072cd',
 };
 //# sourceMappingURL=latest-versions.js.map
