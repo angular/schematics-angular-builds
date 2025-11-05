44a0f258282ecc05278a69041f063858a3782f36
Angular Builds <dev-infra@angular.dev>
2025-11-05 18:12:48 +0000

    ea4fb9d fix(@angular/cli): promote zoneless migration MCP tool to stable

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.0+sha-11569c2',
-    AngularBuild: '^21.1.0-next.0+sha-11569c2',
-    AngularSSR: '^21.1.0-next.0+sha-11569c2',
+    DevkitBuildAngular: '^21.1.0-next.0+sha-ea4fb9d',
+    AngularBuild: '^21.1.0-next.0+sha-ea4fb9d',
+    AngularSSR: '^21.1.0-next.0+sha-ea4fb9d',
 };
 //# sourceMappingURL=latest-versions.js.map
