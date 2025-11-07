7eb5b8c0ab8bfd53db6511ff312f3c185093163c
Angular Builds <dev-infra@angular.dev>
2025-11-07 12:09:49 +0000

    a9c700a refactor(@angular/cli): import type improvements & mcp cleanups

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.0+sha-8a17fe8',
-    AngularBuild: '^21.1.0-next.0+sha-8a17fe8',
-    AngularSSR: '^21.1.0-next.0+sha-8a17fe8',
+    DevkitBuildAngular: '^21.1.0-next.0+sha-a9c700a',
+    AngularBuild: '^21.1.0-next.0+sha-a9c700a',
+    AngularSSR: '^21.1.0-next.0+sha-a9c700a',
 };
 //# sourceMappingURL=latest-versions.js.map
