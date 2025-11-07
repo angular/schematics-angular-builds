344d36350cfa6f03ad6127fa2e36564b3cfe1113
Angular Builds <dev-infra@angular.dev>
2025-11-07 18:16:53 +0000

    b3908f6 fix(@angular/build): do not remove `@angular/localize` when having external packages (#31721)

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.9+sha-6fbd106',
-    AngularBuild: '^20.3.9+sha-6fbd106',
-    AngularSSR: '^20.3.9+sha-6fbd106',
+    DevkitBuildAngular: '^20.3.9+sha-b3908f6',
+    AngularBuild: '^20.3.9+sha-b3908f6',
+    AngularSSR: '^20.3.9+sha-b3908f6',
 };
