21caf8eaeda7c65fc05450751d11221f9da28abb
Angular Builds <dev-infra@angular.dev>
2025-11-07 10:58:55 +0000

    3a7b328 refactor: fix several typos (#31716)

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-rc.1+sha-0497b01',
-    AngularBuild: '^21.0.0-rc.1+sha-0497b01',
-    AngularSSR: '^21.0.0-rc.1+sha-0497b01',
+    DevkitBuildAngular: '^21.0.0-rc.1+sha-3a7b328',
+    AngularBuild: '^21.0.0-rc.1+sha-3a7b328',
+    AngularSSR: '^21.0.0-rc.1+sha-3a7b328',
 };
 //# sourceMappingURL=latest-versions.js.map
