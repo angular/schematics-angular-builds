bac2e8fbc9dc46f3e612225f981058b999f866a9
Angular Builds <dev-infra@angular.dev>
2025-11-08 08:38:01 +0000

    cc2668f fix(@angular/build): simplify SSL handling for `ng serve` with SSR (#31723)

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-rc.1+sha-b76962d',
-    AngularBuild: '^21.0.0-rc.1+sha-b76962d',
-    AngularSSR: '^21.0.0-rc.1+sha-b76962d',
+    DevkitBuildAngular: '^21.0.0-rc.1+sha-cc2668f',
+    AngularBuild: '^21.0.0-rc.1+sha-cc2668f',
+    AngularSSR: '^21.0.0-rc.1+sha-cc2668f',
 };
 //# sourceMappingURL=latest-versions.js.map
