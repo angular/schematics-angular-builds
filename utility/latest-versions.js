d3195228bfae5a0c04ea2d648afb6c607320354f
Angular Builds <dev-infra@angular.dev>
2025-11-08 10:36:32 +0000

    53bdbf1 fix(@angular/build): dynamically select Vitest DOM environment

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.0+sha-ad99e00',
-    AngularBuild: '^21.1.0-next.0+sha-ad99e00',
-    AngularSSR: '^21.1.0-next.0+sha-ad99e00',
+    DevkitBuildAngular: '^21.1.0-next.0+sha-53bdbf1',
+    AngularBuild: '^21.1.0-next.0+sha-53bdbf1',
+    AngularSSR: '^21.1.0-next.0+sha-53bdbf1',
 };
 //# sourceMappingURL=latest-versions.js.map
