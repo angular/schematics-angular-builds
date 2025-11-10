9513b25c3c38e5295fbc2490882200a56a852565
Angular Builds <dev-infra@angular.dev>
2025-11-10 07:33:24 +0000

    c854a71 fix(@schematics/angular): correct `tsconfig.spec.json` include for spec files

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.9+sha-b3908f6',
-    AngularBuild: '^20.3.9+sha-b3908f6',
-    AngularSSR: '^20.3.9+sha-b3908f6',
+    DevkitBuildAngular: '^20.3.9+sha-c854a71',
+    AngularBuild: '^20.3.9+sha-c854a71',
+    AngularSSR: '^20.3.9+sha-c854a71',
 };
