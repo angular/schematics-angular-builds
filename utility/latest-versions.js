45533b351174e7906a47ddf99e3a04e860379c71
Angular Builds <dev-infra@angular.dev>
2025-11-13 09:56:36 +0000

    dea6ced refactor: address issue with bazel resolution of peer dependencies

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-rc.1+sha-4d2809c',
-    AngularBuild: '^21.0.0-rc.1+sha-4d2809c',
-    AngularSSR: '^21.0.0-rc.1+sha-4d2809c',
+    DevkitBuildAngular: '^21.0.0-rc.2+sha-dea6ced',
+    AngularBuild: '^21.0.0-rc.2+sha-dea6ced',
+    AngularSSR: '^21.0.0-rc.2+sha-dea6ced',
 };
 //# sourceMappingURL=latest-versions.js.map
