5941123f352feec3f37ec28dc9a2ac9a86cf04b2
Angular Builds <dev-infra@angular.dev>
2025-11-13 19:36:09 +0000

    138649e fix(@angular/build): remove explicit test isolation configuration

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.0+sha-6f2d4a9',
-    AngularBuild: '^21.1.0-next.0+sha-6f2d4a9',
-    AngularSSR: '^21.1.0-next.0+sha-6f2d4a9',
+    DevkitBuildAngular: '^21.1.0-next.0+sha-138649e',
+    AngularBuild: '^21.1.0-next.0+sha-138649e',
+    AngularSSR: '^21.1.0-next.0+sha-138649e',
 };
 //# sourceMappingURL=latest-versions.js.map
