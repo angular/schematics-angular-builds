a89325401ccf010ccd58cd26e8f0882b7f87876b
Angular Builds <dev-infra@angular.dev>
2025-11-13 17:47:47 +0000

    6f2d4a9 refactor(@angular/build): remove deprecated browser-level isolate option

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.0+sha-d74626f',
-    AngularBuild: '^21.1.0-next.0+sha-d74626f',
-    AngularSSR: '^21.1.0-next.0+sha-d74626f',
+    DevkitBuildAngular: '^21.1.0-next.0+sha-6f2d4a9',
+    AngularBuild: '^21.1.0-next.0+sha-6f2d4a9',
+    AngularSSR: '^21.1.0-next.0+sha-6f2d4a9',
 };
 //# sourceMappingURL=latest-versions.js.map
