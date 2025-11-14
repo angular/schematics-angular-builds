19d16a615b4ec5e60a53e7f56af3947991b6a21a
Angular Builds <dev-infra@angular.dev>
2025-11-14 00:20:44 +0000

    8bde4a3 test: stabilize Vitest snapshot E2E test on Windows

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.0+sha-138649e',
-    AngularBuild: '^21.1.0-next.0+sha-138649e',
-    AngularSSR: '^21.1.0-next.0+sha-138649e',
+    DevkitBuildAngular: '^21.1.0-next.0+sha-8bde4a3',
+    AngularBuild: '^21.1.0-next.0+sha-8bde4a3',
+    AngularSSR: '^21.1.0-next.0+sha-8bde4a3',
 };
 //# sourceMappingURL=latest-versions.js.map
