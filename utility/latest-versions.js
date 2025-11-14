54d675e1ccd8331d905e171826308bc567e23738
Angular Builds <dev-infra@angular.dev>
2025-11-14 19:18:01 +0000

    0830f4f fix(@angular/build): ensure TestBed cleanup hooks are always registered

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-rc.3+sha-8d63d59',
-    AngularBuild: '^21.0.0-rc.3+sha-8d63d59',
-    AngularSSR: '^21.0.0-rc.3+sha-8d63d59',
+    DevkitBuildAngular: '^21.0.0-rc.3+sha-0830f4f',
+    AngularBuild: '^21.0.0-rc.3+sha-0830f4f',
+    AngularSSR: '^21.0.0-rc.3+sha-0830f4f',
 };
 //# sourceMappingURL=latest-versions.js.map
