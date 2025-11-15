b4f0e66a884bb5f3fa54d71e123500d7abdda5da
Angular Builds <dev-infra@angular.dev>
2025-11-15 02:20:30 +0000

    49b65ab fix(@angular/build): disable Vitest test isolation by default

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-rc.4+sha-6b2a88a',
-    AngularBuild: '^21.0.0-rc.4+sha-6b2a88a',
-    AngularSSR: '^21.0.0-rc.4+sha-6b2a88a',
+    DevkitBuildAngular: '^21.0.0-rc.4+sha-49b65ab',
+    AngularBuild: '^21.0.0-rc.4+sha-49b65ab',
+    AngularSSR: '^21.0.0-rc.4+sha-49b65ab',
 };
 //# sourceMappingURL=latest-versions.js.map
