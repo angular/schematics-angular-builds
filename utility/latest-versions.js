2ec6dd1a753b1a8ca16f08e603653a9a37fa8a3c
Angular Builds <dev-infra@angular.dev>
2025-11-15 13:32:49 +0000

    bb29254 test(@angular/build): add e2e for a larger project with Vitest

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-rc.4+sha-49b65ab',
-    AngularBuild: '^21.0.0-rc.4+sha-49b65ab',
-    AngularSSR: '^21.0.0-rc.4+sha-49b65ab',
+    DevkitBuildAngular: '^21.0.0-rc.4+sha-bb29254',
+    AngularBuild: '^21.0.0-rc.4+sha-bb29254',
+    AngularSSR: '^21.0.0-rc.4+sha-bb29254',
 };
 //# sourceMappingURL=latest-versions.js.map
