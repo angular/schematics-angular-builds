9227be7e9b6eea6947bdfd2a3ea013f2d0b5b3fe
Angular Builds <dev-infra@angular.dev>
2025-11-17 17:32:34 +0000

    e8feba9 fix(@schematics/angular): add missing typeSeparator to main.ts.template file

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-rc.4+sha-f7c9f26',
-    AngularBuild: '^21.0.0-rc.4+sha-f7c9f26',
-    AngularSSR: '^21.0.0-rc.4+sha-f7c9f26',
+    DevkitBuildAngular: '^21.0.0-rc.4+sha-e8feba9',
+    AngularBuild: '^21.0.0-rc.4+sha-e8feba9',
+    AngularSSR: '^21.0.0-rc.4+sha-e8feba9',
 };
 //# sourceMappingURL=latest-versions.js.map
