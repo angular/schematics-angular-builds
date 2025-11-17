fac205496e4c3103412841e905875a790c2e8644
Angular Builds <dev-infra@angular.dev>
2025-11-17 12:31:30 +0000

    a11dd31 fix(@angular/build): configure Vitest cache to use Angular cache

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-rc.4+sha-f05ffd1',
-    AngularBuild: '^21.0.0-rc.4+sha-f05ffd1',
-    AngularSSR: '^21.0.0-rc.4+sha-f05ffd1',
+    DevkitBuildAngular: '^21.0.0-rc.4+sha-a11dd31',
+    AngularBuild: '^21.0.0-rc.4+sha-a11dd31',
+    AngularSSR: '^21.0.0-rc.4+sha-a11dd31',
 };
 //# sourceMappingURL=latest-versions.js.map
