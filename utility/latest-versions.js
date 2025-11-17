46ca072fdbccce886d84dcdc786f62da8f5e061b
Angular Builds <dev-infra@angular.dev>
2025-11-17 17:11:56 +0000

    102ca8b test(@angular/build): temporarily disable Vitest coverage check on Windows

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.0+sha-f074f13',
-    AngularBuild: '^21.1.0-next.0+sha-f074f13',
-    AngularSSR: '^21.1.0-next.0+sha-f074f13',
+    DevkitBuildAngular: '^21.1.0-next.0+sha-102ca8b',
+    AngularBuild: '^21.1.0-next.0+sha-102ca8b',
+    AngularSSR: '^21.1.0-next.0+sha-102ca8b',
 };
 //# sourceMappingURL=latest-versions.js.map
