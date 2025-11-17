1b2a2a478ad38f728382f45b132f2310519ae1d4
Angular Builds <dev-infra@angular.dev>
2025-11-17 18:47:35 +0000

    fa5c923 fix(@angular/build): prioritize string type for runnerConfig schema

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-rc.4+sha-e8feba9',
-    AngularBuild: '^21.0.0-rc.4+sha-e8feba9',
-    AngularSSR: '^21.0.0-rc.4+sha-e8feba9',
+    DevkitBuildAngular: '^21.0.0-rc.4+sha-fa5c923',
+    AngularBuild: '^21.0.0-rc.4+sha-fa5c923',
+    AngularSSR: '^21.0.0-rc.4+sha-fa5c923',
 };
 //# sourceMappingURL=latest-versions.js.map
