35cb4fc9a14e1d881a00ce728107fdec408fc777
Angular Builds <dev-infra@angular.dev>
2025-11-17 14:32:33 +0000

    e0a8183 test: improve vitest e2e test stability and performance

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-rc.4+sha-a11dd31',
-    AngularBuild: '^21.0.0-rc.4+sha-a11dd31',
-    AngularSSR: '^21.0.0-rc.4+sha-a11dd31',
+    DevkitBuildAngular: '^21.0.0-rc.4+sha-e0a8183',
+    AngularBuild: '^21.0.0-rc.4+sha-e0a8183',
+    AngularSSR: '^21.0.0-rc.4+sha-e0a8183',
 };
 //# sourceMappingURL=latest-versions.js.map
