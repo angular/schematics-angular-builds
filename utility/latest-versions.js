aac4d923eabd1a3f5fb5b54017fd88180a251f02
Angular Builds <dev-infra@angular.dev>
2025-11-17 11:52:52 +0000

    f05ffd1 fix(@angular/build): correct Vitest coverage include handling for virtual files

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.0.0-rc.4+sha-bb29254',
-    AngularBuild: '^21.0.0-rc.4+sha-bb29254',
-    AngularSSR: '^21.0.0-rc.4+sha-bb29254',
+    DevkitBuildAngular: '^21.0.0-rc.4+sha-f05ffd1',
+    AngularBuild: '^21.0.0-rc.4+sha-f05ffd1',
+    AngularSSR: '^21.0.0-rc.4+sha-f05ffd1',
 };
 //# sourceMappingURL=latest-versions.js.map
