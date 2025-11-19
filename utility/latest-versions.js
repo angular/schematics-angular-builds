3bc528c6215e0e8e1cdf44a9419b65973a64bc7f
Angular Builds <dev-infra@angular.dev>
2025-11-19 23:31:08 +0000

    d097df2 fix(@angular/build): correct Vitest coverage path resolution for JSDOM on Windows

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0',
     NgPackagr: '^21.0.0',
-    DevkitBuildAngular: '^21.0.0+sha-c568c0d',
-    AngularBuild: '^21.0.0+sha-c568c0d',
-    AngularSSR: '^21.0.0+sha-c568c0d',
+    DevkitBuildAngular: '^21.0.0+sha-d097df2',
+    AngularBuild: '^21.0.0+sha-d097df2',
+    AngularSSR: '^21.0.0+sha-d097df2',
 };
 //# sourceMappingURL=latest-versions.js.map
