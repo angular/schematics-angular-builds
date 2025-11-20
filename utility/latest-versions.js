ddbcb818c46c35e75d803e831228e2297874a7a0
Angular Builds <dev-infra@angular.dev>
2025-11-20 10:58:31 +0000

    ee4944a build: add README.md to the SSR `ng_package`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0',
     NgPackagr: '^21.0.0',
-    DevkitBuildAngular: '^21.0.0+sha-a13b07f',
-    AngularBuild: '^21.0.0+sha-a13b07f',
-    AngularSSR: '^21.0.0+sha-a13b07f',
+    DevkitBuildAngular: '^21.0.0+sha-ee4944a',
+    AngularBuild: '^21.0.0+sha-ee4944a',
+    AngularSSR: '^21.0.0+sha-ee4944a',
 };
 //# sourceMappingURL=latest-versions.js.map
