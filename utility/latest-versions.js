ca9984529034f2ff42d4d38550dc2bda95d2944a
Angular Builds <dev-infra@angular.dev>
2025-11-20 08:03:00 +0000

    a13b07f build: update to latest ng-dev tooling for caretaker handoff update

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0',
     NgPackagr: '^21.0.0',
-    DevkitBuildAngular: '^21.0.0+sha-d097df2',
-    AngularBuild: '^21.0.0+sha-d097df2',
-    AngularSSR: '^21.0.0+sha-d097df2',
+    DevkitBuildAngular: '^21.0.0+sha-a13b07f',
+    AngularBuild: '^21.0.0+sha-a13b07f',
+    AngularSSR: '^21.0.0+sha-a13b07f',
 };
 //# sourceMappingURL=latest-versions.js.map
