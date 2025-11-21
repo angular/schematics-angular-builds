062e474c14e17a29dae3ec8e6344c64777348228
Angular Builds <dev-infra@angular.dev>
2025-11-21 18:02:46 +0000

    25bb7e6 fix(@angular/build): ensure correct URL joining for prerender routes

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.11+sha-4dfc314',
-    AngularBuild: '^20.3.11+sha-4dfc314',
-    AngularSSR: '^20.3.11+sha-4dfc314',
+    DevkitBuildAngular: '^20.3.11+sha-25bb7e6',
+    AngularBuild: '^20.3.11+sha-25bb7e6',
+    AngularSSR: '^20.3.11+sha-25bb7e6',
 };
