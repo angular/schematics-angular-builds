4ab22bd3e2b58ed730655d0f96b230446a21d714
Angular Builds <dev-infra@angular.dev>
2025-11-21 14:35:18 +0000

    53012c8 fix(@angular/build): force dev-server to use HTTP/1.1 when using SSR with SSL

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0-next.0',
     NgPackagr: '^21.0.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.0+sha-61a027d',
-    AngularBuild: '^21.1.0-next.0+sha-61a027d',
-    AngularSSR: '^21.1.0-next.0+sha-61a027d',
+    DevkitBuildAngular: '^21.1.0-next.0+sha-53012c8',
+    AngularBuild: '^21.1.0-next.0+sha-53012c8',
+    AngularSSR: '^21.1.0-next.0+sha-53012c8',
 };
 //# sourceMappingURL=latest-versions.js.map
