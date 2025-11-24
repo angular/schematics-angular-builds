f58021a22de409a53cca010fdc4cf96468265163
Angular Builds <dev-infra@angular.dev>
2025-11-24 16:02:25 +0000

    cceb862 fix(@angular/ssr): handle `X-Forwarded-Prefix` and `APP_BASE_HREF` in redirects

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.11+sha-25bb7e6',
-    AngularBuild: '^20.3.11+sha-25bb7e6',
-    AngularSSR: '^20.3.11+sha-25bb7e6',
+    DevkitBuildAngular: '^20.3.11+sha-cceb862',
+    AngularBuild: '^20.3.11+sha-cceb862',
+    AngularSSR: '^20.3.11+sha-cceb862',
 };
