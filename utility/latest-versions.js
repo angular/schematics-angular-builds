1a34d753b46f0b0806a7083b397144d605effd27
Angular Builds <dev-infra@angular.dev>
2025-11-24 14:56:17 +0000

    afac5e7 refactor: simplify dynamic import type assertions and remove resolution mode hints

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0',
     NgPackagr: '^21.0.0',
-    DevkitBuildAngular: '^21.0.0+sha-03e2312',
-    AngularBuild: '^21.0.0+sha-03e2312',
-    AngularSSR: '^21.0.0+sha-03e2312',
+    DevkitBuildAngular: '^21.0.0+sha-afac5e7',
+    AngularBuild: '^21.0.0+sha-afac5e7',
+    AngularSSR: '^21.0.0+sha-afac5e7',
 };
 //# sourceMappingURL=latest-versions.js.map
