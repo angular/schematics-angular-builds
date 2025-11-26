db31dd589fa5596dcc140468fd1534237014d123
Angular Builds <dev-infra@angular.dev>
2025-11-26 15:23:17 +0000

    2bfcbc3 build: update all github actions

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0',
     NgPackagr: '^21.0.0',
-    DevkitBuildAngular: '^21.0.0+sha-39ab18c',
-    AngularBuild: '^21.0.0+sha-39ab18c',
-    AngularSSR: '^21.0.0+sha-39ab18c',
+    DevkitBuildAngular: '^21.0.0+sha-2bfcbc3',
+    AngularBuild: '^21.0.0+sha-2bfcbc3',
+    AngularSSR: '^21.0.0+sha-2bfcbc3',
 };
 //# sourceMappingURL=latest-versions.js.map
