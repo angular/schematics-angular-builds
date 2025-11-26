e75ec031bcdfc7095fb810a6fd4fc357e42e904e
Angular Builds <dev-infra@angular.dev>
2025-11-26 15:39:13 +0000

    4aa1f7e build: update pnpm to v10.23.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0',
     NgPackagr: '^21.0.0',
-    DevkitBuildAngular: '^21.0.0+sha-2bfcbc3',
-    AngularBuild: '^21.0.0+sha-2bfcbc3',
-    AngularSSR: '^21.0.0+sha-2bfcbc3',
+    DevkitBuildAngular: '^21.0.0+sha-4aa1f7e',
+    AngularBuild: '^21.0.0+sha-4aa1f7e',
+    AngularSSR: '^21.0.0+sha-4aa1f7e',
 };
 //# sourceMappingURL=latest-versions.js.map
