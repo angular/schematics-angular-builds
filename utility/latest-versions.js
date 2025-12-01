9db78a72e48cfbe7e88e75de75922a8cbc128ed8
Angular Builds <dev-infra@angular.dev>
2025-12-01 18:23:07 +0000

    86dd329 fix(@angular/build): allow overriding Vitest coverage `reportsDirectory` option

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0',
     NgPackagr: '^21.0.0',
-    DevkitBuildAngular: '^21.0.1+sha-f8a1939',
-    AngularBuild: '^21.0.1+sha-f8a1939',
-    AngularSSR: '^21.0.1+sha-f8a1939',
+    DevkitBuildAngular: '^21.0.1+sha-86dd329',
+    AngularBuild: '^21.0.1+sha-86dd329',
+    AngularSSR: '^21.0.1+sha-86dd329',
 };
 //# sourceMappingURL=latest-versions.js.map
