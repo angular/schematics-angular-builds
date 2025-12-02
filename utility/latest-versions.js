c0de16726f15615dee38b3ba002a4faea5ce91f0
Angular Builds <dev-infra@angular.dev>
2025-12-02 23:14:23 +0000

    5465262 build: use native ESM and improve templates script performance

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.0+sha-1c154aa',
-    AngularBuild: '^21.1.0-next.0+sha-1c154aa',
-    AngularSSR: '^21.1.0-next.0+sha-1c154aa',
+    DevkitBuildAngular: '^21.1.0-next.0+sha-5465262',
+    AngularBuild: '^21.1.0-next.0+sha-5465262',
+    AngularSSR: '^21.1.0-next.0+sha-5465262',
 };
 //# sourceMappingURL=latest-versions.js.map
