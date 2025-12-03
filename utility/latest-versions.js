60d65da29a6701ecbbbc2b9ab9038ee2419a5371
Angular Builds <dev-infra@angular.dev>
2025-12-03 16:57:38 +0000

    816b3d2 test: migrate tar extraction to `tar-stream` for E2E setup

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.1+sha-5dc3b64',
-    AngularBuild: '^21.1.0-next.1+sha-5dc3b64',
-    AngularSSR: '^21.1.0-next.1+sha-5dc3b64',
+    DevkitBuildAngular: '^21.1.0-next.1+sha-816b3d2',
+    AngularBuild: '^21.1.0-next.1+sha-816b3d2',
+    AngularSSR: '^21.1.0-next.1+sha-816b3d2',
 };
 //# sourceMappingURL=latest-versions.js.map
