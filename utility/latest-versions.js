5f27df4705ed85bc630afedca859fbd211388570
Angular Builds <dev-infra@angular.dev>
2025-12-03 13:45:26 +0000

    cfbb616 fix(@angular/cli): update `@modelcontextprotocol/sdk` to v1.24.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.12+sha-d4ca7b7',
-    AngularBuild: '^20.3.12+sha-d4ca7b7',
-    AngularSSR: '^20.3.12+sha-d4ca7b7',
+    DevkitBuildAngular: '^20.3.12+sha-cfbb616',
+    AngularBuild: '^20.3.12+sha-cfbb616',
+    AngularSSR: '^20.3.12+sha-cfbb616',
 };
