e8f92a21710c3e8887458ba392d480232125fe15
Angular Builds <dev-infra@angular.dev>
2025-12-04 18:29:44 +0000

    69b2be8 refactor(@angular/cli): implement structured error handling in package manager API

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.1+sha-d8b76e9',
-    AngularBuild: '^21.1.0-next.1+sha-d8b76e9',
-    AngularSSR: '^21.1.0-next.1+sha-d8b76e9',
+    DevkitBuildAngular: '^21.1.0-next.1+sha-69b2be8',
+    AngularBuild: '^21.1.0-next.1+sha-69b2be8',
+    AngularSSR: '^21.1.0-next.1+sha-69b2be8',
 };
 //# sourceMappingURL=latest-versions.js.map
