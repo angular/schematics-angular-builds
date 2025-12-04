71f75cd6b2859b7422d119272395cad71daf9f3d
Angular Builds <dev-infra@angular.dev>
2025-12-04 08:20:14 +0000

    d8b76e9 fix(@angular/cli): correctly handle yarn classic tag manifest fetching

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.1+sha-ec19c26',
-    AngularBuild: '^21.1.0-next.1+sha-ec19c26',
-    AngularSSR: '^21.1.0-next.1+sha-ec19c26',
+    DevkitBuildAngular: '^21.1.0-next.1+sha-d8b76e9',
+    AngularBuild: '^21.1.0-next.1+sha-d8b76e9',
+    AngularSSR: '^21.1.0-next.1+sha-d8b76e9',
 };
 //# sourceMappingURL=latest-versions.js.map
