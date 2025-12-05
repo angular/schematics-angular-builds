9ab3d1669aabcd6467929804ff96b965087c0209
Angular Builds <dev-infra@angular.dev>
2025-12-05 17:50:04 +0000

    5d85f41 fix(@angular-devkit/build-angular): conditionally provide Zone.js change detection in the built-in test main file

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0',
     NgPackagr: '^21.0.0',
-    DevkitBuildAngular: '^21.0.2+sha-0fc7d57',
-    AngularBuild: '^21.0.2+sha-0fc7d57',
-    AngularSSR: '^21.0.2+sha-0fc7d57',
+    DevkitBuildAngular: '^21.0.2+sha-5d85f41',
+    AngularBuild: '^21.0.2+sha-5d85f41',
+    AngularSSR: '^21.0.2+sha-5d85f41',
 };
 //# sourceMappingURL=latest-versions.js.map
