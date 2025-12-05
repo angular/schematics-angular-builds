976f3bdc9ab814360c05eaa4d1242de3fdeeaf56
Angular Builds <dev-infra@angular.dev>
2025-12-05 17:25:00 +0000

    0fc7d57 fix(@angular/build): ensure locale base href retains leading slash (#32040)

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0',
     NgPackagr: '^21.0.0',
-    DevkitBuildAngular: '^21.0.2+sha-54f1009',
-    AngularBuild: '^21.0.2+sha-54f1009',
-    AngularSSR: '^21.0.2+sha-54f1009',
+    DevkitBuildAngular: '^21.0.2+sha-0fc7d57',
+    AngularBuild: '^21.0.2+sha-0fc7d57',
+    AngularSSR: '^21.0.2+sha-0fc7d57',
 };
 //# sourceMappingURL=latest-versions.js.map
