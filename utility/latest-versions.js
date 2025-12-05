178b52e2cc5b2719b857f5f1f7b1258de1f5778f
Angular Builds <dev-infra@angular.dev>
2025-12-05 17:42:00 +0000

    385165c fix(@angular/build): inject testing polyfills in Karma unit-test executor

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.1+sha-38b16ea',
-    AngularBuild: '^21.1.0-next.1+sha-38b16ea',
-    AngularSSR: '^21.1.0-next.1+sha-38b16ea',
+    DevkitBuildAngular: '^21.1.0-next.1+sha-385165c',
+    AngularBuild: '^21.1.0-next.1+sha-385165c',
+    AngularSSR: '^21.1.0-next.1+sha-385165c',
 };
 //# sourceMappingURL=latest-versions.js.map
