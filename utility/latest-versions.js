b4794bc51bfbcf3ca0c768172550c3305aaf3495
Angular Builds <dev-infra@angular.dev>
2025-12-05 18:06:14 +0000

    78f9013 test: remove RxJS from E2E process utility

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.1+sha-985aa18',
-    AngularBuild: '^21.1.0-next.1+sha-985aa18',
-    AngularSSR: '^21.1.0-next.1+sha-985aa18',
+    DevkitBuildAngular: '^21.1.0-next.1+sha-78f9013',
+    AngularBuild: '^21.1.0-next.1+sha-78f9013',
+    AngularSSR: '^21.1.0-next.1+sha-78f9013',
 };
 //# sourceMappingURL=latest-versions.js.map
