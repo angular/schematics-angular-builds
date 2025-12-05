ee506be93dd3285b6831083d35cc30d24f70cd79
Angular Builds <dev-infra@angular.dev>
2025-12-05 11:30:19 +0000

    9033d94 ci: force dev server host to IPv4 to resolve RBE resolution issues

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.1+sha-668c04c',
-    AngularBuild: '^21.1.0-next.1+sha-668c04c',
-    AngularSSR: '^21.1.0-next.1+sha-668c04c',
+    DevkitBuildAngular: '^21.1.0-next.1+sha-9033d94',
+    AngularBuild: '^21.1.0-next.1+sha-9033d94',
+    AngularSSR: '^21.1.0-next.1+sha-9033d94',
 };
 //# sourceMappingURL=latest-versions.js.map
