510023b04d01e3ab86b8ad804796852156fcd0da
Angular Builds <dev-infra@angular.dev>
2025-12-05 16:50:11 +0000

    0cc102e test: enhance `findFreePort` E2E utility reliability and clarity

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.1+sha-588f3b8',
-    AngularBuild: '^21.1.0-next.1+sha-588f3b8',
-    AngularSSR: '^21.1.0-next.1+sha-588f3b8',
+    DevkitBuildAngular: '^21.1.0-next.1+sha-0cc102e',
+    AngularBuild: '^21.1.0-next.1+sha-0cc102e',
+    AngularSSR: '^21.1.0-next.1+sha-0cc102e',
 };
 //# sourceMappingURL=latest-versions.js.map
