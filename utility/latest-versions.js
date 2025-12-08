59ca79dca1d73e3d6bd53db95629a96cf2a52d86
Angular Builds <dev-infra@angular.dev>
2025-12-08 16:15:31 +0000

    6d21220 fix(@angular/build): support NODE_EXTRA_CA_CERTS in SSR SSL plugin

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.1+sha-9d6891f',
-    AngularBuild: '^21.1.0-next.1+sha-9d6891f',
-    AngularSSR: '^21.1.0-next.1+sha-9d6891f',
+    DevkitBuildAngular: '^21.1.0-next.1+sha-6d21220',
+    AngularBuild: '^21.1.0-next.1+sha-6d21220',
+    AngularSSR: '^21.1.0-next.1+sha-6d21220',
 };
 //# sourceMappingURL=latest-versions.js.map
