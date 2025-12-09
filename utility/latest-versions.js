b6533ee7965fa4007fcb347699c68a4948cfcafb
Angular Builds <dev-infra@angular.dev>
2025-12-09 18:00:12 +0000

    a53e666 test: rename npm E2E suite to webpack

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.1+sha-114cddd',
-    AngularBuild: '^21.1.0-next.1+sha-114cddd',
-    AngularSSR: '^21.1.0-next.1+sha-114cddd',
+    DevkitBuildAngular: '^21.1.0-next.1+sha-a53e666',
+    AngularBuild: '^21.1.0-next.1+sha-a53e666',
+    AngularSSR: '^21.1.0-next.1+sha-a53e666',
 };
 //# sourceMappingURL=latest-versions.js.map
