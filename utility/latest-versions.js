428f0cb3b5f4da84dfcbd3e8b46f1440e5aac348
Angular Builds <dev-infra@angular.dev>
2025-12-09 21:02:56 +0000

    4cdc66b ci: remove single value os matrix field from E2E jobs

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.1+sha-9f28807',
-    AngularBuild: '^21.1.0-next.1+sha-9f28807',
-    AngularSSR: '^21.1.0-next.1+sha-9f28807',
+    DevkitBuildAngular: '^21.1.0-next.1+sha-4cdc66b',
+    AngularBuild: '^21.1.0-next.1+sha-4cdc66b',
+    AngularSSR: '^21.1.0-next.1+sha-4cdc66b',
 };
 //# sourceMappingURL=latest-versions.js.map
