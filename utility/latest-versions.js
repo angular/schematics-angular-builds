677424aed244bb0bd9c3c759e96f7c7a6b1c6859
Angular Builds <dev-infra@angular.dev>
2025-12-11 15:22:57 +0000

    85a28de fix(@schematics/angular): remove `inlineSources` from library tsconfig template

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0',
     NgPackagr: '^21.0.0',
-    DevkitBuildAngular: '^21.0.3+sha-deb4fff',
-    AngularBuild: '^21.0.3+sha-deb4fff',
-    AngularSSR: '^21.0.3+sha-deb4fff',
+    DevkitBuildAngular: '^21.0.3+sha-85a28de',
+    AngularBuild: '^21.0.3+sha-85a28de',
+    AngularSSR: '^21.0.3+sha-85a28de',
 };
 //# sourceMappingURL=latest-versions.js.map
