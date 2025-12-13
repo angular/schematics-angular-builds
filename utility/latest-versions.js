049f6141202e6ab42f5f96d67de65dce86f2ba42
Angular Builds <dev-infra@angular.dev>
2025-12-13 01:00:11 +0000

    b671245 fix(@schematics/angular): improve VS Code background compilation start/end detection

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0',
     NgPackagr: '^21.0.0',
-    DevkitBuildAngular: '^21.0.3+sha-df59b98',
-    AngularBuild: '^21.0.3+sha-df59b98',
-    AngularSSR: '^21.0.3+sha-df59b98',
+    DevkitBuildAngular: '^21.0.3+sha-b671245',
+    AngularBuild: '^21.0.3+sha-b671245',
+    AngularSSR: '^21.0.3+sha-b671245',
 };
 //# sourceMappingURL=latest-versions.js.map
