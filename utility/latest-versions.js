4c91fd3b4e4cf35b8b0871bb372f2b4745965a65
Angular Builds <dev-infra@angular.dev>
2025-12-13 00:38:02 +0000

    fd8b4c8 refactor(@angular-devkit/schematics): update type usage to support isolated declarations

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.2+sha-e4eee62',
-    AngularBuild: '^21.1.0-next.2+sha-e4eee62',
-    AngularSSR: '^21.1.0-next.2+sha-e4eee62',
+    DevkitBuildAngular: '^21.1.0-next.2+sha-fd8b4c8',
+    AngularBuild: '^21.1.0-next.2+sha-fd8b4c8',
+    AngularSSR: '^21.1.0-next.2+sha-fd8b4c8',
 };
 //# sourceMappingURL=latest-versions.js.map
