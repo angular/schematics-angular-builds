faabafc45fa5ae1ea379edf869c95f3135bf1752
Angular Builds <dev-infra@angular.dev>
2025-12-15 23:28:46 +0000

    1b12967 refactor(@angular-devkit/build-angular): remove redundant spinner coloring

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.2+sha-f1ab1d3',
-    AngularBuild: '^21.1.0-next.2+sha-f1ab1d3',
-    AngularSSR: '^21.1.0-next.2+sha-f1ab1d3',
+    DevkitBuildAngular: '^21.1.0-next.2+sha-1b12967',
+    AngularBuild: '^21.1.0-next.2+sha-1b12967',
+    AngularSSR: '^21.1.0-next.2+sha-1b12967',
 };
 //# sourceMappingURL=latest-versions.js.map
