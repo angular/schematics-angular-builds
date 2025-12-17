302ff0543240116e94badd24c78f85a5d883cd24
Angular Builds <dev-infra@angular.dev>
2025-12-17 13:18:37 +0000

    de35632 test: update e2e test assets from Angular 18.0 to 19.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.2+sha-edeb41c',
-    AngularBuild: '^21.1.0-next.2+sha-edeb41c',
-    AngularSSR: '^21.1.0-next.2+sha-edeb41c',
+    DevkitBuildAngular: '^21.1.0-next.2+sha-de35632',
+    AngularBuild: '^21.1.0-next.2+sha-de35632',
+    AngularSSR: '^21.1.0-next.2+sha-de35632',
 };
 //# sourceMappingURL=latest-versions.js.map
