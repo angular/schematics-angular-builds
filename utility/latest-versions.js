7350e0d275d65763c8ed69ea323ac8999725c10c
Angular Builds <dev-infra@angular.dev>
2025-12-17 13:34:01 +0000

    fc1aee3 build: update dependency rules_cc to v0.2.15

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.2+sha-de35632',
-    AngularBuild: '^21.1.0-next.2+sha-de35632',
-    AngularSSR: '^21.1.0-next.2+sha-de35632',
+    DevkitBuildAngular: '^21.1.0-next.2+sha-fc1aee3',
+    AngularBuild: '^21.1.0-next.2+sha-fc1aee3',
+    AngularSSR: '^21.1.0-next.2+sha-fc1aee3',
 };
 //# sourceMappingURL=latest-versions.js.map
