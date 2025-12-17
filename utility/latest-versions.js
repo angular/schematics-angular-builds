272666f3a5ae5df7fbe3197eaa460521c0eca5a3
Angular Builds <dev-infra@angular.dev>
2025-12-17 14:14:23 +0000

    c603548 build: migrate Bazel ignore rules from .bazelignore to REPO.bazel

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0',
     NgPackagr: '^21.0.0',
-    DevkitBuildAngular: '^21.0.3+sha-612e17e',
-    AngularBuild: '^21.0.3+sha-612e17e',
-    AngularSSR: '^21.0.3+sha-612e17e',
+    DevkitBuildAngular: '^21.0.3+sha-c603548',
+    AngularBuild: '^21.0.3+sha-c603548',
+    AngularSSR: '^21.0.3+sha-c603548',
 };
 //# sourceMappingURL=latest-versions.js.map
