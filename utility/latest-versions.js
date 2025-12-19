ec74aa23c1f1cccfddbe243f163e9c148eb3b443
Angular Builds <dev-infra@angular.dev>
2025-12-19 13:56:08 +0000

    dd3b8ed build: remove tools/baseline_browserslist from pnpm workspace

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0',
     NgPackagr: '^21.0.0',
-    DevkitBuildAngular: '^21.0.4+sha-b8829e2',
-    AngularBuild: '^21.0.4+sha-b8829e2',
-    AngularSSR: '^21.0.4+sha-b8829e2',
+    DevkitBuildAngular: '^21.0.4+sha-dd3b8ed',
+    AngularBuild: '^21.0.4+sha-dd3b8ed',
+    AngularSSR: '^21.0.4+sha-dd3b8ed',
 };
 //# sourceMappingURL=latest-versions.js.map
