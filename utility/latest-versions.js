4c8e94522d094a0d54682b70d33156bbf146997b
Angular Builds <dev-infra@angular.dev>
2025-12-22 16:16:46 +0000

    b969432 Revert "build: restore `ng-dev` script"

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.3+sha-5f1f0d1',
-    AngularBuild: '^21.1.0-next.3+sha-5f1f0d1',
-    AngularSSR: '^21.1.0-next.3+sha-5f1f0d1',
+    DevkitBuildAngular: '^21.1.0-next.3+sha-b969432',
+    AngularBuild: '^21.1.0-next.3+sha-b969432',
+    AngularSSR: '^21.1.0-next.3+sha-b969432',
 };
 //# sourceMappingURL=latest-versions.js.map
