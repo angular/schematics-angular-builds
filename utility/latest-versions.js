e611845ced1c821e5aae3d139447034ab31c7697
Angular Builds <dev-infra@angular.dev>
2026-01-06 18:29:28 +0000

    1f30663 test: migrate build and misc E2E tests to Puppeteer

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.1.0-next.3+sha-1638f76',
-    AngularBuild: '^21.1.0-next.3+sha-1638f76',
-    AngularSSR: '^21.1.0-next.3+sha-1638f76',
+    DevkitBuildAngular: '^21.1.0-next.3+sha-1f30663',
+    AngularBuild: '^21.1.0-next.3+sha-1f30663',
+    AngularSSR: '^21.1.0-next.3+sha-1f30663',
 };
 //# sourceMappingURL=latest-versions.js.map
