0bcaf7c5932516fce6a4d5f0ae9d320aae430328
Angular Builds <dev-infra@angular.dev>
2026-01-07 17:32:11 +0000

    2b9be3a fix(@angular/build): ensure correct project targeting during Vitest debugging

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0',
     NgPackagr: '^21.0.0',
-    DevkitBuildAngular: '^21.0.4+sha-8456da3',
-    AngularBuild: '^21.0.4+sha-8456da3',
-    AngularSSR: '^21.0.4+sha-8456da3',
+    DevkitBuildAngular: '^21.0.4+sha-2b9be3a',
+    AngularBuild: '^21.0.4+sha-2b9be3a',
+    AngularSSR: '^21.0.4+sha-2b9be3a',
 };
 //# sourceMappingURL=latest-versions.js.map
