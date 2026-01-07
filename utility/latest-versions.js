d15c59b2e278b00f9e22f4751b407439fd2aeac3
Angular Builds <dev-infra@angular.dev>
2026-01-07 14:07:28 +0000

    8456da3 refactor: update copyright year in license

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0',
     NgPackagr: '^21.0.0',
-    DevkitBuildAngular: '^21.0.4+sha-ac26f4a',
-    AngularBuild: '^21.0.4+sha-ac26f4a',
-    AngularSSR: '^21.0.4+sha-ac26f4a',
+    DevkitBuildAngular: '^21.0.4+sha-8456da3',
+    AngularBuild: '^21.0.4+sha-8456da3',
+    AngularSSR: '^21.0.4+sha-8456da3',
 };
 //# sourceMappingURL=latest-versions.js.map
