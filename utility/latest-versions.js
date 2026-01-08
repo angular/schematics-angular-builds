94daa6af904310fdd3fb27a23f6484a5bde07767
Angular Builds <dev-infra@angular.dev>
2026-01-08 17:58:54 +0000

    6e496cc build: remove `@modelcontextprotocol/sdk` from the `minimumReleaseAgeExclude` list in `pnpm-workspace.yaml`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0',
     NgPackagr: '^21.0.0',
-    DevkitBuildAngular: '^21.0.5+sha-3b3247c',
-    AngularBuild: '^21.0.5+sha-3b3247c',
-    AngularSSR: '^21.0.5+sha-3b3247c',
+    DevkitBuildAngular: '^21.0.5+sha-6e496cc',
+    AngularBuild: '^21.0.5+sha-6e496cc',
+    AngularSSR: '^21.0.5+sha-6e496cc',
 };
 //# sourceMappingURL=latest-versions.js.map
