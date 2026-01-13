50228a6345810ecd518935df5296e662ea0a52fd
Angular Builds <dev-infra@angular.dev>
2026-01-13 14:38:54 +0000

    8ba6967 refactor: improve `findUp` utility correctness and performance

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.2.0-next.0+sha-d9cd609',
-    AngularBuild: '^21.2.0-next.0+sha-d9cd609',
-    AngularSSR: '^21.2.0-next.0+sha-d9cd609',
+    DevkitBuildAngular: '^21.2.0-next.0+sha-8ba6967',
+    AngularBuild: '^21.2.0-next.0+sha-8ba6967',
+    AngularSSR: '^21.2.0-next.0+sha-8ba6967',
 };
 //# sourceMappingURL=latest-versions.js.map
