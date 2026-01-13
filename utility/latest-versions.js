9d43d67d1a6511cb80e31142bb468cb663a5d853
Angular Builds <dev-infra@angular.dev>
2026-01-13 18:09:14 +0000

    730ae66 fix(@angular/ssr): handle platform destruction during rendering

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.0.0',
     NgPackagr: '^21.0.0',
-    DevkitBuildAngular: '^21.0.5+sha-6e496cc',
-    AngularBuild: '^21.0.5+sha-6e496cc',
-    AngularSSR: '^21.0.5+sha-6e496cc',
+    DevkitBuildAngular: '^21.0.5+sha-730ae66',
+    AngularBuild: '^21.0.5+sha-730ae66',
+    AngularSSR: '^21.0.5+sha-730ae66',
 };
 //# sourceMappingURL=latest-versions.js.map
