de2c8822911d0a4c1bad37e74c32a17536a22234
Angular Builds <dev-infra@angular.dev>
2026-01-16 14:46:11 +0000

    b3de0de fix(@angular/build): allow application assets in workspace root

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.2.0-next.0+sha-7b33b80',
-    AngularBuild: '^21.2.0-next.0+sha-7b33b80',
-    AngularSSR: '^21.2.0-next.0+sha-7b33b80',
+    DevkitBuildAngular: '^21.2.0-next.0+sha-b3de0de',
+    AngularBuild: '^21.2.0-next.0+sha-b3de0de',
+    AngularSSR: '^21.2.0-next.0+sha-b3de0de',
 };
 //# sourceMappingURL=latest-versions.js.map
