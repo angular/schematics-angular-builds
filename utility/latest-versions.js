4a6b39ed6ca052ef83afd28a52ce3d4ff65f1fbd
Angular Builds <dev-infra@angular.dev>
2026-01-21 16:40:42 +0000

    9da6d8f fix(@schematics/angular): correct vscode MCP configuration for new projects

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0',
     NgPackagr: '^21.1.0',
-    DevkitBuildAngular: '^21.1.0+sha-4742aae',
-    AngularBuild: '^21.1.0+sha-4742aae',
-    AngularSSR: '^21.1.0+sha-4742aae',
+    DevkitBuildAngular: '^21.1.0+sha-9da6d8f',
+    AngularBuild: '^21.1.0+sha-9da6d8f',
+    AngularSSR: '^21.1.0+sha-9da6d8f',
 };
 //# sourceMappingURL=latest-versions.js.map
