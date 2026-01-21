51d6040251381bb0663dd42b03a7827b8911ec2d
Angular Builds <dev-infra@angular.dev>
2026-01-21 17:23:25 +0000

    a60f8bc fix(@schematics/angular): remove special characters from jasmine-vitest report filename

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0-next.0',
     NgPackagr: '^21.1.0-next.0',
-    DevkitBuildAngular: '^21.2.0-next.0+sha-d288d9f',
-    AngularBuild: '^21.2.0-next.0+sha-d288d9f',
-    AngularSSR: '^21.2.0-next.0+sha-d288d9f',
+    DevkitBuildAngular: '^21.2.0-next.0+sha-a60f8bc',
+    AngularBuild: '^21.2.0-next.0+sha-a60f8bc',
+    AngularSSR: '^21.2.0-next.0+sha-a60f8bc',
 };
 //# sourceMappingURL=latest-versions.js.map
