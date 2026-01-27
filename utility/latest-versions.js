dc79b52f1844b910ab58026ffe54059d7731897d
Angular Builds <dev-infra@angular.dev>
2026-01-27 16:32:03 +0000

    aa7381e feat(@schematics/angular): add a '.prettierrc' file to generated workspaces and add Prettier as dev dependency

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^21.2.0-next.0+sha-0126984',
-    AngularBuild: '^21.2.0-next.0+sha-0126984',
-    AngularSSR: '^21.2.0-next.0+sha-0126984',
+    DevkitBuildAngular: '^21.2.0-next.0+sha-aa7381e',
+    AngularBuild: '^21.2.0-next.0+sha-aa7381e',
+    AngularSSR: '^21.2.0-next.0+sha-aa7381e',
 };
 //# sourceMappingURL=latest-versions.js.map
