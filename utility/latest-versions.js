93fd697e3fa2c6f74d4438b7645b87b3707716b0
Angular Builds <dev-infra@angular.dev>
2026-01-28 15:09:08 +0000

    91b9d28 feat(@angular/cli): integrate file formatting into update migrations

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^21.2.0-next.0+sha-eb9c003',
-    AngularBuild: '^21.2.0-next.0+sha-eb9c003',
-    AngularSSR: '^21.2.0-next.0+sha-eb9c003',
+    DevkitBuildAngular: '^21.2.0-next.0+sha-91b9d28',
+    AngularBuild: '^21.2.0-next.0+sha-91b9d28',
+    AngularSSR: '^21.2.0-next.0+sha-91b9d28',
 };
 //# sourceMappingURL=latest-versions.js.map
