00295d8cb09022986bae15e05231f49451db9610
Angular Builds <dev-infra@angular.dev>
2026-01-28 07:47:36 +0000

    fbae1b6 feat(@angular/cli): automatic formatting files modified by schematics

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^21.2.0-next.0+sha-247855c',
-    AngularBuild: '^21.2.0-next.0+sha-247855c',
-    AngularSSR: '^21.2.0-next.0+sha-247855c',
+    DevkitBuildAngular: '^21.2.0-next.0+sha-fbae1b6',
+    AngularBuild: '^21.2.0-next.0+sha-fbae1b6',
+    AngularSSR: '^21.2.0-next.0+sha-fbae1b6',
 };
 //# sourceMappingURL=latest-versions.js.map
