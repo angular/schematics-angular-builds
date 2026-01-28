cbd9236e5de66be72a5e0683f1b730f618a007db
Angular Builds <dev-infra@angular.dev>
2026-01-28 11:52:18 +0000

    39342af refactor(@angular/cli): remove `relative` path conversion when adding files to format

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^21.2.0-next.0+sha-30bd75c',
-    AngularBuild: '^21.2.0-next.0+sha-30bd75c',
-    AngularSSR: '^21.2.0-next.0+sha-30bd75c',
+    DevkitBuildAngular: '^21.2.0-next.0+sha-39342af',
+    AngularBuild: '^21.2.0-next.0+sha-39342af',
+    AngularSSR: '^21.2.0-next.0+sha-39342af',
 };
 //# sourceMappingURL=latest-versions.js.map
