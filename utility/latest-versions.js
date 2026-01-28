0ac05ca2128c343c3edbe99551ca157ca8a4c26f
Angular Builds <dev-infra@angular.dev>
2026-01-28 12:44:11 +0000

    0dd04f2 feat(@angular/cli): add markdown files to Prettier's formatting list

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^21.2.0-next.0+sha-39342af',
-    AngularBuild: '^21.2.0-next.0+sha-39342af',
-    AngularSSR: '^21.2.0-next.0+sha-39342af',
+    DevkitBuildAngular: '^21.2.0-next.0+sha-0dd04f2',
+    AngularBuild: '^21.2.0-next.0+sha-0dd04f2',
+    AngularSSR: '^21.2.0-next.0+sha-0dd04f2',
 };
 //# sourceMappingURL=latest-versions.js.map
