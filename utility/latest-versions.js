96ef9321932e83ea248b6f5c9ad41fdc98c22047
Angular Builds <dev-infra@angular.dev>
2026-01-29 14:30:52 +0000

    151b58a build: update actions/cache action to v5.0.3

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^21.2.0-next.0+sha-e5d3b94',
-    AngularBuild: '^21.2.0-next.0+sha-e5d3b94',
-    AngularSSR: '^21.2.0-next.0+sha-e5d3b94',
+    DevkitBuildAngular: '^21.2.0-next.0+sha-151b58a',
+    AngularBuild: '^21.2.0-next.0+sha-151b58a',
+    AngularSSR: '^21.2.0-next.0+sha-151b58a',
 };
 //# sourceMappingURL=latest-versions.js.map
