a20018339113de2ba3e30eff4138845f3d2a4289
Angular Builds <dev-infra@angular.dev>
2026-01-30 17:58:29 +0000

    a18196a fix(@schematics/angular): warn when production configuration is missing for service worker

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0',
     NgPackagr: '^21.1.0',
-    DevkitBuildAngular: '^21.1.2+sha-00f5ec1',
-    AngularBuild: '^21.1.2+sha-00f5ec1',
-    AngularSSR: '^21.1.2+sha-00f5ec1',
+    DevkitBuildAngular: '^21.1.2+sha-a18196a',
+    AngularBuild: '^21.1.2+sha-a18196a',
+    AngularSSR: '^21.1.2+sha-a18196a',
 };
 //# sourceMappingURL=latest-versions.js.map
