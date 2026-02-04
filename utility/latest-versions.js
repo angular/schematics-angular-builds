b9307975bfa2b480582bdc747c92d7b7789dcb91
Angular Builds <dev-infra@angular.dev>
2026-02-04 21:31:52 +0000

    de390b3 build: update dependency jsdom to v28

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^21.2.0-next.0+sha-fe4c310',
-    AngularBuild: '^21.2.0-next.0+sha-fe4c310',
-    AngularSSR: '^21.2.0-next.0+sha-fe4c310',
+    DevkitBuildAngular: '^21.2.0-next.0+sha-de390b3',
+    AngularBuild: '^21.2.0-next.0+sha-de390b3',
+    AngularSSR: '^21.2.0-next.0+sha-de390b3',
 };
 //# sourceMappingURL=latest-versions.js.map
