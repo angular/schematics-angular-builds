125e9ad70c483fe3c0c2db07b75d32bd06f08033
Angular Builds <dev-infra@angular.dev>
2026-02-04 14:30:15 +0000

    346973a build: configure Jasmine to allow duplicate describe block names

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^21.2.0-next.0+sha-bc363af',
-    AngularBuild: '^21.2.0-next.0+sha-bc363af',
-    AngularSSR: '^21.2.0-next.0+sha-bc363af',
+    DevkitBuildAngular: '^21.2.0-next.0+sha-346973a',
+    AngularBuild: '^21.2.0-next.0+sha-346973a',
+    AngularSSR: '^21.2.0-next.0+sha-346973a',
 };
 //# sourceMappingURL=latest-versions.js.map
