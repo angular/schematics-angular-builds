6aab7347fef9647ed99a24cb8ad4bf171e038d35
Angular Builds <dev-infra@angular.dev>
2026-02-05 20:37:06 +0000

    e80d7c5 build: update dependency @modelcontextprotocol/sdk to v1.26.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0',
     NgPackagr: '^21.1.0',
-    DevkitBuildAngular: '^21.1.2+sha-f6cc58c',
-    AngularBuild: '^21.1.2+sha-f6cc58c',
-    AngularSSR: '^21.1.2+sha-f6cc58c',
+    DevkitBuildAngular: '^21.1.2+sha-e80d7c5',
+    AngularBuild: '^21.1.2+sha-e80d7c5',
+    AngularSSR: '^21.1.2+sha-e80d7c5',
 };
 //# sourceMappingURL=latest-versions.js.map
