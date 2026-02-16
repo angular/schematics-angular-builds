bd2c2c9392385575ce6e7d4a93bfccd7517106ba
Angular Builds <dev-infra@angular.dev>
2026-02-16 11:24:39 +0000

    e70c9d3 build: update dependency ajv to v8.18.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.1.0',
     NgPackagr: '^21.1.0',
-    DevkitBuildAngular: '^21.1.4+sha-96a428e',
-    AngularBuild: '^21.1.4+sha-96a428e',
-    AngularSSR: '^21.1.4+sha-96a428e',
+    DevkitBuildAngular: '^21.1.4+sha-e70c9d3',
+    AngularBuild: '^21.1.4+sha-e70c9d3',
+    AngularSSR: '^21.1.4+sha-e70c9d3',
 };
 //# sourceMappingURL=latest-versions.js.map
