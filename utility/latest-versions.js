6cfa290555ca48a3a40fc011dc6a75f3e45829ae
Angular Builds <dev-infra@angular.dev>
2026-02-20 10:45:50 +0000

    8eeff46 build: update bazel dependencies

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^21.2.0-rc.0+sha-99ded1d',
-    AngularBuild: '^21.2.0-rc.0+sha-99ded1d',
-    AngularSSR: '^21.2.0-rc.0+sha-99ded1d',
+    DevkitBuildAngular: '^21.2.0-rc.0+sha-8eeff46',
+    AngularBuild: '^21.2.0-rc.0+sha-8eeff46',
+    AngularSSR: '^21.2.0-rc.0+sha-8eeff46',
 };
 //# sourceMappingURL=latest-versions.js.map
