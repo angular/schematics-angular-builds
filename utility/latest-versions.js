dc5590aab97cb4c2fc6fa7fa227785bc1c1aae49
Angular Builds <dev-infra@angular.dev>
2026-02-23 09:51:25 +0000

    cf5a72d fix(@angular/ssr): prevent open redirect via X-Forwarded-Prefix header

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^21.2.0-rc.0+sha-f78f388',
-    AngularBuild: '^21.2.0-rc.0+sha-f78f388',
-    AngularSSR: '^21.2.0-rc.0+sha-f78f388',
+    DevkitBuildAngular: '^21.2.0-rc.0+sha-cf5a72d',
+    AngularBuild: '^21.2.0-rc.0+sha-cf5a72d',
+    AngularSSR: '^21.2.0-rc.0+sha-cf5a72d',
 };
 //# sourceMappingURL=latest-versions.js.map
