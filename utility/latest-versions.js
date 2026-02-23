e4ee8367d3f235890aeb8715c10b14cd1186cb1b
Angular Builds <dev-infra@angular.dev>
2026-02-23 17:20:00 +0000

    4d9442a release: cut the v19.2.21 release

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0',
     NgPackagr: '^19.2.0',
-    DevkitBuildAngular: '^19.2.20+sha-288e228',
-    AngularBuild: '^19.2.20+sha-288e228',
-    AngularSSR: '^19.2.20+sha-288e228',
+    DevkitBuildAngular: '^19.2.21+sha-4d9442a',
+    AngularBuild: '^19.2.21+sha-4d9442a',
+    AngularSSR: '^19.2.21+sha-4d9442a',
 };
