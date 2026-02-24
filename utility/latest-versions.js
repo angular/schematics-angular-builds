94bf2fbfbe4f22ed75e1a4d219da7ecfac2e3a5a
Angular Builds <dev-infra@angular.dev>
2026-02-24 21:11:19 +0000

    dfbbe2d build: update dependency @listr2/prompt-adapter-inquirer to v4

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-1d35e3f',
-    AngularBuild: '^22.0.0-next.0+sha-1d35e3f',
-    AngularSSR: '^22.0.0-next.0+sha-1d35e3f',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-dfbbe2d',
+    AngularBuild: '^22.0.0-next.0+sha-dfbbe2d',
+    AngularSSR: '^22.0.0-next.0+sha-dfbbe2d',
 };
 //# sourceMappingURL=latest-versions.js.map
