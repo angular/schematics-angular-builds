fa776d68cfe02fe4d72dce929c150bbeb9b1e5f4
Angular Builds <dev-infra@angular.dev>
2026-02-24 19:20:10 +0000

    1d35e3f build: update dependency @inquirer/confirm to v6

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-d0e9e81',
-    AngularBuild: '^22.0.0-next.0+sha-d0e9e81',
-    AngularSSR: '^22.0.0-next.0+sha-d0e9e81',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-1d35e3f',
+    AngularBuild: '^22.0.0-next.0+sha-1d35e3f',
+    AngularSSR: '^22.0.0-next.0+sha-1d35e3f',
 };
 //# sourceMappingURL=latest-versions.js.map
