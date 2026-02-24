05f1715ceec9b55d5156dcd195fbff90b396971b
Angular Builds <dev-infra@angular.dev>
2026-02-24 22:00:32 +0000

    6584f5f refactor(@angular/build): remove Node.js v20 conditional file copying logic

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-dfbbe2d',
-    AngularBuild: '^22.0.0-next.0+sha-dfbbe2d',
-    AngularSSR: '^22.0.0-next.0+sha-dfbbe2d',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-6584f5f',
+    AngularBuild: '^22.0.0-next.0+sha-6584f5f',
+    AngularSSR: '^22.0.0-next.0+sha-6584f5f',
 };
 //# sourceMappingURL=latest-versions.js.map
