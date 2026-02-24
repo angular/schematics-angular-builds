25a4db0419746583b2e5a75ab0ee27d444c6f766
Angular Builds <dev-infra@angular.dev>
2026-02-24 14:41:52 +0000

    e2ee6b3 fix(@angular/build): serve extensionless assets without transformation

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-85e3ce2',
-    AngularBuild: '^22.0.0-next.0+sha-85e3ce2',
-    AngularSSR: '^22.0.0-next.0+sha-85e3ce2',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-e2ee6b3',
+    AngularBuild: '^22.0.0-next.0+sha-e2ee6b3',
+    AngularSSR: '^22.0.0-next.0+sha-e2ee6b3',
 };
 //# sourceMappingURL=latest-versions.js.map
