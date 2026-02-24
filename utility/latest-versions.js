c218536844317099a7f2822d0256de248089a326
Angular Builds <dev-infra@angular.dev>
2026-02-24 09:23:29 +0000

    201a036 fix(@angular/cli): simplify Angular version compatibility checks and add special handling for local builds of new major versions

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^21.2.0-rc.1+sha-7040046',
-    AngularBuild: '^21.2.0-rc.1+sha-7040046',
-    AngularSSR: '^21.2.0-rc.1+sha-7040046',
+    DevkitBuildAngular: '^21.2.0-rc.1+sha-201a036',
+    AngularBuild: '^21.2.0-rc.1+sha-201a036',
+    AngularSSR: '^21.2.0-rc.1+sha-201a036',
 };
 //# sourceMappingURL=latest-versions.js.map
