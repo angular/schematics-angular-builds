bbff1e09ce08f8e0f1dddddf2af91071e2d3c10a
Angular Builds <dev-infra@angular.dev>
2026-02-25 15:51:04 +0000

    70a81fa refactor(@schematics/angular): remove migrations that cannot be used with version 22

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-b4885b8',
-    AngularBuild: '^22.0.0-next.0+sha-b4885b8',
-    AngularSSR: '^22.0.0-next.0+sha-b4885b8',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-70a81fa',
+    AngularBuild: '^22.0.0-next.0+sha-70a81fa',
+    AngularSSR: '^22.0.0-next.0+sha-70a81fa',
 };
 //# sourceMappingURL=latest-versions.js.map
