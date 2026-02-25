9daedb724a7e02a18ccbbc2851e0379fe7d20ba4
Angular Builds <dev-infra@angular.dev>
2026-02-25 22:19:39 +0000

    0f8a712 docs: release notes for the v21.2.0 release

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-70a81fa',
-    AngularBuild: '^22.0.0-next.0+sha-70a81fa',
-    AngularSSR: '^22.0.0-next.0+sha-70a81fa',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-0f8a712',
+    AngularBuild: '^22.0.0-next.0+sha-0f8a712',
+    AngularSSR: '^22.0.0-next.0+sha-0f8a712',
 };
 //# sourceMappingURL=latest-versions.js.map
