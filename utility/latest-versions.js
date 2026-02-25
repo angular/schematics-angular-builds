c3d9878ddc1820932d6ba8d20520075b800d8b21
Angular Builds <dev-infra@angular.dev>
2026-02-25 14:03:31 +0000

    dc899e8 fix(@angular/build): normalize `allowedHosts` in dev-server

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^21.2.0-rc.2+sha-4041e14',
-    AngularBuild: '^21.2.0-rc.2+sha-4041e14',
-    AngularSSR: '^21.2.0-rc.2+sha-4041e14',
+    DevkitBuildAngular: '^21.2.0-rc.2+sha-dc899e8',
+    AngularBuild: '^21.2.0-rc.2+sha-dc899e8',
+    AngularSSR: '^21.2.0-rc.2+sha-dc899e8',
 };
 //# sourceMappingURL=latest-versions.js.map
