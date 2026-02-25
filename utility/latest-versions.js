2b25857ba9c034e9d5c603519ce0abee5829c648
Angular Builds <dev-infra@angular.dev>
2026-02-25 15:28:33 +0000

    b4885b8 refactor: remove experimental Jest and Web Test Runner builders

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-bfdc7b0',
-    AngularBuild: '^22.0.0-next.0+sha-bfdc7b0',
-    AngularSSR: '^22.0.0-next.0+sha-bfdc7b0',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-b4885b8',
+    AngularBuild: '^22.0.0-next.0+sha-b4885b8',
+    AngularSSR: '^22.0.0-next.0+sha-b4885b8',
 };
 //# sourceMappingURL=latest-versions.js.map
