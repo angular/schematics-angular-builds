ec133b8c7610085f9a84c2ce17e411e3a5cfce0d
Angular Builds <dev-infra@angular.dev>
2026-02-25 12:42:13 +0000

    0d7972d build: update dependency @inquirer/prompts to v8

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-d5fd9b9',
-    AngularBuild: '^22.0.0-next.0+sha-d5fd9b9',
-    AngularSSR: '^22.0.0-next.0+sha-d5fd9b9',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-0d7972d',
+    AngularBuild: '^22.0.0-next.0+sha-0d7972d',
+    AngularSSR: '^22.0.0-next.0+sha-0d7972d',
 };
 //# sourceMappingURL=latest-versions.js.map
