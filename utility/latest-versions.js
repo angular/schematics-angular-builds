352b4762a4945a9ffdb38721a88ed75e64f7f738
Angular Builds <dev-infra@angular.dev>
2026-02-26 14:30:21 +0000

    9d167ca perf(@angular/build): fix memory leak in `ng serve` with i18n

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-661d510',
-    AngularBuild: '^22.0.0-next.0+sha-661d510',
-    AngularSSR: '^22.0.0-next.0+sha-661d510',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-9d167ca',
+    AngularBuild: '^22.0.0-next.0+sha-9d167ca',
+    AngularSSR: '^22.0.0-next.0+sha-9d167ca',
 };
 //# sourceMappingURL=latest-versions.js.map
