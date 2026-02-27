d18453742750831adaf4b88131d9725b6fdc4046
Angular Builds <dev-infra@angular.dev>
2026-02-27 09:52:16 +0000

    01f4fc0 build: update repository locations for bazel rules

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-27cd355',
-    AngularBuild: '^22.0.0-next.0+sha-27cd355',
-    AngularSSR: '^22.0.0-next.0+sha-27cd355',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-01f4fc0',
+    AngularBuild: '^22.0.0-next.0+sha-01f4fc0',
+    AngularSSR: '^22.0.0-next.0+sha-01f4fc0',
 };
 //# sourceMappingURL=latest-versions.js.map
