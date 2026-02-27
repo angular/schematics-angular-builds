595ae88c0ead0718182d8f1d341fc52e70261d2a
Angular Builds <dev-infra@angular.dev>
2026-02-27 18:04:18 +0000

    6a1471b build: update puppeteer to v24.37.5

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-e0fa7bf',
-    AngularBuild: '^22.0.0-next.0+sha-e0fa7bf',
-    AngularSSR: '^22.0.0-next.0+sha-e0fa7bf',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-6a1471b',
+    AngularBuild: '^22.0.0-next.0+sha-6a1471b',
+    AngularSSR: '^22.0.0-next.0+sha-6a1471b',
 };
 //# sourceMappingURL=latest-versions.js.map
