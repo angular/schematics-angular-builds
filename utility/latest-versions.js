2e8d5d538bcbd476e7b6bd6faa1b9554903bf458
Angular Builds <dev-infra@angular.dev>
2026-02-27 14:35:13 +0000

    069e3d3 fix(@angular/cli): disable npm update notifier in package manager host

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-f7e02fe',
-    AngularBuild: '^22.0.0-next.0+sha-f7e02fe',
-    AngularSSR: '^22.0.0-next.0+sha-f7e02fe',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-069e3d3',
+    AngularBuild: '^22.0.0-next.0+sha-069e3d3',
+    AngularSSR: '^22.0.0-next.0+sha-069e3d3',
 };
 //# sourceMappingURL=latest-versions.js.map
