b2c454220480aa18505d375f57826bc25dc3f02c
Angular Builds <dev-infra@angular.dev>
2026-03-02 15:52:23 +0000

    414320d feat(@angular/build): support runtime Zone.js detection in Vitest unit test runner

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-597e6c3',
-    AngularBuild: '^22.0.0-next.0+sha-597e6c3',
-    AngularSSR: '^22.0.0-next.0+sha-597e6c3',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-414320d',
+    AngularBuild: '^22.0.0-next.0+sha-414320d',
+    AngularSSR: '^22.0.0-next.0+sha-414320d',
 };
 //# sourceMappingURL=latest-versions.js.map
