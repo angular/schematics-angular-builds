096c69127593018b1ba229d10093a05b1f86d97c
Angular Builds <dev-infra@angular.dev>
2026-03-03 21:52:10 +0000

    a173e1e build: update dependency autoprefixer to v10.4.27

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.0+sha-abcc487',
-    AngularBuild: '^21.2.0+sha-abcc487',
-    AngularSSR: '^21.2.0+sha-abcc487',
+    DevkitBuildAngular: '^21.2.0+sha-a173e1e',
+    AngularBuild: '^21.2.0+sha-a173e1e',
+    AngularSSR: '^21.2.0+sha-a173e1e',
 };
 //# sourceMappingURL=latest-versions.js.map
