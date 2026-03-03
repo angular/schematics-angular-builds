b2ff6e16015cdb2f4926d52837e58e6531745a75
Angular Builds <dev-infra@angular.dev>
2026-03-03 21:51:35 +0000

    0299b4d fix(@angular-devkit/build-angular): update copy-webpack-plugin to v14.0.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.18+sha-05b3511',
-    AngularBuild: '^20.3.18+sha-05b3511',
-    AngularSSR: '^20.3.18+sha-05b3511',
+    DevkitBuildAngular: '^20.3.18+sha-0299b4d',
+    AngularBuild: '^20.3.18+sha-0299b4d',
+    AngularSSR: '^20.3.18+sha-0299b4d',
 };
