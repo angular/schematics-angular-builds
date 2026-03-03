7f8522d82cfa1a1e72fe0d14a16a3bd6d738874a
Angular Builds <dev-infra@angular.dev>
2026-03-03 16:08:58 +0000

    73186ba refactor: remove `tree-kill` dependency and refactor `killAllProcesses` to use native `childProc.kill`.

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-0f41585',
-    AngularBuild: '^22.0.0-next.0+sha-0f41585',
-    AngularSSR: '^22.0.0-next.0+sha-0f41585',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-73186ba',
+    AngularBuild: '^22.0.0-next.0+sha-73186ba',
+    AngularSSR: '^22.0.0-next.0+sha-73186ba',
 };
 //# sourceMappingURL=latest-versions.js.map
