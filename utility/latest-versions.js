828f119ac9cb6026db28e718d9cbb5b0f387b579
Angular Builds <dev-infra@angular.dev>
2026-03-03 17:26:26 +0000

    4dc8ea0 build: update dependency verdaccio to v6.2.9

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.0+sha-fd5dcf3',
-    AngularBuild: '^21.2.0+sha-fd5dcf3',
-    AngularSSR: '^21.2.0+sha-fd5dcf3',
+    DevkitBuildAngular: '^21.2.0+sha-4dc8ea0',
+    AngularBuild: '^21.2.0+sha-4dc8ea0',
+    AngularSSR: '^21.2.0+sha-4dc8ea0',
 };
 //# sourceMappingURL=latest-versions.js.map
