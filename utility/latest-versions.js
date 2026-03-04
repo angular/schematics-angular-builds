255071a3ff7966528d4c5f1d587560c990757ce1
Angular Builds <dev-infra@angular.dev>
2026-03-04 09:40:39 +0000

    843ca01 ci: update pull-request labeling action path

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-6293cae',
-    AngularBuild: '^22.0.0-next.0+sha-6293cae',
-    AngularSSR: '^22.0.0-next.0+sha-6293cae',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-843ca01',
+    AngularBuild: '^22.0.0-next.0+sha-843ca01',
+    AngularSSR: '^22.0.0-next.0+sha-843ca01',
 };
 //# sourceMappingURL=latest-versions.js.map
