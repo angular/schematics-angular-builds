4c6c0639153f5693ea56b03377e153453b35cec6
Angular Builds <dev-infra@angular.dev>
2026-03-04 13:38:37 +0000

    0d49f86 fix(@angular/build): resolve style include paths relative to `ng-package.json` in unit-test builder

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.0+sha-d173973',
-    AngularBuild: '^21.2.0+sha-d173973',
-    AngularSSR: '^21.2.0+sha-d173973',
+    DevkitBuildAngular: '^21.2.0+sha-0d49f86',
+    AngularBuild: '^21.2.0+sha-0d49f86',
+    AngularSSR: '^21.2.0+sha-0d49f86',
 };
 //# sourceMappingURL=latest-versions.js.map
