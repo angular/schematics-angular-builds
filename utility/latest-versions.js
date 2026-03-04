8a4dbd7d5c9b329e4afa27ecebc07ca8a49db921
Angular Builds <dev-infra@angular.dev>
2026-03-04 12:15:55 +0000

    6340666 refactor: move `baseUrl` fallback logic to `paths-plugin.ts`

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-843ca01',
-    AngularBuild: '^22.0.0-next.0+sha-843ca01',
-    AngularSSR: '^22.0.0-next.0+sha-843ca01',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-6340666',
+    AngularBuild: '^22.0.0-next.0+sha-6340666',
+    AngularSSR: '^22.0.0-next.0+sha-6340666',
 };
 //# sourceMappingURL=latest-versions.js.map
