d10b50f3ceaa3e6a3e05f917755da9941c9a2401
Angular Builds <dev-infra@angular.dev>
2026-03-04 02:31:28 +0000

    62b1500 ci: create gemini code reviewer configuration

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.0+sha-a173e1e',
-    AngularBuild: '^21.2.0+sha-a173e1e',
-    AngularSSR: '^21.2.0+sha-a173e1e',
+    DevkitBuildAngular: '^21.2.0+sha-62b1500',
+    AngularBuild: '^21.2.0+sha-62b1500',
+    AngularSSR: '^21.2.0+sha-62b1500',
 };
 //# sourceMappingURL=latest-versions.js.map
