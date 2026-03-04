afad9d37f1f9196ef2850679d2b52b60b17030a7
Angular Builds <dev-infra@angular.dev>
2026-03-04 14:02:50 +0000

    f17ed30 fix(@angular/build): treat empty browsers array as undefined in unit-test builder

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0-next.0',
     NgPackagr: '^21.2.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-ac065ad',
-    AngularBuild: '^22.0.0-next.0+sha-ac065ad',
-    AngularSSR: '^22.0.0-next.0+sha-ac065ad',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-f17ed30',
+    AngularBuild: '^22.0.0-next.0+sha-f17ed30',
+    AngularSSR: '^22.0.0-next.0+sha-f17ed30',
 };
 //# sourceMappingURL=latest-versions.js.map
