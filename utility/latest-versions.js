1f439f8f18747dc642c7c9be719085fefc39c05a
Angular Builds <dev-infra@angular.dev>
2026-03-05 12:02:06 +0000

    5d0c64e build: correct deprecated version for architect-cli in monorepo configuration

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.1+sha-74e254b',
-    AngularBuild: '^21.2.1+sha-74e254b',
-    AngularSSR: '^21.2.1+sha-74e254b',
+    DevkitBuildAngular: '^21.2.1+sha-5d0c64e',
+    AngularBuild: '^21.2.1+sha-5d0c64e',
+    AngularSSR: '^21.2.1+sha-5d0c64e',
 };
 //# sourceMappingURL=latest-versions.js.map
