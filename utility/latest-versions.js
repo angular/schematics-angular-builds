45a9d75961749eb1d09b3dcfb37e5782a7e9f8f8
Angular Builds <dev-infra@angular.dev>
2026-03-05 08:57:36 +0000

    500b47d ci: add issue_labels job to dev-infra workflow

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.0+sha-33a3172',
-    AngularBuild: '^21.2.0+sha-33a3172',
-    AngularSSR: '^21.2.0+sha-33a3172',
+    DevkitBuildAngular: '^21.2.0+sha-500b47d',
+    AngularBuild: '^21.2.0+sha-500b47d',
+    AngularSSR: '^21.2.0+sha-500b47d',
 };
 //# sourceMappingURL=latest-versions.js.map
