08445f224af155433e8bfd90c806667c17783400
Angular Builds <dev-infra@angular.dev>
2026-03-06 16:09:00 +0000

    f30f890 fix(@angular/build): resolve assets correctly during i18n prerendering

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-f0fc197',
-    AngularBuild: '^22.0.0-next.0+sha-f0fc197',
-    AngularSSR: '^22.0.0-next.0+sha-f0fc197',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-f30f890',
+    AngularBuild: '^22.0.0-next.0+sha-f30f890',
+    AngularSSR: '^22.0.0-next.0+sha-f30f890',
 };
 //# sourceMappingURL=latest-versions.js.map
