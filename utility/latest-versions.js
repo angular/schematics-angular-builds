b251f90e7b1b8b8c80be74c2e08bb41f8c22fb2e
Angular Builds <dev-infra@angular.dev>
2026-03-09 15:44:07 +0000

    b5fb457 fix(@angular/cli): preserve exact version in ng add when requested

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-6e4aa3b',
-    AngularBuild: '^22.0.0-next.0+sha-6e4aa3b',
-    AngularSSR: '^22.0.0-next.0+sha-6e4aa3b',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-b5fb457',
+    AngularBuild: '^22.0.0-next.0+sha-b5fb457',
+    AngularSSR: '^22.0.0-next.0+sha-b5fb457',
 };
 //# sourceMappingURL=latest-versions.js.map
