3f17092a5a28d761ce3fa353f54cced53cc02d90
Angular Builds <dev-infra@angular.dev>
2026-03-09 13:47:07 +0000

    a39a331 perf(@angular/cli): cache root manifest and resolve restricted package exports in ng add

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-f30f890',
-    AngularBuild: '^22.0.0-next.0+sha-f30f890',
-    AngularSSR: '^22.0.0-next.0+sha-f30f890',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-a39a331',
+    AngularBuild: '^22.0.0-next.0+sha-a39a331',
+    AngularSSR: '^22.0.0-next.0+sha-a39a331',
 };
 //# sourceMappingURL=latest-versions.js.map
