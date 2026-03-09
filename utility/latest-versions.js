ecf9dfc54547638ddd69f3cec21d424c67630a1c
Angular Builds <dev-infra@angular.dev>
2026-03-09 15:16:02 +0000

    6e4aa3b build: fix broken lock file

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-832bb49',
-    AngularBuild: '^22.0.0-next.0+sha-832bb49',
-    AngularSSR: '^22.0.0-next.0+sha-832bb49',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-6e4aa3b',
+    AngularBuild: '^22.0.0-next.0+sha-6e4aa3b',
+    AngularSSR: '^22.0.0-next.0+sha-6e4aa3b',
 };
 //# sourceMappingURL=latest-versions.js.map
