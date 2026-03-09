debb95adfd48dc9910f1c0d166790aeca0aa1be5
Angular Builds <dev-infra@angular.dev>
2026-03-09 14:03:41 +0000

    832bb49 build: update to the TypeScript 6 RC

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-a39a331',
-    AngularBuild: '^22.0.0-next.0+sha-a39a331',
-    AngularSSR: '^22.0.0-next.0+sha-a39a331',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-832bb49',
+    AngularBuild: '^22.0.0-next.0+sha-832bb49',
+    AngularSSR: '^22.0.0-next.0+sha-832bb49',
 };
 //# sourceMappingURL=latest-versions.js.map
