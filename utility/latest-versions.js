8bad8e91eb93262ee3541e6350f91481e9cb5de1
Angular Builds <dev-infra@angular.dev>
2026-03-09 13:43:18 +0000

    28f4d68 perf(@angular/cli): avoid redundant package version resolution in ng add

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.1+sha-4ca6164',
-    AngularBuild: '^21.2.1+sha-4ca6164',
-    AngularSSR: '^21.2.1+sha-4ca6164',
+    DevkitBuildAngular: '^21.2.1+sha-28f4d68',
+    AngularBuild: '^21.2.1+sha-28f4d68',
+    AngularSSR: '^21.2.1+sha-28f4d68',
 };
 //# sourceMappingURL=latest-versions.js.map
