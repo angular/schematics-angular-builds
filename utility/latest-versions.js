ea7172362cbecd238034c9f2f2cfeeaf6ea395fe
Angular Builds <dev-infra@angular.dev>
2026-03-10 15:23:10 +0000

    af2c7e9 feat(@angular/build): rename `experimentalPlatform` to `platform` in application builder

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.0+sha-231f579',
-    AngularBuild: '^22.0.0-next.0+sha-231f579',
-    AngularSSR: '^22.0.0-next.0+sha-231f579',
+    DevkitBuildAngular: '^22.0.0-next.0+sha-af2c7e9',
+    AngularBuild: '^22.0.0-next.0+sha-af2c7e9',
+    AngularSSR: '^22.0.0-next.0+sha-af2c7e9',
 };
 //# sourceMappingURL=latest-versions.js.map
