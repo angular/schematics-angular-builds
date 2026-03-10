93c6ce81366b65fcd99ca6069cbe56a80458750e
Angular Builds <dev-infra@angular.dev>
2026-03-10 10:26:57 +0000

    8447d91 fix(@angular/cli): conditionally quote package names when adding dependencies based on host requirements

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.1+sha-58e2e56',
-    AngularBuild: '^21.2.1+sha-58e2e56',
-    AngularSSR: '^21.2.1+sha-58e2e56',
+    DevkitBuildAngular: '^21.2.1+sha-8447d91',
+    AngularBuild: '^21.2.1+sha-8447d91',
+    AngularSSR: '^21.2.1+sha-8447d91',
 };
 //# sourceMappingURL=latest-versions.js.map
