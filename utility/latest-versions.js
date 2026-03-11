8e2dca0d4d60b42814074ef0244a3704038d65e0
Angular Builds <dev-infra@angular.dev>
2026-03-11 12:41:59 +0000

    0fd6823 fix(@angular/build): pass process environment variables to prerender workers

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.19+sha-93a6f36',
-    AngularBuild: '^20.3.19+sha-93a6f36',
-    AngularSSR: '^20.3.19+sha-93a6f36',
+    DevkitBuildAngular: '^20.3.19+sha-0fd6823',
+    AngularBuild: '^20.3.19+sha-0fd6823',
+    AngularSSR: '^20.3.19+sha-0fd6823',
 };
