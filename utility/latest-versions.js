e7d49dcbde017f64053f9dddc8a54c31a8f7b40d
Angular Builds <dev-infra@angular.dev>
2026-03-12 09:30:26 +0000

    6572a69 fix(@schematics/angular): default components to OnPush change detection

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.1+sha-174c786',
-    AngularBuild: '^22.0.0-next.1+sha-174c786',
-    AngularSSR: '^22.0.0-next.1+sha-174c786',
+    DevkitBuildAngular: '^22.0.0-next.1+sha-6572a69',
+    AngularBuild: '^22.0.0-next.1+sha-6572a69',
+    AngularSSR: '^22.0.0-next.1+sha-6572a69',
 };
 //# sourceMappingURL=latest-versions.js.map
