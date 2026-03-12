fd43b6a4b5d809201f5c1b99bde984f730d4b556
Angular Builds <dev-infra@angular.dev>
2026-03-12 10:23:58 +0000

    cb57740 build: update actions/download-artifact action to v8.0.1

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.1+sha-6572a69',
-    AngularBuild: '^22.0.0-next.1+sha-6572a69',
-    AngularSSR: '^22.0.0-next.1+sha-6572a69',
+    DevkitBuildAngular: '^22.0.0-next.1+sha-cb57740',
+    AngularBuild: '^22.0.0-next.1+sha-cb57740',
+    AngularSSR: '^22.0.0-next.1+sha-cb57740',
 };
 //# sourceMappingURL=latest-versions.js.map
