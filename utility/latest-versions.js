b9c2865a239ff7f6571619911203f0309472cea0
Angular Builds <dev-infra@angular.dev>
2026-03-13 16:32:57 +0000

    4643a8a fix(@angular/build): only use external packages for polyfills when no local files are present

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.1+sha-cb57740',
-    AngularBuild: '^22.0.0-next.1+sha-cb57740',
-    AngularSSR: '^22.0.0-next.1+sha-cb57740',
+    DevkitBuildAngular: '^22.0.0-next.1+sha-4643a8a',
+    AngularBuild: '^22.0.0-next.1+sha-4643a8a',
+    AngularSSR: '^22.0.0-next.1+sha-4643a8a',
 };
 //# sourceMappingURL=latest-versions.js.map
