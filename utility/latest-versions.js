bcf6899d6bc0aa965856ed9ca568d7b90bc6e4e8
Angular Builds <dev-infra@angular.dev>
2026-03-17 10:06:27 +0000

    8438541 fix(@angular/ssr): support custom headers in redirect responses

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.2+sha-411b2db',
-    AngularBuild: '^21.2.2+sha-411b2db',
-    AngularSSR: '^21.2.2+sha-411b2db',
+    DevkitBuildAngular: '^21.2.2+sha-8438541',
+    AngularBuild: '^21.2.2+sha-8438541',
+    AngularSSR: '^21.2.2+sha-8438541',
 };
 //# sourceMappingURL=latest-versions.js.map
