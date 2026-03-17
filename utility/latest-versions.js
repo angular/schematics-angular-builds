f5bffcaccb54bbac050bb4b4e9db3ae8ab40f9fd
Angular Builds <dev-infra@angular.dev>
2026-03-17 14:46:05 +0000

    1505164 fix(@angular/cli): use parsed package name for migrate-only updates

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.2+sha-8438541',
-    AngularBuild: '^21.2.2+sha-8438541',
-    AngularSSR: '^21.2.2+sha-8438541',
+    DevkitBuildAngular: '^21.2.2+sha-1505164',
+    AngularBuild: '^21.2.2+sha-1505164',
+    AngularSSR: '^21.2.2+sha-1505164',
 };
 //# sourceMappingURL=latest-versions.js.map
