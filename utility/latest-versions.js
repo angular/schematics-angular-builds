0d8105e63aa2c57d371eb5ae94944826ae4d3998
Angular Builds <dev-infra@angular.dev>
2026-03-17 17:50:59 +0000

    0a2ff0b fix(@angular/ssr): ensure unique values in redirect response Vary header

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.20+sha-1e7d877',
-    AngularBuild: '^20.3.20+sha-1e7d877',
-    AngularSSR: '^20.3.20+sha-1e7d877',
+    DevkitBuildAngular: '^20.3.20+sha-0a2ff0b',
+    AngularBuild: '^20.3.20+sha-0a2ff0b',
+    AngularSSR: '^20.3.20+sha-0a2ff0b',
 };
