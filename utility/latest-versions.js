ba60800a4bcd8ab336cb24da53c0953660b0ab46
Angular Builds <dev-infra@angular.dev>
2026-03-20 15:15:51 +0000

    1a8376b fix(@angular/build): remove default for unit-test coverage option

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.2+sha-2678f5f',
-    AngularBuild: '^22.0.0-next.2+sha-2678f5f',
-    AngularSSR: '^22.0.0-next.2+sha-2678f5f',
+    DevkitBuildAngular: '^22.0.0-next.2+sha-1a8376b',
+    AngularBuild: '^22.0.0-next.2+sha-1a8376b',
+    AngularSSR: '^22.0.0-next.2+sha-1a8376b',
 };
 //# sourceMappingURL=latest-versions.js.map
