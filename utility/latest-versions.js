87e833344467e87db99d209da47ca38c8640c09f
Angular Builds <dev-infra@angular.dev>
2026-03-23 13:44:56 +0000

    742a874 ci: block esbuild-wasm updates due to a regression

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.2+sha-65f39d0',
-    AngularBuild: '^22.0.0-next.2+sha-65f39d0',
-    AngularSSR: '^22.0.0-next.2+sha-65f39d0',
+    DevkitBuildAngular: '^22.0.0-next.2+sha-742a874',
+    AngularBuild: '^22.0.0-next.2+sha-742a874',
+    AngularSSR: '^22.0.0-next.2+sha-742a874',
 };
 //# sourceMappingURL=latest-versions.js.map
