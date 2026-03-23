2edf75acf5acf20524d2fca0f7dfc13d03a56ace
Angular Builds <dev-infra@angular.dev>
2026-03-23 16:16:28 +0000

    240870e test: ignore HMR disabled console errors

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.2+sha-742a874',
-    AngularBuild: '^22.0.0-next.2+sha-742a874',
-    AngularSSR: '^22.0.0-next.2+sha-742a874',
+    DevkitBuildAngular: '^22.0.0-next.2+sha-240870e',
+    AngularBuild: '^22.0.0-next.2+sha-240870e',
+    AngularSSR: '^22.0.0-next.2+sha-240870e',
 };
 //# sourceMappingURL=latest-versions.js.map
