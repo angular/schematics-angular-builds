cffc2bda2455b051fb98f02a1c10ff89f35381a0
Angular Builds <dev-infra@angular.dev>
2026-03-23 11:39:36 +0000

    c73f137 fix(@angular/build): prevent reporter duplicates by explicitly overriding Vitest configuration

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.3+sha-956ccaa',
-    AngularBuild: '^21.2.3+sha-956ccaa',
-    AngularSSR: '^21.2.3+sha-956ccaa',
+    DevkitBuildAngular: '^21.2.3+sha-c73f137',
+    AngularBuild: '^21.2.3+sha-c73f137',
+    AngularSSR: '^21.2.3+sha-c73f137',
 };
 //# sourceMappingURL=latest-versions.js.map
