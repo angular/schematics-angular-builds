1cbf9d353f2b0de020d1b18819f8b859a8160454
Angular Builds <dev-infra@angular.dev>
2026-03-24 07:44:40 +0000

    ca6f08c fix(@angular/ssr): apply forwarded prefix and vary header in accept-language redirects

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.2+sha-240870e',
-    AngularBuild: '^22.0.0-next.2+sha-240870e',
-    AngularSSR: '^22.0.0-next.2+sha-240870e',
+    DevkitBuildAngular: '^22.0.0-next.2+sha-ca6f08c',
+    AngularBuild: '^22.0.0-next.2+sha-ca6f08c',
+    AngularSSR: '^22.0.0-next.2+sha-ca6f08c',
 };
 //# sourceMappingURL=latest-versions.js.map
