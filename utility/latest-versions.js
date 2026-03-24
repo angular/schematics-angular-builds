82bd4c736866b9d2fea7cb1119ff86011813b865
Angular Builds <dev-infra@angular.dev>
2026-03-24 14:13:29 +0000

    f98cc82 feat(@schematics/angular): rely on strict template default in generated workspaces

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.2+sha-ca6f08c',
-    AngularBuild: '^22.0.0-next.2+sha-ca6f08c',
-    AngularSSR: '^22.0.0-next.2+sha-ca6f08c',
+    DevkitBuildAngular: '^22.0.0-next.2+sha-f98cc82',
+    AngularBuild: '^22.0.0-next.2+sha-f98cc82',
+    AngularSSR: '^22.0.0-next.2+sha-f98cc82',
 };
 //# sourceMappingURL=latest-versions.js.map
