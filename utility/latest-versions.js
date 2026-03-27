e915d211cf844c2f098ddaa2a00168bd03b17aaa
Angular Builds <dev-infra@angular.dev>
2026-03-27 16:27:45 +0000

    107d1a9 fix(@angular/build): preserve error stack traces during prerendering

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.5+sha-7d3844a',
-    AngularBuild: '^21.2.5+sha-7d3844a',
-    AngularSSR: '^21.2.5+sha-7d3844a',
+    DevkitBuildAngular: '^21.2.5+sha-107d1a9',
+    AngularBuild: '^21.2.5+sha-107d1a9',
+    AngularSSR: '^21.2.5+sha-107d1a9',
 };
 //# sourceMappingURL=latest-versions.js.map
