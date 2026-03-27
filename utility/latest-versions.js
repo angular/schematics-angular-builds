d154f2bb033855e8a523a4ec7e021c721aef1696
Angular Builds <dev-infra@angular.dev>
2026-03-27 07:44:51 +0000

    de2da48 fix(@angular-devkit/core): update picomatch to 4.0.4

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0',
     NgPackagr: '^19.2.0',
-    DevkitBuildAngular: '^19.2.22+sha-3d47bd3',
-    AngularBuild: '^19.2.22+sha-3d47bd3',
-    AngularSSR: '^19.2.22+sha-3d47bd3',
+    DevkitBuildAngular: '^19.2.22+sha-de2da48',
+    AngularBuild: '^19.2.22+sha-de2da48',
+    AngularSSR: '^19.2.22+sha-de2da48',
 };
