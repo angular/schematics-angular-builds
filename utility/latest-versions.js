9733d42c3189aae3b7e0ff880bba5d85778e328b
Angular Builds <dev-infra@angular.dev>
2026-03-30 07:35:30 +0000

    ea14f28 fix(@angular/cli): fix sourceRoot resolution for MCP projects tool

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.5+sha-107d1a9',
-    AngularBuild: '^21.2.5+sha-107d1a9',
-    AngularSSR: '^21.2.5+sha-107d1a9',
+    DevkitBuildAngular: '^21.2.5+sha-ea14f28',
+    AngularBuild: '^21.2.5+sha-ea14f28',
+    AngularSSR: '^21.2.5+sha-ea14f28',
 };
 //# sourceMappingURL=latest-versions.js.map
