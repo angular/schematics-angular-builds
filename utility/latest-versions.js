06692e541fa88532fedbe9c58f4f463d1bbd3b01
Angular Builds <dev-infra@angular.dev>
2026-04-07 16:25:40 +0000

    5035705 build: update dependency https-proxy-agent to v9

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.4+sha-be60a63',
-    AngularBuild: '^22.0.0-next.4+sha-be60a63',
-    AngularSSR: '^22.0.0-next.4+sha-be60a63',
+    DevkitBuildAngular: '^22.0.0-next.4+sha-5035705',
+    AngularBuild: '^22.0.0-next.4+sha-5035705',
+    AngularSSR: '^22.0.0-next.4+sha-5035705',
 };
 //# sourceMappingURL=latest-versions.js.map
