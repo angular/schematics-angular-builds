06c6dc3298f8dafd580d8a003ad5b02a5d418c3c
Angular Builds <dev-infra@angular.dev>
2026-04-10 13:48:52 +0000

    6904329 build: move TypeScript and Angular Compiler CLI to dependencies

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.5+sha-9f99724',
-    AngularBuild: '^22.0.0-next.5+sha-9f99724',
-    AngularSSR: '^22.0.0-next.5+sha-9f99724',
+    DevkitBuildAngular: '^22.0.0-next.5+sha-6904329',
+    AngularBuild: '^22.0.0-next.5+sha-6904329',
+    AngularSSR: '^22.0.0-next.5+sha-6904329',
 };
 //# sourceMappingURL=latest-versions.js.map
