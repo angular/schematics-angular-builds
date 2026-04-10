6d223d76f94f86b8e6ca0e5e0c64cd478b7089e0
Angular Builds <dev-infra@angular.dev>
2026-04-10 17:33:28 +0000

    0d984cf fix(@angular/cli): use headless option in MCP test tool

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.5+sha-6904329',
-    AngularBuild: '^22.0.0-next.5+sha-6904329',
-    AngularSSR: '^22.0.0-next.5+sha-6904329',
+    DevkitBuildAngular: '^22.0.0-next.5+sha-0d984cf',
+    AngularBuild: '^22.0.0-next.5+sha-0d984cf',
+    AngularSSR: '^22.0.0-next.5+sha-0d984cf',
 };
 //# sourceMappingURL=latest-versions.js.map
