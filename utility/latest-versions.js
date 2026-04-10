106c652e84eceb94ae59d304cec11ca21590f290
Angular Builds <dev-infra@angular.dev>
2026-04-10 10:39:11 +0000

    b85ec67 fix(@angular/build): allow configuring Access-Control-Allow-Origin via headers option

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.5+sha-cd2ad3c',
-    AngularBuild: '^22.0.0-next.5+sha-cd2ad3c',
-    AngularSSR: '^22.0.0-next.5+sha-cd2ad3c',
+    DevkitBuildAngular: '^22.0.0-next.5+sha-b85ec67',
+    AngularBuild: '^22.0.0-next.5+sha-b85ec67',
+    AngularSSR: '^22.0.0-next.5+sha-b85ec67',
 };
 //# sourceMappingURL=latest-versions.js.map
