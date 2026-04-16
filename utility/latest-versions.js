4074c2953b0a7716b7dd9a55d4edbe44c4968cdd
Angular Builds <dev-infra@angular.dev>
2026-04-16 12:11:58 +0000

    30d8332 fix(@angular-devkit/build-angular): ensure route has leading slash in prerender builder

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.5+sha-c4b659a',
-    AngularBuild: '^22.0.0-next.5+sha-c4b659a',
-    AngularSSR: '^22.0.0-next.5+sha-c4b659a',
+    DevkitBuildAngular: '^22.0.0-next.5+sha-30d8332',
+    AngularBuild: '^22.0.0-next.5+sha-30d8332',
+    AngularSSR: '^22.0.0-next.5+sha-30d8332',
 };
 //# sourceMappingURL=latest-versions.js.map
