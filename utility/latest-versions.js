a75e1f55d549556890af62c2840a15f4833fa08d
Angular Builds <dev-infra@angular.dev>
2026-04-17 17:43:15 +0000

    4f57582 build: update babel monorepo to v7.29.2

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.7+sha-fb0c4bd',
-    AngularBuild: '^21.2.7+sha-fb0c4bd',
-    AngularSSR: '^21.2.7+sha-fb0c4bd',
+    DevkitBuildAngular: '^21.2.7+sha-4f57582',
+    AngularBuild: '^21.2.7+sha-4f57582',
+    AngularSSR: '^21.2.7+sha-4f57582',
 };
 //# sourceMappingURL=latest-versions.js.map
