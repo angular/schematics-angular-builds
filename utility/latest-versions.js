153cd6a8e4f8dd5386e24148bbcdca9615c65a80
Angular Builds <dev-infra@angular.dev>
2026-04-17 14:05:39 +0000

    afb4ef2 build: update schematics dependencies to ~6.2.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.5+sha-cf5b49e',
-    AngularBuild: '^22.0.0-next.5+sha-cf5b49e',
-    AngularSSR: '^22.0.0-next.5+sha-cf5b49e',
+    DevkitBuildAngular: '^22.0.0-next.5+sha-afb4ef2',
+    AngularBuild: '^22.0.0-next.5+sha-afb4ef2',
+    AngularSSR: '^22.0.0-next.5+sha-afb4ef2',
 };
 //# sourceMappingURL=latest-versions.js.map
