f26941e16ca169b7a1cf91bc38cb06929e5d9ef1
Angular Builds <dev-infra@angular.dev>
2026-04-17 13:24:54 +0000

    e87d302 fix(@angular-devkit/build-angular): fix app-shell route format and improve package resolution

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.7+sha-0787786',
-    AngularBuild: '^21.2.7+sha-0787786',
-    AngularSSR: '^21.2.7+sha-0787786',
+    DevkitBuildAngular: '^21.2.7+sha-e87d302',
+    AngularBuild: '^21.2.7+sha-e87d302',
+    AngularSSR: '^21.2.7+sha-e87d302',
 };
 //# sourceMappingURL=latest-versions.js.map
