8e30b905a08c25f29fcb184e29b790a1be49363d
Angular Builds <dev-infra@angular.dev>
2026-04-22 08:12:00 +0000

    0781f74 feat(@angular/build): add quiet option to suppress build noise in unit tests

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.5+sha-a4f11c1',
-    AngularBuild: '^22.0.0-next.5+sha-a4f11c1',
-    AngularSSR: '^22.0.0-next.5+sha-a4f11c1',
+    DevkitBuildAngular: '^22.0.0-next.5+sha-0781f74',
+    AngularBuild: '^22.0.0-next.5+sha-0781f74',
+    AngularSSR: '^22.0.0-next.5+sha-0781f74',
 };
 //# sourceMappingURL=latest-versions.js.map
