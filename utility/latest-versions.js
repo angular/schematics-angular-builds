bd9fada00e62cab2478babf7b05cb6d0b2d1d609
Angular Builds <dev-infra@angular.dev>
2026-04-24 08:15:37 +0000

    5adc925 fix(@angular/ssr): enforce explicit opt-in for proxy headers

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.6+sha-2932ff0',
-    AngularBuild: '^22.0.0-next.6+sha-2932ff0',
-    AngularSSR: '^22.0.0-next.6+sha-2932ff0',
+    DevkitBuildAngular: '^22.0.0-next.6+sha-5adc925',
+    AngularBuild: '^22.0.0-next.6+sha-5adc925',
+    AngularSSR: '^22.0.0-next.6+sha-5adc925',
 };
 //# sourceMappingURL=latest-versions.js.map
