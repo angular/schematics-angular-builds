64627ebfbdfdcc8041790b29b31d0a2585c030d5
Angular Builds <dev-infra@angular.dev>
2026-04-27 15:49:38 +0000

    7cea988 fix(@angular/cli): introduce initial package manager workspace awareness

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.8+sha-94023f6',
-    AngularBuild: '^21.2.8+sha-94023f6',
-    AngularSSR: '^21.2.8+sha-94023f6',
+    DevkitBuildAngular: '^21.2.8+sha-7cea988',
+    AngularBuild: '^21.2.8+sha-7cea988',
+    AngularSSR: '^21.2.8+sha-7cea988',
 };
 //# sourceMappingURL=latest-versions.js.map
