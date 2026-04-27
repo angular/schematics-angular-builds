d4fa64a9c8df3cf138609be25b07531199e92a05
Angular Builds <dev-infra@angular.dev>
2026-04-27 17:11:24 +0000

    7b5a86c refactor: remove unused fetchPackageMetadata and fetchPackageManifest utilities

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.6+sha-1b900f0',
-    AngularBuild: '^22.0.0-next.6+sha-1b900f0',
-    AngularSSR: '^22.0.0-next.6+sha-1b900f0',
+    DevkitBuildAngular: '^22.0.0-next.6+sha-7b5a86c',
+    AngularBuild: '^22.0.0-next.6+sha-7b5a86c',
+    AngularSSR: '^22.0.0-next.6+sha-7b5a86c',
 };
 //# sourceMappingURL=latest-versions.js.map
