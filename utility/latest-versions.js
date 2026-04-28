2c40c5a17e8ed9ba121243d90c8c4305a17266c8
Angular Builds <dev-infra@angular.dev>
2026-04-28 07:09:37 +0000

    126b19b fix(@angular/ssr): add support for configuring trusted proxy headers via environment variable

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.6+sha-7b5a86c',
-    AngularBuild: '^22.0.0-next.6+sha-7b5a86c',
-    AngularSSR: '^22.0.0-next.6+sha-7b5a86c',
+    DevkitBuildAngular: '^22.0.0-next.6+sha-126b19b',
+    AngularBuild: '^22.0.0-next.6+sha-126b19b',
+    AngularSSR: '^22.0.0-next.6+sha-126b19b',
 };
 //# sourceMappingURL=latest-versions.js.map
