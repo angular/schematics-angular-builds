2250c9c35d0168084dbdde5c85af1371dcf4f2f0
Angular Builds <dev-infra@angular.dev>
2026-04-28 11:41:29 +0000

    49ae0ad fix(@angular-devkit/build-angular): upgrade postcss to 8.5.12

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0',
     NgPackagr: '^19.2.0',
-    DevkitBuildAngular: '^19.2.24+sha-bf387fb',
-    AngularBuild: '^19.2.24+sha-bf387fb',
-    AngularSSR: '^19.2.24+sha-bf387fb',
+    DevkitBuildAngular: '^19.2.24+sha-49ae0ad',
+    AngularBuild: '^19.2.24+sha-49ae0ad',
+    AngularSSR: '^19.2.24+sha-49ae0ad',
 };
