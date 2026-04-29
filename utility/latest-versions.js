83a2ad4d3d4924cd39c5be567abc6149ca174a48
Angular Builds <dev-infra@angular.dev>
2026-04-29 21:53:56 +0000

    f8800f7 release: cut the v22.0.0-next.7 release

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.6+sha-61f1b32',
-    AngularBuild: '^22.0.0-next.6+sha-61f1b32',
-    AngularSSR: '^22.0.0-next.6+sha-61f1b32',
+    DevkitBuildAngular: '^22.0.0-next.7+sha-f8800f7',
+    AngularBuild: '^22.0.0-next.7+sha-f8800f7',
+    AngularSSR: '^22.0.0-next.7+sha-f8800f7',
 };
 //# sourceMappingURL=latest-versions.js.map
