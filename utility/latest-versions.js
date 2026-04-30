bda73f6e4c461306c968157802c9893e16a7d852
Angular Builds <dev-infra@angular.dev>
2026-04-30 08:11:16 +0000

    0841637 test: allow underscores in hash for server routes preload links

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.7+sha-b5ba365',
-    AngularBuild: '^22.0.0-next.7+sha-b5ba365',
-    AngularSSR: '^22.0.0-next.7+sha-b5ba365',
+    DevkitBuildAngular: '^22.0.0-next.7+sha-0841637',
+    AngularBuild: '^22.0.0-next.7+sha-0841637',
+    AngularSSR: '^22.0.0-next.7+sha-0841637',
 };
 //# sourceMappingURL=latest-versions.js.map
