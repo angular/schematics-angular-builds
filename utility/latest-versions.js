486585a9408233a98d01474e9048d92800e89e50
Angular Builds <dev-infra@angular.dev>
2026-04-30 15:26:53 +0000

    870cc5d refactor: configure Gemini reviewer to ignore lockfile and changelog

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.7+sha-8aca55d',
-    AngularBuild: '^22.0.0-next.7+sha-8aca55d',
-    AngularSSR: '^22.0.0-next.7+sha-8aca55d',
+    DevkitBuildAngular: '^22.0.0-next.7+sha-870cc5d',
+    AngularBuild: '^22.0.0-next.7+sha-870cc5d',
+    AngularSSR: '^22.0.0-next.7+sha-870cc5d',
 };
 //# sourceMappingURL=latest-versions.js.map
