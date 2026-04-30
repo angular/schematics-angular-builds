1995c516351ce099a5471af8a94a4e8cec893311
Angular Builds <dev-infra@angular.dev>
2026-04-30 13:42:47 +0000

    8aca55d refactor(@angular/cli): remove modernize tool and add instructions for schematic discovery

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.7+sha-0841637',
-    AngularBuild: '^22.0.0-next.7+sha-0841637',
-    AngularSSR: '^22.0.0-next.7+sha-0841637',
+    DevkitBuildAngular: '^22.0.0-next.7+sha-8aca55d',
+    AngularBuild: '^22.0.0-next.7+sha-8aca55d',
+    AngularSSR: '^22.0.0-next.7+sha-8aca55d',
 };
 //# sourceMappingURL=latest-versions.js.map
