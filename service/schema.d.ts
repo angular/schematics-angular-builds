7a292431de462b142243ac34ea59c086351b3ce4
Angular Builds <dev-infra@angular.dev>
2026-05-04 16:59:43 +0000

    7fb59ea fix(@schematics/angular): use service decorator in ng generate

--- service/schema.d.ts
+++ service/schema.d.ts
@@ -15,6 +15,10 @@ export type Schema = {
      */
     flat?: boolean;
     /**
+     * When true, generates an `@Injectable` instead of `@Service`.
+     */
+    injectable?: boolean;
+    /**
      * The name for the new service. This will be used to create the service's class and spec
      * files (e.g., `my-service.service.ts` and `my-service.service.spec.ts`).
      */
--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.7+sha-b2f7a03',
-    AngularBuild: '^22.0.0-next.7+sha-b2f7a03',
-    AngularSSR: '^22.0.0-next.7+sha-b2f7a03',
+    DevkitBuildAngular: '^22.0.0-next.7+sha-7fb59ea',
+    AngularBuild: '^22.0.0-next.7+sha-7fb59ea',
+    AngularSSR: '^22.0.0-next.7+sha-7fb59ea',
 };
 //# sourceMappingURL=latest-versions.js.map
