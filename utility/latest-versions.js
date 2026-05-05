98699257b1192e804e1047c6536793c08165643f
Angular Builds <dev-infra@angular.dev>
2026-05-05 00:09:34 +0000

    deca40b build: update all non-major dependencies

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.7+sha-086e081',
-    AngularBuild: '^22.0.0-next.7+sha-086e081',
-    AngularSSR: '^22.0.0-next.7+sha-086e081',
+    DevkitBuildAngular: '^22.0.0-next.7+sha-deca40b',
+    AngularBuild: '^22.0.0-next.7+sha-deca40b',
+    AngularSSR: '^22.0.0-next.7+sha-deca40b',
 };
 //# sourceMappingURL=latest-versions.js.map
