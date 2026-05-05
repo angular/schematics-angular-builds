334412e6a82425cd5359a20e3b5d3136093980d7
Angular Builds <dev-infra@angular.dev>
2026-05-05 14:48:22 +0000

    111dcc3 refactor(@angular/cli): re-add resolveModule for find examples tool

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.9+sha-bb86119',
-    AngularBuild: '^21.2.9+sha-bb86119',
-    AngularSSR: '^21.2.9+sha-bb86119',
+    DevkitBuildAngular: '^21.2.9+sha-111dcc3',
+    AngularBuild: '^21.2.9+sha-111dcc3',
+    AngularSSR: '^21.2.9+sha-111dcc3',
 };
 //# sourceMappingURL=latest-versions.js.map
