bc2238fc06dcbdd2e6ec2a394ec71474b5dea22f
Angular Builds <dev-infra@angular.dev>
2026-05-06 16:47:29 +0000

    bf94764 build: remove rootDir from root configuration

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.7+sha-a5e1e48',
-    AngularBuild: '^22.0.0-next.7+sha-a5e1e48',
-    AngularSSR: '^22.0.0-next.7+sha-a5e1e48',
+    DevkitBuildAngular: '^22.0.0-next.7+sha-bf94764',
+    AngularBuild: '^22.0.0-next.7+sha-bf94764',
+    AngularSSR: '^22.0.0-next.7+sha-bf94764',
 };
 //# sourceMappingURL=latest-versions.js.map
