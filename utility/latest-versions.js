ae21088315650eb2e6581505cf901087da77f9b7
Angular Builds <dev-infra@angular.dev>
2026-05-06 13:24:20 +0000

    58c0978 feat(@angular/cli): add support for Node.js 26.0.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.7+sha-58c7c7a',
-    AngularBuild: '^22.0.0-next.7+sha-58c7c7a',
-    AngularSSR: '^22.0.0-next.7+sha-58c7c7a',
+    DevkitBuildAngular: '^22.0.0-next.7+sha-58c0978',
+    AngularBuild: '^22.0.0-next.7+sha-58c0978',
+    AngularSSR: '^22.0.0-next.7+sha-58c0978',
 };
 //# sourceMappingURL=latest-versions.js.map
