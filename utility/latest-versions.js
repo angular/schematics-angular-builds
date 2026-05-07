1557410728101817b57848ca67238858eeb7e855
Angular Builds <dev-infra@angular.dev>
2026-05-07 13:41:35 +0000

    93b5206 build: update BASELINE_DATE to 2026-05-07

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.7+sha-d2a1b1c',
-    AngularBuild: '^22.0.0-next.7+sha-d2a1b1c',
-    AngularSSR: '^22.0.0-next.7+sha-d2a1b1c',
+    DevkitBuildAngular: '^22.0.0-next.7+sha-93b5206',
+    AngularBuild: '^22.0.0-next.7+sha-93b5206',
+    AngularSSR: '^22.0.0-next.7+sha-93b5206',
 };
 //# sourceMappingURL=latest-versions.js.map
