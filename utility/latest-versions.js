89412d9dc6f96f22ae1f7b45e72cd72dbc7b83fc
Angular Builds <dev-infra@angular.dev>
2026-05-08 14:12:31 +0000

    240936d refactor(@angular/cli): restrict MCP host process spawning to Angular CLI executable

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.7+sha-fd336d3',
-    AngularBuild: '^22.0.0-next.7+sha-fd336d3',
-    AngularSSR: '^22.0.0-next.7+sha-fd336d3',
+    DevkitBuildAngular: '^22.0.0-next.7+sha-240936d',
+    AngularBuild: '^22.0.0-next.7+sha-240936d',
+    AngularSSR: '^22.0.0-next.7+sha-240936d',
 };
 //# sourceMappingURL=latest-versions.js.map
