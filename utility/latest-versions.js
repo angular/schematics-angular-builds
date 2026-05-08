12d727f135700b40b5b0d307a320ad0638ecb891
Angular Builds <dev-infra@angular.dev>
2026-05-08 13:02:44 +0000

    fd336d3 refactor(@angular-devkit/core): deprecate stringToFileBuffer and fileBufferToString

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.7+sha-db3c5d8',
-    AngularBuild: '^22.0.0-next.7+sha-db3c5d8',
-    AngularSSR: '^22.0.0-next.7+sha-db3c5d8',
+    DevkitBuildAngular: '^22.0.0-next.7+sha-fd336d3',
+    AngularBuild: '^22.0.0-next.7+sha-fd336d3',
+    AngularSSR: '^22.0.0-next.7+sha-fd336d3',
 };
 //# sourceMappingURL=latest-versions.js.map
