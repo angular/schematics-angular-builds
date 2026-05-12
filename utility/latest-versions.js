b8940c07b905ceb0b2f6c2ead8b4606777088428
Angular Builds <dev-infra@angular.dev>
2026-05-12 14:40:32 +0000

    653e4e9 refactor(@angular-devkit/schematics-cli): use custom indent logger and improve tests

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.8+sha-3ab0152',
-    AngularBuild: '^22.0.0-next.8+sha-3ab0152',
-    AngularSSR: '^22.0.0-next.8+sha-3ab0152',
+    DevkitBuildAngular: '^22.0.0-next.8+sha-653e4e9',
+    AngularBuild: '^22.0.0-next.8+sha-653e4e9',
+    AngularSSR: '^22.0.0-next.8+sha-653e4e9',
 };
 //# sourceMappingURL=latest-versions.js.map
