407cab30c3f569ef11746505207e92c30dff16c7
Angular Builds <dev-infra@angular.dev>
2026-05-13 14:58:59 +0000

    0a622bb refactor(@angular/build): remove unused target parameter from getFeatureSupport

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-next.7+sha-f853439',
-    AngularBuild: '^22.0.0-next.7+sha-f853439',
-    AngularSSR: '^22.0.0-next.7+sha-f853439',
+    DevkitBuildAngular: '^22.0.0-next.7+sha-0a622bb',
+    AngularBuild: '^22.0.0-next.7+sha-0a622bb',
+    AngularSSR: '^22.0.0-next.7+sha-0a622bb',
 };
 //# sourceMappingURL=latest-versions.js.map
