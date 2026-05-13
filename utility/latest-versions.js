b456453417c3c42fb8142079c0f920ff6eec8244
Angular Builds <dev-infra@angular.dev>
2026-05-13 09:23:05 +0000

    842fee0 fix(@angular/ssr): allow all hosts in common engine rendering options to prevent validation errors

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^19.2.0',
     NgPackagr: '^19.2.0',
-    DevkitBuildAngular: '^19.2.25+sha-1aa23cd',
-    AngularBuild: '^19.2.25+sha-1aa23cd',
-    AngularSSR: '^19.2.25+sha-1aa23cd',
+    DevkitBuildAngular: '^19.2.25+sha-842fee0',
+    AngularBuild: '^19.2.25+sha-842fee0',
+    AngularSSR: '^19.2.25+sha-842fee0',
 };
