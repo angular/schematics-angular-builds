827eca866b17f5a85254328c6cb19aa04d027bbc
Angular Builds <dev-infra@angular.dev>
2026-05-13 10:41:10 +0000

    b75423d fix(@angular/ssr): support all X-Forwarded-* headers when trustProxyHeaders is true

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-3ef11d3',
-    AngularBuild: '^22.1.0-next.0+sha-3ef11d3',
-    AngularSSR: '^22.1.0-next.0+sha-3ef11d3',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-b75423d',
+    AngularBuild: '^22.1.0-next.0+sha-b75423d',
+    AngularSSR: '^22.1.0-next.0+sha-b75423d',
 };
 //# sourceMappingURL=latest-versions.js.map
