4224c852c7e6be48570ab2ecda8c953ea6c4ab13
Angular Builds <dev-infra@angular.dev>
2026-05-14 15:06:36 +0000

    c150c08 docs: update JSDoc return tag to `@return` and fix description in registry.ts

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-a848bf1',
-    AngularBuild: '^22.1.0-next.0+sha-a848bf1',
-    AngularSSR: '^22.1.0-next.0+sha-a848bf1',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-c150c08',
+    AngularBuild: '^22.1.0-next.0+sha-c150c08',
+    AngularSSR: '^22.1.0-next.0+sha-c150c08',
 };
 //# sourceMappingURL=latest-versions.js.map
