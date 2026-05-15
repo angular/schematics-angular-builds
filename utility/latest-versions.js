3c314f11fb10047e7dd3d146382e3ae48acdaeb1
Angular Builds <dev-infra@angular.dev>
2026-05-15 13:22:43 +0000

    a7ac8e5 fix(@schematics/angular): support spy call arguments migration in refactor-jasmine-vitest

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-rc.0+sha-3f4c79b',
-    AngularBuild: '^22.0.0-rc.0+sha-3f4c79b',
-    AngularSSR: '^22.0.0-rc.0+sha-3f4c79b',
+    DevkitBuildAngular: '^22.0.0-rc.0+sha-a7ac8e5',
+    AngularBuild: '^22.0.0-rc.0+sha-a7ac8e5',
+    AngularSSR: '^22.0.0-rc.0+sha-a7ac8e5',
 };
 //# sourceMappingURL=latest-versions.js.map
