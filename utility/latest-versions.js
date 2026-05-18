4fe1373dd8c1d83477ab0c75cf35e421305d9195
Angular Builds <dev-infra@angular.dev>
2026-05-18 15:01:41 +0000

    c77357f build: upgrade pnpm to v11 and migrate pnpm configuration to pnpm-workspace.yaml

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-5dd4daf',
-    AngularBuild: '^22.1.0-next.0+sha-5dd4daf',
-    AngularSSR: '^22.1.0-next.0+sha-5dd4daf',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-c77357f',
+    AngularBuild: '^22.1.0-next.0+sha-c77357f',
+    AngularSSR: '^22.1.0-next.0+sha-c77357f',
 };
 //# sourceMappingURL=latest-versions.js.map
