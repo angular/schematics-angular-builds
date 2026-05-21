6522faddca7604bd7e3726cbbd0959990938fd7b
Angular Builds <dev-infra@angular.dev>
2026-05-21 14:09:11 +0000

    327cc24 fix(@angular/build): assert that asset input paths are within workspace root

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.0.0-rc.0+sha-80ef0a8',
-    AngularBuild: '^22.0.0-rc.0+sha-80ef0a8',
-    AngularSSR: '^22.0.0-rc.0+sha-80ef0a8',
+    DevkitBuildAngular: '^22.0.0-rc.0+sha-327cc24',
+    AngularBuild: '^22.0.0-rc.0+sha-327cc24',
+    AngularSSR: '^22.0.0-rc.0+sha-327cc24',
 };
 //# sourceMappingURL=latest-versions.js.map
