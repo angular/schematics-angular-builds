5a551c3a41d63913045aa63c6b341c421df72d29
Angular Builds <dev-infra@angular.dev>
2026-05-21 14:43:52 +0000

    bb6a944 build: update minimum Node.js 22 version to 22.22.3

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-163a032',
-    AngularBuild: '^22.1.0-next.0+sha-163a032',
-    AngularSSR: '^22.1.0-next.0+sha-163a032',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-bb6a944',
+    AngularBuild: '^22.1.0-next.0+sha-bb6a944',
+    AngularSSR: '^22.1.0-next.0+sha-bb6a944',
 };
 //# sourceMappingURL=latest-versions.js.map
