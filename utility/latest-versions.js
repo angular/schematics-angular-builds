5b35ac63d8788e9771c2b0a59dc4fbb503ddddc0
Angular Builds <dev-infra@angular.dev>
2026-05-27 17:59:41 +0000

    3c6d26a fix(@angular-devkit/build-angular): remove unconditional CORS wildcard from webpack dev-server

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.12+sha-2b3e955',
-    AngularBuild: '^21.2.12+sha-2b3e955',
-    AngularSSR: '^21.2.12+sha-2b3e955',
+    DevkitBuildAngular: '^21.2.12+sha-3c6d26a',
+    AngularBuild: '^21.2.12+sha-3c6d26a',
+    AngularSSR: '^21.2.12+sha-3c6d26a',
 };
 //# sourceMappingURL=latest-versions.js.map
