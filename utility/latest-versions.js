6cb96dac94c2c962078615b746186538693e6aca
Angular Builds <dev-infra@angular.dev>
2026-05-27 14:58:36 +0000

    484aa86 build: update dev-infra actions to 649c3afeaa46674507b9625537e49de54a695e2b

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-6f0fadf',
-    AngularBuild: '^22.1.0-next.0+sha-6f0fadf',
-    AngularSSR: '^22.1.0-next.0+sha-6f0fadf',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-484aa86',
+    AngularBuild: '^22.1.0-next.0+sha-484aa86',
+    AngularSSR: '^22.1.0-next.0+sha-484aa86',
 };
 //# sourceMappingURL=latest-versions.js.map
