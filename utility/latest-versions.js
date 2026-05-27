0e24aeca113f94ec8fcd0b3ad8364b4daf35e7b9
Angular Builds <dev-infra@angular.dev>
2026-05-27 18:00:42 +0000

    e12299e build: update pnpm to v11.3.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-484aa86',
-    AngularBuild: '^22.1.0-next.0+sha-484aa86',
-    AngularSSR: '^22.1.0-next.0+sha-484aa86',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-e12299e',
+    AngularBuild: '^22.1.0-next.0+sha-e12299e',
+    AngularSSR: '^22.1.0-next.0+sha-e12299e',
 };
 //# sourceMappingURL=latest-versions.js.map
