08025cda209f2b27b2c7af5c968a402629194b37
Angular Builds <dev-infra@angular.dev>
2026-05-29 11:18:25 +0000

    4b1b899 fix(@angular/cli): expand package groups for newly added peer dependencies in update schematic

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-21ca098',
-    AngularBuild: '^22.1.0-next.0+sha-21ca098',
-    AngularSSR: '^22.1.0-next.0+sha-21ca098',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-4b1b899',
+    AngularBuild: '^22.1.0-next.0+sha-4b1b899',
+    AngularSSR: '^22.1.0-next.0+sha-4b1b899',
 };
 //# sourceMappingURL=latest-versions.js.map
