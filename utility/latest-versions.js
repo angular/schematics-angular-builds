e1eb00aedaf62224b01eb8f55ffbd98fa097c6c8
Angular Builds <dev-infra@angular.dev>
2026-05-29 13:34:36 +0000

    ba61bf7 fix(@angular/build): prevent esbuild service child process leakage

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-4b1b899',
-    AngularBuild: '^22.1.0-next.0+sha-4b1b899',
-    AngularSSR: '^22.1.0-next.0+sha-4b1b899',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-ba61bf7',
+    AngularBuild: '^22.1.0-next.0+sha-ba61bf7',
+    AngularSSR: '^22.1.0-next.0+sha-ba61bf7',
 };
 //# sourceMappingURL=latest-versions.js.map
