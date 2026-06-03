b99695038fd5e4c0625eb2d7a609544d7326875a
Angular Builds <dev-infra@angular.dev>
2026-06-03 15:27:10 +0000

    282c095 test: add 20.0-project E2E test asset and update update tests to use it

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.0+sha-9b15fa6',
-    AngularBuild: '^22.0.0+sha-9b15fa6',
-    AngularSSR: '^22.0.0+sha-9b15fa6',
+    DevkitBuildAngular: '^22.0.0+sha-282c095',
+    AngularBuild: '^22.0.0+sha-282c095',
+    AngularSSR: '^22.0.0+sha-282c095',
 };
 //# sourceMappingURL=latest-versions.js.map
