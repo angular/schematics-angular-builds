ddcccc6a3d70be48ea83c1b0ef8d8ba89c43c7f4
Angular Builds <dev-infra@angular.dev>
2026-06-04 13:07:57 +0000

    2779340 fix(@angular/cli): validate registry option is a valid URL in ng add

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.0+sha-72c5d09',
-    AngularBuild: '^22.0.0+sha-72c5d09',
-    AngularSSR: '^22.0.0+sha-72c5d09',
+    DevkitBuildAngular: '^22.0.0+sha-2779340',
+    AngularBuild: '^22.0.0+sha-2779340',
+    AngularSSR: '^22.0.0+sha-2779340',
 };
 //# sourceMappingURL=latest-versions.js.map
