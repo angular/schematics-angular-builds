908a8c7400a8678e256e7fac975b20437d654311
Angular Builds <dev-infra@angular.dev>
2026-06-05 13:58:57 +0000

    21cb628 fix(@schematics/angular): fix browserMode option mapping in refactor-jasmine-vitest

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-3dac9e8',
-    AngularBuild: '^22.1.0-next.0+sha-3dac9e8',
-    AngularSSR: '^22.1.0-next.0+sha-3dac9e8',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-21cb628',
+    AngularBuild: '^22.1.0-next.0+sha-21cb628',
+    AngularSSR: '^22.1.0-next.0+sha-21cb628',
 };
 //# sourceMappingURL=latest-versions.js.map
