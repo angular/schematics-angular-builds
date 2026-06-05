48e8c10ca37d8012361226b1da9af92b26f6f9e9
Angular Builds <dev-infra@angular.dev>
2026-06-05 14:31:41 +0000

    bd413b0 fix(@angular/build): restrict application builder output paths to output directory

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.0+sha-c800122',
-    AngularBuild: '^22.0.0+sha-c800122',
-    AngularSSR: '^22.0.0+sha-c800122',
+    DevkitBuildAngular: '^22.0.0+sha-bd413b0',
+    AngularBuild: '^22.0.0+sha-bd413b0',
+    AngularSSR: '^22.0.0+sha-bd413b0',
 };
 //# sourceMappingURL=latest-versions.js.map
