7b1ae79727a1beaf3435b486384b3f29c680e499
Angular Builds <dev-infra@angular.dev>
2026-06-08 14:23:52 +0000

    e3cab4d fix(@angular/build): prevent concurrent stylesheet bundling esbuild context leaks

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.0+sha-4510dae',
-    AngularBuild: '^22.0.0+sha-4510dae',
-    AngularSSR: '^22.0.0+sha-4510dae',
+    DevkitBuildAngular: '^22.0.0+sha-e3cab4d',
+    AngularBuild: '^22.0.0+sha-e3cab4d',
+    AngularSSR: '^22.0.0+sha-e3cab4d',
 };
 //# sourceMappingURL=latest-versions.js.map
