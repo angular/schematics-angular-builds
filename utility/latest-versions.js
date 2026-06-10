eecef7875bef2e5ef11b131203a2d9c5e2330a75
Angular Builds <dev-infra@angular.dev>
2026-06-10 13:59:44 +0000

    f8c3df4 fix(@angular/cli): remove forceAuth and unscoped credential parsing

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0-next.0',
     NgPackagr: '^22.0.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-96202a3',
-    AngularBuild: '^22.1.0-next.0+sha-96202a3',
-    AngularSSR: '^22.1.0-next.0+sha-96202a3',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-f8c3df4',
+    AngularBuild: '^22.1.0-next.0+sha-f8c3df4',
+    AngularSSR: '^22.1.0-next.0+sha-f8c3df4',
 };
 //# sourceMappingURL=latest-versions.js.map
