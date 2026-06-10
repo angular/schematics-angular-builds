5e6f5500a0a3ee9e36790882bd99f78a45eb725c
Angular Builds <dev-infra@angular.dev>
2026-06-10 14:19:13 +0000

    b54e9a5 fix(@angular/cli): do not sort migrations of the same version alphabetically

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.0+sha-b048b5f',
-    AngularBuild: '^22.0.0+sha-b048b5f',
-    AngularSSR: '^22.0.0+sha-b048b5f',
+    DevkitBuildAngular: '^22.0.0+sha-b54e9a5',
+    AngularBuild: '^22.0.0+sha-b54e9a5',
+    AngularSSR: '^22.0.0+sha-b54e9a5',
 };
 //# sourceMappingURL=latest-versions.js.map
