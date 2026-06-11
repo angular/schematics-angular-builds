73c5b03a4e7373820bfdcf2c1413d5a2a7f93d5f
Angular Builds <dev-infra@angular.dev>
2026-06-11 17:00:51 +0000

    5875b60 fix(@angular/ssr): prioritize options over environment variables in AngularNodeAppEngine

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0-next.0',
     NgPackagr: '^22.1.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-a715b3b',
-    AngularBuild: '^22.1.0-next.0+sha-a715b3b',
-    AngularSSR: '^22.1.0-next.0+sha-a715b3b',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-5875b60',
+    AngularBuild: '^22.1.0-next.0+sha-5875b60',
+    AngularSSR: '^22.1.0-next.0+sha-5875b60',
 };
 //# sourceMappingURL=latest-versions.js.map
