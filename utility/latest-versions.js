6e7ef199c3a5d77e1c3d91914da73946cec3567a
Angular Builds <dev-infra@angular.dev>
2026-06-15 12:34:03 +0000

    38e0fab fix(@angular/cli): support registry metadata fetching under bun package manager

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0-next.0',
     NgPackagr: '^22.1.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-e6e88e2',
-    AngularBuild: '^22.1.0-next.0+sha-e6e88e2',
-    AngularSSR: '^22.1.0-next.0+sha-e6e88e2',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-38e0fab',
+    AngularBuild: '^22.1.0-next.0+sha-38e0fab',
+    AngularSSR: '^22.1.0-next.0+sha-38e0fab',
 };
 //# sourceMappingURL=latest-versions.js.map
