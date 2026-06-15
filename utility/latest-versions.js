eee2087c5b64bfd381334f85c74efb783046b3c4
Angular Builds <dev-infra@angular.dev>
2026-06-15 13:29:59 +0000

    583736a perf(@angular/build): implement semaphore backpressure throttling in JavaScriptTransformer

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0-next.0',
     NgPackagr: '^22.1.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-38e0fab',
-    AngularBuild: '^22.1.0-next.0+sha-38e0fab',
-    AngularSSR: '^22.1.0-next.0+sha-38e0fab',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-583736a',
+    AngularBuild: '^22.1.0-next.0+sha-583736a',
+    AngularSSR: '^22.1.0-next.0+sha-583736a',
 };
 //# sourceMappingURL=latest-versions.js.map
