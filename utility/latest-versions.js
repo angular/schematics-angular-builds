b723a83e12f4bcca8f7b1d87997584c13fb1fc61
Angular Builds <dev-infra@angular.dev>
2026-06-16 09:16:15 +0000

    a527d02 refactor: move eslint disable comment to top of file

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0-next.0',
     NgPackagr: '^22.1.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-3ce6e5f',
-    AngularBuild: '^22.1.0-next.0+sha-3ce6e5f',
-    AngularSSR: '^22.1.0-next.0+sha-3ce6e5f',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-a527d02',
+    AngularBuild: '^22.1.0-next.0+sha-a527d02',
+    AngularSSR: '^22.1.0-next.0+sha-a527d02',
 };
 //# sourceMappingURL=latest-versions.js.map
