b1b8376cb7612ea2a799aa2ad450a20091105c00
Angular Builds <dev-infra@angular.dev>
2026-06-16 13:13:36 +0000

    d996a27 fix(@angular/ssr): avoid caching non-SSG page lookups

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.1+sha-2879ed9',
-    AngularBuild: '^22.0.1+sha-2879ed9',
-    AngularSSR: '^22.0.1+sha-2879ed9',
+    DevkitBuildAngular: '^22.0.1+sha-d996a27',
+    AngularBuild: '^22.0.1+sha-d996a27',
+    AngularSSR: '^22.0.1+sha-d996a27',
 };
 //# sourceMappingURL=latest-versions.js.map
