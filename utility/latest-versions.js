2ddb47944b2d89f038bbfc7d6f7baa61cff758cd
Angular Builds <dev-infra@angular.dev>
2026-06-17 13:20:25 +0000

    48f90f5 fix(@angular/cli): prevent Yarn registry environment variable override

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0-next.0',
     NgPackagr: '^22.1.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-151d426',
-    AngularBuild: '^22.1.0-next.0+sha-151d426',
-    AngularSSR: '^22.1.0-next.0+sha-151d426',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-48f90f5',
+    AngularBuild: '^22.1.0-next.0+sha-48f90f5',
+    AngularSSR: '^22.1.0-next.0+sha-48f90f5',
 };
 //# sourceMappingURL=latest-versions.js.map
