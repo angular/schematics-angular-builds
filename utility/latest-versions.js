d8b4c8bee7258255187eca9e13076e4bbdf721ab
Angular Builds <dev-infra@angular.dev>
2026-06-17 23:17:50 +0000

    01008f1 fix(@schematics/angular): remove default workspace vscode mcp.json configuration

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0-next.0',
     NgPackagr: '^22.1.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-6fbc25b',
-    AngularBuild: '^22.1.0-next.0+sha-6fbc25b',
-    AngularSSR: '^22.1.0-next.0+sha-6fbc25b',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-01008f1',
+    AngularBuild: '^22.1.0-next.0+sha-01008f1',
+    AngularSSR: '^22.1.0-next.0+sha-01008f1',
 };
 //# sourceMappingURL=latest-versions.js.map
