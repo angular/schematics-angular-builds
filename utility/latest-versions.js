49d0ac531759434a5d195a789e5db78ad9b17f13
Angular Builds <dev-infra@angular.dev>
2026-06-17 06:58:18 +0000

    5f7c032 fix(@angular/cli): update pacote to 21.5.1

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.28+sha-a75d78e',
-    AngularBuild: '^20.3.28+sha-a75d78e',
-    AngularSSR: '^20.3.28+sha-a75d78e',
+    DevkitBuildAngular: '^20.3.28+sha-5f7c032',
+    AngularBuild: '^20.3.28+sha-5f7c032',
+    AngularSSR: '^20.3.28+sha-5f7c032',
 };
