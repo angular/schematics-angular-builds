0214ff91f2caa67938a30e2a9fb0ad3e677d011d
Angular Builds <dev-infra@angular.dev>
2026-06-18 12:15:21 +0000

    1f283a4 fix(@angular/build): bump vite to 7.3.5

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.29+sha-a19c3a7',
-    AngularBuild: '^20.3.29+sha-a19c3a7',
-    AngularSSR: '^20.3.29+sha-a19c3a7',
+    DevkitBuildAngular: '^20.3.29+sha-1f283a4',
+    AngularBuild: '^20.3.29+sha-1f283a4',
+    AngularSSR: '^20.3.29+sha-1f283a4',
 };
