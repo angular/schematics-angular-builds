b18e89cc71146960e9675197710ba60f0e2e12ce
Angular Builds <dev-infra@angular.dev>
2026-06-22 14:19:47 +0000

    94fb3c1 fix(@angular/build): bump piscina to 5.2.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.29+sha-1f283a4',
-    AngularBuild: '^20.3.29+sha-1f283a4',
-    AngularSSR: '^20.3.29+sha-1f283a4',
+    DevkitBuildAngular: '^20.3.29+sha-94fb3c1',
+    AngularBuild: '^20.3.29+sha-94fb3c1',
+    AngularSSR: '^20.3.29+sha-94fb3c1',
 };
