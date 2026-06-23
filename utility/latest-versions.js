de54da3ee25a167ea9050dfc5d5ece0b102965e0
Angular Builds <dev-infra@angular.dev>
2026-06-23 14:29:35 +0000

    a583431 refactor(@angular/build): migrate to optimizeDeps.rolldownOptions in Vite config

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0-next.0',
     NgPackagr: '^22.1.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-7ef9ed2',
-    AngularBuild: '^22.1.0-next.0+sha-7ef9ed2',
-    AngularSSR: '^22.1.0-next.0+sha-7ef9ed2',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-a583431',
+    AngularBuild: '^22.1.0-next.0+sha-a583431',
+    AngularSSR: '^22.1.0-next.0+sha-a583431',
 };
 //# sourceMappingURL=latest-versions.js.map
