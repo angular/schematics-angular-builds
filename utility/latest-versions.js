b6a65ae7f731b9b0156c89c79adaadb47dd9bc3f
Angular Builds <dev-infra@angular.dev>
2026-06-23 16:13:36 +0000

    5d7e29c perf(@angular/build): dispose builder result context early in non-watch mode

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.3+sha-46185ce',
-    AngularBuild: '^22.0.3+sha-46185ce',
-    AngularSSR: '^22.0.3+sha-46185ce',
+    DevkitBuildAngular: '^22.0.3+sha-5d7e29c',
+    AngularBuild: '^22.0.3+sha-5d7e29c',
+    AngularSSR: '^22.0.3+sha-5d7e29c',
 };
 //# sourceMappingURL=latest-versions.js.map
