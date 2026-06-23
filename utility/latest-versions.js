bb64cabc06dda654a64a72a7b89474d65950e054
Angular Builds <dev-infra@angular.dev>
2026-06-23 08:54:50 +0000

    7ef9ed2 feat(@angular/ssr): support the standard Forwarded header

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0-next.0',
     NgPackagr: '^22.1.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.0+sha-316cc20',
-    AngularBuild: '^22.1.0-next.0+sha-316cc20',
-    AngularSSR: '^22.1.0-next.0+sha-316cc20',
+    DevkitBuildAngular: '^22.1.0-next.0+sha-7ef9ed2',
+    AngularBuild: '^22.1.0-next.0+sha-7ef9ed2',
+    AngularSSR: '^22.1.0-next.0+sha-7ef9ed2',
 };
 //# sourceMappingURL=latest-versions.js.map
