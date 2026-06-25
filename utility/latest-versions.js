4ba3dd53684a3bc033248a9823cbd57b5b9b1eb9
Angular Builds <dev-infra@angular.dev>
2026-06-25 16:39:17 +0000

    cf033ec fix(@angular/cli): respect release age policy in update bootstrapping logic

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.4+sha-edb32ea',
-    AngularBuild: '^22.0.4+sha-edb32ea',
-    AngularSSR: '^22.0.4+sha-edb32ea',
+    DevkitBuildAngular: '^22.0.4+sha-cf033ec',
+    AngularBuild: '^22.0.4+sha-cf033ec',
+    AngularSSR: '^22.0.4+sha-cf033ec',
 };
 //# sourceMappingURL=latest-versions.js.map
