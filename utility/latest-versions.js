9ea1547934601c6b70d8929133188f945a6d9b75
Angular Builds <dev-infra@angular.dev>
2026-06-25 06:46:30 +0000

    dfee1e7 fix(@angular/build): auto-inject localize/init in library unit tests

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.4+sha-e197652',
-    AngularBuild: '^22.0.4+sha-e197652',
-    AngularSSR: '^22.0.4+sha-e197652',
+    DevkitBuildAngular: '^22.0.4+sha-dfee1e7',
+    AngularBuild: '^22.0.4+sha-dfee1e7',
+    AngularSSR: '^22.0.4+sha-dfee1e7',
 };
 //# sourceMappingURL=latest-versions.js.map
