6c0552dcf49b26f87693e058a1ea7a974f610649
Angular Builds <dev-infra@angular.dev>
2026-06-26 08:54:44 +0000

    010cef6 fix(@angular/cli): gracefully handle package manager errors in command handler

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0-next.0',
     NgPackagr: '^22.1.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.1+sha-edb4594',
-    AngularBuild: '^22.1.0-next.1+sha-edb4594',
-    AngularSSR: '^22.1.0-next.1+sha-edb4594',
+    DevkitBuildAngular: '^22.1.0-next.1+sha-010cef6',
+    AngularBuild: '^22.1.0-next.1+sha-010cef6',
+    AngularSSR: '^22.1.0-next.1+sha-010cef6',
 };
 //# sourceMappingURL=latest-versions.js.map
