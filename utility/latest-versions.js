801d526e848b22cacc1ff024c9cfe2b70b1b2e0d
Angular Builds <dev-infra@angular.dev>
2026-06-29 13:06:15 +0000

    f75085f fix(@angular-devkit/build-angular): prevent OS command injection in ssr-dev-server builder

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.4+sha-f099e64',
-    AngularBuild: '^22.0.4+sha-f099e64',
-    AngularSSR: '^22.0.4+sha-f099e64',
+    DevkitBuildAngular: '^22.0.4+sha-f75085f',
+    AngularBuild: '^22.0.4+sha-f75085f',
+    AngularSSR: '^22.0.4+sha-f75085f',
 };
 //# sourceMappingURL=latest-versions.js.map
