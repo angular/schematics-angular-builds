309b5f8fccaabf6c251d14f11c90c0df5a637afb
Angular Builds <dev-infra@angular.dev>
2026-06-30 07:24:49 +0000

    0010b92 fix(@angular/build): scope createRequire module resolution using paths to prevent parent paths

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0-next.0',
     NgPackagr: '^22.1.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.1+sha-d9fb854',
-    AngularBuild: '^22.1.0-next.1+sha-d9fb854',
-    AngularSSR: '^22.1.0-next.1+sha-d9fb854',
+    DevkitBuildAngular: '^22.1.0-next.1+sha-0010b92',
+    AngularBuild: '^22.1.0-next.1+sha-0010b92',
+    AngularSSR: '^22.1.0-next.1+sha-0010b92',
 };
 //# sourceMappingURL=latest-versions.js.map
