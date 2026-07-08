0a39f35a4743aa730fd70f50f5e998e4eac61710
Angular Builds <dev-infra@angular.dev>
2026-07-08 14:38:37 +0000

    41bd100 fix(@angular/build): fallback to build target preserveSymlinks option in karma runner

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0-next.0',
     NgPackagr: '^22.1.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.2+sha-52ae7f8',
-    AngularBuild: '^22.1.0-next.2+sha-52ae7f8',
-    AngularSSR: '^22.1.0-next.2+sha-52ae7f8',
+    DevkitBuildAngular: '^22.1.0-next.2+sha-41bd100',
+    AngularBuild: '^22.1.0-next.2+sha-41bd100',
+    AngularSSR: '^22.1.0-next.2+sha-41bd100',
 };
 //# sourceMappingURL=latest-versions.js.map
