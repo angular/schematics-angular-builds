9137f8fe14c952d8cf899bcc89377ea6709dda0a
Angular Builds <dev-infra@angular.dev>
2026-07-09 14:32:25 +0000

    cc45a54 build: update dependency http-proxy-middleware to v3.0.7

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.6+sha-31345b6',
-    AngularBuild: '^22.0.6+sha-31345b6',
-    AngularSSR: '^22.0.6+sha-31345b6',
+    DevkitBuildAngular: '^22.0.6+sha-cc45a54',
+    AngularBuild: '^22.0.6+sha-cc45a54',
+    AngularSSR: '^22.0.6+sha-cc45a54',
 };
 //# sourceMappingURL=latest-versions.js.map
