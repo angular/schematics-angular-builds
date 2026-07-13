b715c292506394a765c254d71988997836c1f757
Angular Builds <dev-infra@angular.dev>
2026-07-13 11:28:39 +0000

    e85c10c fix(@angular/cli): copy packageManager field and yarn config for temp installs

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.6+sha-5584a58',
-    AngularBuild: '^22.0.6+sha-5584a58',
-    AngularSSR: '^22.0.6+sha-5584a58',
+    DevkitBuildAngular: '^22.0.6+sha-e85c10c',
+    AngularBuild: '^22.0.6+sha-e85c10c',
+    AngularSSR: '^22.0.6+sha-e85c10c',
 };
 //# sourceMappingURL=latest-versions.js.map
