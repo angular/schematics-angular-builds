c9f2a3197a79b4385c4e46ab27e1863e9e46d531
Angular Builds <dev-infra@angular.dev>
2026-07-14 23:18:49 +0000

    b345bb5 fix(@schematics/angular): remove unused spec tsconfig outDir

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0-next.0',
     NgPackagr: '^22.1.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.3+sha-b0b3a47',
-    AngularBuild: '^22.1.0-next.3+sha-b0b3a47',
-    AngularSSR: '^22.1.0-next.3+sha-b0b3a47',
+    DevkitBuildAngular: '^22.1.0-next.3+sha-b345bb5',
+    AngularBuild: '^22.1.0-next.3+sha-b345bb5',
+    AngularSSR: '^22.1.0-next.3+sha-b345bb5',
 };
 //# sourceMappingURL=latest-versions.js.map
