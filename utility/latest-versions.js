59a16992c4e4b42062cbc35ee643a9b276c2b59f
Angular Builds <dev-infra@angular.dev>
2026-07-16 19:50:06 +0000

    d79c10a build: update dependency vite to v7.3.6

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.7+sha-1e556b3',
-    AngularBuild: '^22.0.7+sha-1e556b3',
-    AngularSSR: '^22.0.7+sha-1e556b3',
+    DevkitBuildAngular: '^22.0.7+sha-d79c10a',
+    AngularBuild: '^22.0.7+sha-d79c10a',
+    AngularSSR: '^22.0.7+sha-d79c10a',
 };
 //# sourceMappingURL=latest-versions.js.map
