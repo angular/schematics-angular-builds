2c6b81a9ea82225c6fdf87652173fe65372acaba
Angular Builds <dev-infra@angular.dev>
2026-07-16 14:56:57 +0000

    f59bcf5 fix(@angular/cli): abort and guide manual mitigation when updating catalog packages

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0-next.0',
     NgPackagr: '^22.1.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.4+sha-6b990bf',
-    AngularBuild: '^22.1.0-next.4+sha-6b990bf',
-    AngularSSR: '^22.1.0-next.4+sha-6b990bf',
+    DevkitBuildAngular: '^22.1.0-next.4+sha-f59bcf5',
+    AngularBuild: '^22.1.0-next.4+sha-f59bcf5',
+    AngularSSR: '^22.1.0-next.4+sha-f59bcf5',
 };
 //# sourceMappingURL=latest-versions.js.map
