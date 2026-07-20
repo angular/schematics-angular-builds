30270f7ced4add8555a9bbb759bad473b6761e93
Angular Builds <dev-infra@angular.dev>
2026-07-20 17:17:47 +0000

    41c3562 fix(@angular/cli): batch Prettier invocations during migration formatting

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.7+sha-5724bf6',
-    AngularBuild: '^22.0.7+sha-5724bf6',
-    AngularSSR: '^22.0.7+sha-5724bf6',
+    DevkitBuildAngular: '^22.0.7+sha-41c3562',
+    AngularBuild: '^22.0.7+sha-41c3562',
+    AngularSSR: '^22.0.7+sha-41c3562',
 };
 //# sourceMappingURL=latest-versions.js.map
