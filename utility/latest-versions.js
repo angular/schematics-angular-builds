d571a47e3ca0fe78d4815ed19374ece054a74a1a
Angular Builds <dev-infra@angular.dev>
2026-07-21 16:28:49 +0000

    b8bdbaa fix(@angular/build): ensure import map integrity keys are valid URL-like specifiers

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.7+sha-bc760b0',
-    AngularBuild: '^22.0.7+sha-bc760b0',
-    AngularSSR: '^22.0.7+sha-bc760b0',
+    DevkitBuildAngular: '^22.0.7+sha-b8bdbaa',
+    AngularBuild: '^22.0.7+sha-b8bdbaa',
+    AngularSSR: '^22.0.7+sha-b8bdbaa',
 };
 //# sourceMappingURL=latest-versions.js.map
