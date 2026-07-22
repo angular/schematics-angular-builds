7403d88585cd0dc703242173918854e050465fc2
Angular Builds <dev-infra@angular.dev>
2026-07-22 14:28:23 +0000

    9671cc6 build: update dependency quicktype-core to v26

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0-next.0',
     NgPackagr: '^22.1.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.4+sha-10dc30f',
-    AngularBuild: '^22.1.0-next.4+sha-10dc30f',
-    AngularSSR: '^22.1.0-next.4+sha-10dc30f',
+    DevkitBuildAngular: '^22.1.0-next.4+sha-9671cc6',
+    AngularBuild: '^22.1.0-next.4+sha-9671cc6',
+    AngularSSR: '^22.1.0-next.4+sha-9671cc6',
 };
 //# sourceMappingURL=latest-versions.js.map
