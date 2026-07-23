5a950647cd5c7f0fad621afb50ac9b2a723e6433
Angular Builds <dev-infra@angular.dev>
2026-07-23 15:31:49 +0000

    15c41fc build: update dependency aspect_rules_ts to v3.9.2

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0-next.0',
     NgPackagr: '^22.1.0-next.0',
-    DevkitBuildAngular: '^22.1.0-next.4+sha-e12e002',
-    AngularBuild: '^22.1.0-next.4+sha-e12e002',
-    AngularSSR: '^22.1.0-next.4+sha-e12e002',
+    DevkitBuildAngular: '^22.1.0-next.4+sha-15c41fc',
+    AngularBuild: '^22.1.0-next.4+sha-15c41fc',
+    AngularSSR: '^22.1.0-next.4+sha-15c41fc',
 };
 //# sourceMappingURL=latest-versions.js.map
