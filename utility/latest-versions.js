4a8382b0c886e93905800f3ff73f196f4d2ffa28
Angular Builds <dev-infra@angular.dev>
2026-07-23 15:41:11 +0000

    1bb2cc5 build: update devinfra digest to 8819f0b

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.7+sha-bc87c85',
-    AngularBuild: '^22.0.7+sha-bc87c85',
-    AngularSSR: '^22.0.7+sha-bc87c85',
+    DevkitBuildAngular: '^22.0.7+sha-1bb2cc5',
+    AngularBuild: '^22.0.7+sha-1bb2cc5',
+    AngularSSR: '^22.0.7+sha-1bb2cc5',
 };
 //# sourceMappingURL=latest-versions.js.map
