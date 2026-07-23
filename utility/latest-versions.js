320674ec1a8d32868cb995e394ffd4454cf7d592
Angular Builds <dev-infra@angular.dev>
2026-07-23 14:11:16 +0000

    bc87c85 fix(@angular/cli): update MCP devserver tool names to conform to spec regex

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.7+sha-212a7bb',
-    AngularBuild: '^22.0.7+sha-212a7bb',
-    AngularSSR: '^22.0.7+sha-212a7bb',
+    DevkitBuildAngular: '^22.0.7+sha-bc87c85',
+    AngularBuild: '^22.0.7+sha-bc87c85',
+    AngularSSR: '^22.0.7+sha-bc87c85',
 };
 //# sourceMappingURL=latest-versions.js.map
