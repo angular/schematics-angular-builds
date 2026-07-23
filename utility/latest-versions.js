3c6330f4db73d424eeb9582b8764757d4249851a
Angular Builds <dev-infra@angular.dev>
2026-07-23 10:33:19 +0000

    0c69328 Revert "perf(@angular/build): skip semantic affected-file walk when type checking is disabled"

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.7+sha-b8bdbaa',
-    AngularBuild: '^22.0.7+sha-b8bdbaa',
-    AngularSSR: '^22.0.7+sha-b8bdbaa',
+    DevkitBuildAngular: '^22.0.7+sha-0c69328',
+    AngularBuild: '^22.0.7+sha-0c69328',
+    AngularSSR: '^22.0.7+sha-0c69328',
 };
 //# sourceMappingURL=latest-versions.js.map
