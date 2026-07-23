2e090e9f5dad2d697dd4078f53d13164ec7a21dc
Angular Builds <dev-infra@angular.dev>
2026-07-23 13:56:52 +0000

    212a7bb fix(@angular/build): favor istanbul coverage provider when browser testing is enabled

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.0.0',
     NgPackagr: '^22.0.0',
-    DevkitBuildAngular: '^22.0.7+sha-0c69328',
-    AngularBuild: '^22.0.7+sha-0c69328',
-    AngularSSR: '^22.0.7+sha-0c69328',
+    DevkitBuildAngular: '^22.0.7+sha-212a7bb',
+    AngularBuild: '^22.0.7+sha-212a7bb',
+    AngularSSR: '^22.0.7+sha-212a7bb',
 };
 //# sourceMappingURL=latest-versions.js.map
