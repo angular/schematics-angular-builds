fe5f022bc090a91107a19b3f565e700c7bdea80f
Angular Builds <dev-infra@angular.dev>
2026-07-29 21:33:15 +0000

    975554c build: update Angular framework and ng-packagr versions to 22.1.0 stable release

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,10 +14,10 @@ const dependencies = require('./latest-versions/package.json')['dependencies'];
 exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
-    Angular: '^22.1.0-next.0',
-    NgPackagr: '^22.1.0-next.0',
-    DevkitBuildAngular: '^22.1.0-rc.0+sha-96ea55a',
-    AngularBuild: '^22.1.0-rc.0+sha-96ea55a',
-    AngularSSR: '^22.1.0-rc.0+sha-96ea55a',
+    Angular: '^22.1.0',
+    NgPackagr: '^22.1.0',
+    DevkitBuildAngular: '^22.1.0-rc.0+sha-975554c',
+    AngularBuild: '^22.1.0-rc.0+sha-975554c',
+    AngularSSR: '^22.1.0-rc.0+sha-975554c',
 };
 //# sourceMappingURL=latest-versions.js.map
