92b9eb02e56ac735b448fdd20bb40cb10dee759c
Angular Builds <dev-infra@angular.dev>
2026-07-30 14:44:18 +0000

    51efbd8 build: update github/codeql-action action to v4.37.4

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.1+sha-5d92c65',
-    AngularBuild: '^22.1.1+sha-5d92c65',
-    AngularSSR: '^22.1.1+sha-5d92c65',
+    DevkitBuildAngular: '^22.1.1+sha-51efbd8',
+    AngularBuild: '^22.1.1+sha-51efbd8',
+    AngularSSR: '^22.1.1+sha-51efbd8',
 };
 //# sourceMappingURL=latest-versions.js.map
