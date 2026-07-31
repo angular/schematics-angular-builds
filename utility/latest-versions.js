b64847b42158b1d7e9451dcc5b3356d77be10a81
Angular Builds <dev-infra@angular.dev>
2026-07-31 22:28:54 +0000

    1cdb3ae ci: use W3C-compliant Sauce Labs tunnelName and remove deprecated tunnelIdentifier

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.2+sha-6af906c',
-    AngularBuild: '^22.1.2+sha-6af906c',
-    AngularSSR: '^22.1.2+sha-6af906c',
+    DevkitBuildAngular: '^22.1.2+sha-1cdb3ae',
+    AngularBuild: '^22.1.2+sha-1cdb3ae',
+    AngularSSR: '^22.1.2+sha-1cdb3ae',
 };
 //# sourceMappingURL=latest-versions.js.map
