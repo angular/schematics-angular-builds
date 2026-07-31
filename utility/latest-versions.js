f03aaf7382d82e20b81d5b7d485da4e926655418
Angular Builds <dev-infra@angular.dev>
2026-07-31 18:45:49 +0000

    0ae5690 fix(@angular/cli): always install package during ng add to inspect manifest on disk for schematics (#33718)

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.0+sha-3316980',
-    AngularBuild: '^22.2.0-next.0+sha-3316980',
-    AngularSSR: '^22.2.0-next.0+sha-3316980',
+    DevkitBuildAngular: '^22.2.0-next.0+sha-0ae5690',
+    AngularBuild: '^22.2.0-next.0+sha-0ae5690',
+    AngularSSR: '^22.2.0-next.0+sha-0ae5690',
 };
 //# sourceMappingURL=latest-versions.js.map
