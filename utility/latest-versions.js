72934b2877852eca0c0e8aefe4374f36dacfd758
Angular Builds <dev-infra@angular.dev>
2026-07-31 08:26:13 +0000

    f5ed8fd build: update dependency node to v22.23.2

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.2+sha-bccc749',
-    AngularBuild: '^22.1.2+sha-bccc749',
-    AngularSSR: '^22.1.2+sha-bccc749',
+    DevkitBuildAngular: '^22.1.2+sha-f5ed8fd',
+    AngularBuild: '^22.1.2+sha-f5ed8fd',
+    AngularSSR: '^22.1.2+sha-f5ed8fd',
 };
 //# sourceMappingURL=latest-versions.js.map
