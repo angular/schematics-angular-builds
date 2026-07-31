63d273d82d1bea33ac562451c422f56353f20ee4
Angular Builds <dev-infra@angular.dev>
2026-07-31 17:33:46 +0000

    72ae936 fix(@angular/cli): correctly report available package updates during bare ng update

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.2+sha-f5ed8fd',
-    AngularBuild: '^22.1.2+sha-f5ed8fd',
-    AngularSSR: '^22.1.2+sha-f5ed8fd',
+    DevkitBuildAngular: '^22.1.2+sha-72ae936',
+    AngularBuild: '^22.1.2+sha-72ae936',
+    AngularSSR: '^22.1.2+sha-72ae936',
 };
 //# sourceMappingURL=latest-versions.js.map
