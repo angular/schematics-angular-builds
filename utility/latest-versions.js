6cb51bdb1419c35771ec816300340160ff15c074
Angular Builds <dev-infra@angular.dev>
2026-08-01 07:33:39 +0000

    d8e8be9 ci: use proxyLocalhost action input in Sauce Connect CI workflow

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.0+sha-5f1ef6e',
-    AngularBuild: '^22.2.0-next.0+sha-5f1ef6e',
-    AngularSSR: '^22.2.0-next.0+sha-5f1ef6e',
+    DevkitBuildAngular: '^22.2.0-next.0+sha-d8e8be9',
+    AngularBuild: '^22.2.0-next.0+sha-d8e8be9',
+    AngularSSR: '^22.2.0-next.0+sha-d8e8be9',
 };
 //# sourceMappingURL=latest-versions.js.map
