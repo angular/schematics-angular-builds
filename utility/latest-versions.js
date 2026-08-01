18b756fa2993d4767116e1754db99ec830a08f04
Angular Builds <dev-infra@angular.dev>
2026-08-01 07:24:19 +0000

    5f1ef6e ci: fix Sauce Labs E2E browser tests with Sauce Connect 5 and legacy JWP capabilities

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.0+sha-1c2a17e',
-    AngularBuild: '^22.2.0-next.0+sha-1c2a17e',
-    AngularSSR: '^22.2.0-next.0+sha-1c2a17e',
+    DevkitBuildAngular: '^22.2.0-next.0+sha-5f1ef6e',
+    AngularBuild: '^22.2.0-next.0+sha-5f1ef6e',
+    AngularSSR: '^22.2.0-next.0+sha-5f1ef6e',
 };
 //# sourceMappingURL=latest-versions.js.map
