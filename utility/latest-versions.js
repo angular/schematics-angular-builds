6ac70441c86d3e5ee111fc839c4da0b9f0674f3b
Angular Builds <dev-infra@angular.dev>
2026-08-03 10:22:13 +0000

    d520dca refactor(@angular/build): implement generic persistent load result cache infrastructure for build pipeline

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.0+sha-3039776',
-    AngularBuild: '^22.2.0-next.0+sha-3039776',
-    AngularSSR: '^22.2.0-next.0+sha-3039776',
+    DevkitBuildAngular: '^22.2.0-next.0+sha-d520dca',
+    AngularBuild: '^22.2.0-next.0+sha-d520dca',
+    AngularSSR: '^22.2.0-next.0+sha-d520dca',
 };
 //# sourceMappingURL=latest-versions.js.map
