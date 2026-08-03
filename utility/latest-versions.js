06b70650748b4430e8ef77c9fc36ba5457857f55
Angular Builds <dev-infra@angular.dev>
2026-08-03 13:50:17 +0000

    5c8a055 build: update dependency webpack-dev-server to v6

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.0+sha-d520dca',
-    AngularBuild: '^22.2.0-next.0+sha-d520dca',
-    AngularSSR: '^22.2.0-next.0+sha-d520dca',
+    DevkitBuildAngular: '^22.2.0-next.0+sha-5c8a055',
+    AngularBuild: '^22.2.0-next.0+sha-5c8a055',
+    AngularSSR: '^22.2.0-next.0+sha-5c8a055',
 };
 //# sourceMappingURL=latest-versions.js.map
