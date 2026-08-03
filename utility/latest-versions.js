4b47576a54097a0c319cd32a537c411d45ea11ec
Angular Builds <dev-infra@angular.dev>
2026-08-03 10:23:53 +0000

    74d6d0b build: update dependency postcss to v8.5.25

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.2+sha-b139e8d',
-    AngularBuild: '^22.1.2+sha-b139e8d',
-    AngularSSR: '^22.1.2+sha-b139e8d',
+    DevkitBuildAngular: '^22.1.2+sha-74d6d0b',
+    AngularBuild: '^22.1.2+sha-74d6d0b',
+    AngularSSR: '^22.1.2+sha-74d6d0b',
 };
 //# sourceMappingURL=latest-versions.js.map
