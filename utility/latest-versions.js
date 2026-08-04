c34a73282206b9052f1919d9fd1633da22ec95e4
Angular Builds <dev-infra@angular.dev>
2026-08-04 16:46:33 +0000

    41555df feat(@angular/cli): add `--root` command line option to `mcp` command

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.0+sha-243f737',
-    AngularBuild: '^22.2.0-next.0+sha-243f737',
-    AngularSSR: '^22.2.0-next.0+sha-243f737',
+    DevkitBuildAngular: '^22.2.0-next.0+sha-41555df',
+    AngularBuild: '^22.2.0-next.0+sha-41555df',
+    AngularSSR: '^22.2.0-next.0+sha-41555df',
 };
 //# sourceMappingURL=latest-versions.js.map
