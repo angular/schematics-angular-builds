78e2c24a6109e16553ee8a784754c03ec151f1fc
Angular Builds <dev-infra@angular.dev>
2026-08-04 17:25:36 +0000

    b24b444 ci: schedule ng-snapshot Renovate updates for early morning only

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.0+sha-e2111ee',
-    AngularBuild: '^22.2.0-next.0+sha-e2111ee',
-    AngularSSR: '^22.2.0-next.0+sha-e2111ee',
+    DevkitBuildAngular: '^22.2.0-next.0+sha-b24b444',
+    AngularBuild: '^22.2.0-next.0+sha-b24b444',
+    AngularSSR: '^22.2.0-next.0+sha-b24b444',
 };
 //# sourceMappingURL=latest-versions.js.map
