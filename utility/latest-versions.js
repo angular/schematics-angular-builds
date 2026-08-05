df9c10ec03b925c9e379380c5b9c029a28b25d23
Angular Builds <dev-infra@angular.dev>
2026-08-05 18:26:16 +0000

    194be20 perf(@angular/build): avoid encoding the inline source map before remapping

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.2+sha-3e9fed9',
-    AngularBuild: '^22.2.0-next.2+sha-3e9fed9',
-    AngularSSR: '^22.2.0-next.2+sha-3e9fed9',
+    DevkitBuildAngular: '^22.2.0-next.2+sha-194be20',
+    AngularBuild: '^22.2.0-next.2+sha-194be20',
+    AngularSSR: '^22.2.0-next.2+sha-194be20',
 };
 //# sourceMappingURL=latest-versions.js.map
