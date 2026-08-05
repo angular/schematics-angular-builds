d7e48bc7c3a2a5e17b3dc219baa12eceb6e7cc99
Angular Builds <dev-infra@angular.dev>
2026-08-05 15:37:54 +0000

    3e9fed9 fix(@angular/build): set target for Rolldown dependency prebundling in Vite dev server

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.2+sha-04888ea',
-    AngularBuild: '^22.2.0-next.2+sha-04888ea',
-    AngularSSR: '^22.2.0-next.2+sha-04888ea',
+    DevkitBuildAngular: '^22.2.0-next.2+sha-3e9fed9',
+    AngularBuild: '^22.2.0-next.2+sha-3e9fed9',
+    AngularSSR: '^22.2.0-next.2+sha-3e9fed9',
 };
 //# sourceMappingURL=latest-versions.js.map
