cdcf3d97f102a2d2f4e1403c283014cde342a49c
Angular Builds <dev-infra@angular.dev>
2026-08-05 06:25:41 +0000

    b6ed402 refactor(@angular/cli): use `serveStdio` to support both 2025 and 2026-07-28 MCP protocol eras

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.0+sha-b24b444',
-    AngularBuild: '^22.2.0-next.0+sha-b24b444',
-    AngularSSR: '^22.2.0-next.0+sha-b24b444',
+    DevkitBuildAngular: '^22.2.0-next.0+sha-b6ed402',
+    AngularBuild: '^22.2.0-next.0+sha-b6ed402',
+    AngularSSR: '^22.2.0-next.0+sha-b6ed402',
 };
 //# sourceMappingURL=latest-versions.js.map
