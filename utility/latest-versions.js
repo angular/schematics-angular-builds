aaf84881d644d8f1d70bb74ca94269054854024e
Angular Builds <dev-infra@angular.dev>
2026-08-06 15:05:27 +0000

    715852b refactor(@angular/build): defer sourcemap remapping in javascript transformer worker

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.2+sha-199a864',
-    AngularBuild: '^22.2.0-next.2+sha-199a864',
-    AngularSSR: '^22.2.0-next.2+sha-199a864',
+    DevkitBuildAngular: '^22.2.0-next.2+sha-715852b',
+    AngularBuild: '^22.2.0-next.2+sha-715852b',
+    AngularSSR: '^22.2.0-next.2+sha-715852b',
 };
 //# sourceMappingURL=latest-versions.js.map
