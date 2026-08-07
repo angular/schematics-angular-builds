17b3a5e91643ad2d95434f3b473ab115cbefeb2a
Angular Builds <dev-infra@angular.dev>
2026-08-07 15:06:37 +0000

    1b0ba5c fix(@angular/cli): serialize configuration as a single argv token in run_target strategies (#33657)

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.3+sha-956c657',
-    AngularBuild: '^22.1.3+sha-956c657',
-    AngularSSR: '^22.1.3+sha-956c657',
+    DevkitBuildAngular: '^22.1.3+sha-1b0ba5c',
+    AngularBuild: '^22.1.3+sha-1b0ba5c',
+    AngularSSR: '^22.1.3+sha-1b0ba5c',
 };
 //# sourceMappingURL=latest-versions.js.map
