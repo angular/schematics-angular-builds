d1d837ccd9a3bb6c2624e8a51112905d86ed3c2f
Angular Builds <dev-infra@angular.dev>
2026-08-07 21:30:45 +0000

    db06933 test(@angular/build): avoid race conditions in incremental-watch e2e test

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.3+sha-bd8a3cb',
-    AngularBuild: '^22.1.3+sha-bd8a3cb',
-    AngularSSR: '^22.1.3+sha-bd8a3cb',
+    DevkitBuildAngular: '^22.1.3+sha-db06933',
+    AngularBuild: '^22.1.3+sha-db06933',
+    AngularSSR: '^22.1.3+sha-db06933',
 };
 //# sourceMappingURL=latest-versions.js.map
