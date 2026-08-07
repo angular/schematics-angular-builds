5725215735abd31bb86a95c566b32bf7b79e5a6f
Angular Builds <dev-infra@angular.dev>
2026-08-07 08:04:15 +0000

    cda8830 refactor(@angular/build): use xxhash-wasm for fast non-cryptographic content hashing

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.2+sha-5ee5420',
-    AngularBuild: '^22.2.0-next.2+sha-5ee5420',
-    AngularSSR: '^22.2.0-next.2+sha-5ee5420',
+    DevkitBuildAngular: '^22.2.0-next.2+sha-cda8830',
+    AngularBuild: '^22.2.0-next.2+sha-cda8830',
+    AngularSSR: '^22.2.0-next.2+sha-cda8830',
 };
 //# sourceMappingURL=latest-versions.js.map
