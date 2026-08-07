e5cf25311900343c2f609397af22f5179c897cf9
Angular Builds <dev-infra@angular.dev>
2026-08-07 18:43:56 +0000

    bd8a3cb test: optimize and stabilize e2e tests

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.3+sha-9440432',
-    AngularBuild: '^22.1.3+sha-9440432',
-    AngularSSR: '^22.1.3+sha-9440432',
+    DevkitBuildAngular: '^22.1.3+sha-bd8a3cb',
+    AngularBuild: '^22.1.3+sha-bd8a3cb',
+    AngularSSR: '^22.1.3+sha-bd8a3cb',
 };
 //# sourceMappingURL=latest-versions.js.map
