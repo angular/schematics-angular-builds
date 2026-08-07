d1f42e5ac3ba59afdba151ba665d4b62eec00e9a
Angular Builds <dev-infra@angular.dev>
2026-08-07 13:37:00 +0000

    3c7ac15 fix(@angular/build): return only lowest version per target engine

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.2+sha-cda8830',
-    AngularBuild: '^22.2.0-next.2+sha-cda8830',
-    AngularSSR: '^22.2.0-next.2+sha-cda8830',
+    DevkitBuildAngular: '^22.2.0-next.2+sha-3c7ac15',
+    AngularBuild: '^22.2.0-next.2+sha-3c7ac15',
+    AngularSSR: '^22.2.0-next.2+sha-3c7ac15',
 };
 //# sourceMappingURL=latest-versions.js.map
