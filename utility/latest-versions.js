5d4e4eb7dc264675587ec12fa268142b40f2ccfe
Angular Builds <dev-infra@angular.dev>
2026-08-10 17:48:56 +0000

    1c00edc perf(@angular/build): optimize sourcemap stripping and loading with buffer fast path

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.2+sha-c536ae3',
-    AngularBuild: '^22.2.0-next.2+sha-c536ae3',
-    AngularSSR: '^22.2.0-next.2+sha-c536ae3',
+    DevkitBuildAngular: '^22.2.0-next.2+sha-1c00edc',
+    AngularBuild: '^22.2.0-next.2+sha-1c00edc',
+    AngularSSR: '^22.2.0-next.2+sha-1c00edc',
 };
 //# sourceMappingURL=latest-versions.js.map
