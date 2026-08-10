93670110d7598fc4fa38e017949fe841e275582f
Angular Builds <dev-infra@angular.dev>
2026-08-10 19:41:07 +0000

    f62a102 refactor(@angular/cli): use Node.js styleText for color helpers

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.2+sha-1c00edc',
-    AngularBuild: '^22.2.0-next.2+sha-1c00edc',
-    AngularSSR: '^22.2.0-next.2+sha-1c00edc',
+    DevkitBuildAngular: '^22.2.0-next.2+sha-f62a102',
+    AngularBuild: '^22.2.0-next.2+sha-f62a102',
+    AngularSSR: '^22.2.0-next.2+sha-f62a102',
 };
 //# sourceMappingURL=latest-versions.js.map
