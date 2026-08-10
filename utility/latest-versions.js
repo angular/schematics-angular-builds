b6481f56e73d3c861f53ed4bb1f950dc4248cd52
Angular Builds <dev-infra@angular.dev>
2026-08-10 13:29:39 +0000

    f348efe fix(@angular-devkit/build-angular): bump undici to 7.29.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.33+sha-3962517',
-    AngularBuild: '^20.3.33+sha-3962517',
-    AngularSSR: '^20.3.33+sha-3962517',
+    DevkitBuildAngular: '^20.3.33+sha-f348efe',
+    AngularBuild: '^20.3.33+sha-f348efe',
+    AngularSSR: '^20.3.33+sha-f348efe',
 };
