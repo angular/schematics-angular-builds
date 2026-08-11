424074569f4eabc62a70a6d46e1ebe24b7129714
Angular Builds <dev-infra@angular.dev>
2026-08-11 22:29:19 +0000

    a56a691 build: update dependency jsdom to v30

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.2+sha-06bdcad',
-    AngularBuild: '^22.2.0-next.2+sha-06bdcad',
-    AngularSSR: '^22.2.0-next.2+sha-06bdcad',
+    DevkitBuildAngular: '^22.2.0-next.2+sha-a56a691',
+    AngularBuild: '^22.2.0-next.2+sha-a56a691',
+    AngularSSR: '^22.2.0-next.2+sha-a56a691',
 };
 //# sourceMappingURL=latest-versions.js.map
