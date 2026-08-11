232bada8f7a728a8fc189a3386e5d158053137b5
Angular Builds <dev-infra@angular.dev>
2026-08-11 16:59:20 +0000

    898ce69 ci: limit schematics dependencies rule to minor and patch updates

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.3+sha-796b576',
-    AngularBuild: '^22.1.3+sha-796b576',
-    AngularSSR: '^22.1.3+sha-796b576',
+    DevkitBuildAngular: '^22.1.3+sha-898ce69',
+    AngularBuild: '^22.1.3+sha-898ce69',
+    AngularSSR: '^22.1.3+sha-898ce69',
 };
 //# sourceMappingURL=latest-versions.js.map
