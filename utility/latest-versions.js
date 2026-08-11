619ad7e4e028c5647ed3c06c9d420fb58549b756
Angular Builds <dev-infra@angular.dev>
2026-08-11 15:29:30 +0000

    6a21ad6 refactor(@angular/build): centralize sourcemap buffer slicing and removal

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.2+sha-2274bab',
-    AngularBuild: '^22.2.0-next.2+sha-2274bab',
-    AngularSSR: '^22.2.0-next.2+sha-2274bab',
+    DevkitBuildAngular: '^22.2.0-next.2+sha-6a21ad6',
+    AngularBuild: '^22.2.0-next.2+sha-6a21ad6',
+    AngularSSR: '^22.2.0-next.2+sha-6a21ad6',
 };
 //# sourceMappingURL=latest-versions.js.map
