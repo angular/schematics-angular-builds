ce05d93a5723fd45d5c96b3faaaac72c7a8773f2
Angular Builds <dev-infra@angular.dev>
2026-08-11 13:54:14 +0000

    7112b75 refactor(@angular/build): bypass worker dispatch for non-Angular files with byte-level linker pre-check

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.2+sha-75849f6',
-    AngularBuild: '^22.2.0-next.2+sha-75849f6',
-    AngularSSR: '^22.2.0-next.2+sha-75849f6',
+    DevkitBuildAngular: '^22.2.0-next.2+sha-7112b75',
+    AngularBuild: '^22.2.0-next.2+sha-7112b75',
+    AngularSSR: '^22.2.0-next.2+sha-7112b75',
 };
 //# sourceMappingURL=latest-versions.js.map
