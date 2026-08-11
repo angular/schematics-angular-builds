87fc947e9b2e071ad86f84543d4d212b3727c3ef
Angular Builds <dev-infra@angular.dev>
2026-08-11 22:29:42 +0000

    9020ddd build: update dependency yargs to v18.1.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.3+sha-898ce69',
-    AngularBuild: '^22.1.3+sha-898ce69',
-    AngularSSR: '^22.1.3+sha-898ce69',
+    DevkitBuildAngular: '^22.1.3+sha-9020ddd',
+    AngularBuild: '^22.1.3+sha-9020ddd',
+    AngularSSR: '^22.1.3+sha-9020ddd',
 };
 //# sourceMappingURL=latest-versions.js.map
