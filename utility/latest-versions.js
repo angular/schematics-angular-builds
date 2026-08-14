1da72a4fbbf343ad3154398c06fc85f438281beb
Angular Builds <dev-infra@angular.dev>
2026-08-14 13:53:37 +0000

    ff1d356 fix(@angular/build): preserve integrity and crossorigin in autoCsp loader

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.4+sha-95b4992',
-    AngularBuild: '^22.1.4+sha-95b4992',
-    AngularSSR: '^22.1.4+sha-95b4992',
+    DevkitBuildAngular: '^22.1.4+sha-ff1d356',
+    AngularBuild: '^22.1.4+sha-ff1d356',
+    AngularSSR: '^22.1.4+sha-ff1d356',
 };
 //# sourceMappingURL=latest-versions.js.map
