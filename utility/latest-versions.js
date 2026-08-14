442abd747c5c2902e91bde9be83647e6ccc1ea28
Angular Builds <dev-infra@angular.dev>
2026-08-14 16:15:03 +0000

    d827ba9 refactor(@angular/build): use translation integrity for i18n inlining cache keys

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.3+sha-50994d7',
-    AngularBuild: '^22.2.0-next.3+sha-50994d7',
-    AngularSSR: '^22.2.0-next.3+sha-50994d7',
+    DevkitBuildAngular: '^22.2.0-next.3+sha-d827ba9',
+    AngularBuild: '^22.2.0-next.3+sha-d827ba9',
+    AngularSSR: '^22.2.0-next.3+sha-d827ba9',
 };
 //# sourceMappingURL=latest-versions.js.map
