08cbb698733d1ef741ef87a76315945a4f11bc90
Angular Builds <dev-infra@angular.dev>
2026-08-20 08:39:21 +0000

    f4dad31 perf(@angular/build): unify Oxc linking and optimization AST traversal passes

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.4+sha-d8e3baf',
-    AngularBuild: '^22.2.0-next.4+sha-d8e3baf',
-    AngularSSR: '^22.2.0-next.4+sha-d8e3baf',
+    DevkitBuildAngular: '^22.2.0-next.4+sha-f4dad31',
+    AngularBuild: '^22.2.0-next.4+sha-f4dad31',
+    AngularSSR: '^22.2.0-next.4+sha-f4dad31',
 };
 //# sourceMappingURL=latest-versions.js.map
