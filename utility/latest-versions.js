dbb01b59d45fa439210ee70f3a9d1fc571b53bab
Angular Builds <dev-infra@angular.dev>
2026-08-20 08:51:36 +0000

    e5a29cf build: update dependency karma-jasmine-html-reporter to ~2.3.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.4+sha-f4dad31',
-    AngularBuild: '^22.2.0-next.4+sha-f4dad31',
-    AngularSSR: '^22.2.0-next.4+sha-f4dad31',
+    DevkitBuildAngular: '^22.2.0-next.4+sha-e5a29cf',
+    AngularBuild: '^22.2.0-next.4+sha-e5a29cf',
+    AngularSSR: '^22.2.0-next.4+sha-e5a29cf',
 };
 //# sourceMappingURL=latest-versions.js.map
