ac68649d835a69755631920ed3c5edc59241f2d2
Angular Builds <dev-infra@angular.dev>
2026-08-20 08:40:17 +0000

    76818cb fix(@angular/ssr): abort web request signal when node request is aborted

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.5+sha-3954730',
-    AngularBuild: '^22.1.5+sha-3954730',
-    AngularSSR: '^22.1.5+sha-3954730',
+    DevkitBuildAngular: '^22.1.5+sha-76818cb',
+    AngularBuild: '^22.1.5+sha-76818cb',
+    AngularSSR: '^22.1.5+sha-76818cb',
 };
 //# sourceMappingURL=latest-versions.js.map
