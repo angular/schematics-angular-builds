09e0f13310aae9b05ab5c717c5a6f8885541cf3d
Angular Builds <dev-infra@angular.dev>
2026-08-21 17:39:51 +0000

    a4c10f3 build: remove scorecard workflow

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.5+sha-087d313',
-    AngularBuild: '^22.1.5+sha-087d313',
-    AngularSSR: '^22.1.5+sha-087d313',
+    DevkitBuildAngular: '^22.1.5+sha-a4c10f3',
+    AngularBuild: '^22.1.5+sha-a4c10f3',
+    AngularSSR: '^22.1.5+sha-a4c10f3',
 };
 //# sourceMappingURL=latest-versions.js.map
