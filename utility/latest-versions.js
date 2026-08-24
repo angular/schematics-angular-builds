ec212c01916ad4df097903566c0dad0b01f3db0a
Angular Builds <dev-infra@angular.dev>
2026-08-24 14:35:08 +0000

    94b620a fix(@angular-devkit/build-angular): update webpack-dev-server to 5.2.6

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.34+sha-40c30ba',
-    AngularBuild: '^20.3.34+sha-40c30ba',
-    AngularSSR: '^20.3.34+sha-40c30ba',
+    DevkitBuildAngular: '^20.3.34+sha-94b620a',
+    AngularBuild: '^20.3.34+sha-94b620a',
+    AngularSSR: '^20.3.34+sha-94b620a',
 };
