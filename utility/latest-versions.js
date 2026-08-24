6493f49f5e074465a8600fc842f1f1614893c7f6
Angular Builds <dev-infra@angular.dev>
2026-08-24 18:10:03 +0000

    e4111ae fix(@angular/build): prevent memory leaks in parallel compilation and caches

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.4+sha-6acadf2',
-    AngularBuild: '^22.2.0-next.4+sha-6acadf2',
-    AngularSSR: '^22.2.0-next.4+sha-6acadf2',
+    DevkitBuildAngular: '^22.2.0-next.4+sha-e4111ae',
+    AngularBuild: '^22.2.0-next.4+sha-e4111ae',
+    AngularSSR: '^22.2.0-next.4+sha-e4111ae',
 };
 //# sourceMappingURL=latest-versions.js.map
