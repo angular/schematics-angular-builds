43e98889d9c278800f02bab55a05f57469a925ba
Angular Builds <dev-infra@angular.dev>
2026-08-25 14:35:15 +0000

    3f132ea fix(@angular/build): ignore side-effect annotations in unit test builds

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.5+sha-2dc7cd7',
-    AngularBuild: '^22.1.5+sha-2dc7cd7',
-    AngularSSR: '^22.1.5+sha-2dc7cd7',
+    DevkitBuildAngular: '^22.1.5+sha-3f132ea',
+    AngularBuild: '^22.1.5+sha-3f132ea',
+    AngularSSR: '^22.1.5+sha-3f132ea',
 };
 //# sourceMappingURL=latest-versions.js.map
