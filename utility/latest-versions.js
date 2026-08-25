18021d9ea0d6e05a73032a770cb35ea3291f88ee
Angular Builds <dev-infra@angular.dev>
2026-08-25 14:54:29 +0000

    11ab5eb test(@angular/build): ensure hash init in i18n inliner tests

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.4+sha-4d0be68',
-    AngularBuild: '^22.2.0-next.4+sha-4d0be68',
-    AngularSSR: '^22.2.0-next.4+sha-4d0be68',
+    DevkitBuildAngular: '^22.2.0-next.4+sha-11ab5eb',
+    AngularBuild: '^22.2.0-next.4+sha-11ab5eb',
+    AngularSSR: '^22.2.0-next.4+sha-11ab5eb',
 };
 //# sourceMappingURL=latest-versions.js.map
