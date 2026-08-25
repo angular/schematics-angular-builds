0586296f7f3071a51b3cf914884b57ad8b650839
Angular Builds <dev-infra@angular.dev>
2026-08-25 06:15:56 +0000

    62999ca perf(@angular/build): batch prerender routes and share in-memory server bundles

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.4+sha-e4111ae',
-    AngularBuild: '^22.2.0-next.4+sha-e4111ae',
-    AngularSSR: '^22.2.0-next.4+sha-e4111ae',
+    DevkitBuildAngular: '^22.2.0-next.4+sha-62999ca',
+    AngularBuild: '^22.2.0-next.4+sha-62999ca',
+    AngularSSR: '^22.2.0-next.4+sha-62999ca',
 };
 //# sourceMappingURL=latest-versions.js.map
