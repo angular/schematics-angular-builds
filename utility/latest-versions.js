d3f79c6ae2ea42be98b45a2269fa9e10e79ad358
Angular Builds <dev-infra@angular.dev>
2026-08-25 18:24:16 +0000

    a877aa7 perf(@angular/build): implement SharedArrayBuffer translation dictionaries for zero-copy worker access

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.4+sha-11ab5eb',
-    AngularBuild: '^22.2.0-next.4+sha-11ab5eb',
-    AngularSSR: '^22.2.0-next.4+sha-11ab5eb',
+    DevkitBuildAngular: '^22.2.0-next.4+sha-a877aa7',
+    AngularBuild: '^22.2.0-next.4+sha-a877aa7',
+    AngularSSR: '^22.2.0-next.4+sha-a877aa7',
 };
 //# sourceMappingURL=latest-versions.js.map
