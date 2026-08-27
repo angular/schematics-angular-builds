21bf674c01cebb3ab586e239bc4997fb5edbd63f
Angular Builds <dev-infra@angular.dev>
2026-08-27 07:55:23 +0000

    e3d55b2 refactor(@angular/build): use ɵParsedTranslation in i18n inliner

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.5+sha-ecbcd87',
-    AngularBuild: '^22.2.0-next.5+sha-ecbcd87',
-    AngularSSR: '^22.2.0-next.5+sha-ecbcd87',
+    DevkitBuildAngular: '^22.2.0-next.5+sha-e3d55b2',
+    AngularBuild: '^22.2.0-next.5+sha-e3d55b2',
+    AngularSSR: '^22.2.0-next.5+sha-e3d55b2',
 };
 //# sourceMappingURL=latest-versions.js.map
