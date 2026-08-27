7de56fb5ebbec198cd8aea6cb1195fdb83322eb6
Angular Builds <dev-infra@angular.dev>
2026-08-27 13:21:13 +0000

    d3aaa48 fix(@angular-devkit/build-angular): update less to 4.9.0

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.22+sha-60dee48',
-    AngularBuild: '^21.2.22+sha-60dee48',
-    AngularSSR: '^21.2.22+sha-60dee48',
+    DevkitBuildAngular: '^21.2.22+sha-d3aaa48',
+    AngularBuild: '^21.2.22+sha-d3aaa48',
+    AngularSSR: '^21.2.22+sha-d3aaa48',
 };
 //# sourceMappingURL=latest-versions.js.map
