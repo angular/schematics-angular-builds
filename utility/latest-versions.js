6936f1cf15b24a87d49c6e0d6daf1c6fa7238f44
Angular Builds <dev-infra@angular.dev>
2026-08-27 13:42:36 +0000

    51565d4 fix(@angular/build): enable code splitting for vitest browser tests

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.5+sha-135662d',
-    AngularBuild: '^22.2.0-next.5+sha-135662d',
-    AngularSSR: '^22.2.0-next.5+sha-135662d',
+    DevkitBuildAngular: '^22.2.0-next.5+sha-51565d4',
+    AngularBuild: '^22.2.0-next.5+sha-51565d4',
+    AngularSSR: '^22.2.0-next.5+sha-51565d4',
 };
 //# sourceMappingURL=latest-versions.js.map
