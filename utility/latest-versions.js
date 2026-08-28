81bb345b4c99f05a36ecd14e4d092d86fde3c39c
Angular Builds <dev-infra@angular.dev>
2026-08-28 15:29:08 +0000

    4b8c62a fix(@angular/build): add splitting option to unit-test builder

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.5+sha-2b85529',
-    AngularBuild: '^22.2.0-next.5+sha-2b85529',
-    AngularSSR: '^22.2.0-next.5+sha-2b85529',
+    DevkitBuildAngular: '^22.2.0-next.5+sha-4b8c62a',
+    AngularBuild: '^22.2.0-next.5+sha-4b8c62a',
+    AngularSSR: '^22.2.0-next.5+sha-4b8c62a',
 };
 //# sourceMappingURL=latest-versions.js.map
