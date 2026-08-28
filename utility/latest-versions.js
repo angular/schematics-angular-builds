7688b848ab4b8e4d31e0eb678513dfd95d68118a
Angular Builds <dev-infra@angular.dev>
2026-08-28 15:54:37 +0000

    c3e5982 fix(@angular/ssr): ensure public directory containment in CommonEngine

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,7 +16,7 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^20.3.0',
     NgPackagr: '^20.3.0',
-    DevkitBuildAngular: '^20.3.35+sha-b4059cd',
-    AngularBuild: '^20.3.35+sha-b4059cd',
-    AngularSSR: '^20.3.35+sha-b4059cd',
+    DevkitBuildAngular: '^20.3.35+sha-c3e5982',
+    AngularBuild: '^20.3.35+sha-c3e5982',
+    AngularSSR: '^20.3.35+sha-c3e5982',
 };
