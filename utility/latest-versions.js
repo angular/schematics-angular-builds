ceb9150c05cd16a4de9686d3f21cd95b0e667da9
Angular Builds <dev-infra@angular.dev>
2026-08-31 13:20:46 +0000

    f1fd823 fix(@angular/build): fail build and exclude routes when prerendering fails

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.6+sha-70748ca',
-    AngularBuild: '^22.1.6+sha-70748ca',
-    AngularSSR: '^22.1.6+sha-70748ca',
+    DevkitBuildAngular: '^22.1.6+sha-f1fd823',
+    AngularBuild: '^22.1.6+sha-f1fd823',
+    AngularSSR: '^22.1.6+sha-f1fd823',
 };
 //# sourceMappingURL=latest-versions.js.map
