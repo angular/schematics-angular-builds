1e14a0e5e477a3dcdf6a7dc2508db71396117f11
Angular Builds <dev-infra@angular.dev>
2026-09-01 16:45:54 +0000

    e9db85c fix(@angular/cli): isolate temporary package installations from parent Yarn workspace

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.6+sha-cefe231',
-    AngularBuild: '^22.1.6+sha-cefe231',
-    AngularSSR: '^22.1.6+sha-cefe231',
+    DevkitBuildAngular: '^22.1.6+sha-e9db85c',
+    AngularBuild: '^22.1.6+sha-e9db85c',
+    AngularSSR: '^22.1.6+sha-e9db85c',
 };
 //# sourceMappingURL=latest-versions.js.map
