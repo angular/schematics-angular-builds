5c62e431abe11cb32ede3261859a5b1c8f3e8bd2
Angular Builds <dev-infra@angular.dev>
2026-09-01 15:46:15 +0000

    1d5377b refactor(@angular/build): pass files directly to inlineAll in i18n inliner

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.5+sha-f329174',
-    AngularBuild: '^22.2.0-next.5+sha-f329174',
-    AngularSSR: '^22.2.0-next.5+sha-f329174',
+    DevkitBuildAngular: '^22.2.0-next.5+sha-1d5377b',
+    AngularBuild: '^22.2.0-next.5+sha-1d5377b',
+    AngularSSR: '^22.2.0-next.5+sha-1d5377b',
 };
 //# sourceMappingURL=latest-versions.js.map
