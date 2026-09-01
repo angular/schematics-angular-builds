b211b8bdf99390e67d20ec361421697be6d30f5e
Angular Builds <dev-infra@angular.dev>
2026-09-01 13:49:57 +0000

    f329174 refactor(@angular/build): eliminate NoopCompilation and introduce primary and secondary compilation contexts

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.5+sha-2dc9aae',
-    AngularBuild: '^22.2.0-next.5+sha-2dc9aae',
-    AngularSSR: '^22.2.0-next.5+sha-2dc9aae',
+    DevkitBuildAngular: '^22.2.0-next.5+sha-f329174',
+    AngularBuild: '^22.2.0-next.5+sha-f329174',
+    AngularSSR: '^22.2.0-next.5+sha-f329174',
 };
 //# sourceMappingURL=latest-versions.js.map
