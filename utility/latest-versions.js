94114f5649a5221b928812f6cf13f74e3e76c540
Angular Builds <dev-infra@angular.dev>
2026-09-02 20:42:28 +0000

    e2582bb release: cut the v21.2.23 release

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^21.2.0',
     NgPackagr: '^21.2.0',
-    DevkitBuildAngular: '^21.2.22+sha-645e41a',
-    AngularBuild: '^21.2.22+sha-645e41a',
-    AngularSSR: '^21.2.22+sha-645e41a',
+    DevkitBuildAngular: '^21.2.23+sha-e2582bb',
+    AngularBuild: '^21.2.23+sha-e2582bb',
+    AngularSSR: '^21.2.23+sha-e2582bb',
 };
 //# sourceMappingURL=latest-versions.js.map
