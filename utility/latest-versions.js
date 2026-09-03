903f422529db28d8d09088befcf338463fa3bad5
Angular Builds <dev-infra@angular.dev>
2026-09-03 13:34:47 +0000

    3b01482 refactor(@angular/build): optimize pruning in sqlite cache store

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.6+sha-14c54b3',
-    AngularBuild: '^22.2.0-next.6+sha-14c54b3',
-    AngularSSR: '^22.2.0-next.6+sha-14c54b3',
+    DevkitBuildAngular: '^22.2.0-next.6+sha-3b01482',
+    AngularBuild: '^22.2.0-next.6+sha-3b01482',
+    AngularSSR: '^22.2.0-next.6+sha-3b01482',
 };
 //# sourceMappingURL=latest-versions.js.map
