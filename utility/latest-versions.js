c0af4c535ceca61b1c32e2d265ab1ed09f3a7f0c
Angular Builds <dev-infra@angular.dev>
2026-09-03 15:55:19 +0000

    54d712f fix(@angular/build): ensure parent directory exists in SQLite cache store

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.6+sha-55583c4',
-    AngularBuild: '^22.2.0-next.6+sha-55583c4',
-    AngularSSR: '^22.2.0-next.6+sha-55583c4',
+    DevkitBuildAngular: '^22.2.0-next.6+sha-54d712f',
+    AngularBuild: '^22.2.0-next.6+sha-54d712f',
+    AngularSSR: '^22.2.0-next.6+sha-54d712f',
 };
 //# sourceMappingURL=latest-versions.js.map
