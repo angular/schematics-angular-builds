fdebc2d16c516f5cf01aa4e5e8d3c807354ac399
Angular Builds <dev-infra@angular.dev>
2026-09-03 13:07:18 +0000

    675f1f5 fix(@angular/build): avoid pure comment pattern in oxc transform

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.6+sha-12459ee',
-    AngularBuild: '^22.2.0-next.6+sha-12459ee',
-    AngularSSR: '^22.2.0-next.6+sha-12459ee',
+    DevkitBuildAngular: '^22.2.0-next.6+sha-675f1f5',
+    AngularBuild: '^22.2.0-next.6+sha-675f1f5',
+    AngularSSR: '^22.2.0-next.6+sha-675f1f5',
 };
 //# sourceMappingURL=latest-versions.js.map
