7e0cd65fd0860b1967b46dd70f2f3405b5a9c849
Angular Builds <dev-infra@angular.dev>
2026-09-03 14:44:51 +0000

    f6cb128 test: update e2e global npm version to 12.0.2

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.6+sha-3b01482',
-    AngularBuild: '^22.2.0-next.6+sha-3b01482',
-    AngularSSR: '^22.2.0-next.6+sha-3b01482',
+    DevkitBuildAngular: '^22.2.0-next.6+sha-f6cb128',
+    AngularBuild: '^22.2.0-next.6+sha-f6cb128',
+    AngularSSR: '^22.2.0-next.6+sha-f6cb128',
 };
 //# sourceMappingURL=latest-versions.js.map
