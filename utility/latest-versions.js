9e7a0235c0ca98cee3e51777981eee438b48f7ba
Angular Builds <dev-infra@angular.dev>
2026-09-08 18:25:42 +0000

    5a0d226 perf(@angular/build): avoid full JSON parsing when updating sourcemap ignore list

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.6+sha-d11a663',
-    AngularBuild: '^22.2.0-next.6+sha-d11a663',
-    AngularSSR: '^22.2.0-next.6+sha-d11a663',
+    DevkitBuildAngular: '^22.2.0-next.6+sha-5a0d226',
+    AngularBuild: '^22.2.0-next.6+sha-5a0d226',
+    AngularSSR: '^22.2.0-next.6+sha-5a0d226',
 };
 //# sourceMappingURL=latest-versions.js.map
