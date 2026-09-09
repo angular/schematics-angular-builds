db754cef1373962921984a8dbdceea2757b17dbc
Angular Builds <dev-infra@angular.dev>
2026-09-09 14:32:43 +0000

    87a1610 test(@angular/build): fix flakiness in dev-server rebuild error detection test

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.7+sha-7e133c4',
-    AngularBuild: '^22.1.7+sha-7e133c4',
-    AngularSSR: '^22.1.7+sha-7e133c4',
+    DevkitBuildAngular: '^22.1.7+sha-87a1610',
+    AngularBuild: '^22.1.7+sha-87a1610',
+    AngularSSR: '^22.1.7+sha-87a1610',
 };
 //# sourceMappingURL=latest-versions.js.map
