4d7cf5b96ec4b32c3b1bf02d19cce183912c3b9e
Angular Builds <dev-infra@angular.dev>
2026-09-10 14:26:05 +0000

    61cfc6d test(@angular/build): prevent flakiness in incremental-watch e2e test

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.7+sha-ca322ca',
-    AngularBuild: '^22.1.7+sha-ca322ca',
-    AngularSSR: '^22.1.7+sha-ca322ca',
+    DevkitBuildAngular: '^22.1.7+sha-61cfc6d',
+    AngularBuild: '^22.1.7+sha-61cfc6d',
+    AngularSSR: '^22.1.7+sha-61cfc6d',
 };
 //# sourceMappingURL=latest-versions.js.map
