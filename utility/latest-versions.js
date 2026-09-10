2e29a7722e12a651ffb7b2208dcf849621a3cf10
Angular Builds <dev-infra@angular.dev>
2026-09-10 14:49:44 +0000

    fa9fc6e build: set verifyDepsBeforeRun to warn in pnpm workspace configuration

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.7+sha-61cfc6d',
-    AngularBuild: '^22.1.7+sha-61cfc6d',
-    AngularSSR: '^22.1.7+sha-61cfc6d',
+    DevkitBuildAngular: '^22.1.7+sha-fa9fc6e',
+    AngularBuild: '^22.1.7+sha-fa9fc6e',
+    AngularSSR: '^22.1.7+sha-fa9fc6e',
 };
 //# sourceMappingURL=latest-versions.js.map
