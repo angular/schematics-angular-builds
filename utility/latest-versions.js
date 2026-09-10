73b2b895d4f68301daf9f19e32553f8e4917515e
Angular Builds <dev-infra@angular.dev>
2026-09-10 11:56:25 +0000

    44129f6 refactor(@angular/build): prevent key collisions in cache namespaces

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.6+sha-0ad1409',
-    AngularBuild: '^22.2.0-next.6+sha-0ad1409',
-    AngularSSR: '^22.2.0-next.6+sha-0ad1409',
+    DevkitBuildAngular: '^22.2.0-next.6+sha-44129f6',
+    AngularBuild: '^22.2.0-next.6+sha-44129f6',
+    AngularSSR: '^22.2.0-next.6+sha-44129f6',
 };
 //# sourceMappingURL=latest-versions.js.map
