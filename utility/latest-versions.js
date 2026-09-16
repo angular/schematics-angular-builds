b8caada106c722be676b2fc6236bcf97cfcf2a22
Angular Builds <dev-infra@angular.dev>
2026-09-16 11:59:08 +0000

    7c93d8d docs(@angular/build): remove experimental prefix from unit-test builder description

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.8+sha-b0cb383',
-    AngularBuild: '^22.1.8+sha-b0cb383',
-    AngularSSR: '^22.1.8+sha-b0cb383',
+    DevkitBuildAngular: '^22.1.8+sha-7c93d8d',
+    AngularBuild: '^22.1.8+sha-7c93d8d',
+    AngularSSR: '^22.1.8+sha-7c93d8d',
 };
 //# sourceMappingURL=latest-versions.js.map
