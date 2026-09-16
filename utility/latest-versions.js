8a75092e7b869e8187d020b0b6b564fe9933ddef
Angular Builds <dev-infra@angular.dev>
2026-09-16 07:27:28 +0000

    6e7f809 perf(@angular/cli): skip eager yargs help message formatting during command execution

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-next.7+sha-31c0456',
-    AngularBuild: '^22.2.0-next.7+sha-31c0456',
-    AngularSSR: '^22.2.0-next.7+sha-31c0456',
+    DevkitBuildAngular: '^22.2.0-next.7+sha-6e7f809',
+    AngularBuild: '^22.2.0-next.7+sha-6e7f809',
+    AngularSSR: '^22.2.0-next.7+sha-6e7f809',
 };
 //# sourceMappingURL=latest-versions.js.map
