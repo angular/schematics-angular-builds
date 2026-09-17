9e815fb0783482a3d9294d46072847d7456b8302
Angular Builds <dev-infra@angular.dev>
2026-09-17 16:45:39 +0000

    c76b2d5 refactor(@angular/build): cap default i18n inlining concurrency to 8

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-rc.0+sha-6ffd267',
-    AngularBuild: '^22.2.0-rc.0+sha-6ffd267',
-    AngularSSR: '^22.2.0-rc.0+sha-6ffd267',
+    DevkitBuildAngular: '^22.2.0-rc.0+sha-c76b2d5',
+    AngularBuild: '^22.2.0-rc.0+sha-c76b2d5',
+    AngularSSR: '^22.2.0-rc.0+sha-c76b2d5',
 };
 //# sourceMappingURL=latest-versions.js.map
