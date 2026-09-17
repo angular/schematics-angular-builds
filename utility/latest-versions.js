e9153668b35e0eaeb3c11af5c66819b65a86a05c
Angular Builds <dev-infra@angular.dev>
2026-09-17 23:12:22 +0000

    d0eb6b6 refactor(@angular/build): extract shared concurrency, watcher, and styling utilities

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.3.0-next.0+sha-758192d',
-    AngularBuild: '^22.3.0-next.0+sha-758192d',
-    AngularSSR: '^22.3.0-next.0+sha-758192d',
+    DevkitBuildAngular: '^22.3.0-next.0+sha-d0eb6b6',
+    AngularBuild: '^22.3.0-next.0+sha-d0eb6b6',
+    AngularSSR: '^22.3.0-next.0+sha-d0eb6b6',
 };
 //# sourceMappingURL=latest-versions.js.map
