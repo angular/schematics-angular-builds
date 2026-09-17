08b4f902fbf942ca4483270dea0797018e35137b
Angular Builds <dev-infra@angular.dev>
2026-09-17 18:23:05 +0000

    f521955 refactor(@angular/cli): remove unused example database path from MCP server

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.3.0-next.0+sha-2b59d84',
-    AngularBuild: '^22.3.0-next.0+sha-2b59d84',
-    AngularSSR: '^22.3.0-next.0+sha-2b59d84',
+    DevkitBuildAngular: '^22.3.0-next.0+sha-f521955',
+    AngularBuild: '^22.3.0-next.0+sha-f521955',
+    AngularSSR: '^22.3.0-next.0+sha-f521955',
 };
 //# sourceMappingURL=latest-versions.js.map
