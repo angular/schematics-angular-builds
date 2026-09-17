5d3cce85c3a815c6fbfb84f888d4b8815933a603
Angular Builds <dev-infra@angular.dev>
2026-09-17 16:59:13 +0000

    9c6037a Revert "refactor(@angular/cli): import markdown files directly for command descriptions"

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-rc.0+sha-c76b2d5',
-    AngularBuild: '^22.2.0-rc.0+sha-c76b2d5',
-    AngularSSR: '^22.2.0-rc.0+sha-c76b2d5',
+    DevkitBuildAngular: '^22.2.0-rc.0+sha-9c6037a',
+    AngularBuild: '^22.2.0-rc.0+sha-9c6037a',
+    AngularSSR: '^22.2.0-rc.0+sha-9c6037a',
 };
 //# sourceMappingURL=latest-versions.js.map
