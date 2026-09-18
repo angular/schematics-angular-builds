e0e30d27d581f84aa7074d3db05e047d2b7ef656
Angular Builds <dev-infra@angular.dev>
2026-09-18 09:08:00 +0000

    5aaad0a fix(@angular-devkit/core): name the unknown option in a schema validation error

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.1.0',
     NgPackagr: '^22.1.0',
-    DevkitBuildAngular: '^22.1.8+sha-7c93d8d',
-    AngularBuild: '^22.1.8+sha-7c93d8d',
-    AngularSSR: '^22.1.8+sha-7c93d8d',
+    DevkitBuildAngular: '^22.1.8+sha-5aaad0a',
+    AngularBuild: '^22.1.8+sha-5aaad0a',
+    AngularSSR: '^22.1.8+sha-5aaad0a',
 };
 //# sourceMappingURL=latest-versions.js.map
