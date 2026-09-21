1ce0d7f0b98443e6e65273691695aa21be1a9665
Angular Builds <dev-infra@angular.dev>
2026-09-21 13:44:51 +0000

    eb1edeb refactor(@angular/build): relocate non-esbuild utilities out of tools/esbuild

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.3.0-next.0+sha-3921ce6',
-    AngularBuild: '^22.3.0-next.0+sha-3921ce6',
-    AngularSSR: '^22.3.0-next.0+sha-3921ce6',
+    DevkitBuildAngular: '^22.3.0-next.0+sha-eb1edeb',
+    AngularBuild: '^22.3.0-next.0+sha-eb1edeb',
+    AngularSSR: '^22.3.0-next.0+sha-eb1edeb',
 };
 //# sourceMappingURL=latest-versions.js.map
