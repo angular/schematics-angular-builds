e3b6307fd8ec2a527764448dc03296f2a0c4a0f9
Angular Builds <dev-infra@angular.dev>
2026-09-21 15:58:16 +0000

    6b0b9be build(@angular/cli): bundle CLI first-party code into ESM chunks with esbuild

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.3.0-next.0+sha-eb1edeb',
-    AngularBuild: '^22.3.0-next.0+sha-eb1edeb',
-    AngularSSR: '^22.3.0-next.0+sha-eb1edeb',
+    DevkitBuildAngular: '^22.3.0-next.0+sha-6b0b9be',
+    AngularBuild: '^22.3.0-next.0+sha-6b0b9be',
+    AngularSSR: '^22.3.0-next.0+sha-6b0b9be',
 };
 //# sourceMappingURL=latest-versions.js.map
