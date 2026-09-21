096e3f4febec9504e05be3f7e4404734934942f8
Angular Builds <dev-infra@angular.dev>
2026-09-21 17:51:18 +0000

    69acf5b fix(@angular/build): include dataurl option in global stylesheet config hash

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.3.0-next.0+sha-6b0b9be',
-    AngularBuild: '^22.3.0-next.0+sha-6b0b9be',
-    AngularSSR: '^22.3.0-next.0+sha-6b0b9be',
+    DevkitBuildAngular: '^22.3.0-next.0+sha-69acf5b',
+    AngularBuild: '^22.3.0-next.0+sha-69acf5b',
+    AngularSSR: '^22.3.0-next.0+sha-69acf5b',
 };
 //# sourceMappingURL=latest-versions.js.map
