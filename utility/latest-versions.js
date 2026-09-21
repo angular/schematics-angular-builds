49bbad35fe73449b64db9168707f55060805ac23
Angular Builds <dev-infra@angular.dev>
2026-09-21 18:17:13 +0000

    1fc2732 fix(@angular/build): support parenthesized expressions in oxc linker

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-rc.0+sha-5cd20e1',
-    AngularBuild: '^22.2.0-rc.0+sha-5cd20e1',
-    AngularSSR: '^22.2.0-rc.0+sha-5cd20e1',
+    DevkitBuildAngular: '^22.2.0-rc.0+sha-1fc2732',
+    AngularBuild: '^22.2.0-rc.0+sha-1fc2732',
+    AngularSSR: '^22.2.0-rc.0+sha-1fc2732',
 };
 //# sourceMappingURL=latest-versions.js.map
