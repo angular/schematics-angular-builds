a59318ac4af4491c82207ffb30d14b1254706d8f
Angular Builds <dev-infra@angular.dev>
2026-09-21 18:03:17 +0000

    5cd20e1 perf(@angular/build): only reset Sass worker pool caches when stylesheet files change

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-rc.0+sha-e7ebbcd',
-    AngularBuild: '^22.2.0-rc.0+sha-e7ebbcd',
-    AngularSSR: '^22.2.0-rc.0+sha-e7ebbcd',
+    DevkitBuildAngular: '^22.2.0-rc.0+sha-5cd20e1',
+    AngularBuild: '^22.2.0-rc.0+sha-5cd20e1',
+    AngularSSR: '^22.2.0-rc.0+sha-5cd20e1',
 };
 //# sourceMappingURL=latest-versions.js.map
