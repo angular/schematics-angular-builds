8cae14ef32afef64a701dce5d1008ec58e34ab09
Angular Builds <dev-infra@angular.dev>
2026-09-22 15:17:40 +0000

    80acd62 fix(@angular/build): execute setup file hooks for each spec file when coverage is enabled

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-rc.0+sha-1fc2732',
-    AngularBuild: '^22.2.0-rc.0+sha-1fc2732',
-    AngularSSR: '^22.2.0-rc.0+sha-1fc2732',
+    DevkitBuildAngular: '^22.2.0-rc.0+sha-80acd62',
+    AngularBuild: '^22.2.0-rc.0+sha-80acd62',
+    AngularSSR: '^22.2.0-rc.0+sha-80acd62',
 };
 //# sourceMappingURL=latest-versions.js.map
