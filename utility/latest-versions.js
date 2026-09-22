f45732dc6c5b34cd3fe33ba8582b7ac51c642b27
Angular Builds <dev-infra@angular.dev>
2026-09-22 15:32:17 +0000

    f47f77f fix(@angular/build): only compile included test files in unit-test builder

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-rc.0+sha-80acd62',
-    AngularBuild: '^22.2.0-rc.0+sha-80acd62',
-    AngularSSR: '^22.2.0-rc.0+sha-80acd62',
+    DevkitBuildAngular: '^22.2.0-rc.0+sha-f47f77f',
+    AngularBuild: '^22.2.0-rc.0+sha-f47f77f',
+    AngularSSR: '^22.2.0-rc.0+sha-f47f77f',
 };
 //# sourceMappingURL=latest-versions.js.map
