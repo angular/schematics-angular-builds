70611cd0c0e522b34155e294dca7d98950ab8060
Angular Builds <dev-infra@angular.dev>
2026-09-23 20:10:54 +0000

    82f1972 refactor: bump framework and `ng-packagr` deps for 22.2.0 release

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -14,10 +14,10 @@ const dependencies = require('./latest-versions/package.json')['dependencies'];
 exports.latestVersions = {
     ...dependencies,
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
-    Angular: '^22.2.0-next.0',
-    NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.2.0-rc.0+sha-10e2be9',
-    AngularBuild: '^22.2.0-rc.0+sha-10e2be9',
-    AngularSSR: '^22.2.0-rc.0+sha-10e2be9',
+    Angular: '^22.2.0',
+    NgPackagr: '^22.2.0',
+    DevkitBuildAngular: '^22.2.0-rc.0+sha-82f1972',
+    AngularBuild: '^22.2.0-rc.0+sha-82f1972',
+    AngularSSR: '^22.2.0-rc.0+sha-82f1972',
 };
 //# sourceMappingURL=latest-versions.js.map
