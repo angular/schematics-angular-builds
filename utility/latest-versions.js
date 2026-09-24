35565e13597268db93e1f4c0e8a33e6019fc2679
Angular Builds <dev-infra@angular.dev>
2026-09-24 18:00:05 +0000

    d968259 docs: fix stale Bazel test targets and toolchain versions

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.3.0-next.0+sha-297ef9c',
-    AngularBuild: '^22.3.0-next.0+sha-297ef9c',
-    AngularSSR: '^22.3.0-next.0+sha-297ef9c',
+    DevkitBuildAngular: '^22.3.0-next.0+sha-d968259',
+    AngularBuild: '^22.3.0-next.0+sha-d968259',
+    AngularSSR: '^22.3.0-next.0+sha-d968259',
 };
 //# sourceMappingURL=latest-versions.js.map
