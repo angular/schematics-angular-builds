6481c9322b742896c16a3828fe85320e796d4800
Angular Builds <dev-infra@angular.dev>
2026-09-24 07:21:01 +0000

    feb41cc feat(@angular/build): add polyfills option to unit-test builder

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.3.0-next.0+sha-4a080b6',
-    AngularBuild: '^22.3.0-next.0+sha-4a080b6',
-    AngularSSR: '^22.3.0-next.0+sha-4a080b6',
+    DevkitBuildAngular: '^22.3.0-next.0+sha-feb41cc',
+    AngularBuild: '^22.3.0-next.0+sha-feb41cc',
+    AngularSSR: '^22.3.0-next.0+sha-feb41cc',
 };
 //# sourceMappingURL=latest-versions.js.map
