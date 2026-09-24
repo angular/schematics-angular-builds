9d88ce4aa543d8197a3aeef22a86d4f148f4655e
Angular Builds <dev-infra@angular.dev>
2026-09-24 11:20:08 +0000

    d09f98e fix(@angular/build): avoid top-level await for Zone.js injection in Vitest runner

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0-next.0',
     NgPackagr: '^22.2.0-next.0',
-    DevkitBuildAngular: '^22.3.0-next.0+sha-feb41cc',
-    AngularBuild: '^22.3.0-next.0+sha-feb41cc',
-    AngularSSR: '^22.3.0-next.0+sha-feb41cc',
+    DevkitBuildAngular: '^22.3.0-next.0+sha-d09f98e',
+    AngularBuild: '^22.3.0-next.0+sha-d09f98e',
+    AngularSSR: '^22.3.0-next.0+sha-d09f98e',
 };
 //# sourceMappingURL=latest-versions.js.map
