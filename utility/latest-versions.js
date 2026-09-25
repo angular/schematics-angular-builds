22502a5b6fedf2d062e2cea2f2421dcf25c58b31
Angular Builds <dev-infra@angular.dev>
2026-09-25 14:49:09 +0000

    37cf399 fix(@angular/build): reset component updates on dev-server SSR page request

--- utility/latest-versions.js
+++ utility/latest-versions.js
@@ -16,8 +16,8 @@ exports.latestVersions = {
     // As Angular CLI works with same minor versions of Angular Framework, a tilde match for the current
     Angular: '^22.2.0',
     NgPackagr: '^22.2.0',
-    DevkitBuildAngular: '^22.2.0+sha-aea1dde',
-    AngularBuild: '^22.2.0+sha-aea1dde',
-    AngularSSR: '^22.2.0+sha-aea1dde',
+    DevkitBuildAngular: '^22.2.0+sha-37cf399',
+    AngularBuild: '^22.2.0+sha-37cf399',
+    AngularSSR: '^22.2.0+sha-37cf399',
 };
 //# sourceMappingURL=latest-versions.js.map
